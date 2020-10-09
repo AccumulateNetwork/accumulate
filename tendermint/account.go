1a2706c3fea2401fb234659321bbabeeb33e23db
Dennis Bunfield <dennis@factoid.org>
2020-10-09 15:29:44 -0500

    initial WIP commit

---
+++ tendermint/account.go
@@ -0,0 +1,31 @@
+package tendermint
+
+import (
+	"errors"
+	//	"fmt"
+	//"github.com/gogo/protobuf/proto"
+	"github.com/iot-sas/SAS-DBlock/database"
+	pb "github.com/iot-sas/SAS-DBlock/proto"
+)
+
+type AccountStateStruct struct {
+	PublicKey         []byte
+	MessageCountDown  int32
+	MessageAllowance  int32
+	LastBlockHeight   int64
+	LastAccess        int64
+	groups []pb.Account_Group
+}
+
+func GetAccount(publicKey []byte) (Account pb.Account, err error){
+
+	var accountBytes []byte
+	accountBytes, err = database.AccountsDB.Get(publicKey)
+	if accountBytes == nil {
+		err = errors.New("Account not found")
+		return Account,err
+	}
+
+	err = Account.Unmarshal(accountBytes)
+	return Account,err
+}
---
+++ validators/Factoid.go
@@ -0,0 +1,35 @@
+package Validator
+
+
+import (
+	"fmt"
+	//"github.com/FactomProject/factomd/common/factoid"
+	factom "github.com/Factom-Asset-Tokens/factom"
+)
+
+type FactoidValidator struct{ValidatorInterface}
+
+func (v *FactoidValidator) Validate(tx []byte) uint32 {
+	//if pass then send to accumulator.
+	//make sure the factoid transaction format is valid.
+
+    //t := new(factoid.Transaction)
+	t := new(factom.Transaction)
+	err := t.UnmarshalBinary(tx)
+
+	if err != nil {
+		fmt.Printf("Invalid Transaction")
+	}
+
+	//make sure there is enough available balance
+//    inp := t.GetInputs()
+    //inp returns a transaction interface.
+
+	//if so, record transaction in factoid database, and pass to validator
+	return 0
+}
+
+func NewFactoidValidator() *FactoidValidator {
+	v := FactoidValidator{}
+	return &v
+}
