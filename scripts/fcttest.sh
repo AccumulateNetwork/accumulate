8a30654d3d160377fb3f238d9c4aa8ca14cdbef2
Dennis Bunfield <dennis@factoid.org>
2020-11-20 17:42:12 -0600

    added test script

---
+++ scripts/fcttest.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+curl -X POST --data-binary '{"jsonrpc": "2.0", "id": 0, "method": "factoid-submit", "params":
+{"transaction":"0201565d109233010100b0a0e100646f3e8750c550e4582eca5047546ffef89c13a175985e320232bacac81cc428afd7c200ce7b98bfdae90f942bc1fe88c3dd44d8f4c81f4eeb88a5602da05abc82ffdb5301718b5edd2914acc2e4677f336c1a32736e5e9bde13663e6413894f57ec272e28dc1908f98b79df30005a99df3c5caf362722e56eb0e394d20d61d34ff66c079afad1d09eee21dcd4ddaafbb65aacea4d5c1afcd086377d77172f15b3aa32250a"}}' \
+ -H 'content-type:text/plain;' http://localhost:1234/v2
