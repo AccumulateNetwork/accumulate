00c47dfb8a24319c9007ca266dc4550bba502468
Paul Snow <snow.paul@gmail.com>
2021-05-23 14:13:48 -0400

    Added a name to the MerkleState, which is marshaled and unmarshaled, added support to marshal and unmarshal byte slices

---
+++ smt/helper_test.go
@@ -0,0 +1,43 @@
+package smt
+
+import (
+	"bytes"
+	"math/rand"
+	"testing"
+)
+
+func TestSliceBytes(t *testing.T) {
+	for i := 0; i < 1024; i++ {
+		var bytetest []byte
+		for j := 0; j < i; j++ {
+			bytetest = append(bytetest, byte(rand.Int()))
+		}
+		counted := SliceBytes(bytetest)
+		slice, left := BytesSlice(counted)
+
+		if len(left) > 0 || !bytes.Equal(bytetest, slice) {
+			t.Errorf(" %d %x %x", len(left), bytetest, slice)
+		}
+	}
+
+	var tests [][]byte
+	var inputs []byte
+	for i := 0; i < 1024; i++ {
+		var bytetest []byte
+		for j := 0; j < i; j++ {
+			bytetest = append(bytetest, byte(rand.Int()))
+		}
+		tests = append(tests, bytetest)
+		counted := SliceBytes(bytetest)
+		inputs = append(inputs, counted...)
+	}
+
+	for i, v := range tests {
+		var slice []byte
+		slice, inputs = BytesSlice(inputs)
+
+		if !bytes.Equal(v, slice) {
+			t.Errorf(" %d %x %x", i, v, slice)
+		}
+	}
+}
