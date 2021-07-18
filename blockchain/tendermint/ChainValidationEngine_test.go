7d69c38d0931ddb161eabd61e33090dec9872290
Dennis Bunfield <dennis@tfalabs.tech>
2021-07-18 17:27:18 -0500

    cve and test

---
+++ blockchain/tendermint/ChainValidationEngine_test.go
@@ -0,0 +1,15 @@
+package tendermint
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestChainValidationEngine(t *testing.T) {
+		done := make(chan bool, 1)
+		go ChainValidationEngine(done)
+
+		<-done
+
+		fmt.Printf("All done...")
+}
