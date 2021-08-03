e10442a9dbe41e99ef5126f3280b54bee7a07718
Paul Snow <snow.paul@gmail.com>
2021-08-03 11:42:52 -0500

    Updated README.md and continued work

--- security/keys.go
+++ security/keys.go
@@ -1,33 +1,8 @@
 package security
 
-
-SignatorEd25519 {
-	Public [32]Byte
-}
-
-var _ Signator = (nil).(*SignatorEd25519)
-
-
-// Signator
-// Signs or authorizes token transactions
-type Signator interface {
-	SignatorHash() [32]byte
-	Validate(signature Signature, data []byte) bool
-	Type() int
-}
-
-
-type Group struct {
-	Chains [][32]byte // List of chains that the Group manages
-	Signatories []Signator
-}
-
-//  Need to be able to find a Group for a chain
-//  The group is managed by the signatories in the Group
-
-
-// SignatorState
-// Holds the signature groups.
-type SignatorState struct {
-	Groups []Group
+// KeyState
+// Required to provide the signatures for managing chains within an ADI
+// A KeyState always has at
+type KeyState struct {
+	[] SigGroup
 }
