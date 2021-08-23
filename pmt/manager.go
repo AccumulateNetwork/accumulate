f1e384475924a2f66350ae9e08c2ff4a6b011ca8
Paul Snow <snow.paul@gmail.com>
2021-08-23 11:54:20 -0500

    Get rid of the extra level of indirection

---
+++ pmt/manager.go
@@ -0,0 +1,62 @@
+package pmt
+
+import (
+	"github.com/AccumulateNetwork/SMT/storage/database"
+)
+
+type Manager struct {
+	DBManager *database.Manager
+	Dirty     []*Node
+	Bpt       *BPT
+	LoadedBB  map[[32]byte]*Node
+}
+
+// NewBPTManager
+// Get a new BPTManager which keeps the BPT on disk.  If the BPT is on
+// disk, then it can be reloaded as needed.
+func NewBPTManager(dbManager *database.Manager) *Manager { // Return a new BPTManager
+	manager := new(Manager)                        //         Allocate the struct
+	manager.DBManager = dbManager                  //         populate with pointer to the database manager
+	manager.Bpt = NewBPT()                         //         Allocate a new BPT
+	manager.Bpt.manager = manager                  //         Allow the Bpt to call back to the manager for db access
+	manager.LoadedBB = make(map[[32]byte]*Node)    //         Allocate an initial map
+	data := dbManager.Get("BPT", "Root", []byte{}) //         Get the BPT settings from disk
+	if data != nil {                               //         If nothing is found, well this is a fresh instance
+		manager.Bpt.UnMarshal(data)        //                 But if data is found, then unmarshal
+		manager.LoadNode(manager.Bpt.Root) //                 and load up the root data for the BPT
+	} //
+	return manager //                                         Return a new BPT manager
+}
+
+// LoadNode
+// Loads the nodes under the given node into the BPT
+func (m *Manager) LoadNode(node *Node) *Node {
+	if node.Height&m.Bpt.mask != 0 { //                                           Throw an error if not a border node
+		panic("load should not be called on a node that is not a border node") // panic -- should not occur
+	}
+	if n := m.LoadedBB[node.BBKey]; n == nil { //                                 If the Byte Block isn't loaded
+		data := m.DBManager.Get("BPT", "", node.BBKey[:]) //                      Get the Byte Block
+		m.Bpt.UnMarshalByteBlock(node, data)              //                      unpack it
+		m.LoadedBB[node.BBKey] = node                     //                      Save the root node of Byte Block
+		return node
+	} else {
+		return n
+	}
+}
+
+// FlushNode
+// Flushes the Byte Block to disk
+func (m *Manager) FlushNode(node *Node) { //   Flush a Byte Block
+	data := m.Bpt.MarshalByteBlock(node)                     //
+	_ = m.DBManager.PutBatch("BPT", "", node.BBKey[:], data) //
+	if node.Height == 0 {
+		data = m.Bpt.Marshal()
+		m.DBManager.PutBatch("BPT", "Root", []byte{}, data)
+	}
+}
+
+// InsertKV
+// Insert Key Value into Bpt
+func (m *Manager) InsertKV(key, value [32]byte) {
+	m.Bpt.Insert(key, value)
+}
---
+++ pmt/manager_test.go
@@ -0,0 +1,66 @@
+package pmt
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"testing"
+
+	"github.com/AccumulateNetwork/SMT/storage/database"
+)
+
+var _ = PrintNode // Avoids highlighting that PrintNode isn't used.  It is useful for debugging.
+// PrintNode
+// Debugging aid that dumps the BPT tree to allow inspection of the BPT by the
+// developer.  Too foamy to use all the time.
+func PrintNode(h int, e Entry) {
+	for i := 0; i < h; i++ {
+		print("_")
+	}
+	switch {
+	case e == nil:
+		println("nil")
+	case e.T() == TNode:
+		n := e.(*Node)
+		fmt.Printf("  %x...%x\n", n.BBKey[:3], n.BBKey[31:])
+		PrintNode(n.Height, n.Left)
+		PrintNode(n.Height, n.Right)
+	case e.T() == TValue:
+		v := e.(*Value)
+		fmt.Printf("  k %x v %x\n", v.Key[:5], v.Hash[:5])
+	case e.T() == TNotLoaded:
+		if h&7 > 0 {
+			println("  **** Bad Not Loaded ****")
+		} else {
+			println("  <not loaded>")
+		}
+	default:
+		panic(fmt.Sprintf("Really? %d ", e.T()))
+	}
+
+}
+
+func TestManager(t *testing.T) {
+
+	d := 100000
+
+	dbManager, err := database.NewDBManager("memory", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	bptManager := NewBPTManager(dbManager)
+	for i := 0; i < d; i++ {
+		key := sha256.Sum256([]byte(fmt.Sprintf("0 key %d", i)))
+		value := sha256.Sum256([]byte(fmt.Sprintf("0 key %d", i)))
+		bptManager.InsertKV(key, value)
+	}
+	bptManager.Bpt.Update()
+	//PrintNode(0, bptManager.Bpt.Root)
+	bptManager = NewBPTManager(dbManager)
+	//PrintNode(0, bptManager.Bpt.Root)
+	for i := 0; i < d; i++ {
+		key := sha256.Sum256([]byte(fmt.Sprintf("1 key %d", i)))
+		value := sha256.Sum256([]byte(fmt.Sprintf("1 key %d", i)))
+		bptManager.InsertKV(key, value)
+
+	}
+}
