36e89669b288145818c932cb71e6bc18229ab745
QVidal <78767535+QVidal@users.noreply.github.com>
2021-09-13 17:51:58 -0400

    added QuentinBen network to networks.go for documentation test

--- router/networks.go
+++ router/networks.go
@@ -1,30 +1,38 @@
 package router
 
 type RouterNode struct {
-    Name string
-    Port int
-    Ip []string
+	Name string
+	Port int
+	Ip   []string
 }
 
 var Networks []RouterNode
 
 func init() {
-  Networks = []RouterNode{
-    RouterNode {
-      Name: "Arches",
-      Port: 33000,
-      Ip:  []string{
-          "13.51.10.110",
-          "13.232.230.216",
-      },
-    },
-    RouterNode {
-      Name: "AmericanSamoa",
-      Port: 33000,
-      Ip:  []string{
-          "18.221.39.36",
-          "44.236.45.58",
-      },
-    },
-  }
+	Networks = []RouterNode{
+		RouterNode{
+			Name: "Arches",
+			Port: 33000,
+			Ip: []string{
+				"13.51.10.110",
+				"13.232.230.216",
+			},
+		},
+		RouterNode{
+			Name: "AmericanSamoa",
+			Port: 33000,
+			Ip: []string{
+				"18.221.39.36",
+				"44.236.45.58",
+			},
+		},
+		RouterNode{
+			Name: "QuentinBen",
+			Port: 33000,
+			Ip: []string{
+				"98.116.107.4",
+				"70.23.18.60",
+			},
+		},
+	}
 }
