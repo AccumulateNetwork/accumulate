b75557b298c0d257c6435e8c1add0c44ec6647b6
bunfield <dennis@factoid.org>
2021-10-06 18:05:18 -0500

    merged query from develop

--- internal/api/query.go
+++ internal/api/query.go
@@ -1,19 +1,14 @@
 package api
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/AccumulateNetwork/accumulated/types/api/transactions"
-	"github.com/AccumulateNetwork/accumulated/types/synthetic"
-
 	"github.com/AccumulateNetwork/accumulated/internal/relay"
 	"github.com/AccumulateNetwork/accumulated/smt/common"
 	"github.com/AccumulateNetwork/accumulated/types"
 	"github.com/AccumulateNetwork/accumulated/types/api"
 	acmeApi "github.com/AccumulateNetwork/accumulated/types/api"
-	"github.com/AccumulateNetwork/accumulated/types/api/response"
+	"github.com/AccumulateNetwork/accumulated/types/api/transactions"
 	"github.com/AccumulateNetwork/accumulated/types/state"
-	tmtypes "github.com/tendermint/tendermint/abci/types"
 	ctypes "github.com/tendermint/tendermint/rpc/core/types"
 )
 
@@ -66,225 +61,47 @@ func (q *Query) BatchSend() {
 
 // GetAdi get the adi state object. Use this to get the nonce.
 func (q *Query) GetAdi(adi *string) (*acmeApi.APIDataResponse, error) {
-
-	var err error
-	if err != nil {
-		return nil, fmt.Errorf("cannot marshal query for Token issuance")
-	}
-
-	aResp, err := q.Query(*adi, nil)
-
+	r, err := q.Query(*adi, nil)
 	if err != nil {
 		return nil, fmt.Errorf("bvc adi query returned error, %v", err)
 	}
 
-	qResp := aResp.Response
-
-	if err != nil {
-		return nil, fmt.Errorf("bvc adi query returned error, %v", err)
-	}
-
-	//unpack the response
-	ret := &acmeApi.APIDataResponse{}
-	ret.Type = "adi"
-
-	if qResp.Code == 0 {
-		//unpack the state object returned from the query
-		adiResp := response.ADI{}
-		err = adiResp.ADI.UnmarshalBinary(qResp.Value)
-
-		//package the response data into json
-		var data json.RawMessage
-		data, err = json.Marshal(adiResp)
-		if err != nil {
-			return nil, fmt.Errorf("cannot extract token information")
-		}
-
-		ret.Data = &data
-	} else {
-		var data json.RawMessage
-		data, err = json.Marshal(qResp.Value)
-		ret.Data = &data
-	}
-
-	return ret, err
+	return unmarshalADI(r.Response)
 }
 
 // GetToken
 // retrieve the informatin regarding a token
 func (q *Query) GetToken(tokenUrl *string) (*acmeApi.APIDataResponse, error) {
-
-	var err error
-	aResp, err := q.Query(*tokenUrl, nil)
-
+	r, err := q.Query(*tokenUrl, nil)
 	if err != nil {
 		return nil, fmt.Errorf("bvc token query returned error, %v", err)
 	}
-	qResp := aResp.Response
 
-	//unpack the response
-
-	ret := &acmeApi.APIDataResponse{}
-	ret.Type = "token"
-
-	if qResp.Code == 0 {
-
-		//unpack the state object returned from the query
-		tokState := &state.Token{}
-		err = tokState.UnmarshalBinary(qResp.Value)
-
-		tokResp := response.Token{}
-
-		tokResp.Precision = tokState.Precision
-		tokResp.URL = tokState.ChainUrl
-		tokResp.Symbol = tokState.Symbol
-		tokResp.Meta = tokState.Meta
-		//package the response data into json
-		var data json.RawMessage
-		data, err = json.Marshal(tokResp)
-		if err != nil {
-			return nil, fmt.Errorf("cannot extract token information")
-		}
-
-		ret.Data = &data
-	} else {
-		var data json.RawMessage
-		data, err = json.Marshal(qResp.Value)
-		ret.Data = &data
-	}
-
-	return ret, err
+	return unmarshalToken(r.Response)
 }
 
 // GetTokenAccount get the token balance for a given url
 func (q *Query) GetTokenAccount(adiChainPath *string) (*acmeApi.APIDataResponse, error) {
-
-	var err error
-
-	aResp, err := q.Query(*adiChainPath, nil)
-
+	r, err := q.Query(*adiChainPath, nil)
 	if err != nil {
 		return nil, fmt.Errorf("bvc token account query returned error, %v", err)
 	}
-	qResp := aResp.Response
 
-	ret := &acmeApi.APIDataResponse{}
-	ret.Type = "tokenAccount"
-
-	if qResp.Code == 0 {
-
-		//unpack the state object returned from the query
-		tokState := &state.TokenAccount{}
-		err = tokState.UnmarshalBinary(qResp.Value)
-
-		ta := acmeApi.NewTokenAccount(tokState.ChainUrl, tokState.TokenUrl.String)
-		tokResp := response.NewTokenAccount(ta, tokState.GetBalance())
-		//package the response data into json
-		var data json.RawMessage
-		data, err = json.Marshal(tokResp)
-		if err != nil {
-			return nil, fmt.Errorf("cannot extract token information")
-		}
-
-		ret.Data = &data
-	} else {
-		var data json.RawMessage
-		data, err = json.Marshal(qResp.Value)
-		ret.Data = &data
-	}
-
-	return ret, err
-}
-
-func (q *Query) packTokenTxResponse(url string, tokenTxRaw []byte, txId types.Bytes, txSynthTxIds types.Bytes) (*acmeApi.APIDataResponse, error) {
-	tx := acmeApi.TokenTx{}
-	err := tx.UnmarshalBinary(tokenTxRaw)
-	if err != nil {
-		return nil, NewAccumulateError(err)
-	}
-	txResp := response.TokenTx{}
-	txResp.From = tx.From.String
-	txResp.TxId = txId
-
-	_, queryPath, err := types.ParseIdentityChainPath(&url)
-	_, txPath, err := types.ParseIdentityChainPath(tx.From.AsString())
-	if queryPath != txPath {
-		return nil, fmt.Errorf("url doesn't match transaction query, expecting %s but received %s", txPath, queryPath)
-	}
-
-	if len(txSynthTxIds)/32 != len(tx.To) {
-		return nil, fmt.Errorf("number of synthetic tx, does not match number of outputs")
-	}
-
-	//should receive tx,unmarshal to output accounts
-	for i, v := range tx.To {
-		j := i * 32
-		synthTxId := txSynthTxIds[j : j+32]
-		txStatus := response.TokenTxOutputStatus{}
-		txStatus.TokenTxOutput.URL = v.URL
-		txStatus.TokenTxOutput.Amount = v.Amount
-		txStatus.SyntheticTxId = synthTxId
-
-		txResp.ToAccount = append(txResp.ToAccount, txStatus)
-	}
-
-	data, err := json.Marshal(&txResp)
-	if err != nil {
-		return nil, err
-	}
-	resp := acmeApi.APIDataResponse{}
-	resp.Type = types.String(types.TxTypeTokenTx.Name())
-	resp.Data = new(json.RawMessage)
-	*resp.Data = data
-	return &resp, err
-}
-
-func (q *Query) packSynthTokenDepositResponse(url string, tokenTxRaw []byte, txId types.Bytes, txSynthTxIds types.Bytes) (*acmeApi.APIDataResponse, error) {
-	tx := synthetic.TokenTransactionDeposit{}
-	err := tx.UnmarshalBinary(tokenTxRaw)
-	if err != nil {
-		return nil, NewAccumulateError(err)
-	}
-
-	if len(txSynthTxIds) != 0 {
-		return nil, fmt.Errorf("there should be no synthetic transaction associated with this transaction")
-	}
-
-	_, queryPath, err := types.ParseIdentityChainPath(&url)
-	if err != nil {
-		return nil, fmt.Errorf("error parsing query url path, %v", err)
-	}
-	_, txPath, err := types.ParseIdentityChainPath(tx.ToUrl.AsString())
-	if err != nil {
-		return nil, fmt.Errorf("error parsing url path associated with transaction, %v", err)
-	}
-	if queryPath != txPath {
-		return nil, fmt.Errorf("url doesn't match transaction query, expecting %s but received %s", txPath, queryPath)
-	}
-
-	data, err := json.Marshal(&tx)
-	if err != nil {
-		return nil, err
-	}
-	resp := acmeApi.APIDataResponse{}
-	resp.Type = types.String(types.TxTypeSyntheticTokenDeposit.Name())
-	resp.Data = new(json.RawMessage)
-	*resp.Data = data
-	return &resp, err
+	return unmarshalTokenAccount(r.Response)
 }
 
 // GetTokenTx
-// get the token tx from the primary adi, then query all the output accounts to get the status
-func (q *Query) GetTokenTx(tokenAccountUrl *string, txId []byte) (resp interface{}, err error) {
-	// need to know the ADI and ChainID, deriving adi and chain id from TokenTx.From
 
-	aResp, err := q.Query(*tokenAccountUrl, txId)
+// GetTransaction
+// get the tx from the primary url, if the transaction spawned synthetic tx's then return the synth txid's
+func (q *Query) GetTransaction(url string, txId []byte) (resp *acmeApi.APIDataResponse, err error) {
 
+	aResp, err := q.Query(url, txId)
 	if err != nil {
 		return nil, fmt.Errorf("bvc token tx query returned error, %v", err)
 	}
-	qResp := aResp.Response
 
+	qResp := aResp.Response
 	if qResp.Value == nil {
 		return nil, fmt.Errorf("no data available for txid %x", txId)
 	}
@@ -293,6 +110,7 @@ func (q *Query) GetTokenTx(tokenAccountUrl *string, txId []byte) (resp interface
 	if txPendingRaw == nil {
 		return nil, fmt.Errorf("unable to obtain data from value")
 	}
+
 	txPendingData, txSynthTxIdsRaw := common.BytesSlice(txPendingRaw)
 	_ = txPendingData
 
@@ -318,76 +136,18 @@ func (q *Query) GetTokenTx(tokenAccountUrl *string, txId []byte) (resp interface
 		return resp, NewAccumulateError(err)
 	}
 
-	txType, _ := common.BytesUint64(txState.Transaction.Bytes())
-	switch types.TxType(txType) {
-	case types.TxTypeTokenTx:
-		resp, err = q.packTokenTxResponse(*tokenAccountUrl, txState.Transaction.Bytes(), txId, txSynthTxIds)
-	case types.TxTypeSyntheticTokenDeposit:
-		resp, err = q.packSynthTokenDepositResponse(*tokenAccountUrl, txState.Transaction.Bytes(), txId, txSynthTxIds)
-	default:
-		err = fmt.Errorf("unable to extract transaction info for type %s : %x", types.TxType(txType).Name(), txState.Transaction.Bytes())
-	}
-
-	return resp, err
-}
-
-var ChainStates = map[types.ChainType]interface{}{
-	types.ChainTypeAdi: func(q *Query, url *string, txid []byte) (*acmeApi.APIDataResponse, error) {
-		return q.GetAdi(url)
-	},
-	types.ChainTypeToken: func(q *Query, url *string, txid []byte) (*acmeApi.APIDataResponse, error) {
-		return q.GetToken(url)
-	},
-	types.ChainTypeTokenAccount: func(q *Query, url *string, txid []byte) (*acmeApi.APIDataResponse, error) {
-		return q.GetTokenAccount(url)
-	},
-	types.ChainTypeSignatureGroup: func(q *Query, url *string, txid []byte) (*acmeApi.APIDataResponse, error) {
-		return q.GetTokenAccount(url)
-	},
-	types.ChainTypeAnonTokenAccount: func(q *Query, url *string, txid []byte) (*acmeApi.APIDataResponse, error) {
-		adi, _, _ := types.ParseIdentityChainPath(url)
-		adi += "/dc/ACME"
-		return q.GetTokenAccount(&adi)
-	},
+	return unmarshalTransaction(url, txState.Transaction.Bytes(), txId, txSynthTxIds)
 }
 
 // GetChainState
 // will return the state object of the chain, which include the chain
 // header and the current state data for the chain
-func (q *Query) GetChainState(adiChainPath *string, txId []byte) (interface{}, error) {
-	var err error
-
-	var qResp *tmtypes.ResponseQuery
-
+func (q *Query) GetChainState(adiChainPath *string, txId []byte) (*api.APIDataResponse, error) {
 	//this QuerySync call is only temporary until we get router setup.
-	aResp, err := q.Query(*adiChainPath, txId)
+	r, err := q.Query(*adiChainPath, txId)
 	if err != nil {
-		return nil, fmt.Errorf("bvc token chain query returned error, %v", err)
-	}
-	qResp = &aResp.Response
-
-	var resp *acmeApi.APIDataResponse
-	if len(txId) == 0 {
-		chainHeader := state.Chain{}
-
-		err = chainHeader.UnmarshalBinary(qResp.Value)
-
-		if err != nil {
-			return nil, fmt.Errorf("invalid state object returned from query of url %s, %v", *adiChainPath, err)
-		}
-
-		//unmarshal the state object if available
-		if val, ok := ChainStates[chainHeader.Type]; ok {
-			//resp, err = val.(func([]byte) (interface{}, error))(qResp.Value)
-			resp, err = val.(func(*Query, *string, []byte) (*acmeApi.APIDataResponse, error))(q, chainHeader.ChainUrl.AsString(), []byte{})
-		} else {
-			resp = &acmeApi.APIDataResponse{}
-			resp.Type = types.String(chainHeader.Type.Name())
-			msg := json.RawMessage{}
-			msg = []byte(fmt.Sprintf("{\"entry\":\"%x\"}", qResp.Value))
-			resp.Data = &msg
-		}
+		return nil, fmt.Errorf("chain query returned error, %v", err)
 	}
 
-	return resp, err
+	return q.unmarshalChainState(r.Response)
 }
--- internal/api/unmarshal.go
+++ internal/api/unmarshal.go
@@ -3,12 +3,12 @@ package api
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/AccumulateNetwork/accumulated/types/synthetic"
 
 	"github.com/AccumulateNetwork/accumulated/smt/common"
 	"github.com/AccumulateNetwork/accumulated/types"
 	"github.com/AccumulateNetwork/accumulated/types/api"
 	"github.com/AccumulateNetwork/accumulated/types/api/response"
-	"github.com/AccumulateNetwork/accumulated/types/api/transactions"
 	"github.com/AccumulateNetwork/accumulated/types/state"
 	tm "github.com/tendermint/tendermint/abci/types"
 )
@@ -72,24 +72,102 @@ func unmarshalTokenAccount(rQuery tm.ResponseQuery) (*api.APIDataResponse, error
 	})
 }
 
-func unmarshalTokenTx(rQuery tm.ResponseQuery) (*transactions.TokenSend, error) {
-	_, txRaw := common.BytesSlice(rQuery.Value)
-	sTx := new(state.Transaction)
-	err := sTx.UnmarshalBinary(txRaw)
+func unmarshalTokenTx(url string, txPayload []byte, txId types.Bytes, txSynthTxIds types.Bytes) (*api.APIDataResponse, error) {
+	tx := api.TokenTx{}
+	err := tx.UnmarshalBinary(txPayload)
 	if err != nil {
 		return nil, NewAccumulateError(err)
 	}
+	txResp := response.TokenTx{}
+	txResp.From = tx.From.String
+	txResp.TxId = txId
 
-	tx := new(transactions.TokenSend)
-	_, err = tx.Unmarshal(sTx.Transaction.Bytes())
-	if err != nil {
-		return nil, NewAccumulateError(err)
+	_, queryPath, err := types.ParseIdentityChainPath(&url)
+	_, txPath, err := types.ParseIdentityChainPath(tx.From.AsString())
+	if queryPath != txPath {
+		return nil, fmt.Errorf("url doesn't match transaction query, expecting %s but received %s", txPath, queryPath)
 	}
 
-	return tx, nil
+	if len(txSynthTxIds)/32 != len(tx.To) {
+		return nil, fmt.Errorf("number of synthetic tx, does not match number of outputs")
+	}
+
+	//should receive tx,unmarshal to output accounts
+	for i, v := range tx.To {
+		j := i * 32
+		synthTxId := txSynthTxIds[j : j+32]
+		txStatus := response.TokenTxOutputStatus{}
+		txStatus.TokenTxOutput.URL = v.URL
+		txStatus.TokenTxOutput.Amount = v.Amount
+		txStatus.SyntheticTxId = synthTxId
+
+		txResp.ToAccount = append(txResp.ToAccount, txStatus)
+	}
+
+	data, err := json.Marshal(&txResp)
+	if err != nil {
+		return nil, err
+	}
+	resp := api.APIDataResponse{}
+	resp.Type = types.String(types.TxTypeTokenTx.Name())
+	resp.Data = new(json.RawMessage)
+	*resp.Data = data
+	return &resp, err
 }
 
-func (q *Query) unmarshalByType(rQuery tm.ResponseQuery) (*api.APIDataResponse, error) {
+//unmarshalSynthTokenDeposit will unpack the synthetic token deposit and pack it into the response
+func unmarshalSynthTokenDeposit(url string, txPayload []byte, txId types.Bytes, txSynthTxIds types.Bytes) (*api.APIDataResponse, error) {
+	_ = txId
+	tx := synthetic.TokenTransactionDeposit{}
+	err := tx.UnmarshalBinary(txPayload)
+	if err != nil {
+		return nil, NewAccumulateError(err)
+	}
+
+	if len(txSynthTxIds) != 0 {
+		return nil, fmt.Errorf("there should be no synthetic transaction associated with this transaction")
+	}
+
+	_, queryPath, err := types.ParseIdentityChainPath(&url)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing query url path, %v", err)
+	}
+	_, txPath, err := types.ParseIdentityChainPath(tx.ToUrl.AsString())
+	if err != nil {
+		return nil, fmt.Errorf("error parsing url path associated with transaction, %v", err)
+	}
+	if queryPath != txPath {
+		return nil, fmt.Errorf("url doesn't match transaction query, expecting %s but received %s", txPath, queryPath)
+	}
+
+	data, err := json.Marshal(&tx)
+	if err != nil {
+		return nil, err
+	}
+	resp := api.APIDataResponse{}
+	resp.Type = types.String(types.TxTypeSyntheticTokenDeposit.Name())
+	resp.Data = new(json.RawMessage)
+	*resp.Data = data
+	return &resp, err
+}
+
+//unmarshalTransaction will unpack the transaction stored on-chain and marshal it into a response
+func unmarshalTransaction(url string, txPayload []byte, txId []byte, txSynthTxIds []byte) (resp *api.APIDataResponse, err error) {
+
+	txType, _ := common.BytesUint64(txPayload)
+	switch types.TxType(txType) {
+	case types.TxTypeTokenTx:
+		resp, err = unmarshalTokenTx(url, txPayload, txId, txSynthTxIds)
+	case types.TxTypeSyntheticTokenDeposit:
+		resp, err = unmarshalSynthTokenDeposit(url, txPayload, txId, txSynthTxIds)
+	default:
+		err = fmt.Errorf("unable to extract transaction info for type %s : %x", types.TxType(txType).Name(), txPayload)
+	}
+
+	return resp, err
+}
+
+func (q *Query) unmarshalChainState(rQuery tm.ResponseQuery) (*api.APIDataResponse, error) {
 	sChain := new(state.Chain)
 	err := sChain.UnmarshalBinary(rQuery.Value)
 	if err != nil {
