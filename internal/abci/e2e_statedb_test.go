7f8c385449f945f09e837a56cea8808c6a60e73c
Ethan Reesor <ethan.reesor@gmail.com>
2021-10-06 17:05:07 -0500

    fix: make state db consistency test a real boy

---
+++ internal/abci/e2e_statedb_test.go
@@ -0,0 +1,37 @@
+package abci_test
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"path/filepath"
+	"testing"
+
+	"github.com/AccumulateNetwork/accumulated/smt/storage/badger"
+	"github.com/AccumulateNetwork/accumulated/types/state"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStateDBConsistency(t *testing.T) {
+	dir := t.TempDir()
+	db := new(badger.DB)
+	err := db.InitDB(filepath.Join(dir, "valacc.db"))
+	require.NoError(t, err)
+
+	bvcId := sha256.Sum256([]byte("foo bar"))
+	sdb := new(state.StateDB)
+	sdb.Load(db, bvcId[:], true)
+
+	app, client := createApp(t, sdb)
+	anonTokenTest(t, app, client, 10)
+
+	height := sdb.BlockIndex()
+	rootHash := sdb.RootHash()
+
+	// Reopen the database
+	sdb = new(state.StateDB)
+	sdb.Load(db, bvcId[:], true)
+
+	// Block 6 does not make changes so is not saved
+	require.Equal(t, height, sdb.BlockIndex(), "Block index does not match after load from disk")
+	require.Equal(t, fmt.Sprintf("%X", rootHash), fmt.Sprintf("%X", sdb.RootHash()), "Hash does not match after load from disk")
+}
