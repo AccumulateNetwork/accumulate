35b859d58292d826893ea748d7be2255fd84ee3d
Ethan Reesor <ethan.reesor@gmail.com>
2021-10-12 10:13:38 -0500

    chore: move wallet to API in prep for reorg
    I plan to add state types to ./protocol. In order to avoid circular
    dependencies, wallet needs to move.

---
+++ internal/api/wallet.go
@@ -0,0 +1,18 @@
+package api
+
+import (
+	"crypto/ed25519"
+
+	anon "github.com/AccumulateNetwork/accumulated/types/anonaddress"
+	"github.com/AccumulateNetwork/accumulated/types/api/transactions"
+)
+
+func NewWalletEntry() *transactions.WalletEntry {
+	wallet := new(transactions.WalletEntry)
+
+	wallet.Nonce = 1 // Put the private key for the origin
+	_, wallet.PrivateKey, _ = ed25519.GenerateKey(nil)
+	wallet.Addr = anon.GenerateAcmeAddress(wallet.PrivateKey[32:]) // Generate the origin address
+
+	return wallet
+}
