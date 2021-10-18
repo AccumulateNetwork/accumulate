4f48c91f34e69a75e3c6a1c8d5444cd580ac8cb2
Ethan Reesor <ethan.reesor@gmail.com>
2021-10-18 18:15:03 -0500

    feat: add a new sig spec to a group
    - Also, when a group is created, assign each sig spec to it
    - And fail if any sig spec is already assigned to a group

--- internal/chain/create_sig_spec.go
+++ internal/chain/create_sig_spec.go
@@ -15,71 +15,88 @@ type CreateSigSpec struct{}
 
 func (CreateSigSpec) Type() types.TxType { return types.TxTypeCreateSigSpec }
 
-func checkCreateSigSpec(st *state.StateEntry, tx *transactions.GenTransaction) (*protocol.CreateSigSpec, *url.URL, error) {
+func checkCreateSigSpec(st *state.StateEntry, tx *transactions.GenTransaction) (*protocol.CreateSigSpec, *protocol.SigSpecGroup, *url.URL, error) {
 	adiUrl, err := url.Parse(tx.SigInfo.URL)
 	if err != nil {
-		return nil, nil, fmt.Errorf("invalid sponsor URL: %v", err)
+		return nil, nil, nil, fmt.Errorf("invalid sponsor URL: %v", err)
 	}
 
 	if st.ChainHeader == nil {
-		return nil, nil, fmt.Errorf("sponsor not found")
+		return nil, nil, nil, fmt.Errorf("sponsor not found")
 	}
 
-	if st.ChainHeader.Type != types.ChainTypeAdi {
-		return nil, nil, fmt.Errorf("%q is not an ADI", tx.SigInfo.URL)
+	var group *protocol.SigSpecGroup
+	switch st.ChainHeader.Type {
+	case types.ChainTypeAdi:
+		// Create an unbound sig spec
+	case types.ChainTypeSigSpecGroup:
+		group = new(protocol.SigSpecGroup)
+		err = st.ChainState.As(group)
+		if err != nil {
+			return nil, nil, nil, fmt.Errorf("failed to decode sponsor: %v", err)
+		}
+	default:
+		return nil, nil, nil, fmt.Errorf("%v cannot sponsor a sig spec", st.ChainHeader.Type)
 	}
 
 	body := new(protocol.CreateSigSpec)
 	err = tx.As(body)
 	if err != nil {
-		return nil, nil, fmt.Errorf("invalid payload: %v", err)
+		return nil, nil, nil, fmt.Errorf("invalid payload: %v", err)
 	}
 
 	if len(body.Keys) == 0 {
-		return nil, nil, fmt.Errorf("cannot create empty sig spec")
+		return nil, nil, nil, fmt.Errorf("cannot create empty sig spec")
 	}
 
 	msUrl, err := url.Parse(body.Url)
 	if err != nil {
-		return nil, nil, fmt.Errorf("invalid target URL: %v", err)
+		return nil, nil, nil, fmt.Errorf("invalid target URL: %v", err)
 	}
 
 	if !bytes.Equal(adiUrl.IdentityChain(), msUrl.IdentityChain()) {
-		return nil, nil, fmt.Errorf("%q does not belong to %q", msUrl, adiUrl)
+		return nil, nil, nil, fmt.Errorf("%q does not belong to %q", msUrl, adiUrl)
 	}
 
-	return body, msUrl, nil
+	return body, group, msUrl, nil
 }
 
 func (CreateSigSpec) CheckTx(st *state.StateEntry, tx *transactions.GenTransaction) error {
-	_, _, err := checkCreateSigSpec(st, tx)
+	_, _, _, err := checkCreateSigSpec(st, tx)
 	return err
 }
 
 func (CreateSigSpec) DeliverTx(st *state.StateEntry, tx *transactions.GenTransaction) (*DeliverTxResult, error) {
-	body, url, err := checkCreateSigSpec(st, tx)
+	body, group, url, err := checkCreateSigSpec(st, tx)
 	if err != nil {
 		return nil, err
 	}
 
-	mss := protocol.NewSigSpec()
-	mss.ChainUrl = types.String(url.String())
+	res := new(DeliverTxResult)
+	spec := protocol.NewSigSpec()
+	spec.ChainUrl = types.String(url.String())
+	res.AddChain(spec)
+
+	if group != nil {
+		groupUrl, err := group.ParseUrl()
+		if err != nil {
+			// Failing here would require writing an invalid URL to the state.
+			// But stuff happens, so don't panic.
+			return nil, fmt.Errorf("invalid sponsor URL: %v", err)
+		}
+
+		res.AddChain(group)
+		group.SigSpecs = append(group.SigSpecs, types.Bytes(url.ResourceChain()).AsBytes32())
+		spec.SigSpecId = types.Bytes(groupUrl.ResourceChain()).AsBytes32()
+	}
 
 	for _, sig := range body.Keys {
 		ss := new(protocol.KeySpec)
 		ss.HashAlgorithm = sig.HashAlgorithm
 		ss.KeyAlgorithm = sig.KeyAlgorithm
 		ss.PublicKey = sig.PublicKey
-		mss.Keys = append(mss.Keys, ss)
+		spec.Keys = append(spec.Keys, ss)
 	}
 
-	data, err := mss.MarshalBinary()
-	if err != nil {
-		return nil, fmt.Errorf("failed to marshal state: %v", err)
-	}
-
-	chainId := types.Bytes(url.ResourceChain()).AsBytes32()
-	txHash := types.Bytes(tx.TransactionHash()).AsBytes32()
-	st.DB.AddStateEntry(&chainId, &txHash, &state.Object{Entry: data})
-	return new(DeliverTxResult), nil
+	return res, nil
 }
