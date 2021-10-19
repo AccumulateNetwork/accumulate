47a8cb5baa8492e512c7230be17e8f27a06ad535
Ethan Reesor <ethan.reesor@gmail.com>
2021-10-19 18:21:51 -0500

    feat: override key book for token account create

--- internal/chain/token_account_create.go
+++ internal/chain/token_account_create.go
@@ -14,46 +14,62 @@ type TokenAccountCreate struct{}
 
 func (TokenAccountCreate) Type() types.TxType { return types.TxTypeTokenAccountCreate }
 
-func checkTokenAccountCreate(st *StateManager, tx *transactions.GenTransaction) (accountUrl, tokenUrl *url.URL, err error) {
+func checkTokenAccountCreate(st *StateManager, tx *transactions.GenTransaction) (accountUrl, tokenUrl, keyBookUrl *url.URL, err error) {
 	body := new(protocol.TokenAccountCreate)
 	err = tx.As(body)
 	if err != nil {
-		return nil, nil, fmt.Errorf("invalid payload: %v", err)
+		return nil, nil, nil, fmt.Errorf("invalid payload: %v", err)
 	}
 
 	accountUrl, err = url.Parse(body.Url)
 	if err != nil {
-		return nil, nil, fmt.Errorf("invalid account URL: %v", err)
+		return nil, nil, nil, fmt.Errorf("invalid account URL: %v", err)
 	}
 
 	tokenUrl, err = url.Parse(body.TokenUrl)
 	if err != nil {
-		return nil, nil, fmt.Errorf("invalid token URL: %v", err)
+		return nil, nil, nil, fmt.Errorf("invalid token URL: %v", err)
 	}
 	// TODO Make sure tokenUrl is a real kind of token
 
 	if !accountUrl.Identity().Equal(st.SponsorUrl) {
-		return nil, nil, fmt.Errorf("%q cannot sponsor %q", st.SponsorUrl, accountUrl)
+		return nil, nil, nil, fmt.Errorf("%q cannot sponsor %q", st.SponsorUrl, accountUrl)
 	}
 
-	return accountUrl, tokenUrl, nil
+	if body.KeyBookUrl != "" {
+		keyBookUrl, err = url.Parse(body.KeyBookUrl)
+		if err != nil {
+			return nil, nil, nil, fmt.Errorf("invalid key book URL: %v", err)
+		}
+
+		ssg := new(protocol.SigSpecGroup)
+		err = st.LoadUrlAs(keyBookUrl, ssg)
+		if err != nil {
+			return nil, nil, nil, fmt.Errorf("invalid key book %q: %v", keyBookUrl, err)
+		}
+	}
+
+	return accountUrl, tokenUrl, keyBookUrl, nil
 }
 
 func (TokenAccountCreate) CheckTx(st *StateManager, tx *transactions.GenTransaction) error {
-	_, _, err := checkTokenAccountCreate(st, tx)
+	_, _, _, err := checkTokenAccountCreate(st, tx)
 	return err
 }
 
 func (TokenAccountCreate) DeliverTx(st *StateManager, tx *transactions.GenTransaction) error {
-	accountUrl, tokenUrl, err := checkTokenAccountCreate(st, tx)
+	accountUrl, tokenUrl, keyBookUrl, err := checkTokenAccountCreate(st, tx)
 	if err != nil {
 		return err
 	}
 
 	account := state.NewTokenAccount(accountUrl.String(), tokenUrl.String())
 
-	// TODO Allow the token account to be overridden
-	account.SigSpecId = st.Sponsor.Header().SigSpecId
+	if keyBookUrl == nil {
+		account.SigSpecId = st.Sponsor.Header().SigSpecId
+	} else {
+		copy(account.SigSpecId[:], keyBookUrl.ResourceChain())
+	}
 
 	scc := new(protocol.SyntheticCreateChain)
 	scc.Cause = types.Bytes(tx.TransactionHash()).AsBytes32()
