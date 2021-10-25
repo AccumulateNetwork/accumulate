0f4c69cfa59b45a06c9b331dcf96d32de06c826c
Ethan Reesor <ethan.reesor@gmail.com>
2021-10-24 23:54:51 -0500

    feat: query transactions directly

--- types/api/APIRequests.go
+++ types/api/APIRequests.go
@@ -55,7 +55,7 @@ type APIRequestURLPagination struct {
 
 // APIDataResponse is used in "get" API method response
 type APIDataResponse struct {
-	Type    types.String       `json:"type" form:"type" query:"type" validate:"oneof:adi,token,tokenAccount,tokenTx,sigSpec,sigSpecGroup,assignSigSpec,addCredits"`
+	Type    types.String       `json:"type" form:"type" query:"type" validate:"oneof:adi,token,tokenAccount,tokenTx,tx,sigSpec,sigSpecGroup,assignSigSpec,addCredits"`
 	Data    *json.RawMessage   `json:"data" form:"data" query:"data"`
 	Sponsor types.String       `json:"sponsor" form:"sponsor" query:"sponsor" validate:"required"`
 	KeyPage *APIRequestKeyPage `json:"keyPage" form:"keyPage" query:"keyPage" validate:"required"`
