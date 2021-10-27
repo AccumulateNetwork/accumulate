f7a8e0878b8c6bb04fd165edf1557ef44b7d3f91
Ethan Reesor <ethan.reesor@gmail.com>
2021-10-27 18:26:46 -0500

    fix: commit genesis states
    - Adds a synthetic transaction to create the genesis block, which can
      only be submitted once
    - Modifies ABCI.Query to ignore uncommitted changes
    - Modifies ABCI.InitChain to execute the genesis TX

--- internal/genesis/faucet.go
+++ internal/genesis/faucet.go
@@ -3,6 +3,7 @@ package genesis
 import (
 	"crypto/ed25519"
 	"crypto/sha256"
+
 	"github.com/AccumulateNetwork/accumulated/internal/url"
 	"github.com/AccumulateNetwork/accumulated/protocol"
 	"github.com/AccumulateNetwork/accumulated/types"
@@ -25,18 +26,11 @@ func init() {
 	FaucetWallet.Addr = FaucetUrl.String()
 }
 
-func createFaucet() (*types.Bytes32, *state.Object) {
+func createFaucet() state.Chain {
 	anon := protocol.NewAnonTokenAccount()
 
 	anon.ChainUrl = types.String(FaucetWallet.Addr)
 	anon.TokenUrl = protocol.AcmeUrl().String()
 	anon.Balance.SetString("314159265358979323846264338327950288419716939937510582097494459", 10)
-	o := new(state.Object)
-	acct, err := anon.MarshalBinary()
-	if err != nil {
-		return nil, nil
-	}
-	o.Entry = acct
-	chainId := types.Bytes(FaucetUrl.ResourceChain()).AsBytes32()
-	return &chainId, o
+	return anon
 }
