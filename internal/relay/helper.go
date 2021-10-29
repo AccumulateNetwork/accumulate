c7e7206698282a0c7d5b2b571dd5ab882fb39d3f
Ethan Reesor <ethan.reesor@gmail.com>
2021-10-29 14:19:44 -0500

    fix: resolve named networks
    Also, fail if the RPC error isn't connection refused

--- internal/relay/helper.go
+++ internal/relay/helper.go
@@ -1,10 +1,6 @@
 package relay
 
 import (
-	"fmt"
-	"net/url"
-	"strconv"
-
 	"github.com/AccumulateNetwork/accumulated/internal/node"
 	"github.com/AccumulateNetwork/accumulated/networks"
 	rpchttp "github.com/tendermint/tendermint/rpc/client/http"
@@ -13,19 +9,12 @@ import (
 func NewClients(targetList ...string) ([]Client, error) {
 	clients := []Client{}
 	for _, nameOrIP := range targetList {
-		net := networks.Networks[nameOrIP]
-		ip, err := url.Parse(nameOrIP)
-		if net != nil {
-			ip = &url.URL{Scheme: "tcp", Host: fmt.Sprintf("%s:%d", net.Nodes[0].IP, net.Port+node.TmRpcPortOffset)}
-		} else if err != nil {
-			return nil, fmt.Errorf("%q is not a URL or a named network", nameOrIP)
-		} else if ip.Port() == "" {
-			return nil, fmt.Errorf("missing port number: %q", nameOrIP)
-		} else if _, err := strconv.ParseInt(ip.Port(), 10, 17); err != nil {
-			return nil, fmt.Errorf("invalid port number: %q", nameOrIP)
+		addr, err := networks.GetRpcAddr(nameOrIP, node.TmRpcPortOffset)
+		if err != nil {
+			return nil, err
 		}
 
-		client, err := rpchttp.New(ip.String())
+		client, err := rpchttp.New(addr)
 		if err != nil {
 			return nil, err
 		}
