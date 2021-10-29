af7151fcf3fc9ceb0395c45ece502fcc63265c72
Ethan Reesor <ethan.reesor@gmail.com>
2021-10-29 14:19:44 -0500

    fix: resolve named networks
    Also, fail if the RPC error isn't connection refused

---
+++ networks/getaddr.go
@@ -0,0 +1,23 @@
+package networks
+
+import (
+	"fmt"
+	"net/url"
+	"strconv"
+)
+
+func GetRpcAddr(netOrIp string, portOffset int) (string, error) {
+	net := Networks[netOrIp]
+	ip, err := url.Parse(netOrIp)
+	if net != nil {
+		ip = &url.URL{Scheme: "tcp", Host: fmt.Sprintf("%s:%d", net.Nodes[0].IP, net.Port+portOffset)}
+	} else if err != nil {
+		return "", fmt.Errorf("%q is not a URL or a named network", netOrIp)
+	} else if ip.Port() == "" {
+		return "", fmt.Errorf("missing port number: %q", netOrIp)
+	} else if _, err := strconv.ParseInt(ip.Port(), 10, 17); err != nil {
+		return "", fmt.Errorf("invalid port number: %q", netOrIp)
+	}
+
+	return ip.String(), nil
+}
