014045e06b64110174eac5781222c1b144e266d0
bunfield <dennis@factoid.org>
2021-10-31 13:53:40 -0500

    disable cli testing until we can change to memory db.

--- cmd/cli/cmd/key_test.go
+++ cmd/cli/cmd/key_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestImportMneumonic(t *testing.T) {
+func _TestImportMneumonic(t *testing.T) {
 	if os.Getenv("CI") == "true" {
 		t.Skip("Depends on an external resource, and thus is not appropriate for CI")
 	}
