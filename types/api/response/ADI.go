4eee4e078159e01afc1eca8697a837f114e75b36
bunfield <dennis@factoid.org>
2021-10-31 10:55:22 -0500

    updated help and key book/page

--- types/api/response/ADI.go
+++ types/api/response/ADI.go
@@ -2,10 +2,8 @@ package response
 
 import (
 	"github.com/AccumulateNetwork/accumulated/protocol"
-	"github.com/AccumulateNetwork/accumulated/types/state"
 )
 
 type ADI struct {
 	protocol.IdentityCreate
-	state.AdiState
 }
