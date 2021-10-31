1a55739393cc02faf64a6db16b1e1138e40367ec
Ethan Reesor <ethan.reesor@gmail.com>
2021-10-31 16:49:22 -0500

    fix: download script

--- scripts/management/download-accumulate.sh
+++ scripts/management/download-accumulate.sh
@@ -3,5 +3,5 @@
 API_V4=https://gitlab.com/api/v4
 PROJECT=29762666
 mkdir -p ~/.local/bin
-curl -LJ -o ~/.local/bin/accumulated "${API_V4}/projects/${PROJECT}/jobs/artifacts/test-dev/raw/accumulated-linux-amd64"
+curl -LJ -o ~/.local/bin/accumulated "${API_V4}/projects/${PROJECT}/jobs/artifacts/test-dev/raw/accumulated-linux-amd64?job=build"
 chmod +x ~/.local/bin/accumulated
