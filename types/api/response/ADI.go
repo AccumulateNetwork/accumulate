9f070665dc286336bfac161c01975801c3c569d0
bunfield <dennis@factoid.org>
2021-10-30 20:00:55 -0500

    added error checks

--- types/api/response/ADI.go
+++ types/api/response/ADI.go
@@ -2,8 +2,10 @@ package response
 
 import (
 	"github.com/AccumulateNetwork/accumulated/protocol"
+	"github.com/AccumulateNetwork/accumulated/types/state"
 )
 
 type ADI struct {
 	protocol.IdentityCreate
+	state.AdiState
 }
