b775b459b3845892a1c39889a12fd2f112e44214
Ethan Reesor <ethan.reesor@gmail.com>
2021-11-03 17:05:17 -0500

    fix: get timestamp

--- scripts/management/reset-history.sh
+++ scripts/management/reset-history.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-tmux send-keys -t 0 'source node.env' 'C-m' 'mkdir -p archive/2021-11-02/bvc-${BVC}-node-${NODE}' 'C-m' 'mv ~/.accumulate/bvc${BVC}/Node${NODE}/{data,valacc.db} archive/2021-11-02/bvc-${BVC}-node-${NODE}/' 'C-m'
-tmux send-keys -t 1 'source node.env' 'C-m' 'mkdir -p archive/2021-11-02/bvc-${BVC}-node-${NODE}' 'C-m' 'mv ~/.accumulate/bvc${BVC}/Node${NODE}/{data,valacc.db} archive/2021-11-02/bvc-${BVC}-node-${NODE}/' 'C-m'
-tmux send-keys -t 2 'source node.env' 'C-m' 'mkdir -p archive/2021-11-02/bvc-${BVC}-node-${NODE}' 'C-m' 'mv ~/.accumulate/bvc${BVC}/Node${NODE}/{data,valacc.db} archive/2021-11-02/bvc-${BVC}-node-${NODE}/' 'C-m'
-tmux send-keys -t 3 'source node.env' 'C-m' 'mkdir -p archive/2021-11-02/bvc-${BVC}-node-${NODE}' 'C-m' 'mv ~/.accumulate/bvc${BVC}/Node${NODE}/{data,valacc.db} archive/2021-11-02/bvc-${BVC}-node-${NODE}/' 'C-m'
-tmux send-keys -t 4 'source node.env' 'C-m' 'mkdir -p archive/2021-11-02/bvc-${BVC}-node-${NODE}' 'C-m' 'mv ~/.accumulate/bvc${BVC}/Node${NODE}/{data,valacc.db} archive/2021-11-02/bvc-${BVC}-node-${NODE}/' 'C-m'
-tmux send-keys -t 5 'source node.env' 'C-m' 'mkdir -p archive/2021-11-02/bvc-${BVC}-node-${NODE}' 'C-m' 'mv ~/.accumulate/bvc${BVC}/Node${NODE}/{data,valacc.db} archive/2021-11-02/bvc-${BVC}-node-${NODE}/' 'C-m'
+DATE=$(date +%Y-%m-%d)
+
+tmux send-keys -t 0 'source node.env' 'C-m' 'mkdir -p archive/'"${DATE}"'/bvc-${BVC}-node-${NODE}' 'C-m' 'mv ~/.accumulate/bvc${BVC}/Node${NODE}/{data,valacc.db} archive/'"${DATE}"'/bvc-${BVC}-node-${NODE}/' 'C-m'
+tmux send-keys -t 1 'source node.env' 'C-m' 'mkdir -p archive/'"${DATE}"'/bvc-${BVC}-node-${NODE}' 'C-m' 'mv ~/.accumulate/bvc${BVC}/Node${NODE}/{data,valacc.db} archive/'"${DATE}"'/bvc-${BVC}-node-${NODE}/' 'C-m'
+tmux send-keys -t 2 'source node.env' 'C-m' 'mkdir -p archive/'"${DATE}"'/bvc-${BVC}-node-${NODE}' 'C-m' 'mv ~/.accumulate/bvc${BVC}/Node${NODE}/{data,valacc.db} archive/'"${DATE}"'/bvc-${BVC}-node-${NODE}/' 'C-m'
+tmux send-keys -t 3 'source node.env' 'C-m' 'mkdir -p archive/'"${DATE}"'/bvc-${BVC}-node-${NODE}' 'C-m' 'mv ~/.accumulate/bvc${BVC}/Node${NODE}/{data,valacc.db} archive/'"${DATE}"'/bvc-${BVC}-node-${NODE}/' 'C-m'
+tmux send-keys -t 4 'source node.env' 'C-m' 'mkdir -p archive/'"${DATE}"'/bvc-${BVC}-node-${NODE}' 'C-m' 'mv ~/.accumulate/bvc${BVC}/Node${NODE}/{data,valacc.db} archive/'"${DATE}"'/bvc-${BVC}-node-${NODE}/' 'C-m'
+tmux send-keys -t 5 'source node.env' 'C-m' 'mkdir -p archive/'"${DATE}"'/bvc-${BVC}-node-${NODE}' 'C-m' 'mv ~/.accumulate/bvc${BVC}/Node${NODE}/{data,valacc.db} archive/'"${DATE}"'/bvc-${BVC}-node-${NODE}/' 'C-m'
 
 tmux send-keys -t 0 'mkdir ~/.accumulate/bvc${BVC}/Node${NODE}/data' 'C-m' "echo '{\"height\": \"0\",\"round\": 0,\"step\": 0}'"' > ~/.accumulate/bvc${BVC}/Node${NODE}/data/priv_validator_state.json' 'C-m'
 tmux send-keys -t 1 'mkdir ~/.accumulate/bvc${BVC}/Node${NODE}/data' 'C-m' "echo '{\"height\": \"0\",\"round\": 0,\"step\": 0}'"' > ~/.accumulate/bvc${BVC}/Node${NODE}/data/priv_validator_state.json' 'C-m'
