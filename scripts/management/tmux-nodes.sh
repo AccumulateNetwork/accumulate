aa1a2f7c5f88eb64e22ce7cc30b72fb17e0307d4
Ethan Reesor <ethan.reesor@gmail.com>
2021-11-04 12:42:53 -0500

    feat: add extra pane to tmux session

--- scripts/management/tmux-nodes.sh
+++ scripts/management/tmux-nodes.sh
@@ -15,4 +15,5 @@ tmux send-keys -t 2 "printf '\\033]2;BVC 1, Node 0 (65.0.156.146)\\033\\\\'" 'C-
 tmux send-keys -t 3 "printf '\\033]2;BVC 1, Node 1 (13.234.254.178)\\033\\\\'" 'C-m' 'ssh ec2-user@13.234.254.178' 'C-m' 'screen -r' 'C-m'
 tmux send-keys -t 4 "printf '\\033]2;BVC 2, Node 0 (13.48.159.117)\\033\\\\'" 'C-m' 'ssh ec2-user@13.48.159.117' 'C-m' 'screen -r' 'C-m'
 tmux send-keys -t 5 "printf '\\033]2;BVC 2, Node 1 (16.170.126.251)\\033\\\\'" 'C-m' 'ssh ec2-user@16.170.126.251' 'C-m' 'screen -r' 'C-m'
+bash -c 'sleep 0.1; tmux split-window -f -l 2' &
 tmux -2 attach-session -t acc
