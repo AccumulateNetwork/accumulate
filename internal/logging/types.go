f15fd233ddceb0f0bfa5d04679336590096748b8
Ethan Reesor <ethan.reesor@gmail.com>
2021-11-05 00:13:55 -0500

    feat: logging tweaks

---
+++ internal/logging/types.go
@@ -0,0 +1,54 @@
+package logging
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+
+	"github.com/AccumulateNetwork/accumulated/types"
+)
+
+type Hex []byte
+
+func (h Hex) MarshalJSON() ([]byte, error) {
+	b := make([]byte, hex.EncodedLen(len(h)))
+	hex.Encode(b, h)
+	return json.Marshal(string(b))
+}
+
+//go:inline
+func AsHex(v interface{}) Hex {
+	switch v := v.(type) {
+	case []byte:
+		return Hex(v)
+	case [32]byte:
+		return Hex(v[:])
+	case string:
+		return Hex(v)
+	case types.Bytes:
+		return Hex(v)
+	case types.Bytes32:
+		return Hex(v[:])
+	case types.String:
+		return Hex(v)
+	case *types.Bytes:
+		if v == nil {
+			return Hex(nil)
+		}
+		return Hex(*v)
+	case *types.Bytes32:
+		if v == nil {
+			return Hex(nil)
+		}
+		return Hex(v[:])
+	case *types.String:
+		if v == nil {
+			return Hex(nil)
+		}
+		return Hex(*v)
+	case fmt.Stringer:
+		return Hex(v.String())
+	default:
+		return Hex(fmt.Sprint(v))
+	}
+}
