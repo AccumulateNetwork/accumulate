0d5ba0382a3751c7f07edf5d95e4f4615515b947
Dennis Bunfield <dennis@tfalabs.tech>
2021-11-15 16:48:06 -0600

    updated to build cli

---
+++ scripts/build_cli.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+go build -o test_cli ../cmd/cli 
--- scripts/cli_create_id.sh
+++ scripts/cli_create_id.sh
@@ -7,9 +7,9 @@
 # issue the account generate command to the specified server
 
 if [ -z $1 ]; then
-	acc="$($cli account generate -j 2>&1 > /dev/null | /usr/bin/jq .name | /usr/bin/sed 's/\"//g')"
+	acc="$($cli account generate -j 2>&1 > /dev/null | jq .name | sed 's/\"//g')"
 else
-	acc="$($cli account generate -j -s http://$1/v1 2>&1 > /dev/null | /usr/bin/jq .name | /usr/bin/sed 's/\"//g')"
+	acc="$($cli account generate -j -s http://$1/v1 2>&1 > /dev/null | jq .name | sed 's/\"//g')"
 fi
 
 # return the generated ID 
