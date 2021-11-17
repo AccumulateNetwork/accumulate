2d5019c137a8ff71b6896c95d4480ea4c3620f0c
Anthony Mayhall <mayhallaj@recovery.attlocal.net>
2021-11-16 18:00:50 -0600

    Script modifications, cli consistency fixes, script creation for test matrix

--- scripts/cli_create_id.sh
+++ scripts/cli_create_id.sh
@@ -2,14 +2,25 @@
 #
 # This script uses the accumulate cli to generate an account ID
 #
-# if no command line parameter entered, prompt 
+# look for jq and sed
+#
+j=`which jq`
+if [ -z $j ]; then
+	echo "jq not found, needed to get account name"
+	exit 0
+fi
+s=`which sed`
+if [ -z $s ]; then
+	echo "jq not found, needed to get account name"
+	exit 0
+fi
 #
 # issue the account generate command to the specified server
 
 if [ -z $1 ]; then
-	acc="$($cli account generate -j 2>&1 > /dev/null | jq .name | sed 's/\"//g')"
+	acc="$($cli account generate -j 2>&1 > /dev/null | $j .name | $s 's/\"//g')"
 else
-	acc="$($cli account generate -j -s http://$1/v1 2>&1 > /dev/null | jq .name | sed 's/\"//g')"
+	acc="$($cli account generate -j -s http://$1/v1 2>&1 > /dev/null | $j .name | $s 's/\"//g')"
 fi
 
 # return the generated ID 
