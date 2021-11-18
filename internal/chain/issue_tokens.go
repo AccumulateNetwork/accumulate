a851f4788138033f03f5eeb27211790fe65d6d76
Ethan Reesor <ethan.reesor@gmail.com>
2021-11-18 17:47:06 -0600

    feat: stub out remaining TXs and chains

---
+++ internal/chain/issue_tokens.go
@@ -0,0 +1,24 @@
+package chain
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/AccumulateNetwork/accumulate/protocol"
+	"github.com/AccumulateNetwork/accumulate/types"
+	"github.com/AccumulateNetwork/accumulate/types/api/transactions"
+)
+
+type IssueTokens struct{}
+
+func (IssueTokens) Type() types.TxType { return types.TxTypeIssueTokens }
+
+func (IssueTokens) Validate(st *StateManager, tx *transactions.GenTransaction) error {
+	body := new(protocol.IssueTokens)
+	err := tx.As(body)
+	if err != nil {
+		return fmt.Errorf("invalid payload: %v", err)
+	}
+
+	return errors.New("not implemented") // TODO
+}
