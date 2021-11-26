9ee786247fbb4d3d78507cb641326df953a91141
Anthony Mayhall <mayhallaj@unknown18c04d8de3cb.attlocal.net>
2021-11-26 12:11:07 -0600

    updating scripts for this branch
    Former-commit-id: b91bf7c7c717f3e344940013e5c8911fa9c58782

--- scripts/test_case_2.5.sh
+++ scripts/test_case_2.5.sh
@@ -43,14 +43,14 @@ echo $bal
 
 # generate a key
 
-Key=`./cli_key_generate t25key $1`
+Key=`./cli_key_generate.sh t25key $1`
 
 echo $key
 
 # create account
 
-echo "./cli_adi_create_account.sh $ID acc://t25acct t25key $1"
+echo `./cli_adi_create_account.sh $ID acc://t25acct t25key $1`
 
 # try again
-echo "./cli_adi_create_account.sh $ID acc://t25acct t25key $1"
+echo `./cli_adi_create_account.sh $ID acc://t25acct t25key $1`
 
