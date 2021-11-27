d8b7eeff2c48130fa9849f4e2021c49ac3e63bc0
Anthony Mayhall <mayhallaj@unknown18c04d8de3cb.attlocal.net>
2021-11-26 20:42:38 -0600

    Fixed scripts and added test case 4.8 script

--- scripts/test_case_3.1.sh
+++ scripts/test_case_3.1.sh
@@ -37,7 +37,8 @@ TxID=`./cli_faucet.sh $ID $1`
 
 # get our balance
 
-bal=`./cli_get_balance.sh $ID $1`
+#sleep 2
+#bal=`./cli_get_balance.sh $ID $1`
 
 echo $bal
 
@@ -49,7 +50,7 @@ echo $key
 
 # create account
 
-echo `./cli_adi_create_account.sh $ID acc://t31acct t31key $1`
+./cli_adi_create_account.sh $ID acc://t31acct t31key $1
 
-echo `./cli account create acc://t31acct t31key acc://t31acct/myacmeacct acc://ACME acc://t31acct/ssg0 -s http://$1/v1`
+$cli account create acc://t31acct t31key acc://t31acct/myacmeacct acc://ACME acc://t31acct/ssg0 -s http://$1/v1
 
