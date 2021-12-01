5332c114068621e83844d17cc965308633cc62fc
Sander Postma <spostma@defidevs.io>
2021-12-01 15:03:40 +0100

    AC-586: embedding is not supported for binary-marshalled types & cleanup
    Former-commit-id: 771ff7a77d73b49c8f6a7d1e7585597746958326

--- types/api/query/chain.go
+++ types/api/query/chain.go
@@ -4,24 +4,15 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/AccumulateNetwork/accumulate/types"
-	"github.com/AccumulateNetwork/accumulate/types/state"
 )
 
 type RequestByUrl struct {
 	Url types.String
 }
 
-type ResponseByUrl struct {
-	state.Object
-}
-
 type RequestDirectory struct {
 	RequestByUrl
 	ExpandChains types.Bool
-}
-
-type ResponseDirectory struct {
-	state.Object
 }
 
 func (*RequestByUrl) Type() types.QueryType { return types.QueryTypeUrl }
@@ -37,20 +28,6 @@ func (r *RequestByUrl) UnmarshalBinary(data []byte) (err error) {
 		}
 	}()
 	return r.Url.UnmarshalBinary(data)
-}
-
-func (r *ResponseByUrl) MarshalBinary() ([]byte, error) {
-	return r.Object.MarshalBinary()
-}
-
-func (r *ResponseByUrl) UnmarshalBinary(data []byte) (err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			err = fmt.Errorf("error unmarshaling ResponseByUrl data %v", r)
-		}
-	}()
-
-	return r.Object.UnmarshalBinary(data)
 }
 
 func (r *RequestDirectory) MarshalBinary() ([]byte, error) {
@@ -77,18 +54,4 @@ func (r *RequestDirectory) UnmarshalBinary(data []byte) (err error) {
 	l := r.Url.Size(nil)
 	err = r.ExpandChains.UnmarshalBinary(data[l:])
 	return err
-}
-
-func (r *ResponseDirectory) MarshalBinary() ([]byte, error) {
-	return r.Object.MarshalBinary()
-}
-
-func (r *ResponseDirectory) UnmarshalBinary(data []byte) (err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			err = fmt.Errorf("error unmarshaling ResponseDirectory data %v", r)
-		}
-	}()
-
-	return r.Object.UnmarshalBinary(data)
 }
