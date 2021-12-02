0b3003063fab8c511d2a3ba96f627c749de54bfc
Dennis Bunfield <dennis@factoid.org>
2021-12-02 10:29:22 -0600

    wip: refactoring data account

--- internal/chain/create_data_account.go
+++ internal/chain/create_data_account.go
@@ -4,6 +4,9 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/AccumulateNetwork/accumulate/internal/url"
+	"github.com/AccumulateNetwork/accumulate/types/state"
+
 	"github.com/AccumulateNetwork/accumulate/protocol"
 	"github.com/AccumulateNetwork/accumulate/types"
 	"github.com/AccumulateNetwork/accumulate/types/api/transactions"
@@ -20,5 +23,34 @@ func (CreateDataAccount) Validate(st *StateManager, tx *transactions.GenTransact
 		return fmt.Errorf("invalid payload: %v", err)
 	}
 
+	dataAccountUrl, err := url.Parse(body.Url)
+	if err != nil {
+		return fmt.Errorf("invalid account URL: %v", err)
+	}
+
+	if !dataAccountUrl.Identity().Equal(st.SponsorUrl) {
+		return fmt.Errorf("%q cannot sponsor %q", st.SponsorUrl, dataAccountUrl)
+	}
+
+	state.NewTokenAccount()
+	account := state.NewDataAccount(dataAccountUrl.String(), tokenUrl.String())
+	if body.KeyBookUrl == "" {
+		account.SigSpecId = st.Sponsor.Header().SigSpecId
+	} else {
+		keyBookUrl, err := url.Parse(body.KeyBookUrl)
+		if err != nil {
+			return fmt.Errorf("invalid key book URL: %v", err)
+		}
+
+		ssg := new(protocol.SigSpecGroup)
+		err = st.LoadUrlAs(keyBookUrl, ssg)
+		if err != nil {
+			return fmt.Errorf("invalid key book %q: %v", keyBookUrl, err)
+		}
+
+		copy(account.SigSpecId[:], keyBookUrl.ResourceChain())
+	}
+
+	st.Create(account)
 	return errors.New("not implemented") // TODO
 }
