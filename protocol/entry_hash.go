73e30f99fd05a37435d0fc42ea5fe976830503f3
bunfield <dennis@factoid.org>
2021-12-03 16:27:43 -0600

    chore: add unit tests for data chain create
    Former-commit-id: ceb15ac861b778f47b8adb64fe394caeb9991fbc

---
+++ protocol/entry_hash.go
@@ -0,0 +1,22 @@
+package protocol
+
+import (
+	"crypto/sha256"
+	"github.com/AccumulateNetwork/accumulate/smt/managed"
+)
+
+// ComputeEntryHash
+// returns the entry hash given external id's and data associated with an entry
+func ComputeEntryHash(root []byte, extIds [][]byte, data []byte) []byte {
+	smt := managed.MerkleState{}
+	//add the external id's to the merkle tree
+	for i := range extIds {
+		h := sha256.Sum256(extIds[i])
+		smt.AddToMerkleTree(h[:])
+	}
+	//add the data to the merkle tree
+	h := sha256.Sum256(data)
+	smt.AddToMerkleTree(h[:])
+	//return the entry hash
+	return smt.GetMDRoot().Bytes()
+}
