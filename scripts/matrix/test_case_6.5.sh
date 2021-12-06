7ac528ff0c5002ee09da15726367301caada5870
Anthony Mayhall <mayhallaj@unknown18c04d8de3cb.attlocal.net>
2021-12-05 21:27:12 -0600

    Modified scripts to check for and return proper exit statuses

--- scripts/matrix/test_case_6.5.sh
+++ scripts/matrix/test_case_6.5.sh
@@ -7,14 +7,14 @@
 #
 # set cli command and see if it exists
 #
-export cli=../cmd/cli/cli
+export cli=../../cmd/cli/cli
 
 if [ ! -f $cli ]; then
-        echo "cli command not found in ../cmd/cli, attempting to build"
+        echo "cli command not found in ../../cmd/cli, attempting to build"
         ./build_cli.sh
         if [ ! -f $cli ]; then
-                echo "cli command failed to build"
-                exit 0
+           echo "cli command failed to build"
+           exit 1
         fi
 fi
 # check for command line parameters
@@ -24,7 +24,7 @@ fi
 
 if [ -z $1 ]; then
         echo "Usage: test_case_6.5.sh ID IPAddress:Port"
-        exit 0
+        exit 1
 fi
 
 # see if $1 is really an ID
@@ -33,21 +33,30 @@ id1=$1
 size=${#id1}
 if [ $size -lt 59 ]; then
         echo "Expected acc://<48 byte string>/ACME"
-        exit 0
+        exit 1
 fi
 
 if [ -z $2 ]; then
         echo "Usage: test_case_6.5.sh ID IPAddress:Port"
-        exit 0
+        exit 1
 fi
 
 # call our faucet script and get the txid
 
 txid=`./cli_faucet.sh $id1 $2` 
 
+if [ $? -ne 0 ]; then
+	echo "cli faucet failed"
+	exit 1
+fi
 # call our get tx status script to get the tx status
 
 status=`./cli_get_tx_status.sh $txid $2`
 
+if [ $? -ne 0 ]; then
+	echo "cli get tx status failed"
+	exit 1
+fi
 echo $status
 
+exit 0
