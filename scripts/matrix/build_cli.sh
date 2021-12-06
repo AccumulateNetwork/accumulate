f31bf68411b483e16e453eb7dc64138c25b5cace
Anthony Mayhall <mayhallaj@unknown18c04d8de3cb.attlocal.net>
2021-12-05 21:27:12 -0600

    Modified scripts to check for and return proper exit statuses

--- scripts/matrix/build_cli.sh
+++ scripts/matrix/build_cli.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-go build -o ../cmd/cli/cli ../cmd/cli 
+go build -o ../../cmd/cli/cli ../../cmd/cli 
--- scripts/matrix/test_case_2.6.sh
+++ scripts/matrix/test_case_2.6.sh
@@ -7,14 +7,14 @@
 #
 # set cli command and see if it exists
 #
-export cli=../cmd/cli/cli
+export cli=../../cmd/cli/cli
 
 if [ ! -f $cli ]; then
-        echo "cli command not found in ../cmd/cli, attempting to build"
+        echo "cli command not found in ../../cmd/cli, attempting to build"
         ./build_cli.sh
         if [ ! -f $cli ]; then
-                echo "cli command failed to build"
-                exit 0
+           echo "cli command failed to build"
+           exit 1
         fi
 fi
 
@@ -22,37 +22,64 @@ fi
 #
 ID=`./cli_create_id.sh $1`
 
-echo $ID
-
-# see if we got an id, if not, exit
-
-if [ -z $ID ]; then
-   echo "Account creation failed"
-   exit 0
+if [ $? -ne 0 ]; then
+	echo "cli create id failed"
+	exit 1
 fi
+echo $ID
 
 # call cli faucet 
 
 TxID=`./cli_faucet.sh $ID $1`
 
+if [ $? -ne 0 ]; then
+	echo "cli faucet failed"
+	exit 1
+fi
+
+sleep 2.5
+
 # get our balance
 
 bal=`./cli_get_balance.sh $ID $1`
 
+if [ $? -ne 0 ]; then
+	echo "cli get balance failed"
+	exit 1
+fi
 echo $bal
 
 # generate keys
 
 Key=`./cli_key_generate.sh t26key $1`
+if [ $? -ne 0 ]; then
+	echo "cli key generate failed"
+	exit 1
+fi
 Key=`./cli_key_generate.sh t262key $1`
+if [ $? -ne 0 ]; then
+	echo "cli key generate failed"
+	exit 1
+fi
 
 echo $key
 
 # create account
 
-echo `./cli_adi_create_account.sh $ID acc://t26acct t26key $1`
+./cli_adi_create_account.sh $ID acc://t26acct t26key $1
+if [ $? -ne 0 ]; then
+	echo "cli adi create account failed"
+	exit 1
+fi
+
+sleep 2.5
 
 # create another adi with that adi
 
-echo `./cli_adi_create_account.sh acc://t26acct acc://t262acct t262key $1`
+./cli_adi_create_account.sh acc://t26acct acc://t262acct t262key $1
+if [ $? -ne 0 ]; then
+	echo "cli adi create account failed"
+	exit 1
+fi
 
+exit 0 
