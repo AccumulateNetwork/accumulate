69674da97ef08ebe56032e908262bc5c54c10732
Sander Postma <spostma@defidevs.io>
2021-12-07 16:49:36 +0100

    AC-544: Rename for consistency

--- types/state/StateDBBuilder.go
+++ types/state/StateDBBuilder.go
@@ -5,26 +5,26 @@ import (
 	"github.com/tendermint/tendermint/libs/log"
 )
 
-type stateDbBuilder struct {
+type stateDBBuilder struct {
 	debug  bool
 	logger log.Logger
 }
 
-func NewStateDB() *stateDbBuilder {
-	return new(stateDbBuilder)
+func NewStateDB() *stateDBBuilder {
+	return new(stateDBBuilder)
 }
 
-func (sb *stateDbBuilder) WithDebug() *stateDbBuilder {
+func (sb *stateDBBuilder) WithDebug() *stateDBBuilder {
 	sb.debug = true
 	return sb
 }
 
-func (sb *stateDbBuilder) WithLogger(logger log.Logger) *stateDbBuilder {
+func (sb *stateDBBuilder) WithLogger(logger log.Logger) *stateDBBuilder {
 	sb.logger = logger
 	return sb
 }
 
-func (sb *stateDbBuilder) OpenInMemory() (*StateDB, error) {
+func (sb *stateDBBuilder) OpenInMemory() (*StateDB, error) {
 	stateDB := new(StateDB)
 	dbType := "memory"
 	createStateLogger(sb, stateDB)
@@ -36,7 +36,7 @@ func (sb *stateDbBuilder) OpenInMemory() (*StateDB, error) {
 	return stateDB, nil
 }
 
-func (sb *stateDbBuilder) OpenFromFile(filePath string) (*StateDB, error) {
+func (sb *stateDBBuilder) OpenFromFile(filePath string) (*StateDB, error) {
 	stateDB := new(StateDB)
 	dbType := "badger"
 	createStateLogger(sb, stateDB)
@@ -49,7 +49,7 @@ func (sb *stateDbBuilder) OpenFromFile(filePath string) (*StateDB, error) {
 	return stateDB, nil
 }
 
-func (sb *stateDbBuilder) LoadKeyValueDB(db storage.KeyValueDB) (*StateDB, error) {
+func (sb *stateDBBuilder) LoadKeyValueDB(db storage.KeyValueDB) (*StateDB, error) {
 	stateDB := new(StateDB)
 	createStateLogger(sb, stateDB)
 	err := stateDB.Load(db)
@@ -57,7 +57,7 @@ func (sb *stateDbBuilder) LoadKeyValueDB(db storage.KeyValueDB) (*StateDB, error
 	return stateDB, err
 }
 
-func createStateLogger(ib *stateDbBuilder, stateDB *StateDB) {
+func createStateLogger(ib *stateDBBuilder, stateDB *StateDB) {
 	if ib.logger != nil {
 		stateDB.logger = ib.logger.With("module", "dbMgr")
 	}
