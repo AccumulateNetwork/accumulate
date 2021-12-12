768f9f664299a5ec84f71cec158469c33d0bfc61
bunfield <dennis@factoid.org>
2021-12-12 15:41:46 -0600

    Update e2e_test.go
    remove sleep that may no longer be needed

--- internal/abci/e2e_test.go
+++ internal/abci/e2e_test.go
@@ -323,7 +323,7 @@ func TestCreateAdiDataAccount(t *testing.T) {
 			require.NoError(t, err)
 			send(tx)
 		})
-		time.Sleep(3 * time.Second)
+		
 		// Test getting the data by URL
 		r2 := n.GetChainDataByUrl("FooBar/oof")
 		if r2 == nil {
