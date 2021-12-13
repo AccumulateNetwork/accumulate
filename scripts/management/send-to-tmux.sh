a25e224764d4a86b5c73c3ee7e6197c24b05210c
Ethan Reesor <ethan.reesor@gmail.com>
2021-12-12 22:54:16 -0600

    fix: init
    Former-commit-id: c8d077a51a1ccfddee28d11c74e4bba032322c26

--- scripts/management/send-to-tmux.sh
+++ scripts/management/send-to-tmux.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-tmux send-keys -t 0 "$@"
-tmux send-keys -t 1 "$@"
-tmux send-keys -t 2 "$@"
-tmux send-keys -t 3 "$@"
-tmux send-keys -t 4 "$@"
-tmux send-keys -t 5 "$@"
-tmux send-keys -t 6 "$@"
-tmux send-keys -t 7 "$@"
-tmux send-keys -t 8 "$@"
+for i in `seq 0 $(($1-1))`; do
+    tmux send-keys -t $i "${@:2}"
+done
