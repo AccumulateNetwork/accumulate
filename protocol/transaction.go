b3d7f01c5d038826135c6ae8b55c2482fb4ba6be
Ethan Reesor <ethan.reesor@gmail.com>
2021-12-20 22:23:28 -0600

    chore: remove types/synthetic
    Former-commit-id: 0423319311b9621c978f4e2a39add1d8ccbd75a7

--- protocol/transaction.go
+++ protocol/transaction.go
@@ -3,6 +3,7 @@ package protocol
 import (
 	"encoding"
 
+	"github.com/AccumulateNetwork/accumulate/internal/url"
 	"github.com/AccumulateNetwork/accumulate/types"
 	"github.com/AccumulateNetwork/accumulate/types/state"
 )
@@ -18,30 +19,40 @@ type SyntheticTransaction interface {
 	GetCause() [32]byte
 }
 
-func (scc *SyntheticCreateChain) GetCause() [32]byte {
-	return scc.Cause
-}
+func (tx *SyntheticCreateChain) GetCause() [32]byte    { return tx.Cause }
+func (tx *SyntheticWriteData) GetCause() [32]byte      { return tx.Cause }
+func (tx *SyntheticDepositTokens) GetCause() [32]byte  { return tx.Cause }
+func (tx *SyntheticDepositCredits) GetCause() [32]byte { return tx.Cause }
+func (tx *SyntheticBurnTokens) GetCause() [32]byte     { return tx.Cause }
+func (tx *SegWitDataEntry) GetCause() [32]byte         { return tx.Cause }
 
-func (scc *SyntheticCreateChain) Create(chains ...state.Chain) error {
+func (tx *SyntheticCreateChain) Create(chains ...state.Chain) error {
 	for _, chain := range chains {
 		b, err := chain.MarshalBinary()
 		if err != nil {
 			return err
 		}
 
-		scc.Chains = append(scc.Chains, ChainParams{Data: b})
+		tx.Chains = append(tx.Chains, ChainParams{Data: b})
 	}
 	return nil
 }
 
-func (scc *SyntheticCreateChain) Update(chains ...state.Chain) error {
+func (tx *SyntheticCreateChain) Update(chains ...state.Chain) error {
 	for _, chain := range chains {
 		b, err := chain.MarshalBinary()
 		if err != nil {
 			return err
 		}
 
-		scc.Chains = append(scc.Chains, ChainParams{Data: b, IsUpdate: true})
+		tx.Chains = append(tx.Chains, ChainParams{Data: b, IsUpdate: true})
 	}
 	return nil
+}
+
+func (tx *SendTokens) AddRecipient(to *url.URL, amount uint64) {
+	recipient := new(TokenRecipient)
+	recipient.Url = to.String()
+	recipient.Amount = amount
+	tx.To = append(tx.To, recipient)
 }
