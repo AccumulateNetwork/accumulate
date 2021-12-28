ac7cfbe8af954f1341bc663ac3002184879ac16c
Ethan Reesor <ethan.reesor@gmail.com>
2021-12-28 11:59:05 -0600

    test: fix race condition panic

--- internal/api/v2/e2e_test.go
+++ internal/api/v2/e2e_test.go
@@ -36,7 +36,7 @@ func TestEndToEnd(t *testing.T) {
 }
 
 func TestValidate(t *testing.T) {
-	// acctesting.SkipPlatform(t, "windows", "flaky")
+	acctesting.SkipPlatform(t, "windows", "flaky")
 	acctesting.SkipPlatform(t, "darwin", "flaky")
 	acctesting.SkipPlatformCI(t, "darwin", "requires setting up localhost aliases")
 
