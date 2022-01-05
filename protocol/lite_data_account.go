02183bbd0220f3086d9e118f2b1748de121cd7a3
bunfield <dennis@factoid.org>
2022-01-04 22:17:48 -0600

    chore: rename from chain to account

--- protocol/lite_data_account.go
+++ protocol/lite_data_account.go
@@ -4,10 +4,10 @@ import (
 	"crypto/sha256"
 )
 
-// ComputeLiteChainId will compute the chain id from the first entry in the chain which defines
+// ComputeLiteDataAccountId will compute the chain id from the first entry in the chain which defines
 // the names as part of the external id's
 // https://github.com/FactomProject/FactomDocs/blob/master/factomDataStructureDetails.md#chainid
-func ComputeLiteChainId(firstEntry *DataEntry) []byte {
+func ComputeLiteDataAccountId(firstEntry *DataEntry) []byte {
 	hash := sha256.New()
 	for _, id := range firstEntry.ExtIds {
 		idSum := sha256.Sum256(id)
