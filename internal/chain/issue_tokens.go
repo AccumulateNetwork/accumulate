59bdf3a3bae346e40eb38bd699cdec86a6da689e
Abhishek <rabhishek822@gmail.com>
2022-01-06 22:10:58 +0530

    feat: implement issue tokens transaction

--- internal/chain/issue_tokens.go
+++ internal/chain/issue_tokens.go
@@ -1,9 +1,9 @@
 package chain
 
 import (
-	"errors"
 	"fmt"
 
+	"github.com/AccumulateNetwork/accumulate/internal/url"
 	"github.com/AccumulateNetwork/accumulate/protocol"
 	"github.com/AccumulateNetwork/accumulate/types"
 	"github.com/AccumulateNetwork/accumulate/types/api/transactions"
@@ -20,5 +20,35 @@ func (IssueTokens) Validate(st *StateManager, tx *transactions.Envelope) error {
 		return fmt.Errorf("invalid payload: %v", err)
 	}
 
-	return errors.New("not implemented") // TODO
+	accountUrl, err := url.Parse(body.Recipient)
+	if err != nil {
+		return fmt.Errorf("invalid account URL: %v", err)
+	}
+
+	var tokenAccount tokenChain
+	if st.Origin != nil {
+		switch origin := st.Origin.(type) {
+		case *protocol.LiteTokenAccount:
+			tokenAccount = origin
+		case *protocol.TokenAccount:
+			tokenAccount = origin
+		default:
+			return fmt.Errorf("invalid origin record: want chain type %v or %v, got %v", types.ChainTypeLiteTokenAccount, types.ChainTypeTokenAccount, origin.Header().Type)
+		}
+	} else if keyHash, _, err := protocol.ParseLiteAddress(tx.Transaction.Origin); err != nil {
+		return fmt.Errorf("invalid lite token account URL: %v", err)
+	} else if keyHash == nil {
+		return fmt.Errorf("could not find token account")
+	}
+
+	account := protocol.NewLiteTokenAccount()
+	account.ChainUrl = types.String(accountUrl.String())
+	account.TokenUrl = tokenAccount.Header().GetChainUrl()
+
+	if !account.CreditTokens(&body.Amount) {
+		return fmt.Errorf("unable to add deposit balance to account")
+	}
+	st.Update(account)
+
+	return nil
 }
