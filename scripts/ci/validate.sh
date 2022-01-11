c563475c4bab8617d80e9551d8122dd0710a9327
Ethan Reesor <ethan.reesor@gmail.com>
2022-01-11 13:48:32 -0600

    ci: skip flaky tests

--- scripts/ci/validate.sh
+++ scripts/ci/validate.sh
@@ -1,42 +1,51 @@
 #!/bin/bash
 
-set -e # Stop immediately on error
+# Stop immediately on error
+set -e
 
+# section <name> - Print a section header
 function section {
     echo -e '\033[1m'"$1"'\033[0m'
 }
 
+# ensure-key <name> - Generate the key if it does not exist
 function ensure-key {
     if ! accumulate key list | grep "$1"; then
         accumulate key generate "$1"
     fi
 }
 
+# wait-for <cmd...> - Execute a transaction and wait for it to complete
 function wait-for {
     TXID=`"$@"` || return 1
     echo -e '\033[2mWaiting for '"$TXID"'\033[0m'
     accumulate tx get -j --wait 10s --wait-synth 10s $TXID | jq -C --indent 0
 }
 
+# cli-tx <args...> - Execute a CLI command and extract the TXID from the result
 function cli-tx {
     JSON=`accumulate -j "$@"` || return 1
     echo "$JSON" | jq -r .txid
 }
 
+# api-v2 <payload> - Send a JSON-RPC message to the API
 function api-v2 {
     curl -s -X POST --data "${1}" -H 'content-type:application/json;' "${ACC_API}/../v2"
 }
 
+# api-tx <payload> - Send a JSON-RPC message to the API and extract the TXID from the result
 function api-tx {
     JSON=`api-v2 "$@"` || return 1
     echo "$JSON" | jq -r .result.txid
 }
 
+# die <message> - Print an error message and exit
 function die {
     echo -e '\033[1;31m'"$@"'\033[0m'
     exit 1
 }
 
+# success - Print 'success' in bold green text
 function success {
     echo -e '\033[1;32m'Success'\033[0m'
     echo
