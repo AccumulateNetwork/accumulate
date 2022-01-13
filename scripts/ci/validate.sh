5478a99913aac873d2a4fe4e056dade9a843f3a6
bunfield <dennis@factoid.org>
2022-01-13 01:38:36 -0600

    chore: add more tests

--- scripts/ci/validate.sh
+++ scripts/ci/validate.sh
@@ -169,10 +169,9 @@ BALANCE=$(accumulate -j account get ${LITE_TOK} | jq -r .data.balance)
 success
 
 section "Create lite data account and write the data"
-ACCOUNT_ID="acc://b36c1c4073305a41edc6353a094329c24ffa54c029a521aa"
-wait-for cli-tx tx execute keytest keytest-0-0 '{"type": "writeDataTo", "url": "'${ACCOUNT_ID}'", "ExtIds": ["Factom PRO", "Tutorial"], "Data": ""}'
-accumulate get $ACCOUNT_ID &> /dev/null || die "Cannot find lite data account"
+ACCOUNT_ID=$(accumulate -j account create lite-data keytest keytest-0-0 "Factom PRO" "Tutorial" | jq -r .accountUrl)
+[ "$ACCOUNT_ID" -eq "acc://b36c1c4073305a41edc6353a094329c24ffa54c029a521aa" ] && success || die "${ACCOUNT_ID} does not match expected value"
 accumulate data get $ACCOUNT_ID 0 1 &> /dev/null || die "lite data entry not found"
-accumulate -j account create lite-data keytest keytest-0-0 "Factom PRO" "Tutorial"
-
+accumulate -j data write-to keytest keytest-0-0 $ACCOUNT_ID "data test"
+accumulate data get $ACCOUNT_ID 0 2 &> /dev/null || die "lite data error"
 success
