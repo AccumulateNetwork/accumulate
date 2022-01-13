9f885894320b0c42c23b85a466b076185cfa2d15
bunfield <dennis@factoid.org>
2022-01-13 01:38:36 -0600

    chore: add more tests

--- cmd/accumulate/cmd/data_test.go
+++ cmd/accumulate/cmd/data_test.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"testing"
 
@@ -56,19 +57,25 @@ func testCase2_9b(t *testing.T, tc *testCmd) {
 	commandLine := fmt.Sprintf("account create lite-data acc://RedWagon/DataAccount red1 466163746f6d2050524f 5475746f7269616c")
 	r, err := tc.executeTx(t, commandLine)
 	require.NoError(t, err)
-	t.Log(r)
 
+	aldr := ActionLiteDataResponse{}
+	require.NoError(t, json.Unmarshal([]byte(r), &aldr))
 	//now read back the response
-	commandLine = fmt.Sprintf("data get acc://RedWagon/DataAccount")
+	commandLine = fmt.Sprintf("data get %s", aldr.AccountUrl)
 	_, err = tc.execute(t, commandLine)
 	require.NoError(t, err)
 
 	//now read it back as a set
-	_, err = tc.execute(t, "data get acc://RedWagon/DataAccount 0 1")
+	_, err = tc.execute(t, fmt.Sprintf("%s 0 1", commandLine))
+	require.NoError(t, err)
+
+	//pass in some hex encoded stuff 2 ext id's and an encoded data entry
+	commandLine = fmt.Sprintf("data write-to acc://RedWagon/DataAccount red1 %s 0badc0de", aldr.AccountUrl)
+	r, err = tc.executeTx(t, commandLine)
 	require.NoError(t, err)
 
 	//now read it back as a expanded set
-	_, err = tc.execute(t, "data get acc://RedWagon/DataAccount 0 1 expand")
+	_, err = tc.execute(t, fmt.Sprintf("%s 0 1 expand", commandLine))
 	require.NoError(t, err)
 }
 
