ec5496c2fd724b1f0bc231933b997e8bdf9e14a5
Dennis Bunfield <dennis@factoid.org>
2022-01-19 12:58:36 -0600

    fix: gosimports

--- internal/api/v2/e2e_test.go
+++ internal/api/v2/e2e_test.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"crypto/ed25519"
 	"encoding/json"
-	"github.com/AccumulateNetwork/accumulate/internal/api/v2"
 	"testing"
 	"time"
 
+	"github.com/AccumulateNetwork/accumulate/internal/api/v2"
 	acctesting "github.com/AccumulateNetwork/accumulate/internal/testing"
 	"github.com/AccumulateNetwork/accumulate/internal/testing/e2e"
 	"github.com/AccumulateNetwork/accumulate/internal/url"
