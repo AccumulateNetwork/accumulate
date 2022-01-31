d7c8f4c132ff0ae58e07df3007ed574526c4e662
Ethan Reesor <ethan.reesor@gmail.com>
2022-01-31 16:42:07 -0600

    feat(sdk): generalize type generators for use with external SDKs

--- internal/api/v2/api.go
+++ internal/api/v2/api.go
@@ -8,7 +8,7 @@ import (
 	"github.com/tendermint/tendermint/libs/log"
 )
 
-//go:generate go run ../../../tools/cmd/gentypes --package api types.yml
+//go:generate go run ../../../tools/cmd/gen-types --package api types.yml
 //go:generate go run ../../../tools/cmd/genapi --package api methods.yml
 //go:generate go run github.com/golang/mock/mockgen -source ../../routing/router.go -package api_test -destination ./mock_router_test.go
 
