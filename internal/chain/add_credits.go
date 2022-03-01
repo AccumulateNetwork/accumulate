b9409b388d6ab51f53e2ffa15a24be37288ae28f
Sam <soroush731@gmail.com>
2022-03-01 09:53:50 -0500

    Testing
    Testing

--- internal/chain/add_credits.go
+++ internal/chain/add_credits.go
@@ -44,11 +44,11 @@ func (AddCredits) Validate(st *StateManager, tx *transactions.Envelope) (protoco
 	// TODO: convert credit purchase from buying exact number of credits to
 	// specifying amount of acme to spend. body.Amount needs to be converted to bigint
 	// If specifying amount of acme to spend, do this instead of amount calculation above:
-	//credits := types.NewAmount(protocol.CreditsPerFiatUnit) // want to obtain credits
-	//credits.Mul(int64(ledgerState.ActiveOracle))            // fiat units / acme
-	//credits.Mul(body.Amount)                                // acme the user wants to spend
-	//credits.Div(protocol.AcmeOraclePrecision)               // adjust the precision of oracle to real units
-	//credits.Div(protocol.AcmePrecision)                     // adjust the precision of acme to spend to real units
+	credits := types.NewAmount(protocol.CreditsPerFiatUnit) // want to obtain credits
+	credits.Mul(int64(ledgerState.ActiveOracle))            // fiat units / acme
+	credits.Mul(int64(body.Amount))                         // acme the user wants to spend
+	credits.Div(protocol.AcmeOraclePrecision)               // adjust the precision of oracle to real units
+	credits.Div(protocol.AcmePrecision)                     // adjust the precision of acme to spend to real units
 
 	recv, err := st.LoadUrl(body.Recipient)
 	if err == nil {
