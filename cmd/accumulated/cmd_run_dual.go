20fb973219e6087a3f06ceebec19c624076a8622
Ethan Reesor <ethan.reesor@gmail.com>
2022-03-02 04:08:58 +0000

    feat(daemon): add run-dual
    Closes AC-1158, updates AC-1148. Add a subcommand to run two nodes side-by-side.

---
+++ cmd/accumulated/cmd_run_dual.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/kardianos/service"
+	"github.com/spf13/cobra"
+	"gitlab.com/accumulatenetwork/accumulate/smt/storage/badger"
+)
+
+var cmdRunDual = &cobra.Command{
+	Use:   "run-dual <primary> <secondary>",
+	Short: "Run a DN and BVN",
+	Run:   runDualNode,
+	Args:  cobra.ExactArgs(2),
+}
+
+var flagRunDual = struct {
+	Truncate bool
+}{}
+
+func init() {
+	cmdMain.AddCommand(cmdRunDual)
+
+	cmdRunDual.PersistentFlags().BoolVar(&flagRunDual.Truncate, "truncate", false, "Truncate Badger if necessary")
+
+	cmdRunDual.PersistentPreRun = func(*cobra.Command, []string) {
+		badger.TruncateBadger = flagRunDual.Truncate
+	}
+}
+
+func runDualNode(cmd *cobra.Command, args []string) {
+	prog := NewProgram(cmd, func(cmd *cobra.Command) (string, error) {
+		return args[0], nil
+	}, func(cmd *cobra.Command) (string, error) {
+		return args[1], nil
+	})
+	svc, err := service.New(prog, serviceConfig)
+	check(err)
+
+	logger, err := svc.Logger(nil)
+	check(err)
+
+	err = svc.Run()
+	if err != nil {
+		_ = logger.Error(err)
+	}
+}
