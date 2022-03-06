434d4af4eb787c47b17be7e072f5c33cdd3f74b5
Dennis B <dennis@factoid.org>
2022-03-06 00:50:25 -0600

    fixed test

--- scripts/ci/validate.sh
+++ scripts/ci/validate.sh
@@ -383,7 +383,7 @@ RESULT=$(accumulate -j oracle  | jq -re .price)
 section "Query votes chain"
 RESULT=$(accumulate -j data get dn/votes)
 RESULT=$(echo $RESULT | jq -re .data.entry.data | xxd -r -p | jq -re .votes[0].validator.address)
-NODE_PUB_KEY=$(jq -re .pub_key.value $NODE_PRIV_VAL)
+NODE_PUB_KEY=$(jq -re .address $NODE_PRIV_VAL)
 HEIGHT=$(echo $RESULT | jq -re .mainChain.height)
 #NOTE: This test will only work consistently if we have a single node on the DN
 [ "$RESULT" == "$NODE_PUB_KEY" ] && success || die "No vote record found at DN height $HEIGHT"
