c7be77aaaa7bb8c50324ada6348c3499bd0c5aa4
Dennis B <dennis@factoid.org>
2022-03-08 09:54:52 -0600

    loop test

--- scripts/ci/validate.sh
+++ scripts/ci/validate.sh
@@ -386,14 +386,25 @@ R1=$(accumulate -j data get dn/votes)
 echo $R1
 R2=$(echo "$R1" | jq -re .data.entry.data )
 echo $R2
-R3=$(echo "$R2" | sed 's/\([0-9A-F]\{2\}\)/\\\\\\x\1/gI' | xargs printf) #xxd -r -p)
+#xxd -r -p doesn't like the R2 string for some reason, so converting using sed instead
+R3=$(echo "$R2" | sed 's/\([0-9A-F]\{2\}\)/\\\\\\x\1/gI' | xargs printf)
 echo $R3
-R4=$(echo "$R3" | jq -re .votes[0].validator.address)
-echo $R4
-R5=$(echo "$R5" | base64 -d)
-echo $R5
-R6=$(echo "$R6" | xxd -p)
-echo $R6
 NODE_ADDRESS=$(jq -re .address $NODE_PRIV_VAL)
+VOTE_COUNT=$(echo "$R3" | jq -re '.votes|length')
+FOUND=0
+for i in {1..$VOTE_COUNT}; do
+  echo "COUNT=$i"
+  R4=$(echo "$R3" | jq -re .votes[$i].validator.address)
+  echo "ADDRESS = $R4"
+  R5=$(echo "$R5" | base64 -d)
+  echo "BINARY ADDRESS = $R5"
+  R6=$(echo "$R6" | xxd -p)
+  echo "HEX ADDRESS $R6"
+  if [ "$R6" == "${NODE_ADDRESS,,}" ]; then
+    echo "FOUND VOTE"
+    FOUND=1
+  fi
+done
+
 #NOTE: This test will only work consistently if we have a single node on the DN
-[ "$RESULT" == "${NODE_ADDRESS,,}" ] && success || die "No vote record found on DN"
+[ "$FOUND" -eq  1 ] && success || die "No vote record found on DN"
