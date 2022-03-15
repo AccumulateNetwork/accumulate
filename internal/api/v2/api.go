0eb80b5063e20eaebde50117bf01ff35bc9d2f18
Ethan Reesor <ethan.reesor@gmail.com>
2022-03-15 10:43:21 +0000

    chore(api): document API methods
    Closes AC-1187. Describes each API method in the YAML file. Adds comments to API and SDK methods.
    
    
    Former-commit-id: ab9da9927729a8d33d544b0c940d9f62349cbedd

--- internal/api/v2/api.go
+++ internal/api/v2/api.go
@@ -9,7 +9,7 @@ import (
 )
 
 //go:generate go run ../../../tools/cmd/gen-types --package api types.yml
-//go:generate go run ../../../tools/cmd/genapi --package api methods.yml
+//go:generate go run ../../../tools/cmd/gen-api --package api methods.yml
 //go:generate go run github.com/golang/mock/mockgen -source ../../routing/router.go -package api_test -destination ./mock_router_test.go
 
 type Options struct {
