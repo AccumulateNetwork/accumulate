d88d630c0cb0b50c4a5deb981879d2fbbf8c4aa3
Sander Postma <spostma@defidevs.io>
2022-03-16 14:23:57 +0100

    AC-811: temp panics

--- internal/chain/executor_txn.go
+++ internal/chain/executor_txn.go
@@ -580,7 +580,13 @@ func (m *Executor) putTransaction(st *StateManager, env *protocol.Envelope, txAc
 	}
 
 	// When the transaction is synthetic, send a receipt back to its origin
-	if txt.IsSynthetic() && st != nil && NeedsReceipt(txt) {
+	if txt.IsSynthetic() && NeedsReceipt(txt) {
+		if st == nil {
+			panic(fmt.Errorf("tx type %v has no state manager", txt))
+		}
+		if st.nodeUrl == nil {
+			panic(fmt.Errorf("tx type %v has no st.nodeUrl", txt))
+		}
 		receipt, sourceUrl := CreateReceipt(env, status, st.nodeUrl)
 		st.logger.Debug("Submitting synth receipt for", st.OriginUrl, " to ", sourceUrl)
 		st.Submit(sourceUrl, receipt)
