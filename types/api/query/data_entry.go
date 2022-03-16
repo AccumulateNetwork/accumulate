3c1aad82a42d337908d751f3d764db2e98b1a1d5
Ethan Reesor <ethan.reesor@gmail.com>
2022-03-16 22:20:50 +0000

    chore(prot): mostly eliminate account-by-id lookups
    Closes AC-1210. Mostly eliminate looking up an account by its ID.

--- types/api/query/data_entry.go
+++ types/api/query/data_entry.go
@@ -1,17 +1,8 @@
 package query
 
 import (
-	"gitlab.com/accumulatenetwork/accumulate/protocol"
 	"gitlab.com/accumulatenetwork/accumulate/types"
 )
-
-type RequestDataEntry struct {
-	protocol.RequestDataEntry
-}
-
-type RequestDataEntrySet struct {
-	protocol.RequestDataEntrySet
-}
 
 func (*RequestDataEntry) Type() types.QueryType { return types.QueryTypeData }
 
