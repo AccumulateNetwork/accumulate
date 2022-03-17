6c56a3fe2171886a026fd393734ffac25fb73b88
Dennis B <bunfield@gmail.com>
2022-03-17 18:22:52 +0000

    chore(node-cli): add simplified "accumulated init" for dual mode operation
    Related to AC-1219
    
    - resolve port conflict issue if the website is enabled.  
    - set to use bootstrap peers instead of persistent peers.
    
    usage :
    accumulated init dual <bvn#> http://<dn ip|url>:<dn port>

--- cmd/accumulated/cmd_init_network.go
+++ cmd/accumulated/cmd_init_network.go
@@ -70,7 +70,7 @@ func initNetwork(cmd *cobra.Command, args []string) {
 		//while we are at it, also find the directory.
 		if v.Type == config.Directory {
 			if directory != nil {
-				check(fmt.Errorf("more than one directory subnet is defined, can only have 1"))
+				fatalf("more than one directory subnet is defined, can only have 1")
 			}
 			directory = &network.Subnet[i]
 		}
@@ -80,12 +80,12 @@ func initNetwork(cmd *cobra.Command, args []string) {
 	}
 
 	if directory == nil {
-		check(fmt.Errorf("cannot find directory configuration in %v", networkConfigFile))
+		fatalf("cannot find directory configuration in %v", networkConfigFile)
 		panic("not reached") // For static analysis
 	}
 
 	if directory.Name != "Directory" {
-		check(fmt.Errorf("directory name specified in file was %s, but accumulated requires it to be \"Directory\"", directory.Name))
+		fatalf("directory name specified in file was %s, but accumulated requires it to be \"Directory\"", directory.Name)
 	}
 	//quick validation to make sure the directory node maps to each of the BVN's defined
 	for _, dnn := range directory.Nodes {
@@ -106,7 +106,7 @@ func initNetwork(cmd *cobra.Command, args []string) {
 			}
 		}
 		if !found {
-			check(fmt.Errorf("%s is defined in the directory nodes networks file, but has no supporting BVN node", dnn.IP))
+			fatalf("%s is defined in the directory nodes networks file, but has no supporting BVN node", dnn.IP)
 		}
 	}
 
