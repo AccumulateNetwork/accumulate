ffeb391d2f66c8a3988f58093ab272cf45b8ecb8
Ethan Reesor <ethan.reesor@gmail.com>
2022-03-19 00:10:05 +0000

    feat(prot): implement key page locking
    - Closes AC-878
    - Adds the ability to 'lock' a key page such that the page is no longer authorized to sign `UpdateKeyPage` transactions
    - Generalizes support for union types in `gen-types`
    - Turns update key page operations into a union

---
+++ protocol/access_control.go
@@ -0,0 +1,50 @@
+package protocol
+
+type AllowedTransactionBit uint8
+
+// Mask returns the bit mask.
+func (bit AllowedTransactionBit) Mask() AllowedTransactions {
+	return 1 << bit
+}
+
+type AllowedTransactions uint64
+
+// Set sets the bit to 1.
+func (v *AllowedTransactions) Set(bit AllowedTransactionBit) {
+	*v = *v | bit.Mask()
+}
+
+// Clear sets the bit to 0.
+func (v *AllowedTransactions) Clear(bit AllowedTransactionBit) {
+	*v = *v & ^bit.Mask()
+}
+
+// IsSet returns true if the bit is set.
+func (v *AllowedTransactions) IsSet(bit AllowedTransactionBit) bool {
+	if v == nil {
+		return false
+	}
+	return (*v)&bit.Mask() != 0
+}
+
+// GetEnumValue implements the enumeration encoding interface.
+func (v AllowedTransactions) GetEnumValue() uint64 {
+	return uint64(v)
+}
+
+// SetEnumValue implements the enumeration encoding interface.
+func (v *AllowedTransactions) SetEnumValue(u uint64) bool {
+	*v = AllowedTransactions(u)
+	return true
+}
+
+// AllowedTransactionBit returns the AllowedTransactionBit associated with this
+// transaction type.
+func (typ TransactionType) AllowedTransactionBit() (AllowedTransactionBit, bool) {
+	switch typ {
+	case TransactionTypeUpdateKeyPage:
+		return AllowedTransactionBitUpdateKeyPage, true
+	}
+
+	return 0, false
+}
