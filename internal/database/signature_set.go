9e8d1b5a1e1b4812ef4a7e14e91fb8a06498d867
Ethan Reesor <ethan.reesor@gmail.com>
2022-03-24 02:53:14 +0000

    fix: ensure receipts are sent and required
    Closes AC-1232. Some recent change (probably !214) effectively disabled synthetic transaction receipts. This change fixes that, so they are sent, and it updates the executor to reject synthetic transactions that don't have either a receipt or an ED25519 signature.

--- internal/database/signature_set.go
+++ internal/database/signature_set.go
@@ -3,6 +3,7 @@ package database
 import (
 	"crypto/sha256"
 
+	"gitlab.com/accumulatenetwork/accumulate/internal/encoding/hash"
 	"gitlab.com/accumulatenetwork/accumulate/protocol"
 )
 
@@ -14,11 +15,11 @@ func (s *SignatureSet) Add(newSignatures ...protocol.Signature) int {
 	// Only keep one signature per public key
 	seen := map[[32]byte]bool{}
 	for _, sig := range s.Signatures {
-		hash := sha256.Sum256(sig.GetPublicKey())
+		hash := hashSignatureForUniqueness(sig)
 		seen[hash] = true
 	}
 	for _, sig := range newSignatures {
-		hash := sha256.Sum256(sig.GetPublicKey())
+		hash := hashSignatureForUniqueness(sig)
 		if seen[hash] {
 			continue
 		}
@@ -27,4 +28,37 @@ func (s *SignatureSet) Add(newSignatures ...protocol.Signature) int {
 	}
 
 	return len(s.Signatures)
+}
+
+// hashSignatureForUniqueness returns a hash that is used to prevent two
+// signatures from the same key.
+func hashSignatureForUniqueness(sig protocol.Signature) [32]byte {
+	switch sig := sig.(type) {
+	case *protocol.SyntheticSignature:
+		// Multiple synthetic signatures doesn't make sense, but if they're
+		// unique... ok
+		hasher := make(hash.Hasher, 0, 3)
+		hasher.AddUrl(sig.SourceNetwork)
+		hasher.AddUrl(sig.DestinationNetwork)
+		hasher.AddUint(sig.SequenceNumber)
+		return *(*[32]byte)(hasher.MerkleHash())
+
+	case *protocol.ReceiptSignature:
+		// Multiple receipts doesn't make sense, but if they anchor to a unique
+		// root... ok
+		return *(*[32]byte)(sig.Result)
+
+	case *protocol.InternalSignature:
+		// Internal signatures only make any kind of sense if they're coming
+		// from the local network, so they should never be different
+		return sha256.Sum256([]byte("Accumulate Internal Signature"))
+
+	default:
+		// Normal signatures must come from a unique key
+		key := sig.GetPublicKey()
+		if key == nil {
+			panic("attempted to add a signature that doesn't have a key!")
+		}
+		return sha256.Sum256(key)
+	}
 }
