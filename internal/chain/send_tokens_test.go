4e4dcc421e302935e5e8be1f0bf9112a513cc942
Ethan Reesor <ethan.reesor@gmail.com>
2022-03-24 20:45:14 +0000

    chore(prot): use a sub-batch for the state manager
    Updates AC-1244. Use a sub-batch in the state manager. This will allow us to eliminate state operations, and possibly eliminate most of the state manager.

--- internal/chain/send_tokens_test.go
+++ internal/chain/send_tokens_test.go
@@ -36,6 +36,7 @@ func TestLiteTokenTransactions(t *testing.T) {
 
 	st, err := NewStateManager(db.Begin(true), protocol.SubnetUrl(t.Name()), gtx)
 	require.NoError(t, err)
+	defer st.Discard()
 
 	_, err = SendTokens{}.Validate(st, gtx)
 	require.NoError(t, err)
