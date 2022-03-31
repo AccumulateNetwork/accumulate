1e61aff8d68d9dce5e48ad4b94d97771eb3ebbe9
Dennis B <dennis@factoid.org>
2022-03-31 18:58:27 -0500

    test fix

--- scripts/ci/validate.sh
+++ scripts/ci/validate.sh
@@ -91,6 +91,7 @@ if [ -f "$NODE_PRIV_VAL" ] && [ -f "/.dockerenv" ] && which accumulated > /dev/n
    #spin up 2 DN validators, we cannot have 2 validators, so need either 1 or 3, since 1 is running we need to add 2
    declare -g TEST_NODE_WORK_DIR_1=${NODE_ROOT:-~/.testnode1}
    declare -g TEST_NODE_WORK_DIR_2=${NODE_ROOT:-~/.testnode2}
+   echo "$TEST_NODE_WORK_DIR_2 ========================="
    accumulated init node tcp://dn-0:26656 --listen=tcp://127.0.1.100:26656 -w "$TEST_NODE_WORK_DIR_1" --skip-version-check --no-website
    accumulated init node tcp://dn-0:26656 --listen=tcp://127.0.1.101:26656 -w "$TEST_NODE_WORK_DIR_2" --skip-version-check --no-website
    accumulated run -n 0 -w "$TEST_NODE_WORK_DIR_1/dn" &
@@ -98,7 +99,7 @@ if [ -f "$NODE_PRIV_VAL" ] && [ -f "/.dockerenv" ] && which accumulated > /dev/n
    accumulated run -n 0 -w "$TEST_NODE_WORK_DIR_2/dn" &
    declare -g ACCPID_2=$!
 
-   sleep 5
+   #sleep 5
    # Get Keys
    pubkey=$(jq -re .pub_key.value $TEST_NODE_WORK_DIR_1/dn/Node0/config/priv_validator_key.json)
    pubkey=$(echo $pubkey | base64 -d | od -t x1 -An )
