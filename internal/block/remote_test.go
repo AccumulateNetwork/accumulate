27642d1f6c656deca09faf64e6c92524fbce5676
Ethan Reesor <ethan.reesor@gmail.com>
2022-04-05 18:32:27 -0500

    feat(prot): support initiating txn with remote sig

--- internal/block/remote_test.go
+++ internal/block/remote_test.go
@@ -1,14 +1,17 @@
 package block_test
 
 import (
+	"crypto/ed25519"
 	"crypto/sha256"
 	"math/big"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	tmed25519 "github.com/tendermint/tendermint/crypto/ed25519"
 	"gitlab.com/accumulatenetwork/accumulate/internal/block/simulator"
 	acctesting "gitlab.com/accumulatenetwork/accumulate/internal/testing"
 	"gitlab.com/accumulatenetwork/accumulate/internal/url"
+	"gitlab.com/accumulatenetwork/accumulate/protocol"
 	. "gitlab.com/accumulatenetwork/accumulate/protocol"
 )
 
@@ -17,17 +20,18 @@ func doSha256(data []byte) []byte {
 	return hash[:]
 }
 
-func TestRemoteSignatures(t *testing.T) {
-	var timestamp uint64
+func delivered(status *protocol.TransactionStatus) bool {
+	return status.Delivered
+}
 
-	// Initialize
-	sim := simulator.New(t, 3)
-	sim.InitChain()
+func pending(status *protocol.TransactionStatus) bool {
+	return status.Pending
+}
 
-	alice := acctesting.GenerateTmKey(t.Name())
-	aliceUrl := acctesting.AcmeLiteAddressTmPriv(alice)
+func SetupForRemoteSignatures(sim *simulator.Simulator, timestamp *uint64, alice, bob, charlie ed25519.PrivateKey) {
+	aliceTm := tmed25519.PrivKey(alice)
+	aliceUrl := acctesting.AcmeLiteAddressTmPriv(aliceTm)
 	bobUrl, charlieUrl := url.MustParse("bob"), url.MustParse("charlie")
-	bobKey, charlieKey := acctesting.GenerateKey(), acctesting.GenerateKey()
 
 	sim.SetRouteFor(aliceUrl.RootIdentity(), "BVN0")
 	sim.SetRouteFor(bobUrl, "BVN1")
@@ -36,42 +40,42 @@ func TestRemoteSignatures(t *testing.T) {
 	// Create and fund a lite address
 	batch := sim.SubnetFor(aliceUrl).Database.Begin(true)
 	defer batch.Discard()
-	require.NoError(t, acctesting.CreateLiteTokenAccountWithCredits(batch, alice, 1e9, 1e9))
-	require.NoError(t, batch.Commit())
+	require.NoError(sim, acctesting.CreateLiteTokenAccountWithCredits(batch, aliceTm, 1e9, 1e9))
+	require.NoError(sim, batch.Commit())
 
 	// Create the ADIs
 	envs := sim.MustSubmitAndExecuteBlock(
 		acctesting.NewTransaction().
 			WithPrincipal(aliceUrl).
 			WithSigner(aliceUrl, 1).
-			WithTimestampVar(&timestamp).
+			WithTimestampVar(timestamp).
 			WithBody(&CreateIdentity{
 				Url:        bobUrl,
 				KeyBookUrl: bobUrl.JoinPath("book"),
-				KeyHash:    doSha256(bobKey[32:]),
+				KeyHash:    doSha256(bob[32:]),
 			}).
 			Initiate(SignatureTypeED25519, alice).
 			Build(),
 		acctesting.NewTransaction().
 			WithPrincipal(aliceUrl).
 			WithSigner(aliceUrl, 1).
-			WithTimestampVar(&timestamp).
+			WithTimestampVar(timestamp).
 			WithBody(&CreateIdentity{
 				Url:        charlieUrl,
 				KeyBookUrl: charlieUrl.JoinPath("book"),
-				KeyHash:    doSha256(charlieKey[32:]),
+				KeyHash:    doSha256(charlie[32:]),
 			}).
 			Initiate(SignatureTypeED25519, alice).
 			Build(),
 	)
-	sim.WaitForTransactions(envs...)
+	sim.WaitForTransactions(delivered, envs...)
 
 	// Add credits to the key pages
 	envs = sim.MustSubmitAndExecuteBlock(
 		acctesting.NewTransaction().
 			WithPrincipal(aliceUrl).
 			WithSigner(aliceUrl, 1).
-			WithTimestampVar(&timestamp).
+			WithTimestampVar(timestamp).
 			WithBody(&AddCredits{
 				Recipient: bobUrl.JoinPath("book", "1"),
 				Amount:    *big.NewInt(AcmePrecision * 1e3),
@@ -82,7 +86,7 @@ func TestRemoteSignatures(t *testing.T) {
 		acctesting.NewTransaction().
 			WithPrincipal(aliceUrl).
 			WithSigner(aliceUrl, 1).
-			WithTimestampVar(&timestamp).
+			WithTimestampVar(timestamp).
 			WithBody(&AddCredits{
 				Recipient: charlieUrl.JoinPath("book", "1"),
 				Amount:    *big.NewInt(AcmePrecision * 1e3),
@@ -91,36 +95,58 @@ func TestRemoteSignatures(t *testing.T) {
 			Initiate(SignatureTypeED25519, alice).
 			Build(),
 	)
-	sim.WaitForTransactions(envs...)
+	sim.WaitForTransactions(delivered, envs...)
 
 	// Create the data account
 	envs = sim.MustSubmitAndExecuteBlock(
 		acctesting.NewTransaction().
 			WithPrincipal(bobUrl).
 			WithSigner(bobUrl.JoinPath("book", "1"), 1).
-			WithTimestampVar(&timestamp).
+			WithTimestampVar(timestamp).
 			WithBody(&CreateDataAccount{
 				Url: bobUrl.JoinPath("account"),
 			}).
-			Initiate(SignatureTypeED25519, bobKey).
+			Initiate(SignatureTypeED25519, bob).
 			Build(),
 	)
-	sim.WaitForTransactions(envs...)
+	sim.WaitForTransactions(delivered, envs...)
 
 	// Add the second authority
 	envs = sim.MustSubmitAndExecuteBlock(
 		acctesting.NewTransaction().
 			WithPrincipal(bobUrl.JoinPath("account")).
 			WithSigner(bobUrl.JoinPath("book", "1"), 1).
-			WithTimestampVar(&timestamp).
+			WithTimestampVar(timestamp).
 			WithBody(&UpdateAccountAuth{Operations: []AccountAuthOperation{
 				&AddAccountAuthorityOperation{Authority: charlieUrl.JoinPath("book")},
 			}}).
-			Initiate(SignatureTypeED25519, bobKey).
+			Initiate(SignatureTypeED25519, bob).
 			Build(),
 	)
-	sim.WaitForTransactions(envs...)
+	sim.WaitForTransactions(delivered, envs...)
+}
 
+func TestRemoteSignatures_SignPending(t *testing.T) {
+	var timestamp uint64
+
+	// Initialize
+	sim := simulator.New(t, 3)
+	sim.InitChain()
+
+	alice := acctesting.GenerateKey(t.Name())
+	aliceUrl := acctesting.AcmeLiteAddressTmPriv(tmed25519.PrivKey(alice))
+	bobUrl, charlieUrl := url.MustParse("bob"), url.MustParse("charlie")
+	bobKey, charlieKey := acctesting.GenerateKey(), acctesting.GenerateKey()
+
+	// Force the accounts onto different BVNs
+	sim.SetRouteFor(aliceUrl.RootIdentity(), "BVN0")
+	sim.SetRouteFor(bobUrl, "BVN1")
+	sim.SetRouteFor(charlieUrl, "BVN2")
+
+	// Setup
+	SetupForRemoteSignatures(sim, &timestamp, alice, bobKey, charlieKey)
+
+	// Initiate with the local authority
 	env := acctesting.NewTransaction().
 		WithPrincipal(bobUrl.JoinPath("account")).
 		WithBody(&WriteData{
@@ -132,6 +158,8 @@ func TestRemoteSignatures(t *testing.T) {
 		WithTimestampVar(&timestamp).
 		Initiate(SignatureTypeED25519, bobKey).
 		Build()
+
+	// Sign with the remote authority
 	sig := acctesting.NewTransaction().
 		WithPrincipal(bobUrl.JoinPath("account")).
 		WithBody(&RemoteTransactionBody{}).
@@ -140,9 +168,182 @@ func TestRemoteSignatures(t *testing.T) {
 		WithTimestamp(0).
 		Sign(SignatureTypeED25519, charlieKey).
 		Build()
-	envs = sim.MustSubmitAndExecuteBlock(env, sig)
-	sim.WaitForTransactions(envs...)
 
+	envs := sim.MustSubmitAndExecuteBlock(env, sig)
+	sim.WaitForTransactions(delivered, envs...)
+
+	// Validate
+	batch := sim.SubnetFor(bobUrl).Database.Begin(true)
+	defer batch.Discard()
+	data, err := batch.Account(bobUrl.JoinPath("account")).Data()
+	require.NoError(t, err)
+	de, err := data.Entry(data.Height() - 1)
+	require.NoError(t, err)
+	require.Equal(t, "foo", string(de.Data[0]))
+}
+
+func TestRemoteSignatures_SameBVN(t *testing.T) {
+	var timestamp uint64
+
+	// Initialize
+	sim := simulator.New(t, 3)
+	sim.InitChain()
+
+	alice := acctesting.GenerateKey(t.Name())
+	aliceUrl := acctesting.AcmeLiteAddressTmPriv(tmed25519.PrivKey(alice))
+	bobUrl, charlieUrl := url.MustParse("bob"), url.MustParse("charlie")
+	bobKey, charlieKey := acctesting.GenerateKey(), acctesting.GenerateKey()
+
+	// Force the accounts onto the same BVN
+	sim.SetRouteFor(aliceUrl.RootIdentity(), "BVN1")
+	sim.SetRouteFor(bobUrl, "BVN1")
+	sim.SetRouteFor(charlieUrl, "BVN1")
+
+	// Setup
+	SetupForRemoteSignatures(sim, &timestamp, alice, bobKey, charlieKey)
+
+	// Initiate with the local authority
+	env := acctesting.NewTransaction().
+		WithPrincipal(bobUrl.JoinPath("account")).
+		WithBody(&WriteData{
+			Entry: DataEntry{Data: [][]byte{
+				[]byte("foo"),
+			}},
+		}).
+		WithSigner(bobUrl.JoinPath("book", "1"), 1).
+		WithTimestampVar(&timestamp).
+		Initiate(SignatureTypeED25519, bobKey).
+		Build()
+
+	// Sign with the remote authority
+	sig := acctesting.NewTransaction().
+		WithPrincipal(bobUrl.JoinPath("account")).
+		WithBody(&RemoteTransactionBody{}).
+		WithTxnHash(env.GetTxHash()).
+		WithSigner(charlieUrl.JoinPath("book", "1"), 1).
+		WithTimestamp(0).
+		Sign(SignatureTypeED25519, charlieKey).
+		Build()
+
+	envs := sim.MustSubmitAndExecuteBlock(env, sig)
+	sim.WaitForTransactions(delivered, envs...)
+
+	// Validate
+	batch := sim.SubnetFor(bobUrl).Database.Begin(true)
+	defer batch.Discard()
+	data, err := batch.Account(bobUrl.JoinPath("account")).Data()
+	require.NoError(t, err)
+	de, err := data.Entry(data.Height() - 1)
+	require.NoError(t, err)
+	require.Equal(t, "foo", string(de.Data[0]))
+}
+
+func TestRemoteSignatures_Initiate(t *testing.T) {
+	var timestamp uint64
+
+	// Initialize
+	sim := simulator.New(t, 3)
+	sim.InitChain()
+
+	alice := acctesting.GenerateKey(t.Name())
+	aliceUrl := acctesting.AcmeLiteAddressTmPriv(tmed25519.PrivKey(alice))
+	bobUrl, charlieUrl := url.MustParse("bob"), url.MustParse("charlie")
+	bobKey, charlieKey := acctesting.GenerateKey(), acctesting.GenerateKey()
+
+	// Force the accounts onto different BVNs
+	sim.SetRouteFor(aliceUrl.RootIdentity(), "BVN0")
+	sim.SetRouteFor(bobUrl, "BVN1")
+	sim.SetRouteFor(charlieUrl, "BVN2")
+
+	// Setup
+	SetupForRemoteSignatures(sim, &timestamp, alice, bobKey, charlieKey)
+
+	// Initiate with the remote authority
+	envs := sim.MustSubmitAndExecuteBlock(
+		acctesting.NewTransaction().
+			WithPrincipal(bobUrl.JoinPath("account")).
+			WithBody(&WriteData{
+				Entry: DataEntry{Data: [][]byte{
+					[]byte("foo"),
+				}},
+			}).
+			WithTimestampVar(&timestamp).
+			WithSigner(charlieUrl.JoinPath("book", "1"), 1).
+			Initiate(SignatureTypeED25519, charlieKey).
+			Build(),
+	)
+	sim.WaitForTransactions(pending, envs...)
+
+	// Sign with the local authority
+	envs = sim.MustSubmitAndExecuteBlock(
+		acctesting.NewTransaction().
+			WithPrincipal(bobUrl.JoinPath("account")).
+			WithBody(&RemoteTransactionBody{}).
+			WithTxnHash(envs[0].GetTxHash()).
+			WithTimestamp(0).
+			WithSigner(bobUrl.JoinPath("book", "1"), 1).
+			Sign(SignatureTypeED25519, bobKey).
+			Build(),
+	)
+	sim.WaitForTransactions(delivered, envs...)
+
+	// Validate
+	batch := sim.SubnetFor(bobUrl).Database.Begin(true)
+	defer batch.Discard()
+	data, err := batch.Account(bobUrl.JoinPath("account")).Data()
+	require.NoError(t, err)
+	de, err := data.Entry(data.Height() - 1)
+	require.NoError(t, err)
+	require.Equal(t, "foo", string(de.Data[0]))
+}
+
+func TestRemoteSignatures_Singlesig(t *testing.T) {
+	var timestamp uint64
+
+	// Initialize
+	sim := simulator.New(t, 3)
+	sim.InitChain()
+
+	alice := acctesting.GenerateKey(t.Name())
+	aliceUrl := acctesting.AcmeLiteAddressTmPriv(tmed25519.PrivKey(alice))
+	bobUrl, charlieUrl := url.MustParse("bob"), url.MustParse("charlie")
+	bobKey, charlieKey := acctesting.GenerateKey(), acctesting.GenerateKey()
+
+	// Force the accounts onto different BVNs
+	sim.SetRouteFor(aliceUrl.RootIdentity(), "BVN0")
+	sim.SetRouteFor(bobUrl, "BVN1")
+	sim.SetRouteFor(charlieUrl, "BVN2")
+
+	// Setup
+	SetupForRemoteSignatures(sim, &timestamp, alice, bobKey, charlieKey)
+
+	// Remove the second authority directly
+	var account *protocol.DataAccount
+	batch := sim.SubnetFor(bobUrl).Database.Begin(true)
+	defer batch.Discard()
+	require.NoError(t, batch.Account(bobUrl.JoinPath("account")).GetStateAs(&account))
+	require.True(t, account.RemoveAuthority(bobUrl.JoinPath("book")))
+	require.Len(t, account.Authorities, 1)
+	require.NoError(t, batch.Account(bobUrl.JoinPath("account")).PutState(account))
+	require.NoError(t, batch.Commit())
+
+	// Execute with the remote authority
+	envs := sim.MustSubmitAndExecuteBlock(
+		acctesting.NewTransaction().
+			WithPrincipal(bobUrl.JoinPath("account")).
+			WithBody(&WriteData{
+				Entry: DataEntry{Data: [][]byte{
+					[]byte("foo"),
+				}},
+			}).
+			WithTimestampVar(&timestamp).
+			WithSigner(charlieUrl.JoinPath("book", "1"), 1).
+			Initiate(SignatureTypeED25519, charlieKey).
+			Build(),
+	)
+	sim.WaitForTransactions(delivered, envs...)
+
+	// Validate
 	batch = sim.SubnetFor(bobUrl).Database.Begin(true)
 	defer batch.Discard()
 	data, err := batch.Account(bobUrl.JoinPath("account")).Data()
