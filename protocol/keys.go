7672e26911a051bc34d3c91848131c804848cf25
Dennis B <bunfield@gmail.com>
2022-04-06 21:20:17 +0000

    chore(prot): add acceptance, rejection, response, and block threshold fields to key page
    Related to AC-1272
    
    - added acceptance, rejection, response, and block threshold fields to key page

--- protocol/keys.go
+++ protocol/keys.go
@@ -59,7 +59,7 @@ func (p *KeyPage) EntryByOwner(owner *url.URL) (int, *KeySpec, bool) {
 // m of n signatures, where m <= n, and n is the number of keys on the key page.
 // m is the Threshold number of signatures required to validate a transaction
 func (ms *KeyPage) GetMofN() (m, n uint64) {
-	m = ms.Threshold
+	m = ms.AcceptThreshold
 	n = uint64(len(ms.Keys))
 	return m, n
 }
@@ -68,7 +68,7 @@ func (ms *KeyPage) GetMofN() (m, n uint64) {
 // set the signature threshold to M.  Returns an error if m > n
 func (ms *KeyPage) SetThreshold(m uint64) error {
 	if m <= uint64(len(ms.Keys)) {
-		ms.Threshold = m
+		ms.AcceptThreshold = m
 	} else {
 		return fmt.Errorf("cannot require %d signatures on a key page with %d keys", m, len(ms.Keys))
 	}
