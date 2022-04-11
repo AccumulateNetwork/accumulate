ad762f9d090b69e1531e896418b901d27d4e6333
Sander Postma <spostma@defidevs.io>
2022-04-11 16:35:02 +0200

    AC-407: test coverage show file size

--- scripts/ci/validate-minor-blocks.sh
+++ scripts/ci/validate-minor-blocks.sh
@@ -5,31 +5,35 @@ set -e
 
 # section <name> - Print a section header
 function section {
-    echo -e '\033[1m'"$1"'\033[0m'
+  echo -e '\033[1m'"$1"'\033[0m'
 }
 
 section "Test minor block API omit-tx on BVN0"
-accumulate blocks minor acc://bvn-bvn0 10 5 omit
-# TODO assert output
+accumulate blocks minor acc://bvn-bvn0 10 5 omit >/tmp/minor.txt
+FILESIZE=$(stat -c%s "/tmp/minor.txt")
+echo File size is ${FILESIZE}
 
 section "Test minor block API omit-tx on DN"
-accumulate blocks minor acc://dn 10 5 omit
-# TODO assert output
+accumulate blocks minor acc://dn 10 5 omit >/tmp/minor.txt
+FILESIZE=$(stat -c%s "/tmp/minor.txt")
+echo File size is ${FILESIZE}
 
 section "Test minor block API count-only on BVN0"
-accumulate blocks minor acc://bvn-bvn0 10 5 countOnly
-# TODO assert output
+accumulate blocks minor acc://bvn-bvn0 10 5 countOnly >/tmp/minor.txt
+FILESIZE=$(stat -c%s "/tmp/minor.txt")
+echo File size is ${FILESIZE}
 
 section "Test minor block API count-only on DN"
-accumulate blocks minor acc://bvn-dn 10 5 countOnly
-# TODO assert output
+accumulate blocks minor acc://bvn-dn 10 5 countOnly >/tmp/minor.txt
+FILESIZE=$(stat -c%s "/tmp/minor.txt")
+echo File size is ${FILESIZE}
 
 section "Test minor block API expand on BVN0"
-accumulate blocks minor acc://bvn-bvn0 10 5  expand true
-# TODO assert output
+accumulate blocks minor acc://bvn-bvn0 10 5 expand true >/tmp/minor.txt
+FILESIZE=$(stat -c%s "/tmp/minor.txt")
+echo File size is ${FILESIZE}
 
 section "Test minor block API expand on DN"
-accumulate blocks minor acc://bvn-dn 10 5  expand true
-# TODO assert output
-
-
+accumulate blocks minor acc://bvn-dn 10 5 expand true >/tmp/minor.txt
+FILESIZE=$(stat -c%s "/tmp/minor.txt")
+echo File size is ${FILESIZE}
