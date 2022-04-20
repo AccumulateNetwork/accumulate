c39c144a7138071770f5980caea59490c5b74392
Sander Postma <spostma@defidevs.io>
2022-04-20 22:48:00 +0200

    Test coverage fix

--- scripts/ci/validate-minor-blocks.sh
+++ scripts/ci/validate-minor-blocks.sh
@@ -24,42 +24,42 @@ section "Test minor block API omit-tx on BVN0"
 accumulate blocks minor acc://bvn-bvn0 10 5 omit >/tmp/minor.txt
 FILESIZE=$(stat -c%s "/tmp/minor.txt")
 if [ "${FILESIZE}" -lt "500" ]; then
-  die "minor block result set too small"
+  die "minor block result set too small: ${FILESIZE}"
 fi
 
 section "Test minor block API omit-tx on DN"
 accumulate blocks minor acc://dn 10 5 omit >/tmp/minor.txt
 FILESIZE=$(stat -c%s "/tmp/minor.txt")
 if [ "${FILESIZE}" -lt "500" ]; then
-  die "minor block result set too small"
+  die "minor block result set too small: ${FILESIZE}"
 fi
 
 section "Test minor block API count-only on BVN0"
 accumulate blocks minor acc://bvn-bvn0 10 5 countOnly >/tmp/minor.txt
 FILESIZE=$(stat -c%s "/tmp/minor.txt")
 if [ "${FILESIZE}" -lt "500" ]; then
-  die "minor block result set too small"
+  die "minor block result set too small: ${FILESIZE}"
 fi
 
 section "Test minor block API count-only on DN"
 accumulate blocks minor acc://dn 10 5 countOnly >/tmp/minor.txt
 FILESIZE=$(stat -c%s "/tmp/minor.txt")
 if [ "${FILESIZE}" -lt "500" ]; then
-  die "minor block result set too small"
+  die "minor block result set too small: ${FILESIZE}"
 fi
 
 section "Test minor block API expand on BVN0"
-accumulate blocks minor acc://bvn-bvn0 10 5 expand true >/tmp/minor.txt
+accumulate blocks minor acc://bvn-bvn0 10 50 expand true >/tmp/minor.txt
 FILESIZE=$(stat -c%s "/tmp/minor.txt")
 if [ "${FILESIZE}" -lt "500" ]; then
-  die "minor block result set too small"
+  die "minor block result set too small: ${FILESIZE}"
 fi
 
 section "Test minor block API expand on DN"
-accumulate blocks minor acc://dn 10 5 expand true >/tmp/minor.txt
+accumulate blocks minor acc://dn 10 50 expand true >/tmp/minor.txt
 FILESIZE=$(stat -c%s "/tmp/minor.txt")
 if [ "${FILESIZE}" -lt "500" ]; then
-  die "minor block result set too small"
+  die "minor block result set too small: ${FILESIZE}"
 fi
 
 success
