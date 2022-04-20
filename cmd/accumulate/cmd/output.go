ba48643562bed70c7089970f0bb5c3861f82454f
Sander Postma <spostma@defidevs.io>
2022-04-20 22:08:37 +0200

    Print hashes as hex string in cmd output in printReflection()

--- cmd/accumulate/cmd/output.go
+++ cmd/accumulate/cmd/output.go
@@ -498,9 +498,26 @@ func printReflection(field, indent string, value reflect.Value) string {
 		}
 		return printReflection(field, indent, value.Elem())
 	case reflect.Slice, reflect.Array:
-		out += "\n"
-		for i, n := 0, value.Len(); i < n; i++ {
-			out += printReflection(fmt.Sprintf("%d (elem)", i), indent+"   ", value.Index(i))
+		if value.Len() == 32 && value.Index(0).Type().Bits() == 8 {
+			out += " "
+			var hashString string
+			hash, ok := value.Interface().([32]uint8)
+			if ok {
+				hashString = hex.EncodeToString(hash[:])
+			} else {
+				hash, ok := value.Interface().([]uint8)
+				if ok {
+					hashString = hex.EncodeToString(hash)
+				} else {
+					hashString = "(unknown hash format)"
+				}
+			}
+			out += fmt.Sprintf("%s%s\n", indent+"   ", hashString)
+		} else {
+			out += "\n"
+			for i, n := 0, value.Len(); i < n; i++ {
+				out += printReflection(fmt.Sprintf("%d (elem)", i), indent+"   ", value.Index(i))
+			}
 		}
 		return out
 	case reflect.Map:
