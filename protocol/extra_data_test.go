823a34342b4824e746910638eeb54dfec3e76124
Ethan Reesor <ethan.reesor@gmail.com>
2022-04-21 12:52:17 -0500

    fix test

--- protocol/extra_data_test.go
+++ protocol/extra_data_test.go
@@ -1,13 +1,25 @@
 package protocol
 
 import (
+	"bytes"
 	"math/big"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"gitlab.com/accumulatenetwork/accumulate/internal/encoding"
 	"gitlab.com/accumulatenetwork/accumulate/internal/url"
 	"gitlab.com/accumulatenetwork/accumulate/smt/storage"
 )
+
+func makeExtraData(t *testing.T, fn func(*encoding.Writer)) []byte {
+	buf := new(bytes.Buffer)
+	wr := encoding.NewWriter(buf)
+	fn(wr)
+	_, _, err := wr.Reset(nil)
+	require.NoError(t, err)
+	return buf.Bytes()
+}
 
 func TestExtraData(t *testing.T) {
 	txn1 := new(Transaction)
@@ -15,12 +27,12 @@ func TestExtraData(t *testing.T) {
 	txn1.Header.Initiator = storage.MakeKey(t.Name())
 	txn1.Header.Memo = "asdf"
 	txn1.Header.Metadata = []byte("qwer")
-	txn1.Header.extraData = []byte("extra header data")
+	txn1.Header.extraData = makeExtraData(t, func(w *encoding.Writer) { w.WriteString(10, "extra header data") })
 
 	body1 := new(SendTokens)
 	txn1.Body = body1
 	body1.To = []*TokenRecipient{{Url: url.MustParse("bar"), Amount: *big.NewInt(10)}, {Url: url.MustParse("baz"), Amount: *big.NewInt(20)}}
-	body1.extraData = []byte("extra body data")
+	body1.extraData = makeExtraData(t, func(w *encoding.Writer) { w.WriteString(10, "extra body data") })
 
 	// Marshal and unmarshal
 	data, err := txn1.MarshalBinary()
@@ -30,7 +42,7 @@ func TestExtraData(t *testing.T) {
 	body2 := txn2.Body.(*SendTokens)
 
 	// Check
-	require.Equal(t, txn1.Header.extraData, txn2.Header.extraData)
-	require.Equal(t, body1.extraData, body2.extraData)
-	require.Equal(t, txn1.GetHash(), txn2.GetHash())
+	assert.Equal(t, txn1.Header.extraData, txn2.Header.extraData)
+	assert.Equal(t, body1.extraData, body2.extraData)
+	assert.Equal(t, txn1.GetHash(), txn2.GetHash())
 }
