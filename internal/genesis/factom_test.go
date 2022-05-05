57b15ec0af39d93d92cec205638da6f3917c7cc4
Abhishek <rabhishek822@gmail.com>
2022-05-05 21:48:33 +0530

    suggested fix

--- internal/genesis/factom_test.go
+++ internal/genesis/factom_test.go
@@ -3,13 +3,13 @@ package genesis
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestFactomAddressesUpload(t *testing.T) {
 	value, err := LoadFactomAddressesAndBalances("test_factom_addresses")
-	if err != nil {
-		fmt.Println(err.Error())
-	}
+	require.NoError(t, err)
 	for _, v := range value {
 		fmt.Print("Address : ", v.Address)
 		fmt.Println("Balance : ", v.Balance)
