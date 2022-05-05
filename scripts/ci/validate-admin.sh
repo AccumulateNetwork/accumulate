20f40c1413b154bbf00b2009d07cd4f822e33e9f
Sander Postma <spostma@defidevs.io>
2022-05-05 23:43:52 +0200

    validator add pub key

--- scripts/ci/validate-admin.sh
+++ scripts/ci/validate-admin.sh
@@ -42,8 +42,12 @@ if [ -f "$(nodePrivKey 0)" ] && [ -f "/.dockerenv" ] && [ "$NUM_DNNS" -ge "3" ];
   # NUM_DNNS already contains the next node number (which starts counting at 0)
   accumulated init node "$NUM_DNNS" tcp://dn-0:26656 --listen=tcp://127.0.1.100:26656 -w "$DN_NODES_DIR" --skip-version-check --no-website
 
+  pubkey=$(jq -re .pub_key.value "$(nodePrivKey $NUM_DNNS)")
+  pubkey=$(echo $pubkey | base64 -d | od -t x1 -An )
+  declare -g hexPubKey=$(echo $pubkey | tr -d ' ')
+
   # Register new validator
-  TXID=$(cli-tx validator add dn "$(nodePrivKey 0)" "$(nodePrivKey $NUM_DNNS)")
+  TXID=$(cli-tx validator add dn "$(nodePrivKey 0)" $hexPubKey)
   wait-for-tx $TXID
 
   # Sign the required number of times
