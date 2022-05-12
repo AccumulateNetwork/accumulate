5deebdc6921948e2a3422d0c85aed20572cfdbfc
Dennis B <dennis@factoid.org>
2022-05-12 16:52:21 -0500

    improve isSystem tx check

--- internal/block/signature.go
+++ internal/block/signature.go
@@ -627,8 +627,8 @@ func verifyReceiptSignature(transaction *protocol.Transaction, receipt *protocol
 		return errors.New(errors.StatusBadRequest, "a receipt signature cannot be nested within another signature")
 	}
 
-	if !transaction.Body.Type().IsSynthetic() {
-		return fmt.Errorf("receipt signatures are not allowed for non-synthetic transactions")
+	if !transaction.Body.Type().IsSynthetic() && !transaction.Body.Type().IsSystem() {
+		return fmt.Errorf("receipt signatures are not allowed for non-synthetic or non-system transactions")
 	}
 
 	if !md.Initiated {
