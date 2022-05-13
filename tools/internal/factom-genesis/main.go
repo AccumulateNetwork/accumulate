3aa2c0197b4d3613598af193791004a5ae79ea2f
Abhishek <rabhishek822@gmail.com>
2022-05-13 11:11:47 +0530

    utility methods added

--- tools/internal/factom-genesis/main.go
+++ tools/internal/factom-genesis/main.go
@@ -1,6 +1,12 @@
 package factom
 
-import "gitlab.com/accumulatenetwork/accumulate/internal/client"
+import (
+	"context"
+
+	"gitlab.com/accumulatenetwork/accumulate/internal/api/v2"
+	"gitlab.com/accumulatenetwork/accumulate/internal/client"
+	"gitlab.com/accumulatenetwork/accumulate/protocol"
+)
 
 var factomChainData map[string]*Queue
 
@@ -8,10 +14,28 @@ func accountFromPrivateKey(privateKeyHex string) {
 
 }
 
-func WriteDataToAccumulate(env string) error {
+func WriteDataToAccumulate(env string, data *protocol.LiteDataEntry) error {
 	client, err := client.New(env)
 	if err != nil {
 		return err
 	}
-	client.ExecuteWriteDataTo()
+	req := &api.TxRequest{}
+	_, err = client.ExecuteWriteDataTo(context.Background(), req)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func WriteDataFromQueueToAccumulate() {
+	for chainId, data := range factomChainData {
+		go executeQueueToWriteData(chainId, data)
+	}
+}
+
+func executeQueueToWriteData(chainId string, queue *Queue) {
+	if len(*queue) > 0 {
+		data := queue.Pop().(*protocol.LiteDataEntry)
+		WriteDataToAccumulate("", data)
+	}
 }
