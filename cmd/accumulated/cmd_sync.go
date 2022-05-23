036e25fc99281bf1ac9e688f5cae6b4808fdc759
Ethan Reesor <ethan.reesor@gmail.com>
2022-05-23 16:55:40 -0500

    AC-1508: working on snapshots

--- cmd/accumulated/cmd_sync.go
+++ cmd/accumulated/cmd_sync.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"encoding/hex"
 	"strconv"
 
 	"github.com/spf13/cobra"
+	"github.com/tendermint/tendermint/rpc/client/http"
 	"gitlab.com/accumulatenetwork/accumulate/config"
 )
 
@@ -19,27 +22,48 @@ var cmdSync = &cobra.Command{
 }
 
 var cmdSyncSnapshot = &cobra.Command{
-	Use:   "snapshot [height] [hash]",
+	Use:   "snapshot [tendermint RPC server] [height] [hash]",
 	Short: "Configures state sync to pull a snapshot",
-	Args:  cobra.ExactArgs(2),
+	Args:  cobra.ExactArgs(3),
 	Run:   syncToSnapshot,
 }
 
 func syncToSnapshot(_ *cobra.Command, args []string) {
-	height, err := strconv.ParseInt(args[0], 10, 64)
-	checkf(err, "parse height")
+	client, err := http.New(args[0])
+	checkf(err, "server")
 
-	_, err = hex.DecodeString(args[1])
-	checkf(err, "check hash")
+	height, err := strconv.ParseInt(args[1], 10, 64)
+	checkf(err, "height")
+
+	hash, err := hex.DecodeString(args[2])
+	checkf(err, "hash")
 
 	c, err := config.Load(flagMain.WorkDir)
 	checkf(err, "load configuration")
 
+	// The Tendermint height is one more than the last commit height
+	blockHeight := height + 1
+
+	tmblock, err := client.Block(context.Background(), &blockHeight)
+	checkf(err, "fetching block")
+
+	if tmblock == nil {
+		fatalf("Block not found")
+	}
+
+	if tmblock.Block.LastCommit.Height != height {
+		fatalf("Last commit height does not match: want %d, got %d", height, tmblock.Block.Height)
+	}
+
+	if !bytes.Equal(tmblock.Block.AppHash, hash) {
+		fatalf("App hash does not match: want %x, got %x", hash, tmblock.Block.AppHash)
+	}
+
 	ss := c.StateSync
 	ss.Enable = true
 	ss.UseP2P = true
-	ss.TrustHeight = height
-	ss.TrustHash = args[1]
+	ss.TrustHeight = tmblock.Block.Height
+	ss.TrustHash = tmblock.Block.Header.Hash().String()
 
 	err = config.Store(c)
 	checkf(err, "store configuration")
