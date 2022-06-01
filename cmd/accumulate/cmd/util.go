b5d3932c43ca1db131e08af929a47ae81607fbf2
JATHIN JAGANNATH <jp@ph.iitr.ac.in>
2022-06-01 18:13:57 +0000

    chore(prot): synthetic transaction failure output [AC-1546]
    Print the synthetic transactions output when user uses --wait flag , so that we dont need to query the status of each synthetic transaction.
    ## Review Checklist
    
    **If any item is not complete, the merge request is not ready to be reviewed and must be marked `Draft:`.**
    
    - [x] The merge request title is in the format `<change type>(<change scope>): <short description> [<task id>]`
      - For example, `feat(cli): add QR code generation [AC-123]`
      - For details, see [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [x] The description includes `Closes <jira task ID>` (or rarely `Updates <jira task ID>`)
    - [x] The change is fully validated by tests that are run during CI
      - In most cases this means a test in "validate.sh"
      - In some cases, a Go test may be acceptable
      - Validation is not applicable to things like documentation updates
      - Purely UI/UX changes can be manually validated, such as changes to human-readable output
      - For all other changes, automated validation tests are an absolute requirement unless a maintainer specifically explains why they are not in a comment on this merge request
    - [x] The change is marked with one of the validation labels
      - ~"validation::ci/cd" for changes validated by CI tests
      - ~"validation::manual" for changes validated by hand
      - ~"validation::deferred" for changes validated by a follow up merge request
      - ~"validation::not applicable" for changes where validation is not applicable
    
    ## Merge Checklist
    
    - [x] CI is passing
    - [ ] Merge conflicts are resolved
    - [ ] All discussions are resolved
    
    Related to AC-1546

--- cmd/accumulate/cmd/util.go
+++ cmd/accumulate/cmd/util.go
@@ -300,31 +300,44 @@ func dispatchTxRequest(payload protocol.TransactionBody, txHash []byte, origin *
 	return res, nil
 }
 
-func dispatchTxAndWait(payload protocol.TransactionBody, txHash []byte, origin *url2.URL, signer *signing.Builder) (*api2.TxResponse, error) {
+func dispatchTxAndWait(payload protocol.TransactionBody, txHash []byte, origin *url2.URL, signer *signing.Builder) (*api2.TxResponse, []*api.TransactionQueryResponse, error) {
 	res, err := dispatchTxRequest(payload, txHash, origin, signer)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	if TxWait == 0 {
-		return res, nil
+		return res, nil, nil
 	}
 
-	_, err = waitForTxn(res.TransactionHash, TxWait, TxIgnorePending)
+	resps, err := waitForTxn(res.TransactionHash, TxWait, TxIgnorePending)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	return res, nil
+	return res, resps, nil
 }
 
 func dispatchTxAndPrintResponse(payload protocol.TransactionBody, txHash []byte, origin *url2.URL, signer *signing.Builder) (string, error) {
-	res, err := dispatchTxAndWait(payload, txHash, origin, signer)
+	res, resps, err := dispatchTxAndWait(payload, txHash, origin, signer)
 	if err != nil {
 		return PrintJsonRpcError(err)
 	}
 
-	return ActionResponseFrom(res).Print()
+	result, err := ActionResponseFrom(res).Print()
+	if err != nil {
+		return "", err
+	}
+	if res.Code == 0 {
+		for _, response := range resps {
+			str, err := PrintTransactionQueryResponseV2(response)
+			if err != nil {
+				return PrintJsonRpcError(err)
+			}
+			result = fmt.Sprint(result, str, "\n")
+		}
+	}
+	return result, nil
 }
 
 func buildEnvelope(payload protocol.TransactionBody, origin *url2.URL) (*protocol.Envelope, error) {
@@ -374,6 +387,7 @@ type ActionResponse struct {
 	Error           types.String                `json:"error"`
 	Mempool         types.String                `json:"mempool"`
 	Result          *protocol.TransactionStatus `json:"result"`
+	SynthTxns       types.String                `json:"synthtxns"`
 }
 
 type ActionDataResponse struct {
@@ -637,4 +651,20 @@ func GetAccountStateProof(principal, accountToProve *url2.URL) (proof *protocol.
 
 		}
 	}
+}
+
+func GetSynthTxnsString(res *api.TxResponse, resps []*api.TransactionQueryResponse) (string, error) {
+	result := ""
+	if res.Code == 0 {
+
+		for _, response := range resps {
+			str, err := PrintTransactionQueryResponseV2(response)
+			if err != nil {
+				return PrintJsonRpcError(err)
+			}
+			result = fmt.Sprint(result, str, "\n")
+		}
+
+	}
+	return result, nil
 }
