9c0bc3f6f320f51d1d7d18f78ebda1342ba56f33
Ethan Reesor <ethan.reesor@gmail.com>
2022-06-02 15:13:49 -0500

    fix

--- scripts/ci/validate-minor-blocks.sh
+++ scripts/ci/validate-minor-blocks.sh
@@ -17,42 +17,42 @@ function dieMinor {
 section "Test minor block API omit-tx on BVN0"
 accumulate blocks minor acc://bvn-bvn0 10 5 omit >/tmp/minor.txt
 FILESIZE=$(stat -c%s "/tmp/minor.txt")
-if [ "${FILESIZE}" -lt "500" ]; then
+if [ "${FILESIZE}" -lt "250" ]; then
   dieMinor "minor block result set too small: ${FILESIZE}"
 fi
 
 section "Test minor block API omit-tx on DN"
 accumulate blocks minor acc://dn 10 5 omit >/tmp/minor.txt
 FILESIZE=$(stat -c%s "/tmp/minor.txt")
-if [ "${FILESIZE}" -lt "500" ]; then
+if [ "${FILESIZE}" -lt "250" ]; then
   dieMinor "minor block result set too small: ${FILESIZE}"
 fi
 
 section "Test minor block API count-only on BVN0"
 accumulate blocks minor acc://bvn-bvn0 10 5 countOnly >/tmp/minor.txt
 FILESIZE=$(stat -c%s "/tmp/minor.txt")
-if [ "${FILESIZE}" -lt "500" ]; then
+if [ "${FILESIZE}" -lt "250" ]; then
   dieMinor "minor block result set too small: ${FILESIZE}"
 fi
 
 section "Test minor block API count-only on DN"
 accumulate blocks minor acc://dn 10 5 countOnly >/tmp/minor.txt
 FILESIZE=$(stat -c%s "/tmp/minor.txt")
-if [ "${FILESIZE}" -lt "500" ]; then
+if [ "${FILESIZE}" -lt "250" ]; then
   dieMinor "minor block result set too small: ${FILESIZE}"
 fi
 
 section "Test minor block API expand on BVN0"
 accumulate blocks minor acc://bvn-bvn0 10 50 expand excludeempty >/tmp/minor.txt
 FILESIZE=$(stat -c%s "/tmp/minor.txt")
-if [ "${FILESIZE}" -lt "500" ]; then
+if [ "${FILESIZE}" -lt "250" ]; then
   dieMinor "minor block result set too small: ${FILESIZE}"
 fi
 
 section "Test minor block API expand on DN"
 accumulate blocks minor acc://dn 10 50 expand >/tmp/minor.txt
 FILESIZE=$(stat -c%s "/tmp/minor.txt")
-if [ "${FILESIZE}" -lt "500" ]; then
+if [ "${FILESIZE}" -lt "250" ]; then
   dieMinor "minor block result set too small: ${FILESIZE}"
 fi
 
