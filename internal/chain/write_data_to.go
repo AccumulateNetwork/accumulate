4ae93cfa11b1887faada75ea8fae1b6203cb3298
Ethan Reesor <ethan.reesor@gmail.com>
2022-06-02 16:36:02 +0000

    feat(prot): store data entry in account state [AC-1580]
    Closes AC-1580. Allow a data entry to be stored in the account state. When writing to a non-scratch ADI data account, if the transaction specifies `writeToState: true`, write the entry to the account state as well as the main chain.
    
    This MR modifies how hashes are calculated for WriteData, WriteDataTo, and SyntheticWriteData transactions to improve robustness.
    ## Review Checklist
    
    **If any item is not complete, the merge request is not ready to be reviewed and must be marked `Draft:`.**
    
    - [x] The merge request title is in the format `<change type>(<change scope>): <short description> [<task id>]`
      - For example, `feat(cli): add QR code generation [AC-123]`
      - For details, see [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [x] The description includes `Closes <jira task ID>` (or rarely `Updates <jira task ID>`)
    - [x] The change is fully validated by tests that are run during CI
      - In most cases this means a test in "validate.sh"
      - In some cases, a Go test may be acceptable
      - Validation is not applicable to things like documentation updates
      - Purely UI/UX changes can be manually validated, such as changes to human-readable output
      - For all other changes, automated validation tests are an absolute requirement unless a maintainer specifically explains why they are not in a comment on this merge request
    - [x] The change is marked with one of the validation labels
      - ~"validation::ci/cd" for changes validated by CI tests
      - ~"validation::manual" for changes validated by hand
      - ~"validation::deferred" for changes validated by a follow up merge request
      - ~"validation::not applicable" for changes where validation is not applicable
    
    ## Merge Checklist
    
    - [ ] CI is passing
    - [ ] Merge conflicts are resolved
    - [ ] All discussions are resolved
    
    Related to AC-1580
    
    
    Former-commit-id: 1cb6ed89e7b59699abb79fb47494f511dd7c1ae4

--- internal/chain/write_data_to.go
+++ internal/chain/write_data_to.go
@@ -1,8 +1,7 @@
 package chain
 
 import (
-	"fmt"
-
+	"gitlab.com/accumulatenetwork/accumulate/internal/errors"
 	"gitlab.com/accumulatenetwork/accumulate/protocol"
 )
 
@@ -17,15 +16,19 @@ func (WriteDataTo) Execute(st *StateManager, tx *Delivery) (protocol.Transaction
 func (WriteDataTo) Validate(st *StateManager, tx *Delivery) (protocol.TransactionResult, error) {
 	body, ok := tx.Transaction.Body.(*protocol.WriteDataTo)
 	if !ok {
-		return nil, fmt.Errorf("invalid payload: want %T, got %T", new(protocol.WriteDataTo), tx.Transaction.Body)
+		return nil, errors.Format(errors.StatusInternalError, "invalid payload: want %T, got %T", new(protocol.WriteDataTo), tx.Transaction.Body)
+	}
+
+	if body.Entry == nil {
+		return nil, errors.Format(errors.StatusBadRequest, "entry is missing")
 	}
 
 	if _, ok := body.Entry.(*protocol.FactomDataEntry); ok {
-		return nil, fmt.Errorf("writing new Factom-formatted data entries is not supported")
+		return nil, errors.Format(errors.StatusBadRequest, "writing new Factom-formatted data entries is not supported")
 	}
 
 	if _, err := protocol.ParseLiteDataAddress(body.Recipient); err != nil {
-		return nil, fmt.Errorf("only writes to lite data accounts supported: %s: %v", body.Recipient, err)
+		return nil, errors.Format(errors.StatusBadRequest, "only writes to lite data accounts supported: %s: %v", body.Recipient, err)
 	}
 
 	writeThis := new(protocol.SyntheticWriteData)
