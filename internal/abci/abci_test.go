052f5beb94c097e618638f1d2a094ec9d24614c2
Abhishek <rabhishek822@gmail.com>
2022-06-02 22:31:06 +0530

    import cycle error fix

--- internal/abci/abci_test.go
+++ internal/abci/abci_test.go
@@ -1,39 +1,44 @@
-package abci
+package abci_test
 
 import (
+	"log"
 	"math/big"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	abci "github.com/tendermint/tendermint/abci/types"
 	acctesting "gitlab.com/accumulatenetwork/accumulate/internal/testing"
+	"gitlab.com/accumulatenetwork/accumulate/internal/url"
 	"gitlab.com/accumulatenetwork/accumulate/protocol"
 )
 
 func TestTransactionPriority(t *testing.T) {
-	var app *Accumulator
 	subnets, daemons := acctesting.CreateTestNet(t, 1, 1, 0, false)
 	nodes := RunTestNet(t, subnets, daemons, nil, true, nil)
 	n := nodes[subnets[1]][0]
-	app = n.app
+	fooKey := generateKey()
+	batch := n.db.Begin(true)
+	require.NoError(t, acctesting.CreateAdiWithCredits(batch, fooKey, "foo", 1e9))
+	require.NoError(t, acctesting.CreateTokenAccount(batch, "foo/tokens", protocol.AcmeUrl().String(), 1, false))
+	require.NoError(t, batch.Commit())
+
 	cases := make(map[string]struct {
 		Envelope       *protocol.Envelope
 		ExpectPriority int
 	})
-	txns := []*protocol.Transaction{}
-	txns = append(txns, &protocol.Transaction{
-		Body: &protocol.SyntheticDepositTokens{
-			Token:  protocol.AcmeUrl(),
-			Amount: *big.NewInt(100),
-		},
-	})
+	body := &protocol.SyntheticDepositTokens{
+		Token:  protocol.AcmeUrl(),
+		Amount: *big.NewInt(100),
+	}
+	env := newTxn("foo/tokens").
+		WithSigner(url.MustParse("foo/book0/1"), 1).
+		WithBody(body).
+		Initiate(protocol.SignatureTypeLegacyED25519, fooKey).Build()
 	cases["syntheticDepositToken"] = struct {
 		Envelope       *protocol.Envelope
 		ExpectPriority int
 	}{
-		Envelope: &protocol.Envelope{
-			Transaction: txns,
-		},
+		Envelope:       env,
 		ExpectPriority: 1,
 	}
 
@@ -41,7 +46,8 @@ func TestTransactionPriority(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			b, err := c.Envelope.MarshalBinary()
 			require.NoError(t, err)
-			resp := app.CheckTx(abci.RequestCheckTx{Tx: b})
+			resp := n.app.CheckTx(abci.RequestCheckTx{Tx: b})
+			log.Println(resp)
 			require.Equal(t, c.ExpectPriority, resp.Priority)
 		})
 	}
