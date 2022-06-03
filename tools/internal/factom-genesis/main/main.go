d7f50cf2d3fbf235c5e41a9fe301d6f7ef1c31a4
Abhishek <rabhishek822@gmail.com>
2022-06-03 17:48:27 +0530

    suggested changes

--- tools/internal/factom-genesis/main/main.go
+++ tools/internal/factom-genesis/main/main.go
@@ -9,10 +9,7 @@ import (
 )
 
 const (
-	// Key_Name         = "keytest-0-0"
 	Key_Private_Key = "d125672c7f0af6fd82c87c884560c6fcbaf03bcd51ac578057369d7e99274f3c"
-	// Key_Public_Key   = "f40e4b1b3bf80938c4e9a541f395af8fd8a3ef39ea779b3bd85ce65fb17deb72"
-	// OriginAccountUrl = "acc://c1dd0c5b8540f64be83eb5704678d3556829498f3726b7d8/ACME"
 )
 
 var faucet = true
--- tools/internal/factom-genesis/queue.go
+++ tools/internal/factom-genesis/queue.go
@@ -10,16 +10,16 @@ type Queue struct {
 func (self *Queue) Push(x interface{}) {
 	self.m.Lock()
 	defer self.m.Unlock()
-	*&self.q = append(*&self.q, x)
+	self.q = append(self.q, x)
 }
 
 func (self *Queue) Pop() interface{} {
 	self.m.Lock()
 	defer self.m.Unlock()
-	h := *&self.q
+	h := self.q
 	var el interface{}
 	l := len(h)
-	el, *&self.q = h[0], h[1:l]
+	el, self.q = h[0], h[1:l]
 	// Or use this instead for a Stack
 	// el, *self = h[l-1], h[0:l-1]
 	return el
