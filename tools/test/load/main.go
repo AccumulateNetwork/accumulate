66fcc99d7af1faa85140a926601c625b00c4fd67
Pavel Fomenko <fomenkop145@gmail.com>
2022-06-07 15:32:27 +0200

    AC-1518 remove unused code and set max routines

--- tools/test/load/main.go
+++ tools/test/load/main.go
@@ -1,21 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"context"
 	"crypto/ed25519"
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
-	"syscall"
 	"time"
 
-	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 	"gitlab.com/accumulatenetwork/accumulate/internal/api/v2"
 	"gitlab.com/accumulatenetwork/accumulate/internal/client"
@@ -33,112 +25,29 @@ func main() {
 var cmd = &cobra.Command{
 	Use: "devnet",
 	Run: func(*cobra.Command, []string) {
-		err := initClient("http://localhost:26660/v2")
+		err := initClient("http://127.0.1.1:26660/v2")
 		if err != nil {
 			os.Exit(1)
 		}
 	},
 }
 
-var resetColor = color.New(color.Reset)
-
-func assertInModuleRoot() {
-	cwd, err := os.Getwd()
-	checkf(err, "getwd")
-
-	_, err = os.Stat(filepath.Join(cwd, "go.mod"))
-	checkf(err, "stat go.mod failed - are we in the repo root?")
-}
-
-func build(tool string) {
-	buildCmd := exec.Command("go", "build", tool)
-	buildCmd.Stdout = os.Stdout
-	buildCmd.Stderr = os.Stdout
-	err := buildCmd.Run()
-	checkf(err, "build %s", tool)
-}
-
-func launch() *exec.Cmd {
-	// Initialize the devnet command
-	args := append([]string{"init", "devnet", "--work-dir", ".nodes"}, extraFlags...)
-	initCmd := exec.Command("./accumulated", args...)
-	initCmd.Stdout = os.Stdout
-	initCmd.Stderr = os.Stdout
-	err := initCmd.Run()
-	checkf(err, "init devnet")
-
-	// Configure the devnet command
-	runCmd := exec.Command("./accumulated", "run", "devnet", "--work-dir", ".nodes")
-	runCmd.Env = os.Environ()
-	runCmd.Env = append(runCmd.Env, "FORCE_COLOR=true")
-
-	// Don't interrupt the run process if the parent process is interrupted
-	runCmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-
-	// Forward output
-	runRd, runWr := io.Pipe()
-	runCmd.Stdout = runWr
-	runCmd.Stderr = runWr
-	started := make(chan struct{})
-
-	go func() {
-		bufRd := bufio.NewReader(runRd)
-		for {
-			line, err := bufRd.ReadString('\n')
-			if errors.Is(err, io.EOF) {
-				return
-			}
-			checkf(err, "read devnet stdout/stderr")
-
-			if strings.Contains(line, "----- Started -----") {
-				close(started)
-			}
-
-			print(line + resetColor.Sprint(""))
-		}
-	}()
-
-	// Start the devnet
-	err = runCmd.Start()
-	checkf(err, "start devnet")
-	<-started
-
-	return runCmd
-}
-
-func stop(runCmd *exec.Cmd) {
-	err := runCmd.Process.Signal(os.Interrupt)
-	if err != nil {
-		fmt.Printf("Error: interrupt devnet: %v\n", err)
-	}
-
-	go func() {
-		time.Sleep(time.Minute)
-		_ = runCmd.Process.Kill()
-	}()
-
-	err = runCmd.Wait()
-	if err != nil {
-		fmt.Printf("Error: wait for devnet: %v\n", err)
-	}
-}
-
 // Init new client from server URL input using client.go
 func initClient(server string) error {
 	// Create new client on localhost
-	client, err := client.New("http://127.0.1.1:26660/v2")
+	client, err := client.New(server)
 	checkf(err, "creating client")
 	client.DebugRequest = true
 
 	// Limit amount of goroutines
-	maxGoroutines := 1
+	maxGoroutines := 25
 	guard := make(chan struct{}, maxGoroutines)
 
 	// Start time
 	start := time.Now()
 
 	// run key generation in cycle
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 50; i++ {
 		// create accounts and store them
 		acc, _ := createAccount(i)
 
@@ -178,14 +87,15 @@ func initClient(server string) error {
 		<-timer.C
 	}
 
-	//Stop time Tx executions
-	stop := time.Now()
-	fmt.Printf("The Tx execution took %v to run.\n", stop.Sub(start))
-
 	// wait for goroutines to finish
 	for i := 0; i < maxGoroutines; i++ {
 		guard <- struct{}{}
 	}
+
+	//Stop time Tx executions
+	stop := time.Now()
+	fmt.Printf("The Txs execution took %v to run.\n", stop.Sub(start))
+
 	return nil
 }
 
