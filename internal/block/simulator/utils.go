4a03523baf59aaa9eeb7bce3e56757b195190d15
Sander Postma <spostma@defidevs.io>
2022-06-09 15:38:26 +0200

    post-merge fix

--- internal/block/simulator/utils.go
+++ internal/block/simulator/utils.go
@@ -57,7 +57,7 @@ func CheckTx(t TB, db *database.Database, exec *Executor, delivery *chain.Delive
 	batch := db.Begin(false)
 	defer batch.Discard()
 
-	result, err := exec.ValidateEnvelope(batch, delivery)
+	result, err := exec.ValidateEnvelope(delivery)
 	if err != nil {
 		return nil, errors.Wrap(errors.StatusUnknown, err)
 	}
