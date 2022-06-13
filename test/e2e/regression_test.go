c7bfc2f5e686f850cac9a13bd1f65d70a08a8bfe
Ethan Reesor <ethan.reesor@gmail.com>
2022-06-13 14:38:13 -0500

    attempting to reproduce the issue

--- test/e2e/regression_test.go
+++ test/e2e/regression_test.go
@@ -1,15 +1,16 @@
 package e2e
 
 import (
+	"context"
 	"fmt"
 	"math/big"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"gitlab.com/accumulatenetwork/accumulate/internal/api/v2"
 	"gitlab.com/accumulatenetwork/accumulate/internal/block/simulator"
 	"gitlab.com/accumulatenetwork/accumulate/internal/database"
 	acctesting "gitlab.com/accumulatenetwork/accumulate/internal/testing"
-	"gitlab.com/accumulatenetwork/accumulate/protocol"
 	. "gitlab.com/accumulatenetwork/accumulate/protocol"
 	"gitlab.com/accumulatenetwork/accumulate/types/api/query"
 )
@@ -23,7 +24,7 @@ func TestIssueAC1555(t *testing.T) {
 
 	// Setup accounts
 	const initialBalance = 100
-	alice := protocol.AccountUrl("alice")
+	alice := AccountUrl("alice")
 	aliceKey, liteKey := acctesting.GenerateKey(alice), acctesting.GenerateKey("lite")
 	liteUrl := acctesting.AcmeLiteAddressStdPriv(liteKey)
 	sim.CreateAccount(&LiteIdentity{Url: liteUrl.RootIdentity(), CreditBalance: 1e9})
@@ -66,7 +67,7 @@ func TestQueryKeyIndexWithRemoteAuthority(t *testing.T) {
 	sim.InitFromGenesis()
 
 	// Setup
-	alice, bob := protocol.AccountUrl("alice"), protocol.AccountUrl("bob")
+	alice, bob := AccountUrl("alice"), AccountUrl("bob")
 	aliceKey, bobKey := acctesting.GenerateKey(alice), acctesting.GenerateKey(bob)
 	sim.SetRouteFor(alice, "BVN0")
 	sim.SetRouteFor(bob, "BVN1")
@@ -76,7 +77,7 @@ func TestQueryKeyIndexWithRemoteAuthority(t *testing.T) {
 	sim.CreateAccount(&TokenAccount{
 		Url:         alice.JoinPath("managed-tokens"),
 		AccountAuth: AccountAuth{Authorities: []AuthorityEntry{{Url: bob.JoinPath("book")}}},
-		TokenUrl:    protocol.AcmeUrl(),
+		TokenUrl:    AcmeUrl(),
 	})
 
 	// Query key
@@ -91,4 +92,60 @@ func TestQueryKeyIndexWithRemoteAuthority(t *testing.T) {
 		require.EqualError(t, err, fmt.Sprintf("no authority of %s holds %X", req.Url, req.Key))
 		return nil
 	})
+}
+
+func TestAC1704(t *testing.T) {
+	var timestamp uint64
+
+	// Initialize
+	sim := simulator.New(t, 3)
+	sim.InitFromGenesis()
+
+	// Setup accounts
+	const initialBalance = 100
+	aliceKey := acctesting.GenerateKey("alice")
+	alice := acctesting.AcmeLiteAddressStdPriv(aliceKey)
+	sim.CreateAccount(&LiteIdentity{Url: alice.RootIdentity(), CreditBalance: 1e9})
+	sim.CreateAccount(&LiteTokenAccount{Url: alice, TokenUrl: AcmeUrl(), Balance: *big.NewInt(1e9 * AcmePrecision)})
+
+	// Create a few deposits
+	for i := 0; i < 10; i++ {
+		_, txns := sim.WaitForTransactions(delivered, sim.MustSubmitAndExecuteBlock(
+			acctesting.NewTransaction().
+				WithPrincipal(alice).
+				WithSigner(alice, 1).
+				WithTimestampVar(&timestamp).
+				WithBody(&SendTokens{To: []*TokenRecipient{{
+					Url:    AccountUrl("bob", "tokens"),
+					Amount: *big.NewInt(1),
+				}}}).
+				Initiate(SignatureTypeED25519, aliceKey).
+				Build(),
+		)...)
+		if i != 7 {
+			continue
+		}
+
+		synthTxn := txns[1]
+		require.Equal(t, TransactionTypeSyntheticDepositTokens, synthTxn.Body.Type())
+
+		req := new(api.TxnQuery)
+		req.Txid = synthTxn.GetHash()
+		res, err := sim.SubnetFor(synthTxn.Header.Principal).API.QueryTx(context.Background(), req)
+		require.NoError(t, err)
+
+		var sig KeySignature
+		for _, s := range res.Signatures {
+			if s, ok := s.(KeySignature); ok {
+				sig = s
+				break
+			}
+		}
+		require.NotNil(t, sig)
+
+		sim.MustSubmitAndExecuteBlock(&Envelope{
+			Transaction: []*Transaction{synthTxn},
+			Signatures:  []Signature{sig},
+		})
+	}
 }
