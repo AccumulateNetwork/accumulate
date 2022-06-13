bed3f8c03b9b6077cb9207658e4218393ec0f82b
Sander Postma <spostma@defidevs.io>
2022-06-13 15:21:51 +0000

    feat(api+cli):  Major block api [AC-1217]
    Closes AC-1217. *Major block api implementation.*
    Example CLI command
    ```
    accumulate --use-unencrypted-wallet blocks major bvn-bvn0 1 4
    ```
    
    ## Review Checklist
    
    **If any item is not complete, the merge request is not ready to be reviewed and must be marked `Draft:`.**
    
    - [x] The merge request title is in the format `<change type>(<change scope>): <short description> [<task id>]`
      - For example, `feat(cli): add QR code generation [AC-123]`
      - For details, see [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [x] The description includes `Closes <jira task ID>` (or rarely `Updates <jira task ID>`)
    - [x] The change is fully validated by tests that are run during CI
      - In most cases this means a test in "validate.sh"
      - In some cases, a Go test may be acceptable
      - Validation is not applicable to things like documentation updates
      - Purely UI/UX changes can be manually validated, such as changes to human-readable output
      - For all other changes, automated validation tests are an absolute requirement unless a maintainer specifically explains why they are not in a comment on this merge request
    - [x] The change is marked with one of the validation labels
      - ~"validation::ci/cd" for changes validated by CI tests
      - ~"validation::manual" for changes validated by hand
      - ~"validation::deferred" for changes validated by a follow up merge request
      - ~"validation::not applicable" for changes where validation is not applicable
    
    ## Merge Checklist
    
    - [ ] CI is passing
    - [ ] Merge conflicts are resolved
    - [ ] All discussions are resolved
    
    Related to AC-1217
    
    
    Former-commit-id: a53bf5ad991f643ae9bfb72c2c788e333fc4d220

--- cmd/accumulate/cmd/output.go
+++ cmd/accumulate/cmd/output.go
@@ -187,6 +187,24 @@ func PrintMinorBlockQueryResponseV2(res *api2.MinorQueryResponse) (string, error
 	return str, nil
 }
 
+func PrintMajorBlockQueryResponseV2(res *api2.MajorQueryResponse) (string, error) {
+	if WantJsonOutput {
+		return PrintJson(res)
+	}
+
+	str := fmt.Sprintf("--- major block #%d, major blocktime %v:\n", res.MajorBlockIndex, getBlockTime(res.MajorBlockTime))
+
+	if len(res.MinorBlocks) > 0 {
+		for _, mnrBlk := range res.MinorBlocks {
+			str += fmt.Sprintf("    minor block index: %d\n", mnrBlk.BlockIndex)
+			str += fmt.Sprintf("    minor block time : %s\n", getBlockTime(mnrBlk.BlockTime))
+		}
+	} else {
+		str += fmt.Sprintf("    (empty)")
+	}
+	return str, nil
+}
+
 func getBlockTime(blockTime *time.Time) string {
 	if blockTime != nil {
 		return blockTime.String()
@@ -259,7 +277,7 @@ func PrintMultiResponse(res *api2.MultiResponse) (string, error) {
 		for i := range res.Items {
 			str += fmt.Sprintln("==========================================================================")
 
-			// Convert the item to a transaction query response
+			// Convert the item to a minor query response
 			mtr := new(api2.MinorQueryResponse)
 			err := Remarshal(res.Items[i], mtr)
 			if err != nil {
@@ -267,6 +285,26 @@ func PrintMultiResponse(res *api2.MultiResponse) (string, error) {
 			}
 
 			s, err := PrintMinorBlockQueryResponseV2(mtr)
+			if err != nil {
+				return "", err
+			}
+			str += s
+			str += fmt.Sprintln()
+		}
+		out += str
+	case "majorBlock":
+		str := fmt.Sprintf("\n\tMajor block result Start: %d\t Count: %d\t Total blocks: %d\n", res.Start, res.Count, res.Total)
+		for i := range res.Items {
+			str += fmt.Sprintln("==========================================================================")
+
+			// Convert the item to a major query response
+			mtr := new(api2.MajorQueryResponse)
+			err := Remarshal(res.Items[i], mtr)
+			if err != nil {
+				return "", err
+			}
+
+			s, err := PrintMajorBlockQueryResponseV2(mtr)
 			if err != nil {
 				return "", err
 			}
