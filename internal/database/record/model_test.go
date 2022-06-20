9b8e82b8a3c87345f051801a7337491ff2cae2c3
Ethan Reesor <ethan.reesor@gmail.com>
2022-06-20 17:46:01 +0000

    chore(prot): implement a caching data model [AC-1695]
    Closes AC-1695. Implements a caching data model. Adds a generator to generate a caching data model implementation from an abstract data model described in a YAML file. Re-implements the SMT database manager as a partial caching data model.
    ## Review Checklist
    
    **If any item is not complete, the merge request is not ready to be reviewed and must be marked `Draft:`.**
    
    - [x] The merge request title is in the format `<change type>(<change scope>): <short description> [<task id>]`
      - For example, `feat(cli): add QR code generation [AC-123]`
      - For details, see [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [x] The description includes `Closes <jira task ID>` (or rarely `Updates <jira task ID>`)
    - [x] The change is fully validated by tests that are run during CI
      - In most cases this means a test in "validate.sh"
      - In some cases, a Go test may be acceptable
      - Validation is not applicable to things like documentation updates
      - Purely UI/UX changes can be manually validated, such as changes to human-readable output
      - For all other changes, automated validation tests are an absolute requirement unless a maintainer specifically explains why they are not in a comment on this merge request
    - [x] The change is marked with one of the validation labels
      - ~"validation::ci/cd" for changes validated by CI tests
      - ~"validation::manual" for changes validated by hand
      - ~"validation::deferred" for changes validated by a follow up merge request
      - ~"validation::not applicable" for changes where validation is not applicable
    
    ## Merge Checklist
    
    - [x] CI is passing
    - [x] Merge conflicts are resolved
    - [ ] All discussions are resolved
    
    Related to AC-1695

---
+++ internal/database/record/model_test.go
@@ -0,0 +1,94 @@
+package record_test
+
+import (
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gitlab.com/accumulatenetwork/accumulate/internal/database/record"
+	"gitlab.com/accumulatenetwork/accumulate/internal/encoding"
+	"gitlab.com/accumulatenetwork/accumulate/internal/url"
+	"gitlab.com/accumulatenetwork/accumulate/protocol"
+	"gitlab.com/accumulatenetwork/accumulate/smt/storage/memory"
+)
+
+//go:generate go run ../../../tools/cmd/gen-model --package record_test --out model_gen_test.go model_test.yml
+
+const markPower = 8
+
+func TestRecords(t *testing.T) {
+	store := memory.New(nil).Begin(true)
+	cs := new(ChangeSet)
+	cs.store = record.KvStore{store}
+
+	txn1 := new(protocol.Transaction)
+	txn1.Header.Principal = url.MustParse("foo/1")
+	txn1.Body = new(protocol.CreateIdentity)
+
+	txn2 := new(protocol.Transaction)
+	txn2.Header.Principal = url.MustParse("foo/2")
+	txn2.Body = new(protocol.CreateIdentity)
+
+	// Write stuff
+	require.NoError(t, cs.Entity("foo").Union().Put(&protocol.LiteTokenAccount{Url: url.MustParse("foo")}))
+	require.NoError(t, cs.Entity("bar").Set().Add(txn1.ID()))
+	require.NoError(t, cs.Entity("bar").Set().Add(txn1.ID()))
+	require.NoError(t, cs.Entity("bar").Set().Add(txn2.ID()))
+	require.NoError(t, cs.Entity("baz").Chain().AddHash(txn1.GetHash(), true))
+	require.NoError(t, cs.Entity("baz").Chain().AddHash(txn2.GetHash(), true))
+	require.NoError(t, cs.Commit())
+
+	// Verify
+	cs = new(ChangeSet)
+	cs.store = record.KvStore{store}
+
+	// Verify the URL of the union
+	account, err := cs.Entity("foo").Union().Get()
+	require.NoError(t, err)
+	require.Equal(t, "foo", account.GetUrl().ShortString())
+
+	// Verify the size of the set
+	set, err := cs.Entity("bar").Set().Get()
+	require.NoError(t, err)
+	require.Len(t, set, 2)
+
+	// Verify the state of the chain
+	chain, err := cs.Entity("baz").Chain().Head().Get()
+	require.NoError(t, err)
+	require.Equal(t, 2, int(chain.Count))
+
+	// Verify the changelog was updated
+	cl, err := cs.ChangeLog().GetAll()
+	require.NoError(t, err)
+	require.ElementsMatch(t, []string{"Entity.foo", "Entity.bar", "Entity.baz"}, cl)
+}
+
+func (e *Entity) Commit() error {
+	if !e.IsDirty() {
+		return nil
+	}
+
+	// Log the update
+	err := e.parent.ChangeLog().Put(e.key.String())
+	if err != nil {
+		return err
+	}
+
+	// Do the normal commit thing
+	return e.baseCommit()
+}
+
+type StructType struct{}
+
+func (*StructType) Compare(*StructType) int             { return 0 }
+func (*StructType) CopyAsInterface() interface{}        { return nil }
+func (*StructType) MarshalBinary() ([]byte, error)      { return nil, nil }
+func (*StructType) UnmarshalBinary(data []byte) error   { return nil }
+func (*StructType) UnmarshalBinaryFrom(io.Reader) error { return nil }
+
+type UnionType interface {
+	encoding.BinaryValue
+	Compare(UnionType) int
+}
+
+func UnmarshalUnionType([]byte) (UnionType, error) { return nil, nil }
---
+++ tools/cmd/gen-model/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/spf13/cobra"
+	"gitlab.com/accumulatenetwork/accumulate/tools/internal/typegen"
+	"gopkg.in/yaml.v3"
+)
+
+var flags struct {
+	Package  string
+	Out      string
+	Language string
+	Exclude  []string
+}
+
+func main() {
+	cmd := cobra.Command{
+		Use:  "gen-model [file]",
+		Args: cobra.MinimumNArgs(1),
+		Run:  run,
+	}
+
+	cmd.Flags().StringVarP(&flags.Language, "language", "l", "Go", "Output language or template file")
+	cmd.Flags().StringVar(&flags.Package, "package", "", "Package name")
+	cmd.Flags().StringVarP(&flags.Out, "out", "o", "model_gen.go", "Output file")
+	cmd.Flags().StringSliceVarP(&flags.Exclude, "exclude", "x", nil, "Exclude specific records")
+
+	_ = cmd.Execute()
+}
+
+func fatalf(format string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, "Error: "+format+"\n", args...)
+	os.Exit(1)
+}
+
+func check(err error) {
+	if err != nil {
+		fatalf("%v", err)
+	}
+}
+
+func run(_ *cobra.Command, args []string) {
+	data, err := ioutil.ReadFile(args[0])
+	check(err)
+	var raw interface{}
+	check(yaml.Unmarshal(data, &raw))
+	data, err = json.Marshal(raw)
+	check(err)
+	var records []*typegen.EntityRecord
+	check(json.Unmarshal(data, &records))
+	for _, r := range records {
+		containerize(r.Attributes, r)
+	}
+
+	w := new(bytes.Buffer)
+	check(Templates.Execute(w, flags.Language, struct {
+		Package string
+		Records []*typegen.EntityRecord
+	}{flags.Package, records}))
+	check(typegen.WriteFile(flags.Out, w))
+}
+
+func containerize(r []typegen.Record, c *typegen.EntityRecord) {
+	for _, r := range r {
+		switch r := r.(type) {
+		case *typegen.EntityRecord:
+			r.Parent = c
+			containerize(r.Attributes, r)
+		case *typegen.ChainRecord:
+			r.Parent = c
+		case *typegen.StateRecord:
+			r.Parent = c
+		case *typegen.IndexRecord:
+			r.Parent = c
+		case *typegen.OtherRecord:
+			r.Parent = c
+		default:
+			panic(fmt.Errorf("unknown record type %T", r))
+		}
+	}
+}
