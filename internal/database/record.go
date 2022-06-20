a849b7d72967ea5ae96870c359476adf66e58f9a
Ethan Reesor <ethan.reesor@gmail.com>
2022-06-20 17:46:01 +0000

    chore(prot): implement a caching data model [AC-1695]
    Closes AC-1695. Implements a caching data model. Adds a generator to generate a caching data model implementation from an abstract data model described in a YAML file. Re-implements the SMT database manager as a partial caching data model.
    ## Review Checklist
    
    **If any item is not complete, the merge request is not ready to be reviewed and must be marked `Draft:`.**
    
    - [x] The merge request title is in the format `<change type>(<change scope>): <short description> [<task id>]`
      - For example, `feat(cli): add QR code generation [AC-123]`
      - For details, see [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [x] The description includes `Closes <jira task ID>` (or rarely `Updates <jira task ID>`)
    - [x] The change is fully validated by tests that are run during CI
      - In most cases this means a test in "validate.sh"
      - In some cases, a Go test may be acceptable
      - Validation is not applicable to things like documentation updates
      - Purely UI/UX changes can be manually validated, such as changes to human-readable output
      - For all other changes, automated validation tests are an absolute requirement unless a maintainer specifically explains why they are not in a comment on this merge request
    - [x] The change is marked with one of the validation labels
      - ~"validation::ci/cd" for changes validated by CI tests
      - ~"validation::manual" for changes validated by hand
      - ~"validation::deferred" for changes validated by a follow up merge request
      - ~"validation::not applicable" for changes where validation is not applicable
    
    ## Merge Checklist
    
    - [x] CI is passing
    - [x] Merge conflicts are resolved
    - [ ] All discussions are resolved
    
    Related to AC-1695

---
+++ internal/database/record.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"gitlab.com/accumulatenetwork/accumulate/internal/database/record"
+	"gitlab.com/accumulatenetwork/accumulate/internal/encoding"
+	"gitlab.com/accumulatenetwork/accumulate/internal/errors"
+)
+
+type RecordStore struct {
+	Batch *Batch
+}
+
+func (m RecordStore) PutValue(key record.Key, value record.ValueReader) error {
+	v, err := value.GetValue()
+	if err != nil {
+		return errors.Wrap(errors.StatusUnknown, err)
+	}
+	m.Batch.putValue(key.Hash(), v)
+	return nil
+}
+
+func (m *RecordStore) GetValue(key record.Key, value record.ValueWriter) error {
+	var didUnmarshal bool
+	v, err := m.Batch.getValue(key.Hash(), func(data []byte) (TypedValue, error) {
+		didUnmarshal = true
+		err := value.LoadBytes(data)
+		if err != nil {
+			return nil, errors.Wrap(errors.StatusUnknown, err)
+		}
+		return value.GetValue()
+	})
+	if didUnmarshal || err != nil {
+		return errors.Wrap(errors.StatusUnknown, err)
+	}
+
+	return value.LoadValue(recordValueReader{v}, false)
+}
+
+type recordValueReader struct {
+	value TypedValue
+}
+
+func (r recordValueReader) GetValue() (encoding.BinaryValue, error) {
+	return r.value.(encoding.BinaryValue), nil
+}
