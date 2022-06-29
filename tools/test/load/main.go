256e8598b727f2568c2e84c20dad2ff2368042d2
Pavel Fomenko <fomenkop145@gmail.com>
2022-06-29 22:49:54 +0000

    test(prot): BVN/DN performance study and analysis (2 of 2) [AC-1518]
    Closes AC-1518. Made changes to load testing tool so that it can:
    - choose server as flag
    - added multiple clients
    - fixed client closing issue
    - added parallelization 
    
    ## Review Checklist
    
    **If any item is not complete, the merge request is not ready to be reviewed and must be marked `Draft:`.**
    
    - [x] The merge request title is in the format `<change type>(<change scope>): <short description> [<task id>]`
      - For example, `feat(cli): add QR code generation [AC-123]`
      - For details, see [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [x] The description includes `Closes <jira task ID>` (or rarely `Updates <jira task ID>`)
    - [x] The change is fully validated by tests that are run during CI
      - In most cases this means a test in "validate.sh"
      - In some cases, a Go test may be acceptable
      - Validation is not applicable to things like documentation updates
      - Purely UI/UX changes can be manually validated, such as changes to human-readable output
      - For all other changes, automated validation tests are an absolute requirement unless a maintainer specifically explains why they are not in a comment on this merge request
    - [ ] The change is marked with one of the validation labels
      - ~"validation::ci/cd" for changes validated by CI tests
      - ~"validation::manual" for changes validated by hand
      - ~"validation::deferred" for changes validated by a follow up merge request
      - ~"validation::not applicable" for changes where validation is not applicable
    
    ## Merge Checklist
    
    - [ ] CI is passing
    - [ ] Merge conflicts are resolved
    - [ ] All discussions are resolved
    
    Related to AC-1518

--- tools/test/load/main.go
+++ tools/test/load/main.go
@@ -1,223 +1,206 @@
 package main
 
 import (
-	"bufio"
 	"context"
 	"crypto/ed25519"
-	"errors"
+	"flag"
 	"fmt"
-	"io"
 	"log"
 	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
-	"syscall"
+	"sync"
 	"time"
 
-	"github.com/fatih/color"
-	"github.com/spf13/cobra"
 	"gitlab.com/accumulatenetwork/accumulate/internal/api/v2"
 	"gitlab.com/accumulatenetwork/accumulate/internal/client"
+	"gitlab.com/accumulatenetwork/accumulate/internal/logging"
 	"gitlab.com/accumulatenetwork/accumulate/internal/url"
 	"gitlab.com/accumulatenetwork/accumulate/protocol"
 )
 
-var extraFlags []string
+var serverUrl string
+var transactions int
+var duration int
+var maxGoroutines = 25
+
+// Start logging with dataset log
+var dsl = logging.DataSetLog{}
+var start time.Time
 
 func main() {
-	cmd.PersistentFlags().StringSliceVarP(&extraFlags, "flags", "X", nil, "Extra flags for init")
-	_ = cmd.Execute()
-}
+	flag.Parse()
 
-var cmd = &cobra.Command{
-	Use: "devnet",
-	Run: func(*cobra.Command, []string) {
-		_, err := initClient("http://localhost:26660/v2")
-		if err != nil {
-			os.Exit(1)
-		}
-	},
-}
+	//desired TPS is "transactions"
+	transactionsPerClient := maxGoroutines
+	numClientsPerBurst := transactions / transactionsPerClient
 
-var resetColor = color.New(color.Reset)
+	maxNumClients := numClientsPerBurst * duration
 
-func assertInModuleRoot() {
-	cwd, err := os.Getwd()
-	checkf(err, "getwd")
+	totalTransactions := maxNumClients * transactionsPerClient
+	// run clients in parallel
+	wg := &sync.WaitGroup{}
 
-	_, err = os.Stat(filepath.Join(cwd, "go.mod"))
-	checkf(err, "stat go.mod failed - are we in the repo root?")
-}
-
-func build(tool string) {
-	buildCmd := exec.Command("go", "build", tool)
-	buildCmd.Stdout = os.Stdout
-	buildCmd.Stderr = os.Stdout
-	err := buildCmd.Run()
-	checkf(err, "build %s", tool)
-}
-
-func launch() *exec.Cmd {
-	// Initialize the devnet command
-	args := append([]string{"init", "devnet", "--work-dir", ".nodes"}, extraFlags...)
-	initCmd := exec.Command("./accumulated", args...)
-	initCmd.Stdout = os.Stdout
-	initCmd.Stderr = os.Stdout
-	err := initCmd.Run()
-	checkf(err, "init devnet")
-
-	// Configure the devnet command
-	runCmd := exec.Command("./accumulated", "run", "devnet", "--work-dir", ".nodes")
-	runCmd.Env = os.Environ()
-	runCmd.Env = append(runCmd.Env, "FORCE_COLOR=true")
-
-	// Don't interrupt the run process if the parent process is interrupted
-	runCmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-
-	// Forward output
-	runRd, runWr := io.Pipe()
-	runCmd.Stdout = runWr
-	runCmd.Stderr = runWr
-	started := make(chan struct{})
-
-	go func() {
-		bufRd := bufio.NewReader(runRd)
-		for {
-			line, err := bufRd.ReadString('\n')
-			if errors.Is(err, io.EOF) {
-				return
-			}
-			checkf(err, "read devnet stdout/stderr")
-
-			if strings.Contains(line, "----- Started -----") {
-				close(started)
-			}
-
-			print(line + resetColor.Sprint(""))
-		}
-	}()
-
-	// Start the devnet
-	err = runCmd.Start()
-	checkf(err, "start devnet")
-	<-started
-
-	return runCmd
-}
-
-func stop(runCmd *exec.Cmd) {
-	err := runCmd.Process.Signal(os.Interrupt)
+	clients, err := initializeClients(maxNumClients)
 	if err != nil {
-		fmt.Printf("Error: interrupt devnet: %v\n", err)
+		log.Fatalf("%v, failed to initialize client %d", err, maxNumClients)
 	}
 
-	go func() {
-		time.Sleep(time.Minute)
-		_ = runCmd.Process.Kill()
-	}()
+	// Start the global clock
+	start = time.Now()
 
-	err = runCmd.Wait()
+	for simTime := 0; simTime < duration; simTime++ {
+		tick := time.Now()
+		for ii := 0; ii < numClientsPerBurst; ii++ {
+			//	guard <- struct{}{} // would block if guard channel is already filled
+			wg.Add(1)
+			go func(simTime float64, v int) {
+				defer wg.Done()
+				err := initTxs(simTime, transactionsPerClient, clients[v])
+				if err != nil {
+					fmt.Printf("Error: %v\n", err)
+				}
+			}(float64(simTime), simTime*numClientsPerBurst+ii)
+		}
+		time.Sleep(time.Second - time.Since(tick))
+	}
+
+	// force close channel
+	wg.Wait()
+	txCount := 0
+	for j := 0; j < maxNumClients; j++ {
+		txCount += clients[j].TxCount
+	}
+	txPassed := txCount
+	txFailed := totalTransactions - txPassed
+
+	header := fmt.Sprintf("## Runtime %d seconds with transaction load per second: %d\n", duration, transactions)
+	header += fmt.Sprintf("## Total Tx : %d, Tx Passed : %d, Tx Failed : %d\n", txCount, txPassed, txFailed)
+	dsl.SetHeader(header)
+
+	_, err = dsl.DumpDataSetToDiskFile()
 	if err != nil {
-		fmt.Printf("Error: wait for devnet: %v\n", err)
+		log.Fatalf("cannot dump data set to disk, %v", err)
 	}
 }
 
-// Init new client from server URL input using client.go
-func initClient(server string) (string, error) {
+func init() {
+	flag.StringVar(&serverUrl, "s", "http://127.0.1.1:26660/v2", "Accumulate server URL")
+	flag.IntVar(&transactions, "t", 100, "Number of transactions per second")
+	flag.IntVar(&maxGoroutines, "r", 25, "Number of transactions per client (i.e. go routines)")
+	flag.IntVar(&duration, "d", 5, "Throttle go routines per client")
+}
 
-	// build Accumulate deamon
-	assertInModuleRoot()
-	build("./cmd/accumulated")
+// Init account creation and transaction sending
+func initTxs(simTime float64, transactionsPerClient int, c *Client) error {
+	var m sync.Mutex
+	deltas := make([]float64, transactions)
+	times := make([]float64, transactions)
 
-	// Launch the devnet
-	runCmd := launch()
-	defer func() { _ = runCmd.Process.Kill() }()
-
-	// Create new client on localhost
-	client, err := client.New("http://127.0.1.1:26660/v2")
-	checkf(err, "creating client")
-	client.DebugRequest = true
-
-	// Limit amount of goroutines
-	maxGoroutines := 1
-	guard := make(chan struct{}, maxGoroutines)
-
+	txWaitGroup := sync.WaitGroup{}
 	// run key generation in cycle
-	for i := 0; i < 2; i++ {
-		guard <- struct{}{} // would block if guard channel is already filled
+	for i := 0; i < transactionsPerClient; i++ {
+		// create accounts and store them
+		acc, _ := createAccount()
 
-		// Add timer to measure TPS
-		timer := time.NewTimer(time.Microsecond)
+		txWaitGroup.Add(1)
 
 		// generate accounts and faucet in goroutines
 		go func(n int) {
-			// create accounts and store them
-			acc, _ := createAccount(n)
-
+			defer txWaitGroup.Done()
 			// start timer
-			timer.Reset(time.Microsecond)
+			t := time.Now()
 
 			// faucet account and wait for Tx execution
-			resp, err := client.Faucet(context.Background(), &protocol.AcmeFaucet{Url: acc})
+			resp, err := c.Client.Faucet(context.Background(), &protocol.AcmeFaucet{Url: acc})
 			if err != nil {
-				fmt.Printf("Error: faucet: %v\n", err)
+				log.Printf("Error: fauceting account with error: %v, on client %d, tx %d\n", err, c.Id, c.TxCount+1)
+				return
 			}
-
 			txReq := api.TxnQuery{}
 			txReq.Txid = resp.TransactionHash
 			txReq.Wait = time.Second * 100
 			txReq.IgnorePending = false
 
-			_, err = client.QueryTx(context.Background(), &txReq)
+			_, err = c.Client.QueryTx(context.Background(), &txReq)
 			if err != nil {
+				log.Printf("Error: waiting for transaction to complete account with error: %v, on client %d, tx %d\n", err, c.Id, c.TxCount+1)
 				return
 			}
 
-			// wait for timer to fire
-			log.Printf("Execution time %s\n", time.Since(<-timer.C))
-
-			// time to release goroutine
-			<-guard
+			m.Lock()
+			deltas[n] = time.Since(t).Seconds()
+			c.TxCount++
+			m.Unlock()
 		}(i)
-		// stop timer
-		<-timer.C
+
+		//capture the timestamp of when the transaction started
+		times[i] = time.Since(start).Seconds()
 	}
 
-	// wait for goroutines to finish
-	for i := 0; i < maxGoroutines; i++ {
-		guard <- struct{}{}
+	txWaitGroup.Wait()
+
+	c.Client.CloseIdleConnections()
+
+	for i := 0; i < transactionsPerClient; i++ {
+		c.DataSet.Lock()
+		c.DataSet.Save("index", (transactionsPerClient*c.Id)+i, 10, true)
+		c.DataSet.Save("simTime", simTime, 10, false)
+		c.DataSet.Save("clientId", c.Id, 10, false)
+		c.DataSet.Save("settlementTime", deltas[i], 10, false)
+		c.DataSet.Unlock()
 	}
 
-	stop(runCmd)
+	//Stop time Tx executions
+	log.Printf("The Txs execution took %v to run.\n", time.Since(start).Seconds())
 
-	return "", nil
+	return nil
+}
+
+//Client structure for client info
+type Client struct {
+	DataSet *logging.DataSet
+	Client  *client.Client
+	Id      int
+	TxCount int
+}
+
+// initializeClients the client
+func initializeClients(c int) ([]*Client, error) {
+	path := "load_tester"
+	err := os.MkdirAll(path, 0755)
+	if err != nil {
+		return nil, fmt.Errorf("error creating directory %v", err)
+	}
+	dsl.SetPath(path)
+	dsl.SetProcessName("load")
+	dsName := fmt.Sprintf("settlement")
+	dsl.Initialize(dsName, logging.DefaultOptions())
+	ds := dsl.GetDataSet(dsName)
+
+	clients := make([]*Client, c)
+
+	//initialize the datasets and clients
+	for i := 0; i < c; i++ {
+		cl, err := client.New(serverUrl)
+		if err != nil {
+			return nil, err
+		}
+		cl.DebugRequest = false
+		clients[i] = &Client{ds, cl, i, 0}
+	}
+
+	return clients, nil
 }
 
 // helper function to generate key and create account and return the address
-func createAccount(i int) (*url.URL, error) {
+func createAccount() (*url.URL, error) {
 	pub, _, err := ed25519.GenerateKey(nil)
 	if err != nil {
-		fmt.Printf("Error: generating keys: %v\n", err)
+		return nil, fmt.Errorf("Error: generating keys: %v\n", err)
 	}
 
 	acc, err := protocol.LiteTokenAddress(pub, protocol.ACME, protocol.SignatureTypeED25519)
 	if err != nil {
-		fmt.Printf("Error: creating Lite Token account: %v\n", err)
+		return nil, fmt.Errorf("Error: creating Lite Token account: %v\n", err)
 	}
-
-	fmt.Printf("Account %d: %s\n", i, acc)
-
 	return acc, nil
-}
-
-func fatalf(format string, args ...interface{}) {
-	fmt.Fprintf(os.Stderr, "Error: "+format+"\n", args...)
-	os.Exit(1)
-}
-
-func checkf(err error, format string, otherArgs ...interface{}) {
-	if err != nil {
-		fatalf(format+": %v", append(otherArgs, err)...)
-	}
 }
