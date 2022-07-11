c6ca467b6bf61ec9efee0bf9d9fb1bde5a61b9e6
jjhbk <jp@ph.iitr.ac.in>
2022-07-11 17:34:48 +0530

    add test

---
+++ internal/database/account_state_test.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gitlab.com/accumulatenetwork/accumulate/internal/url"
+	"gitlab.com/accumulatenetwork/accumulate/protocol"
+)
+
+func TestAccountState(t *testing.T) {
+	db := OpenInMemory(nil)
+	batch := db.Begin(true)
+	defer batch.Discard()
+	adiurl := url.MustParse("acc://testadi1.acme")
+	bookurl := url.MustParse("acc://testadi1.acme/testbook1")
+	testurl := url.MustParse("acc://testurl")
+	a := batch.Account(adiurl)
+	acc := new(protocol.ADI)
+	acc = &protocol.ADI{Url: adiurl, AccountAuth: protocol.AccountAuth{Authorities: []protocol.AuthorityEntry{protocol.AuthorityEntry{Url: bookurl}}}}
+	err := a.PutState(acc)
+	require.NoError(t, err)
+	h1, err := a.hashState()
+	require.NoError(t, err)
+	a.Directory().Add(testurl)
+	h2, err := a.hashState()
+	require.NoError(t, err)
+	require.NotEqual(t, h1.MerkleHash(), h2.MerkleHash())
+}
