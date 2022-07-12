91c74b883991fc90c3a00a5199ec44fe7a29d2aa
Dennis B <dennis@factoid.org>
2022-07-11 20:23:35 -0500

    fix more tests

--- cmd/accumulated/cmd_init_test.go
+++ cmd/accumulated/cmd_init_test.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/require"
 	proxy_testing "gitlab.com/accumulatenetwork/accumulate/pkg/proxy/testing"
+	"strings"
 	"testing"
 )
 
@@ -12,13 +15,17 @@ func TestInitSeeds(t *testing.T) {
 	_ = proxyClient
 	_ = accClient
 	var args []string
-	args = append(args, "0")
-	args = append(args, "--work-dir")
-	args = append(args, t.TempDir())
-	args = append(args, "--seed-proxy")
-	args = append(args, proxy_testing.Endpoint)
+	workDir := t.TempDir()
+	commandLine := fmt.Sprintf("accumulated init node 0 --listen tcp://0.0.0.0:%d --work-dir %s --seed %s", proxy_testing.Partitions[1].BasePort, workDir, proxy_testing.Endpoint)
+	args = strings.Split(commandLine, " ")
+
+	e := bytes.NewBufferString("")
+	b := bytes.NewBufferString("")
 	cmd := new(cobra.Command)
-	cmd.AddCommand(cmdInit)
+	cmd.SetErr(e)
+	cmd.SetOut(b)
+
+	cmd.AddCommand(cmdMain)
 	cmd.SetArgs(args)
 	require.NoError(t, cmd.Execute())
 }
