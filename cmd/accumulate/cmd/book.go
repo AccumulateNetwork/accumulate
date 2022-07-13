e31a2d253209f49587acf5dcfde2d2f4e1d9ee54
JATHIN JAGANNATH <jp@ph.iitr.ac.in>
2022-07-13 18:05:28 +0000

    feat(cli): remove key page height and index parameters [AC-2057]
    Closes AC-2057. the cli commands containing key height and key index optional parameters are modified
    
    ## Review Checklist
    
    **If any item is not complete, the merge request is not ready to be reviewed and must be marked `Draft:`.**
    
    - [x] The merge request title is in the format `<change type>(<change scope>): <short description> [<task id>]`
      - For example, `feat(cli): add QR code generation [AC-123]`
      - For details, see [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [x] The description includes `Closes <jira task ID>` (or rarely `Updates <jira task ID>`)
    - [x] The change is fully validated by tests that are run during CI
      - In most cases this means a test in "validate.sh"
      - In some cases, a Go test may be acceptable
      - Validation is not applicable to things like documentation updates
      - Purely UI/UX changes can be manually validated, such as changes to human-readable output
      - For all other changes, automated validation tests are an absolute requirement unless a maintainer specifically explains why they are not in a comment on this merge request
    - [x] The change is marked with one of the validation labels
      - ~"validation::ci/cd" for changes validated by CI tests
      - ~"validation::manual" for changes validated by hand
      - ~"validation::deferred" for changes validated by a follow up merge request
      - ~"validation::not applicable" for changes where validation is not applicable
    
    ## Merge Checklist
    
    - [x] CI is passing
    - [x] Merge conflicts are resolved
    - [ ] All discussions are resolved
    
    Related to AC-2057

--- cmd/accumulate/cmd/book.go
+++ cmd/accumulate/cmd/book.go
@@ -16,6 +16,7 @@ func init() {
 
 	// Add auth cmd for backwards compatability
 	bookCmd.AddCommand(authCmd)
+
 }
 
 // bookCmd are the commands associated with managing key books
@@ -32,7 +33,7 @@ var bookGetCmd = &cobra.Command{
 }
 
 var bookCreateCmd = &cobra.Command{
-	Use:   "create [origin adi url] [signing key name] [key index (optional)] [key height (optional)] [new key book url] [public key 1 (optional)] ... [public key hex or name n + 1",
+	Use:   "create [origin adi url] [key name[@key book or page]] [new key book url] [public key 1 (optional)] ... [public key hex or name n + 1",
 	Short: "Create new key book and page. When public key 1 is specified it will be assigned to the page, otherwise the origin key is used.",
 	Args:  cobra.MinimumNArgs(3),
 	Run:   runCmdFunc(CreateKeyBook),
