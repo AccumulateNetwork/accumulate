ce9fc01d35f6b00026aaf8419d6d0ab15f44af50
Ethan Reesor <ethan.reesor@gmail.com>
2022-07-21 13:33:11 -0500

    improve parameterized attribute perf

--- internal/database/record/helpers.go
+++ internal/database/record/helpers.go
@@ -2,6 +2,7 @@ package record
 
 import (
 	"bytes"
+	"crypto/sha256"
 	"fmt"
 
 	"gitlab.com/accumulatenetwork/accumulate/internal/encoding"
@@ -18,6 +19,18 @@ func CompareTxid(u, v *url.TxID) int { return u.Compare(v) }
 func CompareUrl(u, v *url.URL) int   { return u.Compare(v) }
 
 func ParseString(s string) (string, error) { return s, nil }
+
+func MapKeyBytes(v []byte) [32]byte {
+	if len(v) == 32 {
+		// Most (all?) of our byte-keys are actually [32]byte so this should help
+		return *(*[32]byte)(v)
+	}
+	return sha256.Sum256(v)
+}
+
+func MapKeyUrl(v *url.URL) [32]byte {
+	return v.AccountID32()
+}
 
 type ValueMarshaller[T any] func(value T) ([]byte, error)
 type ValueUnmarshaller[T any] func(data []byte) (T, error)
