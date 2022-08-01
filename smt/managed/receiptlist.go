5ea3e7f4125ed1375ace76bf8d322612bbb0956a
Paul Snow <snow.paul@gmail.com>
2022-08-01 00:47:44 -0500

    Feat:(SMT) Added proofs for lists of entries in a Merkle Tree

--- smt/managed/receiptlist.go
+++ smt/managed/receiptlist.go
@@ -3,251 +3,118 @@ package managed
 import (
 	"bytes"
 	"fmt"
-	"math"
 )
 
-// String
-// Convert the receipt to a string
-func (r *Receipt) String() string {
-	var b bytes.Buffer
-	b.WriteString(fmt.Sprintf("\nStart      %x\n", r.Start))    // Start of proof
-	b.WriteString(fmt.Sprintf("StartIndex %d\n", r.StartIndex)) // Start of proof
-	b.WriteString(fmt.Sprintf("End        %x\n", r.End))        // End point in the Merkle Tree
-	b.WriteString(fmt.Sprintf("EndIndex   %d\n", r.EndIndex))   // End point in the Merkle Tree
-	b.WriteString(fmt.Sprintf("Anchor     %x\n", r.Anchor))     // Anchor result of evaluating the receipt path
-	working := r.Start                                          // Calculate the receipt path; for debugging print the
-	for i, v := range r.Entries {                               // intermediate hashes
-		r := "L"
-		if v.Right {
-			r = "R"
-			working = Sha256(append(working[:], v.Hash[:]...))
-		} else {
-			working = Sha256(append(v.Hash[:], working[:]...))
-		}
-		b.WriteString(fmt.Sprintf(" %10d Apply %s %x working: %x \n", i, r, v.Hash, working))
-	}
-	return b.String()
-}
-
-func (n *ReceiptEntry) Apply(hash Hash) Hash {
-	if n.Right {
-		// If this hash comes from the right, apply it that way
-		return hash.Combine(Sha256, n.Hash)
-	}
-	// If this hash comes from the left, apply it that way
-	return Hash(n.Hash).Combine(Sha256, hash)
+// ReceiptList
+// A ReceiptList is a generic proof that can be used to prove each element
+// in the elements list are part of the merkle tree, and exist in the order
+// given.  The last element of ReceiptList.Elements is provably part of the
+// Overall Merkle Tree, as proven by the Receipt.
+//
+// If anchored into another Second Merkle Tree, this proof can be extended
+// by combining receipt built off the Second Merkle Tree with the receipt
+// given in the ReceiptList
+type ReceiptList struct {
+	Element     []byte       // Element we wish to prove to be in the list
+	MerkleState *MerkleState // Starting Merkle State
+	Elements    [][]byte     // Element hashes.  Last entry is the anchor
+	Receipt     *Receipt     // Receipt for the last hash in Elements (the anchor)
 }
 
 // Validate
 // Take a receipt and validate that the element hash progresses to the
 // Merkle Dag Root hash (MDRoot) in the receipt
-func (r *Receipt) Validate() bool {
-	MDRoot := r.Start // To begin with, we start with the object as the MDRoot
-	// Now apply all the path hashes to the MDRoot
-	for _, node := range r.Entries {
-		MDRoot = node.Apply(MDRoot)
-	}
-	// In the end, MDRoot should be the same hash the receipt expects.
-	return Hash(MDRoot).Equal(r.Anchor)
-}
-
-// Contains returns true if the 2nd receipt is equal to or contained within the
-// first.
-func (r *Receipt) Contains(other *Receipt) bool {
-	hashSelf, hashOther := r.Start, other.Start
-	var posSelf int
-	for !bytes.Equal(hashSelf, hashOther) {
-		if posSelf >= len(r.Entries) {
-			return false
-		}
-		hashSelf = r.Entries[posSelf].Apply(hashSelf)
-		posSelf++
+func (r *ReceiptList) Validate() bool {
+	// Make sure the ReceiptList isn't empty.  Avoids pointer and nil exceptions
+	if r.MerkleState == nil ||
+		r.Receipt == nil ||
+		r.Elements == nil || len(r.Elements) == 0 {
+		return false
 	}
 
-	for _, entry := range other.Entries {
-		if posSelf >= len(r.Entries) {
-			return false
-		}
-
-		hashSelf = r.Entries[posSelf].Apply(hashSelf)
-		hashOther = entry.Apply(hashOther)
-		posSelf++
-		if !bytes.Equal(hashSelf, hashOther) {
-			return false
-		}
+	MS := r.MerkleState.Copy()     //         Get the Merkle State, and walk it
+	for _, h := range r.Elements { //           to the end of the list.
+		if h == nil || len(h) != 32 { //      Make sure every element in Elements
+			return false //                     is a proper hash and not nil
+		} //
+		MS.AddToMerkleTree(h) //              Add each element to the MS
+	} //                                      Once all elements are added, compute
+	anchor := MS.GetMDRoot()               //   the anchor at this point.
+	if anchor == nil || len(anchor) == 0 { // If an anchor can't be produced, this
+		return false //                         receipt fails.
 	}
 
+	if !bytes.Equal(r.Elements[0],r.Receipt.Start) {
+		return false
+	}
+
+	if !r.Receipt.Validate() { //                 The Receipt must be valid
+		return false
+	}
+
+	// The receipt doesn't match the ReceiptList.
 	return true
 }
 
-// Combine
-// Take a 2nd receipt, attach it to a root receipt, and return the resulting
-// receipt.  The idea is that if this receipt is anchored into another chain,
-// Then we can create a receipt that proves the element in this receipt all
-// the way down to an anchor in the root receipt.
-// Note that both this receipt and the root receipt are expected to be good.
-func (r *Receipt) Combine(rm *Receipt) (*Receipt, error) {
-	if !bytes.Equal(r.Anchor, rm.Start) {
-		return nil, fmt.Errorf("receipts cannot be combined. "+
-			"anchor %x doesn't match root merkle tree %x", r.End, rm.Start)
-	}
-	nr := r.Copy()                 // Make a copy of the first Receipt
-	nr.Anchor = rm.Anchor          // The MDRoot will be the one from the appended receipt
-	for _, n := range rm.Entries { // Make a copy and append the Nodes of the appended receipt
-		nr.Entries = append(nr.Entries, n.Copy())
-	}
-	return nr, nil
-}
-
-// CombineReceipts combines multiple receipts.
-func CombineReceipts(receipts ...*Receipt) (*Receipt, error) {
-	r := receipts[0]
-	var err error
-	for _, s := range receipts[1:] {
-		r, err = r.Combine(s)
-		if err != nil {
-			return nil, fmt.Errorf("failed to combine receipts: %v", err)
-		}
-	}
-
-	return r, nil
-}
-
-func NewReceipt(manager *MerkleManager) *Receipt {
-	r := new(Receipt)
-	r.manager = manager
+// NewReceiptList
+// Return a new ReceiptList with at least a MerkleState initialized
+func NewReceiptList() *ReceiptList {
+	r := new(ReceiptList)
+	r.MerkleState = new(MerkleState)
 	return r
 }
 
 // GetReceiptList
-// This receipt can prove multiple elements.  
-// The proof constructed looks like:
-//    1) A beginning Merkle State
-//    2) A set of hashes, one of which must be the element
-//    3) The receipt constructed from the ending Merkle State
-func GetReceiptList(manager *MerkleManager, element, start, anchor Hash) (r *Receipt, err error) {
-	// Allocate r, the receipt we are building and record our element
-	r = new(Receipt)  // Allocate a r
-	r.Start = element // Add the element to the r
-	r.End = anchor    // Add the anchor hash to the r
-	r.manager = manager
-	if r.StartIndex, err = r.manager.GetElementIndex(element); err != nil {
-		return nil, err
-	}
-	if r.EndIndex, err = r.manager.GetElementIndex(anchor); err != nil {
-		return nil, err
+// Given a merkle tree with a start point and an end point, create a ReceiptList for
+// all the elements from the start hash to the end hash, inclusive.
+func GetReceiptList(manager *MerkleManager, element []byte, Start int64, End int64) (r *ReceiptList, err error) {
+	if Start > End { // Start must be before the end
+		return nil, fmt.Errorf("start %d and end %d is invalid for ReceiptList", Start, End)
 	}
 
-	head, err := manager.Head().Get()
+	// Make sure the end is in the range of the Merkle Tree
+	ms, err := manager.Head().Get()
+	if ms.Count <= End {
+		return nil, fmt.Errorf("end %d is out of range for SMT length %d", End, ms.Count)
+	}
+
+	// Make sure the element is the right length for a hash
+	if element == nil || len(element) != 32 {
+		lenElement := len(element)
+		truncate := ""
+		if lenElement > 32 {
+			element = element[:32]
+			truncate = fmt.Sprintf("... Len(element)=%d", lenElement)
+		}
+		return nil, fmt.Errorf("The element provided is not a valid hash %x%s", element, truncate)
+	}
+
+	// Allocate the ReceiptList, add all the elements to the ReceiptList
+	r = NewReceiptList()
+	r.Element = element
+	found := false
+	for i := Start; i <= End; i++ { // Get all the elements for the list
+		h, err := manager.Get(i)
+		if err != nil {
+			return nil, err
+		}
+		if bytes.Equal(element, h) {
+			found = true
+		}
+		r.Elements = append(r.Elements, h)
+	}
+	if !found {
+		return nil, fmt.Errorf("%x is not found in the elements of the ReceiptList", element)
+	}
+
+	r.MerkleState, err = manager.GetAnyState(Start - 1)
+	if err != nil {
+		return nil, err
+	}
+	r.Receipt, err = GetReceipt(manager, r.Elements[0], r.Elements[len(r.Elements)-1])
 	if err != nil {
 		return nil, err
 	}
 
-	if r.StartIndex > r.EndIndex ||
-		r.StartIndex < 0 ||
-		r.StartIndex > head.Count { // The element must be at the anchorIndex or before
-		return nil, fmt.Errorf("invalid indexes for the element %d and anchor %d", r.StartIndex, r.EndIndex)
-	}
-
-	if r.StartIndex == 0 && r.EndIndex == 0 { // If this is the first element in the Merkle Tree, we are already done.
-		r.Anchor = element // A Merkle Tree of one element has a root of the element itself.
-		return r, nil      // And we are done!
-	}
-
-	if err := r.BuildReceipt(); err != nil {
-		return nil, err
-	}
 	return r, nil
-}
-
-// BuildReceipt
-// takes the values collected by GetReceipt and flushes out the data structures
-// in the Receipt to represent a fully populated version.
-func (r *Receipt) BuildReceipt() error {
-	state, _ := r.manager.GetAnyState(r.EndIndex) // Get the state at the Anchor Index
-	state.Trim()                                  // If Pending has any trailing nils, remove them.
-	return r.BuildReceiptWith(r.manager.GetIntermediate, Sha256, state)
-}
-
-type GetIntermediateFunc func(element, height int64) (l, r Hash, err error)
-
-func (r *Receipt) BuildReceiptWith(getIntermediate GetIntermediateFunc, hashFunc HashFunc, anchorState *MerkleState) error {
-	height := int64(1) // Start the height at 1, because the element isn't part
-	r.Anchor = r.Start // of the nodes collected.  To begin with, the element is the Merkle Dag Root
-	stay := true       // stay represents the fact that the proof is already in this column
-
-	// The path from a intermediateHash added to the merkle tree to the anchor
-	// starts at the element index and goes to the anchor index.
-	// Some indexes have multiple hashes as hashes cascade.  Other
-	// indexes are skipped, as they summarized by values in sub
-	// merkle trees on the way to the anchor index.
-	//
-	// This for loop adds the hashes leading up to the highest
-	// sub Merkle Tree between the element index and the anchor index
-	for idx := r.StartIndex; idx <= r.EndIndex; { // Range over all the elements
-		if idx&1 == 0 { // Handle the even cases. Merkle Trees add elements at 0, and combine elements at odd numbers
-			idx++        // No point in handling the first intermediateHash, so move to the next column
-			stay = false // The proof is lagging in the previous column.
-		} else { // The Odd cases hold summary hashes
-			lHash, rHash, err := getIntermediate(idx, height) // Get the previous hight left/right hashes
-			if err != nil {                                   // Error means end of the column has been reached
-				next := int64(math.Pow(2, float64(height-1))) //            Move to the next column 2^(height-1) columns
-				idx += next                                   //
-				stay = false                                  //            Changing columns
-				continue
-			}
-			r.Anchor = lHash.Combine(hashFunc, rHash) // We don't have to calculate the MDRoot, but it
-			if stay {                                 //   helps debugging.  Check if still in column
-				r.Entries = append(r.Entries, &ReceiptEntry{Hash: lHash, Right: false}) // If so, combine from left
-			} else { //                                                     Otherwise
-				r.Entries = append(r.Entries, &ReceiptEntry{Hash: rHash, Right: true}) //  combine from right
-			}
-			stay = true // By default assume a stay in the column
-			height++    // and increment the height.
-		}
-	}
-
-	// At this point, we have reached the highest sub Merkle Tree root.
-	// All we have to do is calculate the set of intermediate hashes that
-	// will be combined with the current state of the receipt to match
-	// the Merkle Dag Root at the Anchor Index
-
-	if r.EndIndex == 0 { // Special case the first entry in a Merkle Tree
-		r.Anchor = r.Start // whose Merkle Dag Root is just the first element
-		return nil         // added to the merkle tree
-	}
-
-	stay = false // Indicate no elements for the first index have been added
-	state := anchorState
-
-	var intermediateHash, lastIH Hash // The intermediateHash tracks the combining of hashes as we go. The
-	for i, v := range state.Pending { // last hash computed is the last intermediate Hash used in an anchor
-		if v == nil { //                  Skip in Pending until a value is found
-			continue
-		}
-		if intermediateHash == nil { //   If no computations have been started,
-			intermediateHash = v.Copy() //   just move the value from pending over
-			if height-1 == int64(i) {   // If height is just above entry in pending
-				stay = true //                consider processing to continue
-			}
-			continue
-		}
-		lastIH = intermediateHash.Copy()                         // compute a new intermediate hash
-		intermediateHash = v.Combine(hashFunc, intermediateHash) // Combine Pending with intermediate
-		if int64(i) < height-1 {                                 // If not to the proof height, skip
-			continue //                                                                 adding to the receipt
-		}
-		if stay { //                                                     If in the same column
-			if int64(i) >= height { //                                    And the proof is at this hight or higher
-				r.Entries = append(r.Entries, &ReceiptEntry{Hash: v, Right: false}) // Add to the receipt
-			}
-			continue
-		}
-		r.Entries = append(r.Entries, &ReceiptEntry{Hash: lastIH, Right: true}) // First time in this column, so add to receipt
-		stay = true                                                             // Indicate processing the same column now.
-
-	}
-	r.Anchor = intermediateHash // The Merkle Dag Root is the last intermediate Hash produced.
-
-	return nil
 }
--- smt/managed/receiptlist_test.go
+++ smt/managed/receiptlist_test.go
@@ -1,291 +1,67 @@
 package managed
 
 import (
-	"bytes"
-	"crypto/sha256"
-	"fmt"
-	"os"
-	"path/filepath"
-	"sync/atomic"
 	"testing"
-	"time"
 
-	"github.com/dustin/go-humanize"
 	"github.com/stretchr/testify/require"
-	"gitlab.com/accumulatenetwork/accumulate/internal/database/record"
 	"gitlab.com/accumulatenetwork/accumulate/smt/common"
-	"gitlab.com/accumulatenetwork/accumulate/smt/storage/badger"
 )
 
-func GetHash(i int) Hash {
-	return Sha256([]byte(fmt.Sprint(i)))
-}
+func TestReceiptList(t *testing.T) {
+	const MSize = int64(20)                                     // Use a Merkle Tree size
+	for startTest := int64(0); startTest < MSize; startTest++ { // Test a range
+		for endTest := startTest; endTest < startTest+10 && endTest < MSize; endTest++ {
 
-func TestReceipt(t *testing.T) {
-	const testMerkleTreeSize = 7
-	// Create a memory based database
-	store := begin()
+			var rb common.RandHash
 
-	// Create a MerkleManager for the memory database
-	manager := testChain(store, 2)
-	// populate the database
-	for i := 0; i < testMerkleTreeSize; i++ {
-		v := GetHash(i)
-		require.NoError(t, manager.AddHash(v, false))
-		fmt.Printf("e%-6d %x %v\n", i, v, v[:3])
-	}
-	e0 := GetHash(0)
-	e1 := GetHash(1)
-	e2 := GetHash(2)
-	e3 := GetHash(3)
-	e4 := GetHash(4)
-	e5 := GetHash(5)
-	e6 := GetHash(6)
-	e01 := sha256.Sum256(append(e0[:], e1[:]...))
-	e23 := sha256.Sum256(append(e2[:], e3[:]...))
-	e45 := sha256.Sum256(append(e4[:], e5[:]...))
-	e0123 := sha256.Sum256(append(e01[:], e23[:]...))
-
-	fmt.Printf("%-7s %x %v\n", "e01", e01, e01[:3])
-	fmt.Printf("%-7s %x %v\n", "e23", e23, e23[:3])
-	fmt.Printf("%-7s %x %v\n", "e45", e45, e45[:3])
-	fmt.Printf("%-7s %x %v\n", "e0123", e0123, e0123[:3])
-
-	fmt.Printf("\n\n %3x %3x %3x %3x %3x %3x %3x\n", e0[:3], e1[:3], e2[:3], e3[:3], e4[:3], e5[:3], e6[:3])
-	fmt.Printf("     %3x        %3x        %3x\n", e01[:3], e23[:3], e45[:3])
-	fmt.Printf("            %3x\n", e0123[:3])
-
-	fmt.Printf("\n\n %3v %3v   %3v %3v   %3v %3v   %3v\n", e0[:3], e1[:3], e2[:3], e3[:3], e4[:3], e5[:3], e6[:3])
-	fmt.Printf("         %3v                %3v                  %3v\n", e01[:3], e23[:3], e45[:3])
-	fmt.Printf("                       %3v\n", e0123[:3])
-
-	element := GetHash(0)
-	anchor := GetHash(3)
-
-	r, err1 := GetReceipt(manager, element, anchor)
-	if err1 != nil {
-		t.Fatal("Failed to generate receipt")
-	}
-	fmt.Println(r.String())
-	if !r.Validate() {
-		t.Fatal("Receipt fails")
-	}
-}
-
-func TestReceiptAll(t *testing.T) {
-	cnt := 0
-	const testMerkleTreeSize = 150
-
-	store := begin()
-	manager := testChain(store, 2, "one")     // Populate a database
-	var rh common.RandHash                    // A source of random hashes
-	for i := 0; i < testMerkleTreeSize; i++ { // Then for all the hashes for our test
-		require.NoError(t, manager.AddHash(rh.NextList(), false)) // Add a hash
-	}
-
-	for i := 0; i < testMerkleTreeSize; i++ {
-		for j := 0; j < testMerkleTreeSize; j++ {
-			//fmt.Println("--------------i,j ", i, ",", j, " ---------------")
-			element := rh.Next()
-			if i >= 0 && i < testMerkleTreeSize {
-				element = rh.List[i]
-			}
-			anchor := rh.Next()
-			if j >= 0 && j < testMerkleTreeSize {
-				anchor = rh.List[j]
+			store := begin()                    // Create a memory database
+			manager := testChain(store, 4)      // Create a test chain
+			manager2 := testChain2(store, 4)    // and create another
+			for i := int64(0); i < MSize; i++ { // Now put MSize elements in the first test chain
+				v := rb.NextList()                                                  // Get a random hash
+				require.NoError(t, manager.AddHash(v, false))                       //   and put it in the first chain
+				ms, err := manager.Head().Get()                                     // Get the current merkle state
+				require.NoError(t, err)                                             //
+				require.NoError(t, manager2.AddHash(ms.GetMDRoot().Bytes(), false)) // Then anchor the first chain into the second
 			}
 
-			cnt++
-			r, err := GetReceipt(manager, element, anchor)
+			element, err := manager.Get(startTest) // Get the first element of a test of a list
+			require.NoError(t, err)
 
-			if i < 0 || i >= testMerkleTreeSize || //       If i is out of range
-				j < 0 || j >= testMerkleTreeSize || //        Or j is out of range
-				j < i { //                                    Or if the anchor is before the element
-				if r != nil { //                            then you should not be able to generate a receipt
-					t.Fatal("Should not be able to generate a receipt")
-				}
-			} else {
-				require.Nilf(t, err, "Failed to get a receipt: %v", err)
-				if r == nil {
-					t.Fatal("Failed to generate receipt", i, j)
-				}
-				if !r.Validate() {
-					t.Errorf("Receipt fails for element/anchor [%d %d]\n", i, int64(j))
-				}
-				//require.Truef(t, bytes.Equal(r.MDRoot, mdRoots[j]),
-				//	"Anchors should match %d %d got %x expected %x", i, j, r.MDRoot[:4], mdRoots[j][:4])
+			receiptList, err := GetReceiptList(manager, element, startTest, endTest) // Get a receipt list
+			require.NoError(t, err)                                                  // It should work
+			require.NotNil(t, receiptList)                                           // It should return a receiptList
+			require.True(t, receiptList.Validate())                                  // And the receiptList should validate
+			anchor1 := receiptList.Receipt.Anchor                                    // Get the anchor1 that
+
+			for i := startTest; i <= endTest; i++ { //     Test every list we can make for the given range
+				element, err := manager.Get(i)          // Get an element
+				require.NoError(t, err)                 //
+				receiptList.Element = element           // Stuff it into the receiptList
+				require.NoError(t, err)                 // This should work, because we look for the element in the list
+				require.NotNil(t, receiptList)          // and the list Receipt is proof of all its elements
+				require.True(t, receiptList.Validate()) //
 			}
+
+			receipt, err := GetReceipt(manager2, anchor1, anchor1)         // Build a receipt of the anchor in the second chain
+			require.NoError(t, err)                                        //
+			require.NotNil(t, receipt)                                     //
+			receipt2, err := CombineReceipts(receiptList.Receipt, receipt) // Combine the receipts
+			require.NoError(t, err)                                        //
+			require.NotNil(t, receipt2)                                    //
+			receiptList.Receipt = receipt2                                 // Now use the combined receipt in the receiptList
+
+			require.True(t, receipt2.Validate()) //                            Test the receipt for grins.
+
+			for i := startTest; i <= endTest; i++ { //     Rerun the test of every list we can make for the given range
+				element, err := manager.Get(i)          // Note we are testing a combined receipt here.  Get an element
+				require.NoError(t, err)                 //
+				receiptList.Element = element           // Stuff the element into the receiptList
+				require.NoError(t, err)                 //
+				require.NotNil(t, receiptList)          //
+				require.True(t, receiptList.Validate()) // Check that the receipt continues to validate.  This goes to the
+			}
+
 		}
 	}
-	fmt.Println("Ran ", cnt, " tests")
-}
-
-func PopulateDatabase(t *testing.T, manager *MerkleManager, treeSize int64) {
-	// populate the database
-	head, err := manager.Head().Get()
-	require.NoError(t, err)
-	startCount := head.Count
-	for i := startCount; i < treeSize; i++ {
-		v := GetHash(int(i))
-		require.NoError(t, manager.AddHash(v, false))
-	}
-}
-
-func GenerateReceipts(manager *MerkleManager, receiptCount int64, t *testing.T) {
-	start := time.Now()
-	total := new(int64)
-	atomic.StoreInt64(total, 0)
-	running := new(int64)
-	printed := new(int64)
-
-	head, err := manager.Head().Get()
-	require.NoError(t, err)
-	for i := 0; i < int(head.Count); i++ {
-		atomic.AddInt64(running, 1)
-		for j := i; j < int(head.Count); j++ {
-			element := GetHash(i)
-			anchor := GetHash(j)
-
-			r, _ := GetReceipt(manager, element, anchor)
-			if i < 0 || i >= int(head.Count) || //       If (i) is out of range
-				j < 0 || j >= int(head.Count) || //        Or j is out of range
-				j < i { //                                    Or if the anchor is before the element
-				if r != nil { //                            then you should not be able to generate a receipt
-					t.Error("Should not be able to generate a receipt")
-					return
-				}
-			} else {
-				if r == nil {
-					t.Error("Failed to generate receipt", i, j)
-					return
-				}
-				if !r.Validate() {
-					t.Error("Receipt fails for element ", i, " anchor ", j)
-					return
-				}
-				atomic.AddInt64(total, 1)
-			}
-			t := atomic.LoadInt64(total)
-			if t-atomic.LoadInt64(printed) >= 100000 {
-				atomic.StoreInt64(printed, t)
-				seconds := int64(time.Since(start).Seconds()) + 1
-				fmt.Printf("Element: %7d     Receipts generated: %12s     Rate: %8d/s\n",
-					i, humanize.Comma(t), t/seconds)
-			}
-			if t > receiptCount {
-				return
-			}
-		}
-		atomic.AddInt64(running, -1)
-		if atomic.LoadInt64(total) > receiptCount {
-			break
-		}
-		if atomic.LoadInt64(running) > 5 {
-			for atomic.LoadInt64(running) > 2 {
-				time.Sleep(time.Millisecond)
-			}
-		}
-	}
-	for atomic.LoadInt64(running) > 0 {
-	}
-}
-
-func TestBadgerReceipts(t *testing.T) {
-	// acctesting.SkipCI(t, "flaky")
-	if os.Getenv("CI") == "true" {
-		t.Skip("Skipping test: running CI: flaky")
-	}
-
-	badger, err := badger.New(filepath.Join(t.TempDir(), "badger.db"), nil)
-	require.NoError(t, err)
-	defer badger.Close()
-
-	batch := badger.Begin(true)
-	defer batch.Discard()
-
-	manager := testChain(record.KvStore{Store: batch}, 2)
-
-	PopulateDatabase(t, manager, 700)
-
-	GenerateReceipts(manager, 1500, t)
-
-}
-
-func TestReceipt_Combine(t *testing.T) {
-	testCnt := int64(50)
-	var rh common.RandHash
-	store := begin()
-	m1 := testChain(store, 2, "m1")
-	m2 := testChain(store, 2, "m2")
-
-	for i := int64(0); i < testCnt; i++ {
-		require.NoError(t, m1.AddHash(rh.NextList(), false))
-		head, err := m1.Head().Get()
-		require.NoError(t, err)
-		root1 := head.GetMDRoot()
-		require.NoError(t, m2.AddHash(root1, false))
-	}
-	for i := int64(0); i < testCnt; i++ {
-		for j := i; j < testCnt; j++ {
-			element, _ := m1.Get(i)
-			anchor, _ := m1.Get(j)
-			r, _ := GetReceipt(m1, element, anchor)
-			state, _ := m1.GetAnyState(j)
-			mdRoot := state.GetMDRoot()
-
-			require.Truef(t, bytes.Equal(r.Anchor, mdRoot), "m1 MDRoot not right %d %d", i, j)
-			element, _ = m2.Get(i)
-			anchor, _ = m2.Get(j)
-			r, _ = GetReceipt(m2, element, anchor)
-			state, _ = m2.GetAnyState(j)
-			mdRoot = state.GetMDRoot()
-			require.Truef(t, bytes.Equal(r.Anchor, mdRoot), "m2 MDRoot not right %d %d", i, j)
-		}
-	}
-	for i := int64(0); i < testCnt; i++ {
-		for j := i + 1; j < testCnt; j++ {
-			element, _ := m1.Get(i)
-			anchor, _ := m1.Get(j)
-			r1, _ := GetReceipt(m1, element, anchor)
-			require.Truef(t, r1.Validate(), "receipt failed %d %d", i, j)
-			require.NotNilf(t, r1, "test case i %d j %d failed to create r1", i, j)
-			for k := j; k < testCnt; k++ {
-				element, _ = m2.Get(j)
-				anchor, _ = m2.Get(k)
-				r2, _ := GetReceipt(m2, element, anchor)
-				require.Truef(t, r2.Validate(), "receipt failed %d %d", i, j, k)
-				require.NotNilf(t, r2, "test case i %d j %d k %d failed to create r2", i, j, k)
-				r3, err := r1.Combine(r2)
-				require.NoErrorf(t, err, "no error expected combining receipts %d %d %d", i, j, k)
-				require.Truef(t, r3.Validate(), "combined receipt failed. %d %d %d", i, j, k)
-			}
-		}
-	}
-
-}
-
-// TestReceiptSimple
-// Make a simple SMT from a list of hashes and values
-func TestReceiptSimple(t *testing.T) {
-
-	var cnt = 5000 //                          number of tests
-
-	var rh common.RandHash     //              Create a list of values
-	var list [][]byte          //
-	for i := 0; i < cnt; i++ { //              Create a value for every numberValues
-		list = append(list, rh.Next()) //
-	}
-
-	store := begin()         //  Set up a memory db
-	m := testChain(store, 2) //
-
-	for _, v := range list { //                Put all the values into the SMT
-		require.NoError(t, m.AddHash(v, false), "Error") //
-	}
-
-	// We can now generate a receipt
-	receipt, err := GetReceipt(m, list[0], list[cnt-1])
-	require.Nil(t, err, "fail GetReceipt")
-	require.True(t, receipt.Validate(), "Receipt failed")
-
 }
