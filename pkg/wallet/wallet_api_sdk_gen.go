344df0b1eda55a42f52649adda4eeeb215b38b1e
Dennis B <dennis@factoid.org>
2022-08-04 13:17:17 -0500

    generate

--- pkg/wallet/wallet_api_sdk_gen.go
+++ pkg/wallet/wallet_api_sdk_gen.go
@@ -8,11 +8,11 @@ import (
 	"gitlab.com/accumulatenetwork/cmd/accumulate/walletd/api"
 )
 
-// DecodeTransaction unmarshal a binary transaction body and return the json transaction body.
-func (c *Client) DecodeTransaction(ctx context.Context, req *api.DecodeTransactionRequest) (*api.DecodeTransactionResponse, error) {
-	var resp api.DecodeTransactionResponse
+// CreateEnvelope create an envelope by name.
+func (c *Client) CreateEnvelope(ctx context.Context, req *api.CreateEnvelopeRequest) (*api.GeneralResponse, error) {
+	var resp api.GeneralResponse
 
-	err := c.RequestAPIv2(ctx, "decode-transaction", req, &resp)
+	err := c.RequestAPIv2(ctx, "create-envelope", req, &resp)
 	if err != nil {
 		return nil, err
 	}
@@ -20,16 +20,40 @@ func (c *Client) DecodeTransaction(ctx context.Context, req *api.DecodeTransacti
 	return &resp, nil
 }
 
-// EncodeTransaction binary marshal a json transaction body and return encoded hex.
-func (c *Client) EncodeTransaction(ctx context.Context, req *api.EncodeTransactionRequest) (*api.EncodeTransactionResponse, error) {
-	var resp api.EncodeTransactionResponse
+// CreateTransaction create a transaction by name.
+func (c *Client) CreateTransaction(ctx context.Context, req *api.CreateTransactionRequest) (*api.GeneralResponse, error) {
+	var resp api.GeneralResponse
 
-	err := c.RequestAPIv2(ctx, "encode-transaction", req, &resp)
+	err := c.RequestAPIv2(ctx, "create-transaction", req, &resp)
 	if err != nil {
 		return nil, err
 	}
 
 	return &resp, nil
+}
+
+// Decode unmarshal a binary transaction or account and return the json transaction body.
+func (c *Client) Decode(ctx context.Context, req *api.DecodeRequest) (*api.DecodeResponse, error) {
+	var resp api.DecodeResponse
+
+	err := c.RequestAPIv2(ctx, "decode", req, &resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &resp, nil
+}
+
+// Encode binary marshal a json transaction or account and return encoded hex.
+func (c *Client) Encode(ctx context.Context, req *api.EncodeRequest) (interface{}, error) {
+	var resp interface{}
+
+	err := c.RequestAPIv2(ctx, "encode", req, &resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
 }
 
 // KeyList returns a list of available keys in the wallet.
@@ -42,6 +66,18 @@ func (c *Client) KeyList(ctx context.Context, req *api.KeyListRequest) (interfac
 	}
 
 	return resp, nil
+}
+
+// ResolveKey returns a public key from either a label or keyhash.
+func (c *Client) ResolveKey(ctx context.Context, req interface{}) (*api.ResolveKeyResponse, error) {
+	var resp api.ResolveKeyResponse
+
+	err := c.RequestAPIv2(ctx, "resolve-key", req, &resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &resp, nil
 }
 
 // Sign sign a transaction.
