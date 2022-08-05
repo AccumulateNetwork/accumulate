e92d878b2bb7272ddcae15950ce3bd1febe781d7
Abhishek <rabhishek822@gmail.com>
2022-08-05 20:39:40 +0530

    key page remove changes

--- internal/chain/update_key_page.go
+++ internal/chain/update_key_page.go
@@ -170,7 +170,7 @@ func (UpdateKeyPage) executeOperation(page *protocol.KeyPage, book *protocol.Key
 		if !ok {
 			return errors.Format(errors.StatusInternalError, "principal is not a key page")
 		}
-		if len(page.Keys) == 1 && pageIndex == 1 {
+		if len(page.Keys) == 1 && pageIndex == 0 {
 			return fmt.Errorf("cannot delete last key of the highest priority page of a key book")
 		}
 
