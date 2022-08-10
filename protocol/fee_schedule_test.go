52092c5b25283adc487869caee89043afef20fd8
Ethan Reesor <ethan.reesor@gmail.com>
2022-08-10 15:26:35 +0000

    feat(prot): variable fee schedule [AC-2239]
    Closes AC-2239. Some transactions can involve multiple operations. The fee for those transactions should increase for each extra operation beyond the first.
    
    ## Review Checklist
    
    **If any item is not complete, the merge request is not ready to be reviewed and must be marked `Draft:`.**
    
    - [x] The merge request title is in the format `<change type>(<change scope>): <short description> [<task id>]`
      - For example, `feat(cli): add QR code generation [AC-123]`
      - For details, see [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [x] The description includes `Closes <jira task ID>` (or rarely `Updates <jira task ID>`)
    - [x] The change is fully validated by tests that are run during CI
      - In most cases this means a test in "validate.sh"
      - In some cases, a Go test may be acceptable
      - Validation is not applicable to things like documentation updates
      - Purely UI/UX changes can be manually validated, such as changes to human-readable output
      - For all other changes, automated validation tests are an absolute requirement unless a maintainer specifically explains why they are not in a comment on this merge request
    - [x] The change is marked with one of the validation labels
      - ~"validation::ci/cd" for changes validated by CI tests
      - ~"validation::manual" for changes validated by hand
      - ~"validation::deferred" for changes validated by a follow up merge request
      - ~"validation::not applicable" for changes where validation is not applicable
    
    ## Merge Checklist
    
    - [ ] CI is passing
    - [ ] Merge conflicts are resolved
    - [ ] All discussions are resolved
    
    Related to AC-2239

--- protocol/fee_schedule_test.go
+++ protocol/fee_schedule_test.go
@@ -1,7 +1,7 @@
 package protocol_test
 
 import (
-	"math"
+	"math/big"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -10,27 +10,17 @@ import (
 )
 
 func TestFee(t *testing.T) {
-	t.Run("Unknown", func(t *testing.T) {
-		_, err := BaseTransactionFee(TransactionTypeUnknown)
-		require.Error(t, err)
-	})
-
-	t.Run("Invalid", func(t *testing.T) {
-		_, err := BaseTransactionFee(TransactionType(math.MaxUint64))
-		require.Error(t, err)
-	})
-
 	t.Run("SendTokens", func(t *testing.T) {
 		env := acctesting.NewTransaction().
 			WithCurrentTimestamp().
 			WithPrincipal(AcmeUrl()).
 			WithSigner(AccountUrl("foo", "book", "1"), 1).
 			WithCurrentTimestamp().
-			WithBody(new(SendTokens)).
+			WithBody(&SendTokens{To: []*TokenRecipient{{}}}).
 			Initiate(SignatureTypeLegacyED25519, acctesting.GenerateKey(t.Name()))
 		fee, err := ComputeTransactionFee(env.Transaction[0])
 		require.NoError(t, err)
-		require.Equal(t, FeeSendTokens, fee)
+		require.Equal(t, FeeTransferTokens, fee)
 	})
 
 	t.Run("Lots of data", func(t *testing.T) {
@@ -38,12 +28,12 @@ func TestFee(t *testing.T) {
 			WithPrincipal(AcmeUrl()).
 			WithSigner(AccountUrl("foo", "book", "1"), 1).
 			WithCurrentTimestamp().
-			WithBody(new(SendTokens)).
+			WithBody(&SendTokens{To: []*TokenRecipient{{}}}).
 			Initiate(SignatureTypeLegacyED25519, acctesting.GenerateKey(t.Name()))
 		env.Transaction[0].Header.Metadata = make([]byte, 1024)
 		fee, err := ComputeTransactionFee(env.Transaction[0])
 		require.NoError(t, err)
-		require.Equal(t, FeeSendTokens+FeeData*4, fee)
+		require.Equal(t, FeeTransferTokens+FeeData*4, fee)
 	})
 
 	t.Run("Scratch data", func(t *testing.T) {
@@ -58,4 +48,71 @@ func TestFee(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, FeeScratchData*5, fee)
 	})
+
+	t.Run("All types", func(t *testing.T) {
+		// Test every valid user transaction
+		for i := TransactionType(0); i < 1<<16; i++ {
+			body, err := NewTransactionBody(i)
+			if err != nil {
+				continue
+			}
+
+			t.Run(i.String(), func(t *testing.T) {
+				txn := new(Transaction)
+				txn.Header.Principal = AccountUrl("foo")
+				txn.Body = body
+				fee, err := ComputeTransactionFee(txn)
+				if i == TransactionTypeRemote {
+					// Remote transactions should never be passed to
+					// ComputeTransactionFee so it returns an error
+					require.Error(t, err)
+					return
+				} else {
+					// Every other legal transaction type must have a fee
+					require.NoError(t, err)
+				}
+
+				switch i {
+				case TransactionTypeAcmeFaucet,
+					TransactionTypeAddCredits:
+					// A few transactions are free
+					require.Zero(t, fee)
+				default:
+					if i.IsUser() {
+						// Most user transactions should cost something
+						require.NotZero(t, fee)
+					} else {
+						// Synthetic and system transactions are free
+						require.Zero(t, fee)
+					}
+				}
+			})
+		}
+	})
+}
+
+func TestMultiOutputRefund(t *testing.T) {
+	// Verifies that a two output transaction with a good and a bad output does
+	// not cost less than a single output transaction
+
+	body := new(SendTokens)
+	txn := new(Transaction)
+	txn.Header.Principal = AccountUrl("foo")
+	txn.Body = body
+
+	// Calculate fee and refund for a single output transaction
+	body.AddRecipient(AccountUrl("bar"), big.NewInt(0))
+	fee1, err := ComputeTransactionFee(txn)
+	require.NoError(t, err)
+
+	// Calculate fee and refund for a two output transaction
+	body.AddRecipient(AccountUrl("bar"), big.NewInt(0))
+	paid2, err := ComputeTransactionFee(txn)
+	require.NoError(t, err)
+	refund2, err := ComputeSyntheticRefund(txn, len(body.To))
+	require.NoError(t, err)
+	fee2 := paid2 - refund2
+
+	// Verify
+	require.GreaterOrEqual(t, fee2, fee1)
 }
