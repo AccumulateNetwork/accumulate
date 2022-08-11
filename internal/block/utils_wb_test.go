3fd7f06812970ec64ca5aa81f18d32d975bb6923
Ethan Reesor <ethan.reesor@gmail.com>
2022-08-11 13:43:34 +0000

    fix(prot): issue refunds indirectly [AC-2226]
    Closes AC-2226. Previously refunds were issued by directly crediting the initial signer, but only if the signer was local. So transactions initiated via a remote signer were not refunded. Now refunds are issued by producing a credit deposit transaction which works regardless of the locality of the initiator.
    
    ## Review Checklist
    
    **If any item is not complete, the merge request is not ready to be reviewed and must be marked `Draft:`.**
    
    - [x] The merge request title is in the format `<change type>(<change scope>): <short description> [<task id>]`
      - For example, `feat(cli): add QR code generation [AC-123]`
      - For details, see [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [x] The description includes `Closes <jira task ID>` (or rarely `Updates <jira task ID>`)
    - [x] The change is fully validated by tests that are run during CI
      - In most cases this means a test in "validate.sh"
      - In some cases, a Go test may be acceptable
      - Validation is not applicable to things like documentation updates
      - Purely UI/UX changes can be manually validated, such as changes to human-readable output
      - For all other changes, automated validation tests are an absolute requirement unless a maintainer specifically explains why they are not in a comment on this merge request
    - [x] The change is marked with one of the validation labels
      - ~"validation::ci/cd" for changes validated by CI tests
      - ~"validation::manual" for changes validated by hand
      - ~"validation::deferred" for changes validated by a follow up merge request
      - ~"validation::not applicable" for changes where validation is not applicable
    
    ## Merge Checklist
    
    - [ ] CI is passing
    - [ ] Merge conflicts are resolved
    - [ ] All discussions are resolved
    
    Related to AC-2226
    
    
    Former-commit-id: 59ba70864ad449f2a26fe2a3049d8964184ab9ca

--- internal/block/utils_wb_test.go
+++
@@ -1,9 +0,0 @@
-package block
-
-import "gitlab.com/accumulatenetwork/accumulate/internal/chain"
-
-// White-box testing utilities
-
-func (x *Executor) SetExecutor(y chain.TransactionExecutor) {
-	x.executors[y.Type()] = y
-}
