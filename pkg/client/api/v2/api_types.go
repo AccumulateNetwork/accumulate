7da9ad3a171ad6fa5e72cba9a013b5869865a7da
Ethan Reesor <ethan.reesor@gmail.com>
2022-08-22 11:44:55 -0500

    all types

--- pkg/client/api/v2/api_types.go
+++ pkg/client/api/v2/api_types.go
@@ -2,22 +2,41 @@ package client
 
 import "gitlab.com/accumulatenetwork/accumulate/internal/api/v2"
 
+type ChainEntry = api.ChainEntry
+type ChainIdQuery = api.ChainIdQuery
 type ChainQueryResponse = api.ChainQueryResponse
 type DataEntryQuery = api.DataEntryQuery
+type DataEntryQueryResponse = api.DataEntryQueryResponse
 type DataEntrySetQuery = api.DataEntrySetQuery
 type DescriptionResponse = api.DescriptionResponse
 type DirectoryQuery = api.DirectoryQuery
 type ExecuteRequest = api.ExecuteRequest
 type GeneralQuery = api.GeneralQuery
+type KeyPage = api.KeyPage
 type KeyPageIndexQuery = api.KeyPageIndexQuery
 type MajorBlocksQuery = api.MajorBlocksQuery
+type MajorQueryResponse = api.MajorQueryResponse
+type MerkleState = api.MerkleState
 type MetricsQuery = api.MetricsQuery
+type MetricsResponse = api.MetricsResponse
+type MinorBlock = api.MinorBlock
 type MinorBlocksQuery = api.MinorBlocksQuery
+type MinorQueryResponse = api.MinorQueryResponse
 type MultiResponse = api.MultiResponse
+type QueryOptions = api.QueryOptions
+type QueryPagination = api.QueryPagination
+type SignatureBook = api.SignatureBook
+type SignaturePage = api.SignaturePage
+type Signer = api.Signer
+type SignerMetadata = api.SignerMetadata
 type StatusResponse = api.StatusResponse
 type SyntheticTransactionRequest = api.SyntheticTransactionRequest
+type TokenDeposit = api.TokenDeposit
+type TokenSend = api.TokenSend
 type TransactionQueryResponse = api.TransactionQueryResponse
 type TxHistoryQuery = api.TxHistoryQuery
 type TxnQuery = api.TxnQuery
 type TxRequest = api.TxRequest
 type TxResponse = api.TxResponse
+type UrlQuery = api.UrlQuery
+type VersionResponse = api.VersionResponse
