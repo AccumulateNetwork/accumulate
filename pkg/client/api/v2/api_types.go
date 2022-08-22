4eedec49daa8209ddb08e6595c50e2c5d36ca433
Ethan Reesor <ethan.reesor@gmail.com>
2022-08-22 11:41:38 -0500

    fix(pkg): export API types

---
+++ pkg/client/api/v2/api_types.go
@@ -0,0 +1,23 @@
+package client
+
+import "gitlab.com/accumulatenetwork/accumulate/internal/api/v2"
+
+type ChainQueryResponse = api.ChainQueryResponse
+type DataEntryQuery = api.DataEntryQuery
+type DataEntrySetQuery = api.DataEntrySetQuery
+type DescriptionResponse = api.DescriptionResponse
+type DirectoryQuery = api.DirectoryQuery
+type ExecuteRequest = api.ExecuteRequest
+type GeneralQuery = api.GeneralQuery
+type KeyPageIndexQuery = api.KeyPageIndexQuery
+type MajorBlocksQuery = api.MajorBlocksQuery
+type MetricsQuery = api.MetricsQuery
+type MinorBlocksQuery = api.MinorBlocksQuery
+type MultiResponse = api.MultiResponse
+type StatusResponse = api.StatusResponse
+type SyntheticTransactionRequest = api.SyntheticTransactionRequest
+type TransactionQueryResponse = api.TransactionQueryResponse
+type TxHistoryQuery = api.TxHistoryQuery
+type TxnQuery = api.TxnQuery
+type TxRequest = api.TxRequest
+type TxResponse = api.TxResponse
