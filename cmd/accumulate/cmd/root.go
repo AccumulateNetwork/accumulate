8eed585417512e15e983273a417811d28714dc66
Dennis B <dennis@factoid.org>
2022-08-24 14:59:26 -0500

    changed endpoint to beta

--- cmd/accumulate/cmd/root.go
+++ cmd/accumulate/cmd/root.go
@@ -75,7 +75,7 @@ func InitRootCmd(database db.DB) *cobra.Command {
 
 	serverAddr := os.Getenv("ACC_API")
 	if serverAddr == "" {
-		serverAddr = "https://alpha.testnet.accumulatenetwork.io/v2"
+		serverAddr = "https://beta.testnet.accumulatenetwork.io/v2"
 	}
 
 	flags := cmd.PersistentFlags()
