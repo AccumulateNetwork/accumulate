c0ca39fc1d939830d77970d6c377cd6e8e473f83
Ethan Reesor <ethan.reesor@gmail.com>
2022-09-01 11:19:17 -0500

    fix(prot): entry hash in transaction result

--- test/e2e/txn_write_data_test.go
+++ test/e2e/txn_write_data_test.go
@@ -8,6 +8,7 @@ import (
 	"gitlab.com/accumulatenetwork/accumulate/internal/database"
 	acctesting "gitlab.com/accumulatenetwork/accumulate/internal/testing"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/url"
+	"gitlab.com/accumulatenetwork/accumulate/protocol"
 	. "gitlab.com/accumulatenetwork/accumulate/protocol"
 )
 
@@ -81,7 +82,7 @@ func TestWriteData_Factom(t *testing.T) {
 	require.True(t, EqualDataEntry(entry.Wrap(), account.Entry), "Account entry does not match")
 }
 
-func TestWriteData_EntryHash(t *testing.T) {
+func TestWriteData_AdiAccumulateEntryHash(t *testing.T) {
 	var timestamp uint64
 
 	// Initialize
@@ -95,7 +96,7 @@ func TestWriteData_EntryHash(t *testing.T) {
 	updateAccount(sim, alice.JoinPath("book", "1"), func(page *KeyPage) { page.CreditBalance = 1e9 })
 	sim.CreateAccount(&DataAccount{Url: alice.JoinPath("data")})
 
-	// Add credits
+	// Write second entry
 	entry := &AccumulateDataEntry{Data: [][]byte{[]byte("foo"), []byte("bar")}}
 	st, _ := sim.WaitForTransactions(delivered, sim.MustSubmitAndExecuteBlock(
 		acctesting.NewTransaction().
@@ -118,6 +119,170 @@ func TestWriteData_EntryHash(t *testing.T) {
 		entryHash, err := data.Entry().Get(0)
 		require.NoError(t, err)
 		require.Equal(t, entry.Hash(), entryHash[:])
+
+		txnHash, err := data.Transaction(entryHash).Get()
+		require.NoError(t, err)
+		require.Equal(t, st[0].TxID.Hash(), txnHash)
+
+		return nil
+	})
+}
+
+func TestWriteData_LiteAccumulateEntryHash(t *testing.T) {
+	var timestamp uint64
+
+	// Initialize
+	sim := simulator.New(t, 3)
+	sim.InitFromGenesis()
+
+	// Setup accounts
+	alice := url.MustParse("alice")
+	aliceKey := acctesting.GenerateKey(alice)
+	sim.CreateIdentity(alice, aliceKey[32:])
+	updateAccount(sim, alice.JoinPath("book", "1"), func(page *KeyPage) { page.CreditBalance = 1e9 })
+
+	entry1 := &FactomDataEntry{ExtIds: [][]byte{[]byte("foo"), []byte("bar")}}
+	entry1.AccountId = *(*[32]byte)(protocol.ComputeLiteDataAccountId(entry1.Wrap()))
+	ldaAddr, err := protocol.LiteDataAddress(entry1.AccountId[:])
+	require.NoError(t, err)
+	sim.WaitForTransactions(delivered, sim.MustSubmitAndExecuteBlock(
+		acctesting.NewTransaction().
+			WithPrincipal(ldaAddr).
+			WithSigner(alice.JoinPath("book", "1"), 1).
+			WithTimestampVar(&timestamp).
+			WithBody(&WriteData{Entry: entry1.Wrap()}).
+			Initiate(SignatureTypeED25519, aliceKey).
+			Build(),
+	)...)
+
+	// Write second entry
+	entry2 := &AccumulateDataEntry{Data: [][]byte{[]byte("foo"), []byte("bar")}}
+	st, _ := sim.WaitForTransactions(delivered, sim.MustSubmitAndExecuteBlock(
+		acctesting.NewTransaction().
+			WithPrincipal(ldaAddr).
+			WithSigner(alice.JoinPath("book", "1"), 1).
+			WithTimestampVar(&timestamp).
+			WithBody(&WriteData{Entry: entry2}).
+			Initiate(SignatureTypeED25519, aliceKey).
+			Build(),
+	)...)
+
+	// Check the result
+	require.Len(t, st, 1)
+	require.IsType(t, (*WriteDataResult)(nil), st[0].Result)
+	result := st[0].Result.(*WriteDataResult)
+	require.Equal(t, entry2.Hash(), result.EntryHash[:])
+
+	_ = sim.PartitionFor(alice).View(func(batch *database.Batch) error {
+		data := batch.Account(ldaAddr).Data()
+		entryHash, err := data.Entry().Get(1)
+		require.NoError(t, err)
+		require.Equal(t, entry2.Hash(), entryHash[:])
+
+		txnHash, err := data.Transaction(entryHash).Get()
+		require.NoError(t, err)
+		require.Equal(t, st[0].TxID.Hash(), txnHash)
+
+		return nil
+	})
+}
+
+func TestWriteData_AdiFactomEntryHash(t *testing.T) {
+	var timestamp uint64
+
+	// Initialize
+	sim := simulator.New(t, 3)
+	sim.InitFromGenesis()
+
+	// Setup accounts
+	alice := url.MustParse("alice")
+	aliceKey := acctesting.GenerateKey(alice)
+	sim.CreateIdentity(alice, aliceKey[32:])
+	updateAccount(sim, alice.JoinPath("book", "1"), func(page *KeyPage) { page.CreditBalance = 1e9 })
+	sim.CreateAccount(&DataAccount{Url: alice.JoinPath("data")})
+
+	// Write second entry
+	entry := &FactomDataEntry{ExtIds: [][]byte{[]byte("foo"), []byte("bar")}, AccountId: [32]byte{1}}
+	st, _ := sim.WaitForTransactions(delivered, sim.MustSubmitAndExecuteBlock(
+		acctesting.NewTransaction().
+			WithPrincipal(alice.JoinPath("data")).
+			WithSigner(alice.JoinPath("book", "1"), 1).
+			WithTimestampVar(&timestamp).
+			WithBody(&WriteData{Entry: entry.Wrap()}).
+			Initiate(SignatureTypeED25519, aliceKey).
+			Build(),
+	)...)
+
+	// Check the result
+	require.Len(t, st, 1)
+	require.IsType(t, (*WriteDataResult)(nil), st[0].Result)
+	result := st[0].Result.(*WriteDataResult)
+	require.Equal(t, entry.Hash(), result.EntryHash[:])
+
+	_ = sim.PartitionFor(alice).View(func(batch *database.Batch) error {
+		data := batch.Account(alice.JoinPath("data")).Data()
+		entryHash, err := data.Entry().Get(0)
+		require.NoError(t, err)
+		require.Equal(t, entry.Hash(), entryHash[:])
+
+		txnHash, err := data.Transaction(entryHash).Get()
+		require.NoError(t, err)
+		require.Equal(t, st[0].TxID.Hash(), txnHash)
+
+		return nil
+	})
+}
+
+func TestWriteData_LiteFactomEntryHash(t *testing.T) {
+	var timestamp uint64
+
+	// Initialize
+	sim := simulator.New(t, 3)
+	sim.InitFromGenesis()
+
+	// Setup accounts
+	alice := url.MustParse("alice")
+	aliceKey := acctesting.GenerateKey(alice)
+	sim.CreateIdentity(alice, aliceKey[32:])
+	updateAccount(sim, alice.JoinPath("book", "1"), func(page *KeyPage) { page.CreditBalance = 1e9 })
+
+	entry1 := &FactomDataEntry{ExtIds: [][]byte{[]byte("foo"), []byte("bar")}}
+	entry1.AccountId = *(*[32]byte)(protocol.ComputeLiteDataAccountId(entry1.Wrap()))
+	ldaAddr, err := protocol.LiteDataAddress(entry1.AccountId[:])
+	require.NoError(t, err)
+	sim.WaitForTransactions(delivered, sim.MustSubmitAndExecuteBlock(
+		acctesting.NewTransaction().
+			WithPrincipal(ldaAddr).
+			WithSigner(alice.JoinPath("book", "1"), 1).
+			WithTimestampVar(&timestamp).
+			WithBody(&WriteData{Entry: entry1.Wrap()}).
+			Initiate(SignatureTypeED25519, aliceKey).
+			Build(),
+	)...)
+
+	// Write second entry
+	entry2 := &FactomDataEntry{ExtIds: [][]byte{[]byte("foo"), []byte("bar")}, AccountId: entry1.AccountId}
+	st, _ := sim.WaitForTransactions(delivered, sim.MustSubmitAndExecuteBlock(
+		acctesting.NewTransaction().
+			WithPrincipal(ldaAddr).
+			WithSigner(alice.JoinPath("book", "1"), 1).
+			WithTimestampVar(&timestamp).
+			WithBody(&WriteData{Entry: entry2.Wrap()}).
+			Initiate(SignatureTypeED25519, aliceKey).
+			Build(),
+	)...)
+
+	// Check the result
+	require.Len(t, st, 1)
+	require.IsType(t, (*WriteDataResult)(nil), st[0].Result)
+	result := st[0].Result.(*WriteDataResult)
+	require.Equal(t, entry2.Hash(), result.EntryHash[:])
+
+	_ = sim.PartitionFor(alice).View(func(batch *database.Batch) error {
+		data := batch.Account(ldaAddr).Data()
+		entryHash, err := data.Entry().Get(1)
+		require.NoError(t, err)
+		require.Equal(t, entry2.Hash(), entryHash[:])
 
 		txnHash, err := data.Transaction(entryHash).Get()
 		require.NoError(t, err)
