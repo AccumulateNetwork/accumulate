4f604241375e4d11604808b853b444b6d8f08caf
Ethan Reesor <ethan.reesor@gmail.com>
2022-09-02 16:29:11 +0000

    test: create a more accurate simulator [DO-46]
    Closes DO-46. Create a new simulator that more faithfully mirrors how things work on a real network.
    
    **If any item is not complete, the merge request is not ready to be reviewed and must be marked `Draft:`.**
    
    - [x] The merge request title is in the format `<change type>(<change scope>): <short description> [<task id>]`
      - For example, `feat(cli): add QR code generation [AC-123]`
      - For details, see [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [x] The description includes `Closes <jira task ID>` (or rarely `Updates <jira task ID>`)
    - [x] The change is fully validated by tests that are run during CI
      - In most cases this means a test in "validate.sh"
      - In some cases, a Go test may be acceptable
      - Validation is not applicable to things like documentation updates
      - Purely UI/UX changes can be manually validated, such as changes to human-readable output
      - For all other changes, automated validation tests are an absolute requirement unless a maintainer specifically explains why they are not in a comment on this merge request
    - [x] The change is marked with one of the validation labels
      - ~"validation::ci/cd" for changes validated by CI tests
      - ~"validation::manual" for changes validated by hand
      - ~"validation::deferred" for changes validated by a follow up merge request
      - ~"validation::not applicable" for changes where validation is not applicable
    
    - [ ] CI is passing
    - [ ] Merge conflicts are resolved
    - [ ] All discussions are resolved
    
    Related to DO-46
    
    
    Former-commit-id: 5cfbe4fa767407514db598950f2d42433c2c6c08

---
+++ test/helpers/simulator.go
@@ -0,0 +1,86 @@
+package helpers
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"gitlab.com/accumulatenetwork/accumulate/internal/accumulated"
+	"gitlab.com/accumulatenetwork/accumulate/internal/chain"
+	"gitlab.com/accumulatenetwork/accumulate/internal/database"
+	acctesting "gitlab.com/accumulatenetwork/accumulate/internal/testing"
+	"gitlab.com/accumulatenetwork/accumulate/pkg/url"
+	"gitlab.com/accumulatenetwork/accumulate/protocol"
+	"gitlab.com/accumulatenetwork/accumulate/test/simulator"
+)
+
+var GenesisTime = time.Date(2022, 7, 1, 0, 0, 0, 0, time.UTC)
+
+type Sim struct {
+	*simulator.Simulator
+	T testing.TB
+}
+
+func NewSim(t testing.TB, database simulator.OpenDatabaseFunc, init *accumulated.NetworkInit, snapshot simulator.SnapshotFunc) *Sim {
+	s, err := simulator.New(acctesting.NewTestLogger(t), database, init, snapshot)
+	require.NoError(t, err)
+	return &Sim{s, t}
+}
+
+func (s *Sim) Submit(delivery *chain.Delivery) *protocol.TransactionStatus {
+	s.T.Helper()
+	status, err := s.Simulator.Submit(delivery)
+	require.NoError(s.T, err)
+	return status
+}
+
+func (s *Sim) SubmitSuccessfully(delivery *chain.Delivery) *protocol.TransactionStatus {
+	s.T.Helper()
+	status := s.Submit(delivery)
+	if status.Error != nil {
+		require.NoError(s.T, status.Error)
+	}
+	return status
+}
+
+func (s *Sim) Step() {
+	s.T.Helper()
+	require.NoError(s.T, s.Simulator.Step())
+}
+
+func (s *Sim) StepUntil(conditions ...Condition) {
+	s.T.Helper()
+	for i := 0; ; i++ {
+		if i >= 50 {
+			s.T.Fatal("Condition not met after 50 blocks")
+		}
+		ok := true
+		for _, c := range conditions {
+			if !c(s) {
+				ok = false
+			}
+		}
+		if ok {
+			break
+		}
+		s.Step()
+	}
+}
+
+func (s *Sim) GetStatus(txid *url.TxID) *protocol.TransactionStatus {
+	s.T.Helper()
+	var v *protocol.TransactionStatus
+	var err error
+	View(s.T, s.DatabaseFor(txid.Account()), func(batch *database.Batch) {
+		s.T.Helper()
+		h := txid.Hash()
+		v, err = batch.Transaction(h[:]).Status().Get()
+		require.NoError(s.T, err)
+
+		// The database returns a new status if it can't find one
+		if v.Equal(new(protocol.TransactionStatus)) {
+			s.T.Fatalf("Transaction %x@%v cannot be found", h[:4], txid.Account())
+		}
+	})
+	return v
+}
