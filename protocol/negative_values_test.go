15d1c8b234077421b8a78e7726624bfc6c63563e
Abhishek <rabhishek822@gmail.com>
2022-09-07 16:36:40 +0530

    sample tests

--- protocol/negative_values_test.go
+++ protocol/negative_values_test.go
@@ -19,4 +19,20 @@ func TestNegativeValues(t *testing.T) {
 		msg := "field To: failed to marshal field: field Amount: negative big int values are not supported"
 		require.PanicsWithError(t, msg, func() { txn.GetHash() })
 	})
+
+	t.Run("BurnTokens", func(t *testing.T) {
+		txn := new(Transaction)
+		txn.Header.Principal = AccountUrl("foo")
+		txn.Body = &BurnTokens{Amount: *big.NewInt(-10)}
+		msg := "field To: failed to marshal field: field Amount: negative big int values are not supported"
+		require.PanicsWithError(t, msg, func() { txn.GetHash() })
+	})
+
+	// t.Run("CreateToken", func(t *testing.T) {
+	// 	txn := new(Transaction)
+	// 	txn.Header.Principal = AccountUrl("foo")
+	// 	txn.Body = &CreateToken{Url: AccountUrl("bar"), SupplyLimit: big.NewInt(-10), Symbol: "BAR", Precision: 8}
+	// 	msg := "field To: failed to marshal field: field SupplyLimit: negative big int values are not supported"
+	// 	require.PanicsWithError(t, msg, func() { txn.GetHash() })
+	// })
 }
