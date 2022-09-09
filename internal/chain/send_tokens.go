b6614cc0483cc2422cc9dbfb3b5c8955c6e8f224
Abhishek Ranjan <rabhishek822@gmail.com>
2022-09-09 16:34:59 +0000

    chore(prot): negative values check [AC-3123]
    Closes AC-3123. negative values check
    
    ## Review Checklist
    
    **If any item is not complete, the merge request is not ready to be reviewed and must be marked `Draft:`.**
    
    - [ ] The merge request title is in the format `<change type>(<change scope>): <short description> [<task id>]`
      - For example, `feat(cli): add QR code generation [AC-123]`
      - For details, see [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [ ] The description includes `Closes <jira task ID>` (or rarely `Updates <jira task ID>`)
    - [ ] The change is fully validated by tests that are run during CI
      - In most cases this means a test in "validate.sh"
      - In some cases, a Go test may be acceptable
      - Validation is not applicable to things like documentation updates
      - Purely UI/UX changes can be manually validated, such as changes to human-readable output
      - For all other changes, automated validation tests are an absolute requirement unless a maintainer specifically explains why they are not in a comment on this merge request
    - [ ] The change is marked with one of the validation labels
      - ~"validation::ci/cd" for changes validated by CI tests
      - ~"validation::manual" for changes validated by hand
      - ~"validation::deferred" for changes validated by a follow up merge request
      - ~"validation::not applicable" for changes where validation is not applicable
    
    ## Merge Checklist
    
    - [ ] CI is passing
    - [ ] Merge conflicts are resolved
    - [ ] All discussions are resolved
    
    Related to AC-3123

--- internal/chain/send_tokens.go
+++ internal/chain/send_tokens.go
@@ -38,6 +38,9 @@ func (SendTokens) Validate(st *StateManager, tx *Delivery) (protocol.Transaction
 	//now check to see if the account is good to send tokens from
 	total := new(big.Int)
 	for _, to := range body.To {
+		if to.Amount.Sign() < 0 {
+			return nil, fmt.Errorf("amount can't be a negative value")
+		}
 		total.Add(total, &to.Amount)
 	}
 
