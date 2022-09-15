8b71a14e6bf22d24b1cc1531d6af27c680db49d5
Ethan Reesor <ethan.reesor@gmail.com>
2022-09-14 19:15:58 -0500

    ci

--- test/helpers/state_manager.go
+++ test/helpers/state_manager.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"gitlab.com/accumulatenetwork/accumulate/config"
 	"gitlab.com/accumulatenetwork/accumulate/internal/chain"
+	"gitlab.com/accumulatenetwork/accumulate/internal/core"
 	"gitlab.com/accumulatenetwork/accumulate/internal/database"
 	"gitlab.com/accumulatenetwork/accumulate/protocol"
 )
@@ -18,7 +19,11 @@ type T interface {
 
 func NewStateManager(testName string, db database.Beginner, transaction *protocol.Transaction) *chain.StateManager {
 	net := &config.Describe{PartitionId: strings.ReplaceAll(strings.ReplaceAll(testName, "/", "-"), "#", "-")}
-	return chain.NewStateManager(net, nil, db.Begin(true), nil, transaction, nil)
+	m := chain.NewStateManager(net, nil, db.Begin(true), nil, transaction, nil)
+	m.Globals = new(core.GlobalValues)
+	m.Globals.Oracle = new(protocol.AcmeOracle)
+	m.Globals.Oracle.Price = protocol.InitialAcmeOracleValue
+	return m
 }
 
 func LoadStateManager(t T, db database.Beginner, transaction *protocol.Transaction) *chain.StateManager {
