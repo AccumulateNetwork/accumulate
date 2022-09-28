dd65419c52cc4d915f00b02383e57218eafaa22a
Ethan Reesor <ethan.reesor@gmail.com>
2022-09-28 14:51:58 +0000

    chore(all): export more types
    Closes DO-58. Exports more of `internal/api/v2`, some of `internal/api/v2/query`, all of `internal/errors`, and most of `internal/encoding`. There are no functional changes; just moving code around, creating shims to avoid renaming imports, and dropping the `Status` prefix from error codes.
    
    ## Review Checklist
    
    **If any item is not complete, the merge request is not ready to be reviewed and must be marked `Draft:`.**
    
    - [x] The merge request title is in the format `<change type>(<change scope>): <short description> [<task id>]`
      - For example, `feat(cli): add QR code generation [AC-123]`
      - For details, see [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [x] The description includes `Closes <jira task ID>` (or rarely `Updates <jira task ID>`)
    - [~] The change is fully validated by tests that are run during CI
      - In most cases this means a test in "validate.sh"
      - In some cases, a Go test may be acceptable
      - Validation is not applicable to things like documentation updates
      - Purely UI/UX changes can be manually validated, such as changes to human-readable output
      - For all other changes, automated validation tests are an absolute requirement unless a maintainer specifically explains why they are not in a comment on this merge request
    - [x] The change is marked with one of the validation labels
      - ~"validation::ci/cd" for changes validated by CI tests
      - ~"validation::manual" for changes validated by hand
      - ~"validation::deferred" for changes validated by a follow up merge request
      - ~"validation::not applicable" for changes where validation is not applicable
    
    ## Merge Checklist
    
    - [ ] CI is passing
    - [ ] Merge conflicts are resolved
    - [ ] All discussions are resolved
    
    Related to DO-58

--- internal/errors/well_known.go
+++ internal/errors/well_known.go
@@ -1,10 +1,5 @@
 package errors
 
-// Error implements error.
-func (s Status) Error() string {
-	return s.String()
-}
-
 func NotFound(format string, args ...interface{}) error {
 	return Format(StatusNotFound, format, args...)
 }
