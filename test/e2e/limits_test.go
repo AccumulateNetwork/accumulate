748a72ca613adcbb3e586cff8d56ed11eb60c8d4
Ethan Reesor <ethan.reesor@gmail.com>
2022-09-28 14:41:39 +0000

    fix(prot): add limits [AC-3202]
    Closes AC-3202. Limit the number of authorities of an account, pages of a book, entries in a page, and parts in a data entry.
    
    ## Review Checklist
    
    **If any item is not complete, the merge request is not ready to be reviewed and must be marked `Draft:`.**
    
    - [x] The merge request title is in the format `<change type>(<change scope>): <short description> [<task id>]`
      - For example, `feat(cli): add QR code generation [AC-123]`
      - For details, see [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [x] The description includes `Closes <jira task ID>` (or rarely `Updates <jira task ID>`)
    - [x] The change is fully validated by tests that are run during CI
      - In most cases this means a test in "validate.sh"
      - In some cases, a Go test may be acceptable
      - Validation is not applicable to things like documentation updates
      - Purely UI/UX changes can be manually validated, such as changes to human-readable output
      - For all other changes, automated validation tests are an absolute requirement unless a maintainer specifically explains why they are not in a comment on this merge request
    - [x] The change is marked with one of the validation labels
      - ~"validation::ci/cd" for changes validated by CI tests
      - ~"validation::manual" for changes validated by hand
      - ~"validation::deferred" for changes validated by a follow up merge request
      - ~"validation::not applicable" for changes where validation is not applicable
    
    ## Merge Checklist
    
    - [ ] CI is passing
    - [ ] Merge conflicts are resolved
    - [ ] All discussions are resolved
    
    Related to AC-3202

---
+++ test/e2e/limits_test.go
@@ -0,0 +1,429 @@
+package e2e
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gitlab.com/accumulatenetwork/accumulate/internal/core"
+	"gitlab.com/accumulatenetwork/accumulate/internal/database"
+	acctesting "gitlab.com/accumulatenetwork/accumulate/internal/testing"
+	"gitlab.com/accumulatenetwork/accumulate/pkg/url"
+	. "gitlab.com/accumulatenetwork/accumulate/protocol"
+	. "gitlab.com/accumulatenetwork/accumulate/test/helpers"
+	"gitlab.com/accumulatenetwork/accumulate/test/simulator"
+)
+
+func urlSliceStrings(v []*url.URL) []string {
+	s := make([]string, len(v))
+	for i, v := range v {
+		s[i] = v.String()
+	}
+	return s
+}
+
+func TestCreateKeyPage_LimitBookPages(t *testing.T) {
+	alice := AccountUrl("alice")
+	aliceKey := acctesting.GenerateKey(alice)
+
+	globals := new(core.GlobalValues)
+	globals.Globals = new(NetworkGlobals)
+	globals.Globals.Limits = new(NetworkLimits)
+	globals.Globals.Limits.BookPages = 1
+
+	// Initialize
+	sim := NewSim(t,
+		simulator.MemoryDatabase,
+		simulator.SimpleNetwork(t.Name(), 3, 3),
+		simulator.GenesisWith(GenesisTime, globals),
+	)
+
+	MakeIdentity(t, sim.DatabaseFor(alice), alice, aliceKey[32:])
+	CreditCredits(t, sim.DatabaseFor(alice), alice.JoinPath("book", "1"), 1e9)
+
+	// Execute
+	st := sim.Submit(
+		acctesting.NewTransaction().
+			WithPrincipal(alice.JoinPath("book")).
+			WithSigner(alice.JoinPath("book", "1"), 1).
+			WithTimestamp(1).
+			WithBody(&CreateKeyPage{Keys: []*KeySpecParams{{KeyHash: hash([]byte{1})}}}).
+			Initiate(SignatureTypeED25519, aliceKey).
+			BuildDelivery())
+	require.NotNil(t, st.Error)
+	require.EqualError(t, st.Error, "book will have too many pages")
+}
+
+func TestCreateKeyPage_LimitPageEntries(t *testing.T) {
+	alice := AccountUrl("alice")
+	aliceKey := acctesting.GenerateKey(alice)
+
+	globals := new(core.GlobalValues)
+	globals.Globals = new(NetworkGlobals)
+	globals.Globals.Limits = new(NetworkLimits)
+	globals.Globals.Limits.PageEntries = 1
+
+	// Initialize
+	sim := NewSim(t,
+		simulator.MemoryDatabase,
+		simulator.SimpleNetwork(t.Name(), 3, 3),
+		simulator.GenesisWith(GenesisTime, globals),
+	)
+
+	MakeIdentity(t, sim.DatabaseFor(alice), alice, aliceKey[32:])
+	CreditCredits(t, sim.DatabaseFor(alice), alice.JoinPath("book", "1"), 1e9)
+
+	// Execute
+	st := sim.Submit(
+		acctesting.NewTransaction().
+			WithPrincipal(alice.JoinPath("book")).
+			WithSigner(alice.JoinPath("book", "1"), 1).
+			WithTimestamp(1).
+			WithBody(&CreateKeyPage{Keys: []*KeySpecParams{{KeyHash: hash([]byte{1})}, {KeyHash: hash([]byte{2})}}}).
+			Initiate(SignatureTypeED25519, aliceKey).
+			BuildDelivery())
+	require.NotNil(t, st.Error)
+	require.EqualError(t, st.Error, "page will have too many entries")
+}
+
+func TestUpdateKeyPage_LimitPageEntries(t *testing.T) {
+	alice := AccountUrl("alice")
+	aliceKey := acctesting.GenerateKey(alice)
+
+	globals := new(core.GlobalValues)
+	globals.Globals = new(NetworkGlobals)
+	globals.Globals.Limits = new(NetworkLimits)
+	globals.Globals.Limits.PageEntries = 1
+
+	// Initialize
+	sim := NewSim(t,
+		simulator.MemoryDatabase,
+		simulator.SimpleNetwork(t.Name(), 3, 3),
+		simulator.GenesisWith(GenesisTime, globals),
+	)
+
+	MakeIdentity(t, sim.DatabaseFor(alice), alice, aliceKey[32:])
+	CreditCredits(t, sim.DatabaseFor(alice), alice.JoinPath("book", "1"), 1e9)
+
+	// Execute
+	st := sim.Submit(
+		acctesting.NewTransaction().
+			WithPrincipal(alice.JoinPath("book", "1")).
+			WithSigner(alice.JoinPath("book", "1"), 1).
+			WithTimestamp(1).
+			WithBody(&UpdateKeyPage{Operation: []KeyPageOperation{
+				&AddKeyOperation{Entry: KeySpecParams{KeyHash: hash([]byte{1})}},
+			}}).
+			Initiate(SignatureTypeED25519, aliceKey).
+			BuildDelivery())
+	require.NotNil(t, st.Error)
+	require.EqualError(t, st.Error, "page will have too many entries")
+}
+
+func TestUpdateAccountAuth_LimitAccountAuthorities(t *testing.T) {
+	alice := AccountUrl("alice")
+	aliceKey := acctesting.GenerateKey(alice)
+
+	globals := new(core.GlobalValues)
+	globals.Globals = new(NetworkGlobals)
+	globals.Globals.Limits = new(NetworkLimits)
+	globals.Globals.Limits.AccountAuthorities = 1
+
+	// Initialize
+	sim := NewSim(t,
+		simulator.MemoryDatabase,
+		simulator.SimpleNetwork(t.Name(), 3, 3),
+		simulator.GenesisWith(GenesisTime, globals),
+	)
+
+	MakeIdentity(t, sim.DatabaseFor(alice), alice, aliceKey[32:])
+	CreditCredits(t, sim.DatabaseFor(alice), alice.JoinPath("book", "1"), 1e9)
+	MakeKeyBook(t, sim.DatabaseFor(alice), alice.JoinPath("book2"), aliceKey[32:])
+	CreditCredits(t, sim.DatabaseFor(alice), alice.JoinPath("book2", "1"), 1e9)
+
+	// Execute
+	st := sim.Submit(
+		acctesting.NewTransaction().
+			WithPrincipal(alice).
+			WithSigner(alice.JoinPath("book", "1"), 1).
+			WithTimestamp(1).
+			WithBody(&UpdateAccountAuth{Operations: []AccountAuthOperation{
+				&AddAccountAuthorityOperation{Authority: alice.JoinPath("book2")},
+			}}).
+			Initiate(SignatureTypeED25519, aliceKey).
+			WithSigner(alice.JoinPath("book2", "1"), 1).
+			Sign(SignatureTypeED25519, aliceKey).
+			BuildDelivery())
+	require.NotNil(t, st.Error)
+	require.EqualError(t, st.Error, "account will have too many authorities")
+}
+
+func TestWriteData_LimitDataEntryParts(t *testing.T) {
+	alice := AccountUrl("alice")
+	aliceKey := acctesting.GenerateKey(alice)
+
+	globals := new(core.GlobalValues)
+	globals.Globals = new(NetworkGlobals)
+	globals.Globals.Limits = new(NetworkLimits)
+	globals.Globals.Limits.DataEntryParts = 1
+
+	// Initialize
+	sim := NewSim(t,
+		simulator.MemoryDatabase,
+		simulator.SimpleNetwork(t.Name(), 3, 3),
+		simulator.GenesisWith(GenesisTime, globals),
+	)
+
+	MakeIdentity(t, sim.DatabaseFor(alice), alice, aliceKey[32:])
+	CreditCredits(t, sim.DatabaseFor(alice), alice.JoinPath("book", "1"), 1e9)
+	MakeAccount(t, sim.DatabaseFor(alice), &DataAccount{Url: alice.JoinPath("data")})
+
+	// Execute
+	entry := new(AccumulateDataEntry)
+	entry.Data = [][]byte{{1}, {2}}
+	st := sim.Submit(
+		acctesting.NewTransaction().
+			WithPrincipal(alice.JoinPath("data")).
+			WithSigner(alice.JoinPath("book", "1"), 1).
+			WithTimestamp(1).
+			WithBody(&WriteData{Entry: entry}).
+			Initiate(SignatureTypeED25519, aliceKey).
+			BuildDelivery())
+	require.NotNil(t, st.Error)
+	require.EqualError(t, st.Error, "data entry contains too many parts")
+}
+
+func TestCreateIdentity_LimitIdentityAccounts(t *testing.T) {
+	alice := AccountUrl("alice")
+	aliceKey := acctesting.GenerateKey(alice)
+
+	globals := new(core.GlobalValues)
+	globals.Globals = new(NetworkGlobals)
+	globals.Globals.Limits = new(NetworkLimits)
+	globals.Globals.Limits.IdentityAccounts = 1
+
+	// Initialize
+	sim := NewSim(t,
+		simulator.MemoryDatabase,
+		simulator.SimpleNetwork(t.Name(), 3, 3),
+		simulator.GenesisWith(GenesisTime, globals),
+	)
+
+	MakeIdentity(t, sim.DatabaseFor(alice), alice, aliceKey[32:])
+	CreditCredits(t, sim.DatabaseFor(alice), alice.JoinPath("book", "1"), 1e9)
+
+	// Execute
+	st := sim.Submit(
+		acctesting.NewTransaction().
+			WithPrincipal(alice).
+			WithSigner(alice.JoinPath("book", "1"), 1).
+			WithTimestamp(1).
+			WithBody(&CreateIdentity{Url: alice.JoinPath("account"), KeyHash: make([]byte, 32), KeyBookUrl: alice.JoinPath("account", "book")}).
+			Initiate(SignatureTypeED25519, aliceKey).
+			BuildDelivery())
+	require.NotNil(t, st.Error)
+	require.EqualError(t, st.Error, "identity would have too many accounts")
+}
+
+// TestCreateIdentity_Directory verifies that CreateIdentity correctly populates
+// the directory index because that impacts the enforcement of the account
+// limit.
+func TestCreateIdentity_Directory(t *testing.T) {
+	t.Run("Root", func(t *testing.T) {
+		alice := AccountUrl("alice")
+		liteKey := acctesting.GenerateKey("lite")
+		lite := acctesting.AcmeLiteAddressStdPriv(liteKey)
+
+		// Initialize
+		sim := NewSim(t,
+			simulator.MemoryDatabase,
+			simulator.SimpleNetwork(t.Name(), 3, 3),
+			simulator.Genesis(GenesisTime),
+		)
+
+		MakeLiteTokenAccount(t, sim.DatabaseFor(lite), liteKey[32:], AcmeUrl())
+		CreditCredits(t, sim.DatabaseFor(lite), lite.RootIdentity(), 1e9)
+
+		// Execute
+		st := sim.SubmitSuccessfully(
+			acctesting.NewTransaction().
+				WithPrincipal(alice).
+				WithSigner(lite, 1).
+				WithTimestamp(1).
+				WithBody(&CreateIdentity{Url: alice, KeyHash: make([]byte, 32), KeyBookUrl: alice.JoinPath("book")}).
+				Initiate(SignatureTypeED25519, liteKey).
+				BuildDelivery())
+
+		sim.StepUntil(
+			Txn(st.TxID).Succeeds())
+
+		// Make sure CreateIdentity doesn't add the ADI to its own directory
+		View(t, sim.DatabaseFor(alice), func(batch *database.Batch) {
+			dir, err := batch.Account(alice).Directory().Get()
+			require.NoError(t, err)
+			require.EqualValues(t, []string{alice.JoinPath("book").String()}, urlSliceStrings(dir))
+		})
+	})
+
+	t.Run("Sub", func(t *testing.T) {
+		alice := AccountUrl("alice")
+		aliceKey := acctesting.GenerateKey(alice)
+
+		// Initialize
+		sim := NewSim(t,
+			simulator.MemoryDatabase,
+			simulator.SimpleNetwork(t.Name(), 3, 3),
+			simulator.Genesis(GenesisTime),
+		)
+
+		MakeIdentity(t, sim.DatabaseFor(alice), alice, aliceKey[32:])
+		CreditCredits(t, sim.DatabaseFor(alice), alice.JoinPath("book", "1"), 1e9)
+
+		// Execute
+		st := sim.SubmitSuccessfully(
+			acctesting.NewTransaction().
+				WithPrincipal(alice).
+				WithSigner(alice.JoinPath("book", "1"), 1).
+				WithTimestamp(1).
+				WithBody(&CreateIdentity{Url: alice.JoinPath("account"), KeyHash: make([]byte, 32), KeyBookUrl: alice.JoinPath("account", "book")}).
+				Initiate(SignatureTypeED25519, aliceKey).
+				BuildDelivery())
+
+		sim.StepUntil(
+			Txn(st.TxID).Succeeds())
+
+		// Make sure CreateIdentity doesn't add the ADI to its own directory
+		View(t, sim.DatabaseFor(alice), func(batch *database.Batch) {
+			dir, err := batch.Account(alice.JoinPath("account")).Directory().Get()
+			require.NoError(t, err)
+			require.EqualValues(t, []string{alice.JoinPath("account", "book").String()}, urlSliceStrings(dir))
+		})
+	})
+}
+
+func TestCreateTokenAccount_LimitIdentityAccounts(t *testing.T) {
+	alice := AccountUrl("alice")
+	aliceKey := acctesting.GenerateKey(alice)
+
+	globals := new(core.GlobalValues)
+	globals.Globals = new(NetworkGlobals)
+	globals.Globals.Limits = new(NetworkLimits)
+	globals.Globals.Limits.IdentityAccounts = 1
+
+	// Initialize
+	sim := NewSim(t,
+		simulator.MemoryDatabase,
+		simulator.SimpleNetwork(t.Name(), 3, 3),
+		simulator.GenesisWith(GenesisTime, globals),
+	)
+
+	MakeIdentity(t, sim.DatabaseFor(alice), alice, aliceKey[32:])
+	CreditCredits(t, sim.DatabaseFor(alice), alice.JoinPath("book", "1"), 1e9)
+
+	// Execute
+	st := sim.Submit(
+		acctesting.NewTransaction().
+			WithPrincipal(alice).
+			WithSigner(alice.JoinPath("book", "1"), 1).
+			WithTimestamp(1).
+			WithBody(&CreateTokenAccount{Url: alice.JoinPath("account"), TokenUrl: AcmeUrl()}).
+			Initiate(SignatureTypeED25519, aliceKey).
+			BuildDelivery())
+	require.NotNil(t, st.Error)
+	require.EqualError(t, st.Error, "identity would have too many accounts")
+}
+
+func TestCreateDataAccount_LimitIdentityAccounts(t *testing.T) {
+	alice := AccountUrl("alice")
+	aliceKey := acctesting.GenerateKey(alice)
+
+	globals := new(core.GlobalValues)
+	globals.Globals = new(NetworkGlobals)
+	globals.Globals.Limits = new(NetworkLimits)
+	globals.Globals.Limits.IdentityAccounts = 1
+
+	// Initialize
+	sim := NewSim(t,
+		simulator.MemoryDatabase,
+		simulator.SimpleNetwork(t.Name(), 3, 3),
+		simulator.GenesisWith(GenesisTime, globals),
+	)
+
+	MakeIdentity(t, sim.DatabaseFor(alice), alice, aliceKey[32:])
+	CreditCredits(t, sim.DatabaseFor(alice), alice.JoinPath("book", "1"), 1e9)
+
+	// Execute
+	st := sim.Submit(
+		acctesting.NewTransaction().
+			WithPrincipal(alice).
+			WithSigner(alice.JoinPath("book", "1"), 1).
+			WithTimestamp(1).
+			WithBody(&CreateDataAccount{Url: alice.JoinPath("account")}).
+			Initiate(SignatureTypeED25519, aliceKey).
+			BuildDelivery())
+	require.NotNil(t, st.Error)
+	require.EqualError(t, st.Error, "identity would have too many accounts")
+}
+
+func TestCreateToken_LimitIdentityAccounts(t *testing.T) {
+	alice := AccountUrl("alice")
+	aliceKey := acctesting.GenerateKey(alice)
+
+	globals := new(core.GlobalValues)
+	globals.Globals = new(NetworkGlobals)
+	globals.Globals.Limits = new(NetworkLimits)
+	globals.Globals.Limits.IdentityAccounts = 1
+
+	// Initialize
+	sim := NewSim(t,
+		simulator.MemoryDatabase,
+		simulator.SimpleNetwork(t.Name(), 3, 3),
+		simulator.GenesisWith(GenesisTime, globals),
+	)
+
+	MakeIdentity(t, sim.DatabaseFor(alice), alice, aliceKey[32:])
+	CreditCredits(t, sim.DatabaseFor(alice), alice.JoinPath("book", "1"), 1e9)
+
+	// Execute
+	st := sim.Submit(
+		acctesting.NewTransaction().
+			WithPrincipal(alice).
+			WithSigner(alice.JoinPath("book", "1"), 1).
+			WithTimestamp(1).
+			WithBody(&CreateToken{Url: alice.JoinPath("account")}).
+			Initiate(SignatureTypeED25519, aliceKey).
+			BuildDelivery())
+	require.NotNil(t, st.Error)
+	require.EqualError(t, st.Error, "identity would have too many accounts")
+}
+
+func TestCreateKeyBook_LimitIdentityAccounts(t *testing.T) {
+	alice := AccountUrl("alice")
+	aliceKey := acctesting.GenerateKey(alice)
+
+	globals := new(core.GlobalValues)
+	globals.Globals = new(NetworkGlobals)
+	globals.Globals.Limits = new(NetworkLimits)
+	globals.Globals.Limits.IdentityAccounts = 1
+
+	// Initialize
+	sim := NewSim(t,
+		simulator.MemoryDatabase,
+		simulator.SimpleNetwork(t.Name(), 3, 3),
+		simulator.GenesisWith(GenesisTime, globals),
+	)
+
+	MakeIdentity(t, sim.DatabaseFor(alice), alice, aliceKey[32:])
+	CreditCredits(t, sim.DatabaseFor(alice), alice.JoinPath("book", "1"), 1e9)
+
+	// Execute
+	st := sim.Submit(
+		acctesting.NewTransaction().
+			WithPrincipal(alice).
+			WithSigner(alice.JoinPath("book", "1"), 1).
+			WithTimestamp(1).
+			WithBody(&CreateKeyBook{Url: alice.JoinPath("account"), PublicKeyHash: make([]byte, 32)}).
+			Initiate(SignatureTypeED25519, aliceKey).
+			BuildDelivery())
+	require.NotNil(t, st.Error)
+	require.EqualError(t, st.Error, "identity would have too many accounts")
+}
