6fc3af1dac2a12ddf924e4d2b2ad797e2f8c4b4d
jjhbk <jp@ph.iitr.ac.in>
2022-09-28 17:04:02 +0530

    remove redundant function

--- cmd/accumulate/cmd/follower.go
+++ cmd/accumulate/cmd/follower.go
@@ -1,11 +1,7 @@
 package cmd
 
 import (
-	"context"
-	"errors"
-
 	"github.com/spf13/cobra"
-	"gitlab.com/accumulatenetwork/accumulate/internal/api/v2"
 	"gitlab.com/accumulatenetwork/accumulate/internal/build"
 	"gitlab.com/accumulatenetwork/accumulate/internal/core"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/client/signing"
@@ -27,61 +23,22 @@ var followerCmd = &cobra.Command{
 var followerAddCmd = &cobra.Command{
 	Use:   "add [partition ID] [key name[@key book or page]] [key name or path]",
 	Short: "Add a follower",
-	Run:   runFolCmdFunc(addFollower),
+	Run:   runValCmdFunc(addFollower),
 	Args:  cobra.RangeArgs(3, 5),
 }
 
 var followerRemoveCmd = &cobra.Command{
 	Use:   "remove [partition ID] [key name[@key book or page]] [key name or path]",
 	Short: "Remove a follower",
-	Run:   runFolCmdFunc(removeFollower),
+	Run:   runValCmdFunc(removeFollower),
 	Args:  cobra.RangeArgs(3, 5),
 }
 
 var followerUpdateKeyCmd = &cobra.Command{
 	Use:   "update-key [partition ID] [key name[@key book or page]] [old key name or path] [new key name or path]",
 	Short: "Update a follower's key",
-	Run:   runFolCmdFunc(updateFollowerKey),
+	Run:   runValCmdFunc(updateFollowerKey),
 	Args:  cobra.RangeArgs(4, 6),
-}
-
-func runFolCmdFunc(fn func(values *core.GlobalValues, pageCount int, signer []*signing.Builder, partition string, args []string) (*protocol.Envelope, error)) func(cmd *cobra.Command, args []string) {
-	return runCmdFunc(func(args []string) (string, error) {
-		describe, err := Client.Describe(context.Background())
-		if err != nil {
-			return PrintJsonRpcError(err)
-		}
-		if describe.Values.Globals == nil {
-			return "", errors.New("cannot determine the network's global values")
-		}
-
-		req := new(api.GeneralQuery)
-		req.Url = protocol.DnUrl().JoinPath(protocol.Operators, "1")
-		resp := new(api.ChainQueryResponse)
-		page := new(protocol.KeyPage)
-		resp.Data = page
-		err = Client.RequestAPIv2(context.Background(), "query", req, resp)
-		if err != nil {
-			return PrintJsonRpcError(err)
-		}
-
-		partition := args[0]
-		if partition == "dn" {
-			partition = protocol.Directory
-		}
-		args[0] = protocol.DnUrl().JoinPath(protocol.Operators, "1").String()
-		args, principal, signers, err := parseArgsAndPrepareSigner(args)
-		if err != nil {
-			return "", err
-		}
-
-		env, err := fn(&describe.Values, len(page.Keys), signers, partition, args)
-		if err != nil {
-			return "", err
-		}
-
-		return dispatchTxAndPrintResponse(env, principal, nil)
-	})
 }
 
 func addFollower(values *core.GlobalValues, pageCount int, signers []*signing.Builder, _ string, args []string) (*protocol.Envelope, error) {
