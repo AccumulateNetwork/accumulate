7e9dc55153c31c1e306afb97ff1c8d5a695c3703
paul snow <snow.paul@gmail.com>
2022-09-30 08:07:12 -0500

    update

--- tools/cmd/staking/sim/url_gen_test.go
+++ tools/cmd/staking/sim/url_gen_test.go
@@ -12,20 +12,19 @@ func TestGenUrl(t *testing.T) {
 	}
 }
 
-
 func TestGenerateInitializationScript(t *testing.T) {
-	fmt.Println ("lta=acc://c83b1ed6b8b6795d3c224dab50a544e2306d743866835260/ACME")
-	fmt.Printf("for i in {0..5}\ndo\n   echo asdfasdf | accumulate faucet $lta\ndone\n")	// accumulate credits [origin token account] [key page or lite identity url] [number of credits wanted] [max acme to spend] [percent slippage (optional)] [flags][BS2]
+	fmt.Println("lta=acc://c83b1ed6b8b6795d3c224dab50a544e2306d743866835260/ACME")
+	fmt.Printf("for i in {0..10}\ndo\n   echo asdfasdf | accumulate faucet $lta\ndone\n") // accumulate credits [origin token account] [key page or lite identity url] [number of credits wanted] [max acme to spend] [percent slippage (optional)] [flags][BS2]
 	fmt.Print("echo asdfasdf | accumulate credits $lta $lta 500000\n\n")
 	for i := 0; i < 1; i++ {
 		adi, url := GenUrls("StakingAccount")
-		fmt.Printf("echo asdfasdf | accumulate adi create $lta %s masterkey\n",adi)
+		fmt.Printf("echo asdfasdf | accumulate adi create $lta %s masterkey\n", adi)
 		// accumulate account create token [actor adi] [signing key name] [key index (optional)] [key height (optional)] [new token account url] [tokenUrl] [keyBook (optional)] [flags]
 		// ./accumulate account create token acc://DefiDevs.acme Key1 acc://DefiDevs.acme/Tokens acc://ACME acc://DefiDevs.acme/Keybook
-
-		fmt.Printf("echo asdfasdf | accumulate account create token $lta masterkey %s\n",url)
-
+		fmt.Printf("echo asdfasdf | accumulate account create token %s masterkey %s acc://acme\n", adi, url)
+		// accumulate tx create [origin url] [signing key name] [key index (optional)] [key height (optional)] [to] [amount] Create new token tx
+		// ./accumulate tx create acc://27259b5544176ede283ba745b5a6d1775a281ad2f28abc3d/ACME acc://DefiDevs.acme/Tokens 100
+		fmt.Printf("echo asdfasdf | accumulate tx create $lta %s %d\n", url, 40)
 		fmt.Println("")
 	}
 }
-
