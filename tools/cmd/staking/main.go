c1b83348c6a2256f92f808d06d297ca0a48dd4da
paul snow <snow.paul@gmail.com>
2022-10-03 17:04:34 -0500

    fix: (staking) update flags and network

--- tools/cmd/staking/main.go
+++ tools/cmd/staking/main.go
@@ -16,7 +16,7 @@ import (
 
 var flagDebug = flag.Bool("debug", false, "Debug API requests")
 var flagSim = flag.Bool("sim", false, "Use the simulator")
-var flagNet = flag.String("net", "https://testnet.accumulatenetwork.io/v2", "The network to run against")
+var strNet = flag.String("net", "https://testnet.accumulatenetwork.io/v2", "The network to run against")
 
 func main() {
 	flag.Parse()
@@ -35,7 +35,7 @@ func main() {
 		return
 	}
 
-	net, err := network.New("http://127.0.1.1:26660/v2", protocol.AccountUrl("staking.acme", "parameters"))
+	net, err := network.New(*strNet, protocol.AccountUrl("staking.acme", "parameters"))
 	if err != nil {
 		log.Fatal(err)
 	}
