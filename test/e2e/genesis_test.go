4c68897f1e1c017d699933c39cafe6914966d36c
Ethan Reesor <ethan.reesor@gmail.com>
2022-10-07 17:28:07 -0500

    test distribution

--- test/e2e/genesis_test.go
+++ test/e2e/genesis_test.go
@@ -1,15 +1,48 @@
 package e2e
 
 import (
+	"math"
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"gitlab.com/accumulatenetwork/accumulate/internal/core"
+	"gitlab.com/accumulatenetwork/accumulate/internal/routing"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/url"
 	. "gitlab.com/accumulatenetwork/accumulate/protocol"
 	. "gitlab.com/accumulatenetwork/accumulate/test/helpers"
 	"gitlab.com/accumulatenetwork/accumulate/test/simulator"
 )
+
+func TestActivationRoutes(t *testing.T) {
+	g := new(core.GlobalValues)
+	g.Routing = new(RoutingTable)
+	g.Routing.Routes = []Route{
+		{Partition: "A", Length: 2, Value: 0},  // 00
+		{Partition: "B", Length: 2, Value: 1},  // 01
+		{Partition: "C", Length: 2, Value: 2},  // 10
+		{Partition: "A", Length: 3, Value: 6},  // 110
+		{Partition: "B", Length: 4, Value: 14}, // 1110
+		{Partition: "C", Length: 4, Value: 15}, // 1111
+	}
+
+	r, err := routing.NewRouteTree(g.Routing)
+	require.NoError(t, err)
+
+	total := 0
+	count := map[string]int{}
+	for i := 0; i < 1e6; i++ {
+		s, err := r.RouteNr(rand.Uint64())
+		require.NoError(t, err)
+		total++
+		count[s]++
+	}
+
+	// Values are within 1% of expectations
+	require.Less(t, math.Abs(float64(count["A"])/float64(total)-0.3750), 0.01)
+	require.Less(t, math.Abs(float64(count["B"])/float64(total)-0.3125), 0.01)
+	require.Less(t, math.Abs(float64(count["C"])/float64(total)-0.3125), 0.01)
+}
 
 func TestGenesisRouting(t *testing.T) {
 	g := new(core.GlobalValues)
