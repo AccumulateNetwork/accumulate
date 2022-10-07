56d08037df91b3cbdaa2c6e626df6ae8c3b579d0
Paul Snow <snow.paul@gmail.com>
2022-10-07 23:06:00 +0000

    merge: branch 'AC-3241-genesis-routing' into 'release-1.0'
    feat(node): make genesis routing controllable [AC-3241]
    
    See merge request accumulatenetwork/accumulate!616
    
    (cherry picked from commit 550b00c0cf6d38197481241fe9b525738fc672a9)
    
    eda9483e feat(node): make genesis routing controllable
    4c68897f test distribution

--- cmd/accumulated/cmd_init.go
+++ cmd/accumulated/cmd_init.go
@@ -162,7 +162,7 @@ func initInitFlags() {
 	cmdInitDevnet.Flags().BoolVar(&flagInitDevnet.UseVolumes, "use-volumes", false, "Use Docker volumes instead of a local directory")
 	cmdInitDevnet.Flags().BoolVar(&flagInitDevnet.Compose, "compose", false, "Only write the Docker Compose file, do not write the configuration files")
 	cmdInitDevnet.Flags().StringVar(&flagInitDevnet.DnsSuffix, "dns-suffix", "", "DNS suffix to add to hostnames used when initializing dockerized nodes")
-	cmdInitDevnet.Flags().StringVar(&flagInitDevnet.Globals, "globals", "", "Override network globals")
+	cmdInit.PersistentFlags().StringVar(&flagInitDevnet.Globals, "globals", "", "Override network globals")
 }
 
 func init() {
