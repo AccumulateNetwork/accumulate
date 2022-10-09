d319e4d31eab9beaf52ae54df8a4b543744c29a9
Ethan Reesor <ethan.reesor@gmail.com>
2022-10-09 17:47:09 +0000

    feat(tools): add a tool to resend an anchor [DO-62]
    Closes DO-62. Adds a tool to make it easy to resend an anchor signature.
    
    ## Review Checklist
    
    **If any item is not complete, the merge request is not ready to be reviewed and must be marked `Draft:`.**
    
    - [x] The merge request title is in the format `<change type>(<change scope>): <short description> [<task id>]`
      - For example, `feat(cli): add QR code generation [AC-123]`
      - For details, see [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [x] The description includes `Closes <jira task ID>` (or rarely `Updates <jira task ID>`)
    - [~] The change is fully validated by tests that are run during CI
      - In most cases this means a test in "validate.sh"
      - In some cases, a Go test may be acceptable
      - Validation is not applicable to things like documentation updates
      - Purely UI/UX changes can be manually validated, such as changes to human-readable output
      - For all other changes, automated validation tests are an absolute requirement unless a maintainer specifically explains why they are not in a comment on this merge request
    - [x] The change is marked with one of the validation labels
      - ~"validation::ci/cd" for changes validated by CI tests
      - ~"validation::manual" for changes validated by hand
      - ~"validation::deferred" for changes validated by a follow up merge request
      - ~"validation::not applicable" for changes where validation is not applicable
    
    ## Merge Checklist
    
    - [x] CI is passing
    - [ ] Merge conflicts are resolved
    - [x] All discussions are resolved
    
    Related to DO-62
    
    
    Former-commit-id: 4e5bf519888e63749d48a5b6baf66c87448e1b03

---
+++ tools/cmd/resend-anchor/consensus.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	stdurl "net/url"
+	"os"
+	"regexp"
+	"strconv"
+	"text/tabwriter"
+
+	"github.com/spf13/cobra"
+	"github.com/tendermint/tendermint/rpc/client/http"
+	"gitlab.com/accumulatenetwork/accumulate/config"
+	"gitlab.com/accumulatenetwork/accumulate/protocol"
+)
+
+var consensusCmd = &cobra.Command{
+	Use:   "consensus",
+	Short: "Get the status of consensus",
+	Args:  cobra.MinimumNArgs(1),
+	Run:   consensus,
+}
+
+func init() {
+	cmd.AddCommand(consensusCmd)
+}
+
+func consensus(_ *cobra.Command, args []string) {
+	netu, err := stdurl.Parse(args[0])
+	checkf(err, "network url")
+	netp, err := strconv.ParseUint(netu.Port(), 10, 64)
+	checkf(err, "network url port")
+	netc, err := http.New(fmt.Sprintf("http://%s:%d", netu.Hostname(), netp+uint64(config.PortOffsetTendermintRpc)+config.PortOffsetDirectory))
+	checkf(err, "rpc client")
+	_, nodes := walkNetwork(args)
+
+	r, err := netc.ConsensusState(context.Background())
+	checkf(err, "query consensus state")
+
+	var rs RoundState
+	check(json.Unmarshal(r.RoundState, &rs))
+
+	fmt.Printf("For %s\n", rs.HRS)
+
+	prevotes := readVotes(rs.HeightVoteSet[0].Prevotes)
+	precommits := readVotes(rs.HeightVoteSet[0].Precommits)
+
+	tw := tabwriter.NewWriter(os.Stdout, 2, 4, 1, ' ', 0)
+	defer tw.Flush()
+
+	fmt.Fprintf(tw, "Validator\tAddress\tPrevote\tPrecommit\n")
+	for _, n := range nodes {
+		if n.Info == nil || !n.Info.IsActiveOn(protocol.Directory) {
+			continue
+		}
+
+		var addr string
+		if n.Hostname != "" {
+			addr = fmt.Sprintf("%s:%d", n.Hostname, n.BasePort)
+		}
+
+		var prevote string
+		if prevotes[*(*[6]byte)(n.ValidatorID[:6])] {
+			prevote = "✔"
+		}
+
+		var precommit string
+		if precommits[*(*[6]byte)(n.ValidatorID[:6])] {
+			precommit = "✔"
+		}
+
+		fmt.Fprintf(tw, "%x\t%s\t%s\t%s\n", sliceOrNil(n.ValidatorID), addr, prevote, precommit)
+	}
+}
+
+var reVote = regexp.MustCompile(`^(?i)Vote\{\d+:([\da-f]+) `)
+
+func readVotes(votes []string) map[[6]byte]bool {
+	v := map[[6]byte]bool{}
+	for _, x := range votes {
+		m := reVote.FindStringSubmatch(x)
+		if len(m) < 2 {
+			continue
+		}
+		key, err := hex.DecodeString(m[1])
+		checkf(err, "decode %s", x)
+		v[*(*[6]byte)(key)] = true
+	}
+	return v
+}
+
+type RoundState struct {
+	HRS           string          `json:"height/round/step"`
+	HeightVoteSet []HeightVoteSet `json:"height_vote_set"`
+}
+
+type HeightVoteSet struct {
+	Round      int
+	Prevotes   []string
+	Precommits []string
+}
