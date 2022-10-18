b2dcd5e812aba8aad0df864b062bb666fbffb81b
Ethan Reesor <ethan.reesor@gmail.com>
2022-10-18 20:14:12 +0000

    chore(cli): prepare for separation [DO-65]
    Closes DO-65. Prepares the CLI for separation. Adds a `go.mod` for the CLI and pulls in Accumulate as a dependency, somewhat enforcing separation. Replaces most usage of internal packages with `pkg` packages.
    
    ## Review Checklist
    
    **If any item is not complete, the merge request is not ready to be reviewed and must be marked `Draft:`.**
    
    - [x] The merge request title is in the format `<change type>(<change scope>): <short description> [<task id>]`
      - For example, `feat(cli): add QR code generation [AC-123]`
      - For details, see [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [x] The description includes `Closes <jira task ID>` (or rarely `Updates <jira task ID>`)
    - [x] The change is fully validated by tests that are run during CI
      - In most cases this means a test in "validate.sh"
      - In some cases, a Go test may be acceptable
      - Validation is not applicable to things like documentation updates
      - Purely UI/UX changes can be manually validated, such as changes to human-readable output
      - For all other changes, automated validation tests are an absolute requirement unless a maintainer specifically explains why they are not in a comment on this merge request
    - [x] The change is marked with one of the validation labels
      - ~"validation::ci/cd" for changes validated by CI tests
      - ~"validation::manual" for changes validated by hand
      - ~"validation::deferred" for changes validated by a follow up merge request
      - ~"validation::not applicable" for changes where validation is not applicable
    
    ## Merge Checklist
    
    - [ ] CI is passing
    - [ ] Merge conflicts are resolved
    - [ ] All discussions are resolved
    
    Related to DO-65
    
    
    Former-commit-id: dc691504e7813bd92f1bf3c8759b700366a55390

---
+++ cmd/x/accumulate/walletd/utils.go
@@ -0,0 +1,20 @@
+package walletd
+
+import (
+	"encoding/binary"
+	"errors"
+)
+
+var ErrNotEnoughData = errors.New("not enough data")
+var ErrOverflow = errors.New("overflow")
+
+func uvarintUnmarshalBinary(b []byte) (uint64, error) {
+	v, n := binary.Uvarint(b)
+	if n == 0 {
+		return 0, ErrNotEnoughData
+	}
+	if n < 0 {
+		return 0, ErrOverflow
+	}
+	return v, nil
+}
