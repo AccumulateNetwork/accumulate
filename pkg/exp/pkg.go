8fabe75321ff96b41975bb539a17b7bc3daaf6ce
Ethan Reesor <ethan.reesor@gmail.com>
2022-11-02 23:52:21 -0500

    chore: export receipts

--- pkg/exp/pkg.go
+++ pkg/exp/pkg.go
@@ -1,6 +1,12 @@
 // Package exp is experimental and may change without warning.
 package exp
 
-import "gitlab.com/accumulatenetwork/accumulate/internal/core"
+import (
+	"gitlab.com/accumulatenetwork/accumulate/internal/core"
+	"gitlab.com/accumulatenetwork/accumulate/smt/managed"
+)
 
 type GlobalValues = core.GlobalValues
+
+type Receipt = managed.Receipt
+type ReceiptEntry = managed.ReceiptEntry
