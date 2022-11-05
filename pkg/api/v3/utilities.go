a7494608206a6ebd64052c7c17ca33c906ecca0e
Ethan Reesor <ethan.reesor@gmail.com>
2022-11-04 19:50:34 -0500

    lint

--- pkg/api/v3/utilities.go
+++ pkg/api/v3/utilities.go
@@ -8,7 +8,7 @@ func RangeAs[U, V Record](r *RecordRange[V]) ([]U, error) {
 		u, ok := any(v).(U)
 		if !ok {
 			var z U
-			return nil, errors.Format(errors.Conflict, "want %T, got %T", z, v)
+			return nil, errors.Conflict.WithFormat("want %T, got %T", z, v)
 		}
 		s[i] = u
 	}
@@ -51,7 +51,7 @@ func MakeRange[V any, U Record](values []V, start, count uint64, fn func(V) (U, 
 	for i, v := range values {
 		u, err := fn(v)
 		if err != nil {
-			return nil, errors.Wrap(errors.UnknownError, err)
+			return nil, errors.UnknownError.Wrap(err)
 		}
 		r.Records[i] = u
 	}
@@ -65,7 +65,7 @@ func ChainEntryAs[U, V Record](r *ChainEntryRecord[V]) (*ChainEntryRecord[U], er
 	if u, ok := any(r.Value).(U); ok {
 		s.Value = u
 	} else if !EqualRecord(r.Value, zv) {
-		return nil, errors.Format(errors.Conflict, "want %T, got %T", zu, r.Value)
+		return nil, errors.Conflict.WithFormat("want %T, got %T", zu, r.Value)
 	}
 
 	s.Account = r.Account
