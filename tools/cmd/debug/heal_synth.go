d7b6573f896774573b6745c3fd54987acae8cb7d
Ethan Reesor <ethan.reesor@gmail.com>
2022-11-07 22:31:40 +0000

    Add a healing tool for synthetic transactions [DO-88]
    Closes DO-88. Add a tool to resend synthetic transactions. Fix a math bug in the debug sequence logic.
    
    Changelog: feature

---
+++ tools/cmd/debug/heal_synth.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"gitlab.com/accumulatenetwork/accumulate/internal/api/v2"
+	client "gitlab.com/accumulatenetwork/accumulate/pkg/client/api/v2"
+	"gitlab.com/accumulatenetwork/accumulate/protocol"
+)
+
+var cmdHealSynth = &cobra.Command{
+	Use:   "heal-synth [server]",
+	Short: "Fixup synthetic transactions",
+	Run:   healSynth,
+}
+
+func init() {
+	cmd.AddCommand(cmdHealSynth)
+}
+
+func healSynth(_ *cobra.Command, args []string) {
+	c, err := client.New(args[0])
+	check(err)
+
+	desc, err := c.Describe(context.Background())
+	check(err)
+
+	synths := map[string]*protocol.SyntheticLedger{}
+	for _, part := range desc.Values.Network.Partitions {
+		// Get synthetic ledger
+		req := new(api.GeneralQuery)
+		req.Url = protocol.PartitionUrl(part.ID).JoinPath(protocol.Synthetic)
+		synth := new(protocol.SyntheticLedger)
+		res := new(api.ChainQueryResponse)
+		res.Data = synth
+		err = c.RequestAPIv2(context.Background(), "query", req, res)
+		check(err)
+		synths[part.ID] = synth
+
+		for _, src := range synth.Sequence {
+			for _, txid := range src.Pending {
+				req.Url = txid.AsUrl()
+				res := new(api.TransactionQueryResponse)
+				err = c.RequestAPIv2(context.Background(), "query", req, res)
+				check(err)
+
+				fmt.Printf("Resubmitting %v\n", txid)
+				xreq := new(api.ExecuteRequest)
+				xreq.Envelope = new(protocol.Envelope)
+				xreq.Envelope.Transaction = []*protocol.Transaction{res.Transaction}
+				for _, sig := range res.Signatures {
+					sig, ok := sig.(*protocol.PartitionSignature)
+					if ok {
+						xreq.Envelope.Signatures = []protocol.Signature{sig}
+					}
+				}
+				xres, err := c.ExecuteDirect(context.Background(), xreq)
+				check(err)
+				if xres.Message != "" {
+					fmt.Fprintf(os.Stderr, "Warning: %s\n", xres.Message)
+				}
+			}
+		}
+	}
+
+	// // Check produced vs received
+	// for i, a := range desc.Values.Network.Partitions {
+	// 	for _, b := range desc.Values.Network.Partitions[i+1:] {
+	// 		checkSequence2(a, b, bad, "synthetic transactions",
+	// 			synths[a.ID].Partition(protocol.PartitionUrl(b.ID)),
+	// 			synths[b.ID].Partition(protocol.PartitionUrl(a.ID)),
+	// 		)
+	// 	}
+	// }
+}
