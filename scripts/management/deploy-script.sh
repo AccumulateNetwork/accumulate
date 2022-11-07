d5267995de179fa4ae0159355587985499b90d2f
Ethan Reesor <ethan.reesor@gmail.com>
2022-11-07 18:12:34 +0000

    Remove files that are no longer relevant [AC-3302]
    Closes AC-3302. Remove files that are no longer relevant: Terraform, devnet management scripts, test matrix scripts, horribly out of date Open RPC specs, and a couple others. And move GitLab CI files into `.gitlab`.
    
    Changelog: cleanup

--- scripts/management/deploy-script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for ip in "${@:3}"; do
-    scp "$1"  "$2" ec2-user@${ip}:
-    ssh ec2-user@${ip} "chmod +x '$1' && chmod +x '$2'"
-done
--- terraform/Testnet/discovery.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_service_discovery_private_dns_namespace" "simple-stack" {
-  name        = "accumulate-testnet"
-  description = "testnet-stack"
-  vpc         = "vpc-0e8d6207813fabfc4"
-}
-
-
