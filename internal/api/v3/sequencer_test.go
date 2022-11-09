22c8f99ab8573dd5450ce2c4ca96d044ce096c22
Ethan Reesor <ethan.reesor@gmail.com>
2022-11-09 18:02:27 +0000

    Implement API v3 node services [AC-3141]
    Closes AC-3141. Implements node status, submitter, and validator services. Adds and implements a private sequencer service.
    
    Changelog: feature
    
    Former-commit-id: 60c8522d77c9034ed9be9fc12308665173284881

---
+++ internal/api/v3/sequencer_test.go
@@ -0,0 +1,86 @@
+package api
+
+import (
+	"context"
+	"math/big"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gitlab.com/accumulatenetwork/accumulate/internal/core"
+	"gitlab.com/accumulatenetwork/accumulate/internal/core/events"
+	"gitlab.com/accumulatenetwork/accumulate/internal/database"
+	"gitlab.com/accumulatenetwork/accumulate/internal/node/config"
+	"gitlab.com/accumulatenetwork/accumulate/pkg/build"
+	"gitlab.com/accumulatenetwork/accumulate/pkg/url"
+	. "gitlab.com/accumulatenetwork/accumulate/protocol"
+	. "gitlab.com/accumulatenetwork/accumulate/test/helpers"
+	"gitlab.com/accumulatenetwork/accumulate/test/simulator"
+	acctesting "gitlab.com/accumulatenetwork/accumulate/test/testing"
+)
+
+func TestSequencer(t *testing.T) {
+	logger := acctesting.NewTestLogger(t)
+	net := simulator.SimpleNetwork(t.Name(), 2, 1)
+	sim := NewSim(t,
+		simulator.MemoryDatabase,
+		net,
+		simulator.Genesis(GenesisTime),
+	)
+
+	aliceKey := acctesting.GenerateKey("alice")
+	bobKey := acctesting.GenerateKey("bob")
+	alice := acctesting.AcmeLiteAddressStdPriv(aliceKey)
+	bob := acctesting.AcmeLiteAddressStdPriv(bobKey)
+	sim.SetRoute(alice, "BVN0")
+	sim.SetRoute(bob, "BVN1")
+
+	g := new(core.GlobalValues)
+	require.NoError(t, g.Load(config.NetworkUrl{URL: PartitionUrl("BVN0")}, func(account *url.URL, target interface{}) error {
+		return sim.DatabaseFor(alice).View(func(batch *database.Batch) error {
+			return batch.Account(account).GetStateAs(target)
+		})
+	}))
+
+	MakeLiteTokenAccount(t, sim.DatabaseFor(alice), aliceKey[32:], AcmeUrl())
+	CreditCredits(t, sim.DatabaseFor(alice), alice.RootIdentity(), 1e9)
+	CreditTokens(t, sim.DatabaseFor(alice), alice, big.NewInt(1e12))
+
+	st := sim.SubmitSuccessfully(MustBuild(t,
+		build.Transaction().For(alice).
+			SendTokens(123, 0).To(bob).
+			SignWith(alice).Version(1).Timestamp(1).PrivateKey(aliceKey)))
+
+	sim.StepUntil(
+		Txn(st.TxID).Succeeds(),
+		Txn(st.TxID).Produced().Succeeds())
+
+	svc := NewSequencer(SequencerParams{
+		Logger:       logger,
+		Database:     sim.DatabaseFor(alice),
+		EventBus:     events.NewBus(logger),
+		Globals:      g,
+		Partition:    "BVN0",
+		ValidatorKey: net.Bvns[0].Nodes[0].PrivValKey,
+	})
+
+	anchor, err := svc.Sequence(context.Background(), PartitionUrl("BVN0").JoinPath(AnchorPool), DnUrl().JoinPath(AnchorPool), 1)
+	require.NoError(t, err)
+	require.IsType(t, (*BlockValidatorAnchor)(nil), anchor.Transaction.Body)
+	require.Len(t, anchor.Signatures.Records, 1)
+	require.IsType(t, (*SignatureSet)(nil), anchor.Signatures.Records[0].Signature)
+	sigs := anchor.Signatures.Records[0].Signature.(*SignatureSet).Signatures
+	require.Len(t, sigs, 2)
+	require.IsType(t, (*PartitionSignature)(nil), sigs[0])
+	require.IsType(t, (*ED25519Signature)(nil), sigs[1])
+
+	synth, err := svc.Sequence(context.Background(), PartitionUrl("BVN0").JoinPath(Synthetic), PartitionUrl("BVN1").JoinPath(Synthetic), 1)
+	require.NoError(t, err)
+	require.IsType(t, (*SyntheticDepositTokens)(nil), synth.Transaction.Body)
+	require.Len(t, synth.Signatures.Records, 1)
+	require.IsType(t, (*SignatureSet)(nil), synth.Signatures.Records[0].Signature)
+	sigs = synth.Signatures.Records[0].Signature.(*SignatureSet).Signatures
+	require.Len(t, sigs, 3)
+	require.IsType(t, (*PartitionSignature)(nil), sigs[0])
+	require.IsType(t, (*ReceiptSignature)(nil), sigs[1])
+	require.IsType(t, (*ED25519Signature)(nil), sigs[2])
+}
---
+++ internal/api/v3/tm/validator.go
@@ -0,0 +1,66 @@
+package tm
+
+import (
+	"context"
+
+	"github.com/tendermint/tendermint/libs/log"
+	coretypes "github.com/tendermint/tendermint/rpc/core/types"
+	"github.com/tendermint/tendermint/types"
+	"gitlab.com/accumulatenetwork/accumulate/internal/logging"
+	"gitlab.com/accumulatenetwork/accumulate/pkg/api/v3"
+	"gitlab.com/accumulatenetwork/accumulate/pkg/errors"
+	"gitlab.com/accumulatenetwork/accumulate/protocol"
+)
+
+type ValidateClient interface {
+	CheckTx(context.Context, types.Tx) (*coretypes.ResultCheckTx, error)
+}
+
+type Validator struct {
+	logger logging.OptionalLogger
+	local  ValidateClient
+}
+
+var _ api.Validator = (*Validator)(nil)
+
+type ValidatorParams struct {
+	Logger log.Logger
+	Local  ValidateClient
+}
+
+func NewValidator(params ValidatorParams) *Validator {
+	s := new(Validator)
+	s.logger.L = params.Logger
+	s.local = params.Local
+	return s
+}
+
+func (s *Validator) Validate(ctx context.Context, envelope *protocol.Envelope, opts api.ValidateOptions) ([]*api.Submission, error) {
+	// if opts.Full == nil || *opts.Full {
+	// 	return nil, errors.NotAllowed.WithFormat("full validation has not been implemented")
+	// }
+
+	b, err := envelope.MarshalBinary()
+	if err != nil {
+		return nil, errors.EncodingError.WithFormat("marshal: %w", err)
+	}
+
+	res, err := s.local.CheckTx(ctx, b)
+	if err != nil {
+		return nil, errors.InternalError.WithFormat("broadcast: %w", err)
+	}
+
+	var message string
+	switch {
+	case len(res.MempoolError) > 0:
+		message = res.MempoolError
+	case len(res.Log) > 0:
+		message = res.Log
+	case len(res.Info) > 0:
+		message = res.Info
+	case res.Code != 0:
+		message = "An unknown error occurred"
+	}
+
+	return unpackConsensusResult(res.Code, message, res.Data)
+}
