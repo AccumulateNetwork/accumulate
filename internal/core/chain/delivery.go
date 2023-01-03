749c975cee3cd9b519536a0c3e82d319257be4c0
Ethan Reesor <ethan.reesor@gmail.com>
2023-01-03 14:45:04 -0600

    saving work

--- internal/core/chain/delivery.go
+++ internal/core/chain/delivery.go
@@ -1,4 +1,4 @@
-// Copyright 2022 The Accumulate Authors
+// Copyright 2023 The Accumulate Authors
 //
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file or at
@@ -7,114 +7,47 @@
 package chain
 
 import (
-	"fmt"
-
 	"gitlab.com/accumulatenetwork/accumulate/internal/database"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/errors"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/types/merkle"
+	"gitlab.com/accumulatenetwork/accumulate/pkg/types/messaging"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/url"
 	"gitlab.com/accumulatenetwork/accumulate/protocol"
 )
 
 // NormalizeEnvelope normalizes the envelope into one or more deliveries.
 func NormalizeEnvelope(envelope *protocol.Envelope) ([]*Delivery, error) {
-	// Validate the envelope's TxHash
-	var envTxHash *[32]byte
-	switch len(envelope.TxHash) {
-	case 32:
-		envTxHash = (*[32]byte)(envelope.TxHash)
-	case 0:
-		// Ok
-	default:
-		return nil, fmt.Errorf("invalid hash length: want 32, got %d", len(envelope.TxHash))
+	messages, err := messaging.NormalizeLegacy(envelope)
+	if err != nil {
+		return nil, err
 	}
 
-	// Create a map (and an ordered list) of all transactions
-	txnMap := make(map[[32]byte]*Delivery, len(envelope.Transaction))
-	txnList := make([]*Delivery, 0, len(envelope.Transaction))
-	for i, txn := range envelope.Transaction {
-		if txn.Body == nil {
-			return nil, fmt.Errorf("transaction %d: nil body", i)
-		}
+	deliveries := make([]*Delivery, len(messages))
+	for i, message := range messages {
+		deliveries[i] = DeliveryFromMessage(message)
+	}
+	return deliveries, nil
+}
 
-		hash := txn.GetHash()
-		switch {
-		case len(hash) == 32:
-			// Normal transaction or a remote transaction that includes a hash
+// DeliveryFromMessage constructs a Delivery from a [messaging.LegacyMessage].
+func DeliveryFromMessage(msg *messaging.LegacyMessage) *Delivery {
+	d := new(Delivery)
+	d.Transaction = msg.Transaction
+	d.Signatures = msg.Signatures
 
-		case envTxHash != nil:
-			// Envelope specifies the transaction hash
-			hash = (*envTxHash)[:]
-
-			// Set the remote transaction's hash
-			if remote, ok := txn.Body.(*protocol.RemoteTransaction); ok {
-				remote.Hash = *(*[32]byte)(hash)
+	// This may be necessary for API v3, but it is a change in behavior that
+	// *might* cause issues when replaying 1.0.0 history
+	if false {
+		for _, sig := range d.Signatures {
+			if sig, ok := sig.(*protocol.PartitionSignature); ok {
+				d.SequenceNumber = sig.SequenceNumber
+				d.SourceNetwork = sig.SourceNetwork
+				d.DestinationNetwork = sig.DestinationNetwork
 			}
-
-		default:
-			// No hash
-			return nil, fmt.Errorf("transaction %d: remote transaction: missing hash", i)
-		}
-
-		delivery := new(Delivery)
-		delivery.Transaction = txn
-		txnMap[*(*[32]byte)(hash)] = delivery
-		txnList = append(txnList, delivery)
-	}
-
-	// Map signatures to transactions
-	for i, sig := range envelope.Signatures {
-		hash := sig.GetTransactionHash()
-		switch {
-		case hash != [32]byte{}:
-			// Signature specifies the transaction hash
-
-		case envTxHash != nil:
-			// Envelope specifies the transaction hash
-			hash = *envTxHash
-
-		case len(txnMap) == 1:
-			// There's only one transaction
-			for hash = range txnMap {
-				break
-			}
-
-		default:
-			return nil, fmt.Errorf("multi-transaction envelope: signature %d: missing hash", i)
-		}
-
-		// Get the existing delivery
-		delivery, ok := txnMap[hash]
-		if !ok {
-			// Or create a new remote transaction
-			body := new(protocol.RemoteTransaction)
-			body.Hash = hash
-			txn := new(protocol.Transaction)
-			txn.Body = body
-			delivery = new(Delivery)
-			delivery.Transaction = txn
-			txnMap[hash] = delivery
-			txnList = append(txnList, delivery)
-		}
-
-		// Add the signature to the delivery
-		delivery.Signatures = append(delivery.Signatures, sig)
-
-		if sig, ok := sig.(*protocol.PartitionSignature); ok {
-			delivery.SequenceNumber = sig.SequenceNumber
-			delivery.SourceNetwork = sig.SourceNetwork
-			delivery.DestinationNetwork = sig.DestinationNetwork
 		}
 	}
 
-	for _, delivery := range txnList {
-		// A transaction with no signatures is invalid
-		if len(delivery.Signatures) == 0 {
-			return nil, errors.BadRequest.WithFormat("the envelope does not contain any signatures matching transaction %X (%v)", delivery.Transaction.GetHash()[:8], delivery.Transaction.Body.Type())
-		}
-	}
-
-	return txnList, nil
+	return nil
 }
 
 type Delivery struct {
