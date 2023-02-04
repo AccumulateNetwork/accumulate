6eb1867415db7ea2ae0257dca20da4ae167f4983
Ethan Reesor <ethan.reesor@gmail.com>
2023-02-04 05:19:25 +0000

    Remove support for partial receipts [#3170]
    Closes #3170. Removes support for partial receipts for synthetic transactions. See issue for details.
    
    Changelog: refactor

--- internal/api/v3/sequencer.go
+++ internal/api/v3/sequencer.go
@@ -15,11 +15,13 @@ import (
 	"gitlab.com/accumulatenetwork/accumulate/internal/core"
 	"gitlab.com/accumulatenetwork/accumulate/internal/core/events"
 	"gitlab.com/accumulatenetwork/accumulate/internal/database"
+	"gitlab.com/accumulatenetwork/accumulate/internal/database/indexing"
 	"gitlab.com/accumulatenetwork/accumulate/internal/logging"
 	"gitlab.com/accumulatenetwork/accumulate/internal/node/config"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/api/v3"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/client/signing"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/errors"
+	"gitlab.com/accumulatenetwork/accumulate/pkg/types/merkle"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/types/messaging"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/url"
 	"gitlab.com/accumulatenetwork/accumulate/protocol"
@@ -161,6 +163,7 @@ func (s *Sequencer) getAnchor(batch *database.Batch, globals *core.GlobalValues,
 }
 
 func (s *Sequencer) getSynth(batch *database.Batch, globals *core.GlobalValues, dst *url.URL, num uint64) (*api.TransactionRecord, error) {
+	// Load the appropriate sequence chain
 	partition, ok := protocol.ParsePartitionUrl(dst)
 	if !ok {
 		return nil, errors.UnknownError.WithFormat("destination is not a partition")
@@ -170,11 +173,15 @@ func (s *Sequencer) getSynth(batch *database.Batch, globals *core.GlobalValues, 
 	if err != nil {
 		return nil, errors.UnknownError.WithFormat("load synthetic sequence chain: %w", err)
 	}
+
+	// Load the Nth sequence chain entry
 	entry := new(protocol.IndexEntry)
 	err = chain.EntryAs(int64(num)-1, entry)
 	if err != nil {
 		return nil, errors.UnknownError.WithFormat("load synthetic sequence chain entry %d: %w", num-1, err)
 	}
+
+	// Load the corresponding main chain entry
 	chain, err = ledger.MainChain().Get()
 	if err != nil {
 		return nil, errors.UnknownError.WithFormat("load synthetic main chain: %w", err)
@@ -184,6 +191,7 @@ func (s *Sequencer) getSynth(batch *database.Batch, globals *core.GlobalValues, 
 		return nil, errors.UnknownError.WithFormat("load synthetic chain entry %d: %w", entry.Source, err)
 	}
 
+	// Load the transaction
 	var msg messaging.MessageWithTransaction
 	err = batch.Message2(hash).Main().GetAs(&msg)
 	if err != nil {
@@ -194,8 +202,32 @@ func (s *Sequencer) getSynth(batch *database.Batch, globals *core.GlobalValues, 
 	if err != nil {
 		return nil, errors.UnknownError.WithFormat("load status: %w", err)
 	}
-	if status.Proof == nil {
-		return nil, errors.NotReady.WithFormat("no proof yet")
+
+	if globals.ExecutorVersion.V2() {
+		// Get the synthetic main chain receipt
+		synthReceipt, entry, err := s.getReceiptForChainEntry(ledger.MainChain(), entry.Source)
+		if err != nil {
+			return nil, errors.UnknownError.Wrap(err)
+		}
+
+		// Get the latest directory anchor receipt
+		dirReceipt, err := s.getLatestDirectoryReceipt(batch)
+		if err != nil {
+			return nil, errors.UnknownError.Wrap(err)
+		}
+
+		// Get the receipt in between the other two
+		rootReceipt, err := s.getRootReceipt(batch, entry.Anchor, dirReceipt.Anchor.RootChainIndex)
+		if err != nil {
+			return nil, errors.UnknownError.Wrap(err)
+		}
+
+		receipt, err := merkle.CombineReceipts(synthReceipt, rootReceipt, dirReceipt.RootChainReceipt)
+		if err != nil {
+			return nil, errors.UnknownError.WithFormat("combine receipts: %w", err)
+		}
+
+		status.Proof = receipt
 	}
 
 	var signatures []protocol.Signature
@@ -212,9 +244,7 @@ func (s *Sequencer) getSynth(batch *database.Batch, globals *core.GlobalValues, 
 		// Add the receipt signature
 		receiptSig := new(protocol.ReceiptSignature)
 		receiptSig.SourceNetwork = status.SourceNetwork
-		if status.Proof != nil {
-			receiptSig.Proof = *status.Proof
-		}
+		receiptSig.Proof = *status.Proof
 		receiptSig.TransactionHash = *(*[32]byte)(hash)
 		signatures = append(signatures, receiptSig)
 	}
@@ -249,4 +279,89 @@ func (s *Sequencer) getSynth(batch *database.Batch, globals *core.GlobalValues, 
 	}
 	r.Status = status
 	return r, nil
+}
+
+// getReceiptForChainEntry gets a receipt from an entry to the first anchor
+// after that entry.
+func (s *Sequencer) getReceiptForChainEntry(chain *database.Chain2, index uint64) (*merkle.Receipt, *protocol.IndexEntry, error) {
+	// Load the index chain
+	indexChain, err := chain.Index().Get()
+	if err != nil {
+		return nil, nil, errors.UnknownError.WithFormat("load %s index chain: %w", chain.Name(), err)
+	}
+	if indexChain.Height() == 0 {
+		return nil, nil, errors.Conflict.WithFormat("%s index chain is empty", chain.Name())
+	}
+
+	// Locate the index entry for the given entry
+	_, entry, err := indexing.SearchIndexChain(indexChain, uint64(indexChain.Height()-1), indexing.MatchAfter, indexing.SearchIndexChainBySource(index))
+	if err != nil {
+		return nil, nil, errors.UnknownError.WithFormat("locate index entry for %s chain entry %d: %w", chain.Name(), index, err)
+	}
+
+	// Load the chain
+	c, err := chain.Get()
+	if err != nil {
+		return nil, nil, errors.UnknownError.WithFormat("load %s chain: %w", chain.Name(), err)
+	}
+
+	// Get a receipt
+	receipt, err := c.Receipt(int64(index), int64(entry.Source))
+	if err != nil {
+		return nil, nil, errors.UnknownError.WithFormat("get %s chain receipt from %d to %d: %w", chain.Name(), index, entry.Source, err)
+	}
+
+	return receipt, entry, nil
+}
+
+// getRootReceipt gets a root chain receipt.
+func (s *Sequencer) getRootReceipt(batch *database.Batch, from, to uint64) (*merkle.Receipt, error) {
+	// Load the root chain
+	root, err := batch.Account(s.partition.Ledger()).RootChain().Get()
+	if err != nil {
+		return nil, errors.UnknownError.WithFormat("load root chain: %w", err)
+	}
+
+	// Get a receipt from the entry to the block's anchor
+	receipt, err := root.Receipt(int64(from), int64(to))
+	if err != nil {
+		return nil, errors.UnknownError.WithFormat("get root chain receipt from %d to %d: %w", from, to, err)
+	}
+	return receipt, nil
+}
+
+// getLatestDirectoryReceipt returns the latest partition anchor receipt from the DN for this partition.
+func (s *Sequencer) getLatestDirectoryReceipt(batch *database.Batch) (*protocol.PartitionAnchorReceipt, error) {
+	chain := batch.Account(s.partition.AnchorPool()).MainChain()
+	head, err := chain.Head().Get()
+	if err != nil {
+		return nil, errors.UnknownError.WithFormat("load DN anchor chain head: %w", err)
+	}
+	if head.Count == 0 {
+		return nil, errors.NotFound.With("DN anchor chain is empty")
+	}
+
+	for i := head.Count - 1; i >= 0; i-- {
+		entry, err := chain.Inner().Get(i)
+		if err != nil {
+			return nil, errors.UnknownError.WithFormat("load DN anchor chain entry %d: %w", i, err)
+		}
+		var msg messaging.MessageWithTransaction
+		err = batch.Message2(entry).Main().GetAs(&msg)
+		if err != nil {
+			return nil, errors.UnknownError.WithFormat("load DN anchor #%d: %w", i, err)
+		}
+
+		anchor, ok := msg.GetTransaction().Body.(*protocol.DirectoryAnchor)
+		if !ok {
+			continue
+		}
+
+		for _, r := range anchor.Receipts {
+			if r.Anchor.Source.Equal(s.partition.URL) {
+				return r, nil
+			}
+		}
+	}
+	return nil, errors.UnknownError.WithFormat("unable to locate a DN anchor for %v", s.partition.URL)
 }
