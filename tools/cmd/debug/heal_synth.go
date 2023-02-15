a555790b86646a16751a6b1a175065c3551dae23
Ethan Reesor <ethan.reesor@gmail.com>
2023-02-15 13:23:55 -0600

    Finish the sequence healing changes
    Former-commit-id: e76f99719fcf3fe41d96e6678ad2abb083dcabbc

--- tools/cmd/debug/heal_synth.go
+++ tools/cmd/debug/heal_synth.go
@@ -9,11 +9,14 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/url"
 	"os"
+	"strconv"
+	"strings"
 
-	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 	"gitlab.com/accumulatenetwork/accumulate/internal/api/v2"
+	"gitlab.com/accumulatenetwork/accumulate/internal/node/config"
 	client "gitlab.com/accumulatenetwork/accumulate/pkg/client/api/v2"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/types/messaging"
 	"gitlab.com/accumulatenetwork/accumulate/protocol"
@@ -81,18 +84,37 @@ func healSynth(_ *cobra.Command, args []string) {
 			ba := synths[b.ID].Partition(protocol.PartitionUrl(a.ID))
 
 			for i := ba.Received + 1; i <= ab.Produced; i++ {
-				resubmitByNumber(c, a.ID, b.ID, i, false)
+				resubmitByNumber(desc, c, a.ID, b.ID, i, false)
+			}
+			if a == b {
+				continue
 			}
 			for i := ab.Received + 1; i <= ba.Produced; i++ {
-				resubmitByNumber(c, b.ID, a.ID, i, false)
+				resubmitByNumber(desc, c, b.ID, a.ID, i, false)
 			}
 		}
 	}
 }
 
-var didWarn bool
+func resubmitByNumber(desc *api.DescriptionResponse, c *client.Client, source, destination string, number uint64, anchor bool) {
+	// Get a client for the destination partition
+	var d *client.Client
+	for _, p := range desc.Network.Partitions {
+		if !strings.EqualFold(p.Id, destination) {
+			continue
+		}
+		if len(p.Nodes) == 0 {
+			fatalf("no nodes for %v", p.Id)
+		}
+		u, err := url.Parse(p.Nodes[0].Address)
+		check(err)
+		port, err := strconv.ParseUint(u.Port(), 10, 16)
+		check(err)
+		d, err = client.New(fmt.Sprintf("http://%s:%d", u.Hostname(), port+config.PortOffsetAccumulateApi.GetEnumValue()))
+		check(err)
+	}
 
-func resubmitByNumber(c *client.Client, source, destination string, number uint64, anchor bool) {
+	// Query the synthetic transaction
 	req := new(api.SyntheticTransactionRequest)
 	req.Source = protocol.PartitionUrl(source)
 	req.Destination = protocol.PartitionUrl(destination)
@@ -101,31 +123,13 @@ func resubmitByNumber(c *client.Client, source, destination string, number uint6
 	res, err := c.QuerySynth(context.Background(), req)
 	check(err)
 
-	if !didWarn {
-		didWarn = true
-		color.Red("WARNING! This doesn't actually submit the transactions (checkOnly = true) because the last time I tried it for real it killed a synthetic transaction")
-
-		// acc://9162703cafc08d8d97daeb3c28ba65dbf01d3e21742c81ae041cd8d5f03f66aa@accumulate.acme/book/2
-		//
-		// This synthetic transaction on mainnet failed with 'missing key
-		// signature'. That's a bug, but also this tool needs to be updated to
-		// make sure it includes a key signature.
-	}
-
-	c.DebugRequest = true
-	defer func() { c.DebugRequest = false }()
+	// Submit the synthetic transaction directly to the destination partition
 	fmt.Printf("Resubmitting %v\n", res.Txid)
 	xreq := new(api.ExecuteRequest)
 	xreq.Envelope = new(messaging.Envelope)
 	xreq.Envelope.Transaction = []*protocol.Transaction{res.Transaction}
-	xreq.CheckOnly = true
-	for _, sig := range res.Signatures {
-		sig, ok := sig.(*protocol.PartitionSignature)
-		if ok {
-			xreq.Envelope.Signatures = []protocol.Signature{sig}
-		}
-	}
-	xres, err := c.ExecuteDirect(context.Background(), xreq)
+	xreq.Envelope.Signatures = res.Signatures
+	xres, err := d.ExecuteLocal(context.Background(), xreq)
 	check(err)
 	if xres.Message != "" {
 		fmt.Fprintf(os.Stderr, "Warning: %s\n", xres.Message)
