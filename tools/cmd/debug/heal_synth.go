63c9c4d0aab9e493b21df9ce2cc20ed4f544a6bf
Ethan Reesor <ethan.reesor@gmail.com>
2023-02-15 11:47:28 -0600

    Fix off by one bug in sequence tool

--- tools/cmd/debug/heal_synth.go
+++ tools/cmd/debug/heal_synth.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 	"gitlab.com/accumulatenetwork/accumulate/internal/api/v2"
 	client "gitlab.com/accumulatenetwork/accumulate/pkg/client/api/v2"
@@ -73,13 +74,60 @@ func healSynth(_ *cobra.Command, args []string) {
 		}
 	}
 
-	// // Check produced vs received
-	// for i, a := range desc.Values.Network.Partitions {
-	// 	for _, b := range desc.Values.Network.Partitions[i+1:] {
-	// 		checkSequence2(a, b, bad, "synthetic transactions",
-	// 			synths[a.ID].Partition(protocol.PartitionUrl(b.ID)),
-	// 			synths[b.ID].Partition(protocol.PartitionUrl(a.ID)),
-	// 		)
-	// 	}
-	// }
+	// Check produced vs received
+	for i, a := range desc.Values.Network.Partitions {
+		for _, b := range desc.Values.Network.Partitions[i:] {
+			ab := synths[a.ID].Partition(protocol.PartitionUrl(b.ID))
+			ba := synths[b.ID].Partition(protocol.PartitionUrl(a.ID))
+
+			for i := ba.Received + 1; i <= ab.Produced; i++ {
+				resubmitByNumber(c, a.ID, b.ID, i, false)
+			}
+			for i := ab.Received + 1; i <= ba.Produced; i++ {
+				resubmitByNumber(c, b.ID, a.ID, i, false)
+			}
+		}
+	}
+}
+
+var didWarn bool
+
+func resubmitByNumber(c *client.Client, source, destination string, number uint64, anchor bool) {
+	req := new(api.SyntheticTransactionRequest)
+	req.Source = protocol.PartitionUrl(source)
+	req.Destination = protocol.PartitionUrl(destination)
+	req.SequenceNumber = number
+	req.Anchor = anchor
+	res, err := c.QuerySynth(context.Background(), req)
+	check(err)
+
+	if !didWarn {
+		didWarn = true
+		color.Red("WARNING! This doesn't actually submit the transactions (checkOnly = true) because the last time I tried it for real it killed a synthetic transaction")
+
+		// acc://9162703cafc08d8d97daeb3c28ba65dbf01d3e21742c81ae041cd8d5f03f66aa@accumulate.acme/book/2
+		//
+		// This synthetic transaction on mainnet failed with 'missing key
+		// signature'. That's a bug, but also this tool needs to be updated to
+		// make sure it includes a key signature.
+	}
+
+	c.DebugRequest = true
+	defer func() { c.DebugRequest = false }()
+	fmt.Printf("Resubmitting %v\n", res.Txid)
+	xreq := new(api.ExecuteRequest)
+	xreq.Envelope = new(messaging.Envelope)
+	xreq.Envelope.Transaction = []*protocol.Transaction{res.Transaction}
+	xreq.CheckOnly = true
+	for _, sig := range res.Signatures {
+		sig, ok := sig.(*protocol.PartitionSignature)
+		if ok {
+			xreq.Envelope.Signatures = []protocol.Signature{sig}
+		}
+	}
+	xres, err := c.ExecuteDirect(context.Background(), xreq)
+	check(err)
+	if xres.Message != "" {
+		fmt.Fprintf(os.Stderr, "Warning: %s\n", xres.Message)
+	}
 }
