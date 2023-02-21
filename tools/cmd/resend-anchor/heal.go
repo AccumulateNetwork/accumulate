31ee6877c283401bb7d6968d9d5b4ef803d7c650
Ethan Reesor <ethan.reesor@gmail.com>
2023-02-21 14:07:16 -0600

    Make healing tool work for DN self anchors
    Former-commit-id: a0750e015e2d1891cdfc1094d5d8fd80e106f595

--- tools/cmd/resend-anchor/heal.go
+++ tools/cmd/resend-anchor/heal.go
@@ -1,4 +1,4 @@
-// Copyright 2022 The Accumulate Authors
+// Copyright 2023 The Accumulate Authors
 //
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file or at
@@ -12,10 +12,12 @@ import (
 	"log"
 	stdurl "net/url"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/spf13/cobra"
 	"gitlab.com/accumulatenetwork/accumulate/internal/api/v2"
+	"gitlab.com/accumulatenetwork/accumulate/internal/core"
 	"gitlab.com/accumulatenetwork/accumulate/internal/node/config"
 	client "gitlab.com/accumulatenetwork/accumulate/pkg/client/api/v2"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/url"
@@ -57,92 +59,40 @@ func heal(_ *cobra.Command, args []string) {
 		if uint64(len(pn)) < g.ValidatorThreshold(part.ID) {
 			fatalf("Error: insufficient nodes for %s; threshold is %d but I only found the address of %d nodes", part.ID, g.ValidatorThreshold(part.ID), len(pn))
 		}
-		partNodes[part.ID] = pn
+		partNodes[strings.ToLower(part.ID)] = pn
 	}
 
-	for {
+heal:
+	// Heal BVN -> DN
+	for _, part := range g.Network.Partitions {
+		if part.Type != protocol.PartitionTypeBlockValidator {
+			continue
+		}
+
+		partUrl := protocol.PartitionUrl(part.ID)
+		ledger := getLedger(netc, partUrl)
+
+		for _, txid := range ledger.Anchor(protocol.DnUrl()).Pending {
+			healTx(g, partNodes, netc, protocol.DnUrl(), partUrl, txid)
+		}
+	}
+
+	// Heal DN -> BVN, DN -> DN
+	{
+		ledger := getLedger(netc, protocol.DnUrl())
+
 		for _, part := range g.Network.Partitions {
-			if part.Type != protocol.PartitionTypeBlockValidator {
-				continue
-			}
-
-			partc := partNodes[part.ID][0].AccumulateAPI(config.PortOffsetBlockValidator)
 			partUrl := protocol.PartitionUrl(part.ID)
-			ledger := getLedger(netc, partUrl)
-
-			for _, txid := range ledger.Anchor(protocol.DnUrl()).Pending {
-				res, err := netc.QueryTx(context.Background(), &api.TxnQuery{TxIdUrl: txid})
-				if err != nil {
-					log.Printf("Failed to query %v: %v\n", txid, err)
-					continue
-				}
-
-				if uint64(len(res.Status.AnchorSigners)) >= g.ValidatorThreshold(protocol.Directory) {
-					continue // Already have enough signers
-				}
-
-				fmt.Printf("Healing anchor %v\n", txid)
-
-				signed := map[[32]byte]bool{}
-				for _, s := range res.Status.AnchorSigners {
-					signed[*(*[32]byte)(s)] = true
-				}
-
-				for _, node := range partNodes[protocol.Directory] {
-					if signed[*(*[32]byte)(node.Info.PublicKey)] {
-						continue
-					}
-
-					csrc := node.AccumulateAPI(config.PortOffsetDirectory)
-					querySynthAndExecute(csrc, partc, protocol.DnUrl(), partUrl, res.Status.SequenceNumber)
-				}
+			for _, txid := range ledger.Anchor(partUrl).Pending {
+				healTx(g, partNodes, netc, partUrl, protocol.DnUrl(), txid)
 			}
 		}
+	}
 
-		{
-			dnc := partNodes[protocol.Directory][0].AccumulateAPI(config.PortOffsetDirectory)
-			ledger := getLedger(netc, protocol.DnUrl())
-
-			for _, part := range g.Network.Partitions {
-				if part.Type != protocol.PartitionTypeBlockValidator {
-					continue
-				}
-
-				partUrl := protocol.PartitionUrl(part.ID)
-				for _, txid := range ledger.Anchor(partUrl).Pending {
-					res, err := netc.QueryTx(context.Background(), &api.TxnQuery{TxIdUrl: txid})
-					if err != nil {
-						log.Printf("Failed to query %v: %v\n", txid, err)
-						continue
-					}
-
-					if uint64(len(res.Status.AnchorSigners)) >= g.ValidatorThreshold(part.ID) {
-						continue // Already have enough signers
-					}
-
-					fmt.Printf("Healing anchor %v\n", txid)
-
-					signed := map[[32]byte]bool{}
-					for _, s := range res.Status.AnchorSigners {
-						signed[*(*[32]byte)(s)] = true
-					}
-
-					// Collect signatures and the transaction
-					for _, node := range partNodes[part.ID] {
-						if signed[*(*[32]byte)(node.Info.PublicKey)] {
-							continue
-						}
-
-						csrc := node.AccumulateAPI(config.PortOffsetBlockValidator)
-						querySynthAndExecute(csrc, dnc, partUrl, protocol.DnUrl(), res.Status.SequenceNumber)
-					}
-				}
-			}
-		}
-		if !healFlag.Continuous {
-			break
-		}
+	// Heal continuously?
+	if healFlag.Continuous {
 		time.Sleep(time.Second)
+		goto heal
 	}
 }
 
@@ -155,4 +105,45 @@ func getLedger(c *client.Client, part *url.URL) *protocol.AnchorLedger {
 	err := c.RequestAPIv2(context.Background(), "query", req, res)
 	checkf(err, "query %s anchor ledger", part)
 	return ledger
+}
+
+func healTx(g *core.GlobalValues, nodes map[string][]*NodeData, netClient *client.Client, srcUrl, dstUrl *url.URL, txid *url.TxID) {
+	dstId, _ := protocol.ParsePartitionUrl(dstUrl)
+	srcId, _ := protocol.ParsePartitionUrl(srcUrl)
+
+	// Query the transaction
+	res, err := netClient.QueryTx(context.Background(), &api.TxnQuery{TxIdUrl: txid})
+	if err != nil {
+		log.Printf("Failed to query %v: %v\n", txid, err)
+		return
+	}
+
+	// Check if there are already enough transactions
+	if uint64(len(res.Status.AnchorSigners)) >= g.ValidatorThreshold(srcId) {
+		return // Already have enough signers
+	}
+
+	fmt.Printf("Healing anchor %v\n", txid)
+
+	// Mark which nodes have signed
+	signed := map[[32]byte]bool{}
+	for _, s := range res.Status.AnchorSigners {
+		signed[*(*[32]byte)(s)] = true
+	}
+
+	// Make a client for the destination
+	dstClient := nodes[strings.ToLower(dstId)][0].AccumulateAPIForUrl(dstUrl)
+
+	// Get a signature from each node that hasn't signed
+	for _, node := range nodes[strings.ToLower(srcId)] {
+		if signed[*(*[32]byte)(node.Info.PublicKey)] {
+			continue
+		}
+
+		// Make a client for the source
+		srcClient := node.AccumulateAPIForUrl(srcUrl)
+
+		// Query and execute the anchor
+		querySynthAndExecute(srcClient, dstClient, srcUrl, dstUrl, res.Status.SequenceNumber)
+	}
 }
