b61aead20d815552ef9061f0dab25cadcefe066f
Ethan Reesor <ethan.reesor@gmail.com>
2023-02-22 20:18:18 +0000

    Introduce authority signatures [#3172]
    Closes #3172. Implements authority signatures. When a user signature is processed, an authority signature is produced if the authority is satisfied. The delegation path, if any, is included in the authority signature. When a delegated authority signature is processed, another authority signature is produced if the delegator is satisfied. This is repeated until the last delegator. When a non-delegated authority signature is processed (either because the user signature was not delegated or because the end of the delegation chain was reached), the transaction is executed if each (direct) authority has signed the transaction.
    
    Changelog: refactor
    
    Former-commit-id: c25dda0fed6faa22e877b4584f154d4253cd10dd

--- test/e2e/sig_delegated_test.go
+++ test/e2e/sig_delegated_test.go
@@ -10,7 +10,6 @@ import (
 	"math/big"
 	"testing"
 
-	"gitlab.com/accumulatenetwork/accumulate/internal/core"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/build"
 	"gitlab.com/accumulatenetwork/accumulate/protocol"
 	. "gitlab.com/accumulatenetwork/accumulate/protocol"
@@ -30,14 +29,15 @@ func TestDoubleDelegated(t *testing.T) {
 	charlieKey := acctesting.GenerateKey(charlie)
 
 	// Initialize
-	g := new(core.GlobalValues)
-	g.ExecutorVersion = ExecutorVersionV2
 	sim := NewSim(t,
 		simulator.MemoryDatabase,
-		simulator.SimpleNetwork(t.Name(), 1, 1),
-		simulator.GenesisWith(GenesisTime, g),
+		simulator.SimpleNetwork(t.Name(), 3, 3),
+		simulator.GenesisWithVersion(GenesisTime, ExecutorVersionV2),
 	)
 
+	sim.SetRoute(alice, "BVN0")
+	sim.SetRoute(bob, "BVN1")
+	sim.SetRoute(charlie, "BVN2")
 	MakeIdentity(t, sim.DatabaseFor(alice), alice, aliceKey[32:])
 	MakeIdentity(t, sim.DatabaseFor(bob), bob, bobKey[32:])
 	MakeIdentity(t, sim.DatabaseFor(charlie), charlie, charlieKey[32:])
@@ -61,6 +61,46 @@ func TestDoubleDelegated(t *testing.T) {
 			BurnTokens(1, protocol.AcmePrecisionPower).
 			SignWith(charlie, "book", "1").Version(1).Timestamp(1).PrivateKey(charlieKey).
 			Delegator(bob, "book", "1").Delegator(alice, "book", "1")))
+
+	sim.StepUntil(
+		Txn(st.TxID).Succeeds(),
+		Txn(st.TxID).Produced().Succeeds())
+}
+
+func TestSingleDelegated(t *testing.T) {
+	// Tests AC-3069
+	alice := AccountUrl("alice")
+	bob := AccountUrl("bob")
+	aliceKey := acctesting.GenerateKey(alice)
+	bobKey := acctesting.GenerateKey(bob)
+
+	// Initialize
+	sim := NewSim(t,
+		simulator.MemoryDatabase,
+		simulator.SimpleNetwork(t.Name(), 3, 1),
+		simulator.GenesisWithVersion(GenesisTime, ExecutorVersionV2),
+	)
+
+	sim.SetRoute(alice, "BVN0")
+	sim.SetRoute(bob, "BVN1")
+	MakeIdentity(t, sim.DatabaseFor(alice), alice, aliceKey[32:])
+	MakeIdentity(t, sim.DatabaseFor(bob), bob, bobKey[32:])
+
+	MakeAccount(t, sim.DatabaseFor(alice), &TokenAccount{Url: alice.JoinPath("tokens"), TokenUrl: AcmeUrl(), Balance: *big.NewInt(1e15)})
+
+	UpdateAccount(t, sim.DatabaseFor(alice), alice.JoinPath("book", "1"), func(p *KeyPage) {
+		p.AddKeySpec(&KeySpec{Delegate: bob.JoinPath("book")})
+		p.CreditBalance = 1e9
+	})
+	UpdateAccount(t, sim.DatabaseFor(bob), bob.JoinPath("book", "1"), func(p *KeyPage) {
+		p.CreditBalance = 1e9
+	})
+
+	st := sim.SubmitSuccessfully(MustBuild(t,
+		build.Transaction().For(alice, "tokens").
+			BurnTokens(1, protocol.AcmePrecisionPower).
+			SignWith(bob, "book", "1").Version(1).Timestamp(1).PrivateKey(bobKey).
+			Delegator(alice, "book", "1")))
 
 	sim.StepUntil(
 		Txn(st.TxID).Succeeds(),
