1d9941e3376b13676fb8ee3221303a6192b05081
Ethan Reesor <ethan.reesor@gmail.com>
2023-03-02 16:29:38 +0000

    Allow reactivation of protocol updates [#3228]
    Closes #3228. Allows reactivation of the latest executor version. Adds a new executor version so we can bump the version.
    
    Changelog: fix
    
    Former-commit-id: 256dcd8db3a7a3686dd92fe8b8bed0cc7dac0d53

--- internal/chain/activate_protocol_version.go
+++ internal/chain/activate_protocol_version.go
@@ -41,8 +41,8 @@ func (ActivateProtocolVersion) Validate(st *StateManager, tx *Delivery) (protoco
 	}
 
 	// Verify the version number is higher than the current number
-	if body.Version <= ledger.ExecutorVersion {
-		return nil, errors.Format(errors.BadRequest, "new version (%d) <= old version (%d)", body.Version, ledger.ExecutorVersion)
+	if body.Version < ledger.ExecutorVersion {
+		return nil, errors.Format(errors.BadRequest, "new version (%d) < old version (%d)", body.Version, ledger.ExecutorVersion)
 	}
 
 	// Update the version number
