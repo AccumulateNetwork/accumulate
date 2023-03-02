78d93e308cdf85689d82dc8f32f87fb23369c043
Ethan Reesor <ethan.reesor@gmail.com>
2023-03-02 18:33:49 +0000

    JSON marshaller for basic types [#3181]
    Closes #3181. Adds JSON marshalling for basic types to encoder machines.
    
    Changelog: refactor
    
    Former-commit-id: 8bc69c4b844234fcb3e50e2a8662e086341a61a8

---
+++ pkg/types/encoding/machine_test.go
@@ -0,0 +1,31 @@
+// Copyright 2023 The Accumulate Authors
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+package encoding_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gitlab.com/accumulatenetwork/accumulate/pkg/errors"
+	acctesting "gitlab.com/accumulatenetwork/accumulate/test/testing"
+)
+
+func TestMachineJSON(t *testing.T) {
+	acctesting.EnableDebugFeatures()
+
+	x := errors.NotFound.With("I couldn't find the thing")
+	x = errors.UnknownError.WithFormat("error doing thing: %w", x)
+
+	b, err := json.Marshal(x)
+	require.NoError(t, err)
+
+	y := new(errors.Error)
+	require.NoError(t, json.Unmarshal(b, y))
+
+	require.True(t, x.Equal(y))
+}
