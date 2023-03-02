067dc3c3c1e94c0d1d19a4fc5a3f784227625e76
Ethan Reesor <ethan.reesor@gmail.com>
2023-03-02 17:39:23 +0000

    Validate API v3 p2p layer [#3221]
    Closes #3221. Updates the simulator's ListenAndServe method to support API v3 (HTTP, P2P, and WebSocket). Adds a variant of the validation test that validates API v3's peer to peer layer by runs all service calls through it.
    
    Changelog: test
    
    Former-commit-id: 06f8ea267f3b87eb228ae2939dd5be8f0911d3b7

---
+++ test/simulator/signing.go
@@ -0,0 +1,85 @@
+// Copyright 2023 The Accumulate Authors
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+package simulator
+
+import (
+	"fmt"
+
+	"github.com/btcsuite/btcd/btcec"
+	"gitlab.com/accumulatenetwork/accumulate/pkg/client/signing"
+	"gitlab.com/accumulatenetwork/accumulate/protocol"
+)
+
+func (s *Simulator) SignWithNode(partition string, i int) nodeSigner {
+	return nodeSigner{s.partitions[partition].nodes[i]}
+}
+
+type nodeSigner struct {
+	*Node
+}
+
+var _ signing.Signer = nodeSigner{}
+
+func (n nodeSigner) Key() []byte { return n.init.PrivValKey }
+
+func (n nodeSigner) SetPublicKey(sig protocol.Signature) error {
+	k := n.init.PrivValKey
+	switch sig := sig.(type) {
+	case *protocol.LegacyED25519Signature:
+		sig.PublicKey = k[32:]
+
+	case *protocol.ED25519Signature:
+		sig.PublicKey = k[32:]
+
+	case *protocol.RCD1Signature:
+		sig.PublicKey = k[32:]
+
+	case *protocol.BTCSignature:
+		_, pubKey := btcec.PrivKeyFromBytes(btcec.S256(), k)
+		sig.PublicKey = pubKey.SerializeCompressed()
+
+	case *protocol.BTCLegacySignature:
+		_, pubKey := btcec.PrivKeyFromBytes(btcec.S256(), k)
+		sig.PublicKey = pubKey.SerializeUncompressed()
+
+	case *protocol.ETHSignature:
+		_, pubKey := btcec.PrivKeyFromBytes(btcec.S256(), k)
+		sig.PublicKey = pubKey.SerializeUncompressed()
+
+	default:
+		return fmt.Errorf("cannot set the public key on a %T", sig)
+	}
+
+	return nil
+}
+
+func (n nodeSigner) Sign(sig protocol.Signature, sigMdHash, message []byte) error {
+	k := n.init.PrivValKey
+	switch sig := sig.(type) {
+	case *protocol.LegacyED25519Signature:
+		protocol.SignLegacyED25519(sig, k, sigMdHash, message)
+
+	case *protocol.ED25519Signature:
+		protocol.SignED25519(sig, k, sigMdHash, message)
+
+	case *protocol.RCD1Signature:
+		protocol.SignRCD1(sig, k, sigMdHash, message)
+
+	case *protocol.BTCSignature:
+		return protocol.SignBTC(sig, k, sigMdHash, message)
+
+	case *protocol.BTCLegacySignature:
+		return protocol.SignBTCLegacy(sig, k, sigMdHash, message)
+
+	case *protocol.ETHSignature:
+		return protocol.SignETH(sig, k, sigMdHash, message)
+
+	default:
+		return fmt.Errorf("cannot sign %T with a key", sig)
+	}
+	return nil
+}
