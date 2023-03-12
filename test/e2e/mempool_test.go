0ff95381c46b40af47b6f0abf619c30e161c5a13
Ethan Reesor <ethan.reesor@gmail.com>
2023-03-12 17:36:37 -0500

    fix

--- test/e2e/mempool_test.go
+++ test/e2e/mempool_test.go
@@ -14,11 +14,14 @@ import (
 	"github.com/stretchr/testify/require"
 	"github.com/tendermint/tendermint/rpc/client/http"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/types/messaging"
+	acctesting "gitlab.com/accumulatenetwork/accumulate/test/testing"
 )
 
 // TestUnconfirmed queries the node for transactions pending in the mempool and
 // decodes them.
 func TestUnconfirmed(t *testing.T) {
+	acctesting.SkipCI(t, "This test should only be run manually")
+
 	addr := "http://bvn0-seed.testnet.accumulatenetwork.io:16592"
 	c, err := http.New(addr, addr+"/websocket")
 	require.NoError(t, err)
