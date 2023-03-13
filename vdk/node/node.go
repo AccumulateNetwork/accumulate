3689a5ce01fc139d78f45a8ee736510f031cca15
Dennis B <dennis@factoid.org>
2023-03-13 12:16:12 -0500

    added missing headers

--- vdk/node/node.go
+++ vdk/node/node.go
@@ -1,4 +1,10 @@
-package vdk
+// Copyright 2023 The Accumulate Authors
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+package node
 
 import (
 	"bytes"
