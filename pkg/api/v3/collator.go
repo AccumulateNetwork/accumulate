39fe44b7f00fe804189773c09fae347268577d67
Ethan Reesor <ethan.reesor@gmail.com>
2023-03-14 18:32:43 +0000

    Collate minor blocks [#3251]
    Closes #3251. Updates `pkg/api/v3.Collator` to make additional queries to populate the `Anchored` entries of minor block records. Also add batching support to reduce the number of connections made.
    
    Changelog: feature
    
    Former-commit-id: e5c4f4210025ee2dac6c9668aa566a438a0102ee

--- pkg/api/v3/collator.go
+++ pkg/api/v3/collator.go
@@ -22,12 +22,23 @@ type Collator struct {
 }
 
 func (c *Collator) Query(ctx context.Context, scope *url.URL, query Query) (Record, error) {
+	ctx, cancel, _ := ContextWithBatchData(ctx)
+	defer cancel()
+
 	switch query := query.(type) {
 	case *MessageHashSearchQuery:
 		r, err := c.messageHashSearch(ctx, scope, query)
 		if r != nil || err != nil {
 			return r, errors.UnknownError.Wrap(err)
 		}
+
+	case *BlockQuery:
+		r, err := c.Querier.Query(ctx, scope, query)
+		if err != nil {
+			return nil, errors.UnknownError.Wrap(err)
+		}
+		err = c.visitBlockResponse(ctx, r)
+		return r, errors.UnknownError.Wrap(err)
 	}
 
 	return c.Querier.Query(ctx, scope, query)
@@ -69,4 +80,49 @@ func (c *Collator) messageHashSearch(ctx context.Context, scope *url.URL, query 
 	rr.Records = values
 	rr.Total = total
 	return rr, nil
+}
+
+func (c *Collator) visitBlockResponse(ctx context.Context, r Record) error {
+	switch r := r.(type) {
+	case *RecordRange[Record]:
+		for _, r := range r.Records {
+			err := c.visitBlockResponse(ctx, r)
+			if err != nil {
+				return errors.UnknownError.Wrap(err)
+			}
+		}
+
+	case *MajorBlockRecord:
+		if r.MinorBlocks != nil {
+			for _, r := range r.MinorBlocks.Records {
+				err := c.visitBlockResponse(ctx, r)
+				if err != nil {
+					return errors.UnknownError.Wrap(err)
+				}
+			}
+		}
+
+	case *MinorBlockRecord:
+		if r.Anchored == nil {
+			break
+		}
+		for i, a := range r.Anchored.Records {
+			b, err := c.Querier.Query(ctx, a.Source, &BlockQuery{Minor: &a.Index})
+			switch {
+			case err == nil:
+				break
+			case errors.Code(err).IsClientError():
+				continue // Not found or something, ignore
+			default:
+				return errors.UnknownError.Wrap(err)
+			}
+			mbr, ok := b.(*MinorBlockRecord)
+			if !ok {
+				continue // Invalid response, ignore
+			}
+			r.Anchored.Records[i] = mbr
+		}
+	}
+
+	return nil
 }
