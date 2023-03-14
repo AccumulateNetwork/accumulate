61bbaeabbac5b652b242447dc609436ec8f811ed
Ethan Reesor <ethan.reesor@gmail.com>
2023-03-14 16:56:57 +0000

    Record messages on the signature chain [#3245]
    Closes #3245. Records signature requests and credit payments on the recipient's signature chain and slightly refactors the code that does the same for signatures.
    
    Changelog: fix
    
    Former-commit-id: 160034d340b3b08ddde9f40c2acb1028f678c112

--- internal/core/execute/v2/block/sig_authority.go
+++ internal/core/execute/v2/block/sig_authority.go
@@ -151,26 +151,11 @@ func (x AuthoritySignature) processDelegated(batch *database.Batch, ctx *Signatu
 		return errors.UnknownError.Wrap(err)
 	}
 
-	// Add the signature to the signer's chain
-	hash := ctx.signature.Hash()
-	chain := batch.Account(signer.GetUrl()).SignatureChain().Inner()
-	head, err := chain.Head().Get()
-	if err != nil {
-		return errors.UnknownError.WithFormat("load signature chain head: %w", err)
-	}
-	chainIndex := head.Count
-
-	err = chain.AddHash(hash, false)
-	if err != nil {
-		return errors.UnknownError.WithFormat("add to signature chain: %w", err)
-	}
-
 	// Add the signature to the transaction's signature set
 	err = addSignature(batch, ctx, signer, &database.SignatureSetEntry{
-		KeyIndex:   uint64(keyIndex),
-		ChainIndex: uint64(chainIndex),
-		Version:    signer.GetVersion(),
-		Hash:       ctx.message.Hash(),
+		KeyIndex: uint64(keyIndex),
+		Version:  signer.GetVersion(),
+		Hash:     ctx.message.Hash(),
 	})
 	if err != nil {
 		return errors.UnknownError.Wrap(err)
