e88a6da31c7d0c6e25cf5cee16b8a50e6559d28b
Ethan Reesor <ethan.reesor@gmail.com>
2023-03-22 15:55:00 +0000

    Marshal record keys [#3264]
    Closes #3264. Implements marshalling of database record keys and moves the record key type to `pkg/types/record`.
    
    Changelog: refactor

---
+++ pkg/types/record/types.go
@@ -0,0 +1,13 @@
+// Copyright 2023 The Accumulate Authors
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+package record
+
+//go:generate go run gitlab.com/accumulatenetwork/accumulate/tools/cmd/gen-enum --package record -i TypeCode --rename TypeCode:typeCode ../../../tools/internal/typegen/enums.yml
+
+// typeCode is identical to tools/internal/typegen.TypeCode, but we can't use
+// that here because its internal, so we duplicate it.
+type typeCode uint64
