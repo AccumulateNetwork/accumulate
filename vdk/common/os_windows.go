63110bfeb8b139a3b62ded89de91ba0415838cd3
Dennis B <dennis@factoid.org>
2023-03-30 16:18:45 -0500

    fixed error message

--- vdk/common/os_windows.go
+++ vdk/common/os_windows.go
@@ -8,7 +8,7 @@ package vdk
 
 import "errors"
 
-var ErrOnePassNotSupported = errors.New("1Password integration is not supported on macOS")
+var ErrOnePassNotSupported = errors.New("1Password integration is not supported on Windows")
 
 func canExecOnePass() error {
 	return ErrOnePassNotSupported
