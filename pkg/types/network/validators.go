dad3a71951361507a410c90a2c2aa82c4981b59b
Ethan Reesor <ethan.reesor@gmail.com>
2023-03-31 20:54:54 -0500

    .

--- pkg/types/network/validators.go
+++ pkg/types/network/validators.go
@@ -7,55 +7,9 @@
 package network
 
 import (
-	"fmt"
 	"math"
 	"strings"
 )
-
-type ValidatorUpdate int
-
-const ValidatorUpdateAdd = 1
-const ValidatorUpdateRemove = 2
-
-func (g *GlobalValues) DiffValidators(h *GlobalValues, partitionID string) (map[[32]byte]ValidatorUpdate, error) {
-	updates := map[[32]byte]ValidatorUpdate{}
-
-	// Mark the old keys for deletion
-	if g != nil {
-		for _, v := range g.Network.Validators {
-			if !v.IsActiveOn(partitionID) {
-				continue
-			}
-
-			if len(v.PublicKey) != 32 {
-				return nil, fmt.Errorf("invalid ED25519 key: wrong length")
-			}
-
-			updates[*(*[32]byte)(v.PublicKey)] = ValidatorUpdateRemove
-		}
-	}
-
-	// Process the new keys
-	for _, v := range h.Network.Validators {
-		if !v.IsActiveOn(partitionID) {
-			continue
-		}
-		if len(v.PublicKey) != 32 {
-			return nil, fmt.Errorf("invalid ED25519 key: wrong length")
-		}
-		k32 := *(*[32]byte)(v.PublicKey)
-
-		if _, ok := updates[k32]; ok {
-			// If the key is present in new and old, unmark it
-			delete(updates, k32)
-		} else {
-			// If the key is only present in new, mark it for addition
-			updates[k32] = 1
-		}
-	}
-
-	return updates, nil
-}
 
 type globalValueMemos struct {
 	threshold map[string]uint64
