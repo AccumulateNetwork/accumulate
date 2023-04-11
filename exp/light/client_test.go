9727f8dfd38b853fc354d21ac25dad821b53ce4d
Ethan Reesor <ethan.reesor@gmail.com>
2023-04-11 11:51:22 -0500

    calculate refunds for the bridge

--- exp/light/client_test.go
+++ exp/light/client_test.go
@@ -8,12 +8,16 @@ package light
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"math/big"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"gitlab.com/accumulatenetwork/accumulate/internal/database/smt/storage/memory"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/build"
+	client "gitlab.com/accumulatenetwork/accumulate/pkg/client/api/v2"
+	"gitlab.com/accumulatenetwork/accumulate/pkg/types/messaging"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/types/network"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/url"
 	"gitlab.com/accumulatenetwork/accumulate/protocol"
@@ -109,4 +113,100 @@ func TestClient(t *testing.T) {
 	require.NoError(t, err)
 	err = c.IndexAccountChains(ctx, alice.JoinPath("book", "1"))
 	require.NoError(t, err)
+}
+
+func TestBridge(t *testing.T) {
+	t.Skip("Manual")
+
+	var err error
+	c := new(Client)
+	c.store = memory.New(nil)
+	c.v2, err = client.New("mainnet")
+	require.NoError(t, err)
+
+	ctx := context.Background()
+	acct := protocol.AccountUrl("bridge.acme", "1-ACME")
+	err = c.PullAccount(ctx, acct)
+	require.NoError(t, err)
+	err = c.PullTransactionsForAccount(ctx, acct, "main")
+	require.NoError(t, err)
+	err = c.IndexAccountChains(ctx, acct)
+	require.NoError(t, err)
+
+	batch := c.OpenDB(false)
+	defer batch.Discard()
+	head, err := batch.Account(acct).MainChain().Head().Get()
+	require.NoError(t, err)
+	entries, err := batch.Account(acct).MainChain().Inner().GetRange(0, head.Count)
+	require.NoError(t, err)
+
+	var txns []*url.TxID
+	deposits := map[[32]byte]*url.TxID{}
+	for _, e := range entries {
+		var msg *messaging.TransactionMessage
+		err = batch.Message2(e).Main().GetAs(&msg)
+		require.NoError(t, err)
+
+		switch body := msg.Transaction.Body.(type) {
+		case *protocol.SyntheticDepositTokens:
+			txns = append(txns, body.Cause)
+			deposits[body.Cause.Hash()] = msg.ID()
+
+		case *protocol.SendTokens:
+			break
+			for _, to := range body.To {
+				fmt.Printf("Sent %s tokens to %v (%v)\n",
+					protocol.FormatBigAmount(&to.Amount, protocol.AcmePrecisionPower),
+					to.Url,
+					msg.ID())
+			}
+		}
+	}
+
+	err = c.PullTransactions(ctx, txns...)
+	require.NoError(t, err)
+
+	var outputs []*protocol.TokenRecipient
+	index := map[[32]byte]int{}
+	addOutput := func(u *url.URL, amount *big.Int) {
+		// Find the existing output or create a new one
+		var out *protocol.TokenRecipient
+		i, ok := index[u.AccountID32()]
+		if ok {
+			out = outputs[i]
+		} else {
+			index[u.AccountID32()] = len(outputs)
+			out = &protocol.TokenRecipient{Url: u}
+			outputs = append(outputs, out)
+		}
+
+		out.Amount.Add(&out.Amount, amount)
+	}
+
+	for _, id := range txns {
+		var msg *messaging.TransactionMessage
+		err = batch.Message(id.Hash()).Main().GetAs(&msg)
+		require.NoError(t, err)
+
+		if msg.Transaction.Header.Memo != "" {
+			continue
+		}
+
+		send, ok := msg.Transaction.Body.(*protocol.SendTokens)
+		if !ok {
+			continue
+		}
+		require.Len(t, send.To, 1) // Need to update the test if there are any multi-output sends
+
+		fmt.Printf("Received %s tokens from %v without memo (%v -> %v)\n",
+			protocol.FormatBigAmount(&send.To[0].Amount, protocol.AcmePrecisionPower),
+			msg.Transaction.Header.Principal,
+			id, deposits[id.Hash()])
+
+		addOutput(msg.Transaction.Header.Principal, &send.To[0].Amount)
+	}
+
+	b, err := json.MarshalIndent(outputs, "", "  ")
+	require.NoError(t, err)
+	fmt.Printf("%s\n", b)
 }
