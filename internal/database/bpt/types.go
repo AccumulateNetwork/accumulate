91d34e97382599fbb2bf6399b2badfba4b8fcd9d
Ethan Reesor <ethan.reesor@gmail.com>
2023-04-11 20:26:35 +0000

    Implement a data model BPT [#3266]
    Closes #3266. Implements a BPT based on the data model approach.
    
    Changelog: refactor

---
+++ internal/database/bpt/types.go
@@ -0,0 +1,13 @@
+// Copyright 2023 The Accumulate Authors
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+package bpt
+
+//go:generate go run gitlab.com/accumulatenetwork/accumulate/tools/cmd/gen-enum --package bpt enums.yml
+//go:generate go run gitlab.com/accumulatenetwork/accumulate/tools/cmd/gen-types --package bpt types.yml
+//go:generate go run gitlab.com/accumulatenetwork/accumulate/tools/cmd/gen-types --package bpt --language go-union --out unions_gen.go types.yml
+//go:generate go run gitlab.com/accumulatenetwork/accumulate/tools/cmd/gen-model --package bpt model.yml
+//go:generate go run gitlab.com/accumulatenetwork/accumulate/tools/cmd/gen-model --package bpt --out model_gen_test.go model_test.yml
