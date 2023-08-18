efa800f92c02770859e829ddf9ba9e3bc0429f9b
Ethan Reesor <ethan.reesor@gmail.com>
2023-08-18 17:18:23 -0500

    Hack around the results problem

--- tools/cmd/debug/heal_test.go
+++ tools/cmd/debug/heal_test.go
@@ -7,6 +7,7 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -16,6 +17,9 @@ import (
 	"github.com/multiformats/go-multiaddr"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/rpc/client/http"
+	coretypes "github.com/tendermint/tendermint/rpc/core/types"
 	"gitlab.com/accumulatenetwork/accumulate/internal/api/routing"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/api/v3"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/api/v3/message"
@@ -141,3 +145,191 @@ func TestHealAnchors(t *testing.T) {
 		assert.True(t, st.Success, st.Message)
 	}
 }
+
+func TestBlockResults(t *testing.T) {
+	t.Skip("Manual")
+
+	first, err := http.New("http://apollo-mainnet.accumulate.defidevs.io:16592", "http://apollo-mainnet.accumulate.defidevs.io:16592/ws")
+	require.NoError(t, err)
+
+	ni, err := first.NetInfo(context.Background())
+	require.NoError(t, err)
+
+	var nodes []*http.HTTP
+	for _, peer := range ni.Peers {
+		u := fmt.Sprintf("http://%s:16592", peer.RemoteIP)
+		n, err := http.New(u, u+"/ws")
+		require.NoError(t, err)
+		nodes = append(nodes, n)
+	}
+
+	var block int64 = 12614506
+	firstResult, err := first.BlockResults(context.Background(), &block)
+	require.NoError(t, err)
+
+	for _, r := range firstResult.TxsResults {
+		rs := new(protocol.TransactionResultSet)
+		require.NoError(t, rs.UnmarshalBinary(r.Data))
+		for _, r := range rs.Results {
+			if r.Result == nil {
+				fmt.Println("nil result")
+			}
+		}
+	}
+	t.FailNow()
+
+	for i, n := range nodes {
+		t.Run(ni.Peers[i].RemoteIP, func(t *testing.T) {
+			r, err := n.BlockResults(context.Background(), &block)
+			require.NoError(t, err)
+			equalResults(t, firstResult, r)
+		})
+	}
+}
+
+func equalResults(t *testing.T, a, b *coretypes.ResultBlockResults) {
+	if len(a.TxsResults) != len(b.TxsResults) {
+		t.Fatal("Different number of results")
+	}
+
+	for i, a := range a.TxsResults {
+		b := b.TxsResults[i]
+		if a.Code != b.Code {
+			t.Log("different Code for", i)
+			t.Fail()
+		}
+		if !bytes.Equal(a.Data, b.Data) {
+			// t.Log("different Data for", i)
+			t.Fail()
+
+			c := new(protocol.TransactionResultSet)
+			require.NoError(t, c.UnmarshalBinary(a.Data))
+			d := new(protocol.TransactionResultSet)
+			require.NoError(t, d.UnmarshalBinary(b.Data))
+
+			if len(c.Results) != len(d.Results) {
+				t.Log("different number of results for", i)
+			} else {
+				for j, a := range c.Results {
+					b := d.Results[j]
+					if a.Equal(b) {
+						continue
+					}
+
+					cmpEq(t, a.TxID, b.TxID, i, j, "TxID")
+					cmp2(t, a.Code, b.Code, i, j, "Code")
+					cmpEq(t, a.Error, b.Error, i, j, "Error")
+					cmp3(t, a.Result, b.Result, i, j, "Result")
+					cmp2(t, a.Received, b.Received, i, j, "Received")
+					cmpEq(t, a.Initiator, b.Initiator, i, j, "Initiator")
+					cmp3(t, a.Signers, b.Signers, i, j, "Signers")
+					cmpEq(t, a.SourceNetwork, b.SourceNetwork, i, j, "SourceNetwork")
+					cmpEq(t, a.DestinationNetwork, b.DestinationNetwork, i, j, "DestinationNetwork")
+					cmp2(t, a.SequenceNumber, b.SequenceNumber, i, j, "SequenceNumber")
+					cmp2(t, a.GotDirectoryReceipt, b.GotDirectoryReceipt, i, j, "GotDirectoryReceipt")
+					cmpEq(t, a.Proof, b.Proof, i, j, "Proof")
+					cmp3(t, a.AnchorSigners, b.AnchorSigners, i, j, "AnchorSigners")
+				}
+			}
+		}
+		if a.Log != b.Log {
+			t.Log("different Log for", i)
+			t.Fail()
+		}
+		if a.Info != b.Info {
+			t.Log("different Info for", i)
+			t.Fail()
+		}
+		if a.GasWanted != b.GasWanted {
+			t.Log("different GasWanted for", i)
+			t.Fail()
+		}
+		if a.GasUsed != b.GasUsed {
+			t.Log("different GasUsed for", i)
+			t.Fail()
+		}
+		if a.Codespace != b.Codespace {
+			t.Log("different Codespace for", i)
+			t.Fail()
+		}
+		// if t.Failed() {
+		// 	printTxResult(t, "A", a)
+		// 	printTxResult(t, "B", b)
+		// 	// t.FailNow()
+		// }
+	}
+}
+
+func cmpEq[T interface {
+	Equal(T) bool
+	comparable
+}](t *testing.T, a, b T, i, j int, field string) {
+	var z T
+	if a == b {
+		return
+	}
+	switch {
+	case a == z:
+		t.Logf("Different result for %d part %d: field %s: A is zero but B is not", i, j, field)
+	case b == z:
+		t.Logf("Different result for %d part %d: field %s: B is zero but A is not", i, j, field)
+	case !a.Equal(b):
+		t.Logf("Different result for %d part %d: field %s: A and B are both set but not equal", i, j, field)
+	}
+}
+
+func cmp2[T comparable](t *testing.T, a, b T, i, j int, field string) {
+	if a == b {
+		return
+	}
+	t.Logf("Different result for %d part %d: field %s", i, j, field)
+}
+
+func cmp3(t *testing.T, a, b any, i, j int, field string) {
+	if assert.ObjectsAreEqual(a, b) {
+		return
+	}
+	switch {
+	case a == nil:
+		t.Logf("Different result for %d part %d: field %s: A is zero but B is not", i, j, field)
+	case b == nil:
+		t.Logf("Different result for %d part %d: field %s: B is zero but A is not", i, j, field)
+	case !assert.ObjectsAreEqual(a, b):
+		t.Logf("Different result for %d part %d: field %s: A and B are both set but not equal", i, j, field)
+	}
+}
+
+func printTxResult(t *testing.T, prefix string, r *abci.ResponseDeliverTx) {
+	// fmt.Printf("Code:\t%v\n", r.Code)
+	// // fmt.Printf("Data:\t%v\n", r.Data)
+	// fmt.Printf("Log:\t%v\n", r.Log)
+	// fmt.Printf("Info:\t%v\n", r.Info)
+	// fmt.Printf("GasWanted:\t%v\n", r.GasWanted)
+	// fmt.Printf("GasUsed:\t%v\n", r.GasUsed)
+	// // fmt.Printf("Events:\t%v\n", r.Events)
+	// fmt.Printf("Codespace:\t%v\n", r.Codespace)
+
+	rset := new(protocol.TransactionResultSet)
+	require.NoError(t, rset.UnmarshalBinary(r.Data))
+	// fmt.Println("Data:")
+	for _, r := range rset.Results {
+
+		b, err := json.Marshal(r)
+		require.NoError(t, err)
+		fmt.Printf("  %s %s\n", prefix, b)
+	}
+}
+
+// TxID      *url.TxID         `json:"txID,omitempty" form:"txID" query:"txID" validate:"required"`
+// Code      errors2.Status    `json:"code,omitempty" form:"code" query:"code" validate:"required"`
+// Error     *errors2.Error    `json:"error,omitempty" form:"error" query:"error" validate:"required"`
+// Result    TransactionResult `json:"result,omitempty" form:"result" query:"result" validate:"required"`
+// Received uint64 `json:"received,omitempty" form:"received" query:"received" validate:"required"`
+// Initiator *url.URL `json:"initiator,omitempty" form:"initiator" query:"initiator" validate:"required"`
+// Signers []Signer `json:"signers,omitempty" form:"signers" query:"signers" validate:"required"`
+// SourceNetwork *url.URL `json:"sourceNetwork,omitempty" form:"sourceNetwork" query:"sourceNetwork" validate:"required"`
+// DestinationNetwork *url.URL `json:"destinationNetwork,omitempty" form:"destinationNetwork" query:"destinationNetwork" validate:"required"`
+// SequenceNumber uint64 `json:"sequenceNumber,omitempty" form:"sequenceNumber" query:"sequenceNumber" validate:"required"`
+// GotDirectoryReceipt bool `json:"gotDirectoryReceipt,omitempty" form:"gotDirectoryReceipt" query:"gotDirectoryReceipt" validate:"required"`
+// Proof *merkle.Receipt `json:"proof,omitempty" form:"proof" query:"proof" validate:"required"`
+// AnchorSigners [][]byte `json:"anchorSigners,omitempty" form:"anchorSigners" query:"anchorSigners" validate:"required"`
