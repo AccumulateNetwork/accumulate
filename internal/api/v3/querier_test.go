295284868c40b1635132df222d41ab51a882f99c
Ethan Reesor <ethan.reesor@gmail.com>
2023-08-24 10:57:20 -0500

    spelling

--- internal/api/v3/querier_test.go
+++ internal/api/v3/querier_test.go
@@ -344,7 +344,7 @@ func (s *QuerierTestSuite) TestQueryMinorBlock() {
 }
 
 func (s *QuerierTestSuite) TestQueryDirectoryMinorBlock() {
-	s.T().Skip("Flakey")
+	s.T().Skip("Flaky")
 	r, err := s.QuerierFor(DnUrl()).QueryMinorBlock(context.Background(), DnUrl(), &api.BlockQuery{Minor: uintp(4)})
 	s.Require().NoError(err)
 	s.Require().NotEmpty(r.Anchored.Records)
