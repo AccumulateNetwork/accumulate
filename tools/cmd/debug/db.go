af85e6209a17700335329d63d5a07cd0f72207aa
Ethan Reesor <ethan.reesor@gmail.com>
2023-08-24 17:02:29 +0000

    Patch the database [#3383]
    Closes #3383. Adds various tools for comparing databases, building summaries, patches, fixes, diffs, etc. More importantly, adds a script and fix file that can be used to revert DN block 12614506, to fix the stall.
    
    Changelog: feature

---
+++ tools/cmd/debug/db.go
@@ -0,0 +1,19 @@
+// Copyright 2023 The Accumulate Authors
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+package main
+
+import "github.com/spf13/cobra"
+
+var cmdDb = &cobra.Command{
+	Use:     "database",
+	Aliases: []string{"db"},
+	Short:   "Database utilities",
+}
+
+func init() {
+	cmd.AddCommand(cmdDb)
+}
