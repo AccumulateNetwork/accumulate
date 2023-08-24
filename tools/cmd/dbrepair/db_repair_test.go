17c0cdff2f65e5a1f90080648432d7153d972cc7
Ethan Reesor <ethan.reesor@gmail.com>
2023-08-24 17:02:29 +0000

    Patch the database [#3383]
    Closes #3383. Adds various tools for comparing databases, building summaries, patches, fixes, diffs, etc. More importantly, adds a script and fix file that can be used to revert DN block 12614506, to fix the stall.
    
    Changelog: feature
    
    Former-commit-id: 3a5552e009af3efd961506506c8a642ee12689ce

---
+++ tools/cmd/dbrepair/db_repair_test.go
@@ -0,0 +1,114 @@
+// Copyright 2023 The Accumulate Authors
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"path/filepath"
+	"testing"
+
+	"github.com/dgraph-io/badger"
+	"github.com/fatih/color"
+)
+
+func TestDbRepair(t *testing.T) {
+	color.NoColor = false
+
+	dir := t.TempDir()                            // create temporary directory that will auto delete after test
+	goodDB := filepath.Join(dir, "good.db")       // A good db
+	badDB := filepath.Join(dir, "bad.db")         // a bad db with added, modified, and deleted key/value pairs
+	summaryF := filepath.Join(dir, "summary.dat") // file for the summary data of good db
+	diffF := filepath.Join(dir, "diff.dat")       // file for the diff between good db vs bad db
+	fixF := filepath.Join(dir, "fix.dat")         // The fix file that can be distributed to fix nodes
+
+	// Note this documents what we want to do.  Of course, the
+	// test actually skips sending files back and forth between good
+	// nodes and bad nodes.
+	buildTestDBs(5e3, goodDB, badDB)  // Build Test DBs
+	buildSummary(goodDB, summaryF)    // First go to node with good db and build a summary
+	buildDiff(summaryF, badDB, diffF) // Send the summary to the bad node and create a diff
+	printDiff(diffF, goodDB)          // What is the diff you say? We can print!
+	buildFix(diffF, goodDB, fixF)     // Send the diff back to the good node to build a fix
+	applyFix(fixF, badDB)             // Send the fix to the bad node and apply it
+	applyFix(fixF, badDB)             // Does it fail if you apply twice?
+	buildDiff(summaryF, badDB, diffF) // Send the summary to bad node to check the fix
+	printDiff(diffF, goodDB)          // Send the new diff back to good node to ensure all is good!
+
+	// Note:  If we have the summaryF on the bad node, we can check for correctness without sending
+	// the diff file back to the good node, but we can't produce addresses for nodes to delete.
+	// Do we want to add this?  A print against the summaryF instead of the good db?
+}
+
+// Sanity check of what we are doing with databases
+func TestCompareDB(t *testing.T) {
+	dir := t.TempDir()
+	goodDB := filepath.Join(dir, "good.db")
+	badDB := filepath.Join(dir, "bad.db")
+	summaryF := filepath.Join(dir, "summary.dat")
+
+	buildTestDBs(2e4, goodDB, badDB)
+	buildSummary(goodDB, summaryF)
+
+	gDB, gClose := OpenDB(goodDB)
+	defer gClose()
+
+	bDB, bClose := OpenDB(badDB)
+	defer bClose()
+
+	readAll := func(db *badger.DB) map[[32]byte][]byte {
+		keyValue := make(map[[32]byte][]byte)
+		err := db.View(func(txn *badger.Txn) error {
+			opts := badger.DefaultIteratorOptions
+			opts.PrefetchValues = false // <= What this does is go through the keys in the db
+			it := txn.NewIterator(opts) //    in whatever order is best for badger for speed
+			defer it.Close()
+			for it.Rewind(); it.Valid(); it.Next() {
+				item := it.Item()
+				k := item.Key()        //   Get the key and hash it.  IF the key isn't a
+				kh := sha256.Sum256(k) //   hash, then this takes care of that case.
+				err := item.Value(func(val []byte) error {
+					value := make([]byte, len(val))
+					copy(value, val)
+					keyValue[kh] = value
+					return nil
+				})
+				checkf(err, "on read of value")
+			}
+			return nil
+		})
+		checkf(err, "on viewing keys")
+		return keyValue
+	}
+
+	good := readAll(gDB)
+	bad := readAll(bDB)
+
+	fmt.Printf("Comparing good %d with bad %d\n", len(good), len(bad))
+
+	var cntMod, cntDel, cntAdd int
+
+	for k, v := range good {
+		bv, exists := bad[k]
+		switch {
+		case !exists:
+			cntDel++
+		case !bytes.Equal(bv, v):
+			cntMod++
+		}
+	}
+	for k := range bad {
+		_, exists := good[k]
+		if !exists {
+			cntAdd++
+		}
+	}
+
+	fmt.Printf("Modified: %d Delete: %d Added: %d\n", cntMod, cntDel, cntAdd)
+
+}
---
+++ tools/cmd/dbrepair/main.go
@@ -0,0 +1,151 @@
+// Copyright 2023 The Accumulate Authors
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"os"
+
+	"github.com/dgraph-io/badger"
+	"github.com/spf13/cobra"
+	"gitlab.com/accumulatenetwork/accumulate/pkg/errors"
+)
+
+var cmd = &cobra.Command{
+	Use:   "dbRepair",
+	Short: "utilities to use a good db to build a repair script for a bad db",
+}
+
+var cmdBuildTestDBs = &cobra.Command{
+	Use:   "buildTestDBs [number of entries] [good database] [bad database]",
+	Short: "Number of entries must be greater than 100, should be greater than 5000",
+	Args:  cobra.ExactArgs(3),
+	Run:   runBuildTestDBs,
+}
+
+var cmdBuildSummary = &cobra.Command{
+	Use:   "buildSummary [good database] [summary file]",
+	Short: "Build a Summary file of the keys and values in a good database",
+	Args:  cobra.ExactArgs(2),
+	Run:   runBuildSummary,
+}
+
+var cmdBuildDiff = &cobra.Command{
+	Use:   "buildDiff [summary file] [bad database] [diff File]",
+	Short: "Given the summary data and the bad database, build a diff File to make the bad database good",
+	Args:  cobra.ExactArgs(3),
+	Run:   runBuildDiff,
+}
+
+var cmdPrintDiff = &cobra.Command{
+	Use:   "printDiff [diff file] [good database] [diff File]",
+	Short: "Given the summary data and the good database, print the diff file",
+	Args:  cobra.ExactArgs(2),
+	Run:   runPrintDiff,
+}
+
+var cmdBuildFix = &cobra.Command{
+	Use:   "buildFix [diff file] [good database] [fix file]",
+	Short: "Take the difference between the good and bad database, the good data base, and build a fix file",
+	Args:  cobra.ExactArgs(3),
+	Run:   runBuildFix,
+}
+
+var cmdApplyFix = &cobra.Command{
+	Use:   "applyFix [fix file] [database]",
+	Short: "Applies the fix to the database",
+	Args:  cobra.ExactArgs(2),
+	Run:   runApplyFix,
+}
+
+func init() {
+	cmd.AddCommand(
+		cmdBuildTestDBs,
+		cmdBuildSummary,
+		cmdBuildDiff,
+		cmdPrintDiff,
+		cmdBuildFix,
+		cmdApplyFix,
+	)
+}
+
+func main() {
+	_ = cmd.Execute()
+}
+
+func fatalf(format string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, "Error: "+format+"\n", args...)
+	os.Exit(1)
+}
+
+func check(err error) {
+	if err != nil {
+		err = errors.UnknownError.Skip(1).Wrap(err)
+		fatalf("%+v", err)
+	}
+}
+
+func checkf(err error, format string, otherArgs ...interface{}) {
+	if err != nil {
+		fatalf(format+": %v", append(otherArgs, err)...)
+	}
+}
+
+func check2(_ any, err error) { check(err) }
+
+func OpenDB(dbName string) (*badger.DB, func()) {
+	db, err := badger.Open(badger.DefaultOptions(dbName))
+	checkf(err, "opening db %v", dbName)
+	return db, func() { checkf(db.Close(), "closing db %v", dbName) }
+}
+
+// Read an 8 byte, uint64 value and return it.
+// As a side effect, the first 8 bytes of buff hold the value
+func read8(f *os.File, buff []byte) uint64 {
+	r, err := f.Read(buff[:8]) // Read 64 bits
+	checkf(err, "failed to read count")
+	if r != 8 {
+		fatalf("failed to read a full 64 bit value")
+	}
+	return binary.BigEndian.Uint64(buff[:8])
+}
+
+// Read 32 bytes into a given buffer
+func read32(f *os.File, buff []byte) {
+	r, err := f.Read(buff[:32]) // Read 32
+	checkf(err, "failed to read address")
+	if r != 32 {
+		fatalf("failed to read a full 64 bit value")
+	}
+}
+
+func read(f *os.File, buff []byte) {
+	r, err := f.Read(buff) // Read 32
+	checkf(err, "failed to read value")
+	if r != len(buff) {
+		fatalf("failed to read the full value")
+	}
+}
+
+func write8(f *os.File, v uint64) {
+	var buff [8]byte
+	binary.BigEndian.PutUint64(buff[:], v)
+	i, err := f.Write(buff[:])
+	checkf(err, "failed a write to fix file %s", f.Name())
+	if i != 8 {
+		fatalf("failed to write the 8 bytes to %s", f.Name())
+	}
+}
+
+func write(f *os.File, buff []byte) {
+	i, err := f.Write(buff)
+	checkf(err, "failed a write to fix file %s", f.Name())
+	if i != len(buff) {
+		fatalf("failed to make a complete write of %d. wrote %d", len(buff), i)
+	}
+}
