645db7ec51ec1e92309b8ebf3097398a1aa7e059
Ethan Reesor <ethan.reesor@gmail.com>
2023-08-24 17:02:29 +0000

    Patch the database [#3383]
    Closes #3383. Adds various tools for comparing databases, building summaries, patches, fixes, diffs, etc. More importantly, adds a script and fix file that can be used to revert DN block 12614506, to fix the stall.
    
    Changelog: feature
    
    Former-commit-id: 74dd3eb2de94eeb40dc21fa60c75c27f5efc91c7

---
+++ tools/cmd/dbrepair/build_diff.go
@@ -0,0 +1,144 @@
+// Copyright 2023 The Accumulate Authors
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/dgraph-io/badger"
+	"github.com/spf13/cobra"
+)
+
+func runBuildDiff(_ *cobra.Command, args []string) {
+	summary := args[0]
+	badDB := args[1]
+	diffFile := args[2]
+	buildDiff(summary, badDB, diffFile)
+}
+
+// buildDiff
+// Builds a diff file on a node with a bad database because they accepted a bad block.
+// The diffFile can be moved to a node with a good database to create
+// a Fix File.  What we hope is that the Fix File will be complete
+// enough to fix all nodes that need to be backed up (have a bad database)
+//
+// Diff file format:
+//
+//	N = 64 bits  -- number of Keys added to the bad state
+//	[N][32]bytes -- keys to delete
+//
+//	N = 64 bits  -- number of keys modified or missing in the bad state
+//	[N][8]bytes  -- keys of entries to restore to previous values
+func buildDiff(summary, badDB, diffFile string) {
+	boldCyan.Println("\n Build Diff")
+	keys := make(map[[8]byte][8]byte)
+
+	// Load up the keys from the good database
+	s, err := os.Open(summary)
+	checkf(err, "summary file failed to open: %v", err)
+	defer func() { err := s.Close(); checkf(err, "summary file failed to close: %v", err) }()
+	var cnt int
+	for {
+		kh := [8]byte{} // 8 bytes of the key hash
+		vh := [8]byte{} // 8 bytes of the value hash
+		_, err := s.Read(kh[:])
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		_, err = s.Read(vh[:])
+		check(err)
+		keys[kh] = vh
+
+		cnt++
+		if cnt%100000 == 0 {
+			print(".")
+		}
+	}
+	println()
+
+	// Collect the differences
+	var addedKeys [][]byte    // Slice of keys to delete from the bad db
+	var modifiedKeys [][]byte // Slice of keys to update from the bad db
+	var cntDel int
+
+	// Open the Badger database that is the good one.
+	db, close := OpenDB(badDB)
+	defer close()
+
+	err = db.View(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		opts.PrefetchValues = false // <= What this does is go through the keys in the db
+		it := txn.NewIterator(opts) //    in whatever order is best for badger for speed
+		defer it.Close()
+		for it.Rewind(); it.Valid(); it.Next() {
+			cnt++
+			if cnt%100000 == 0 {
+				print(".")
+			}
+			item := it.Item()
+			k := *(*[32]byte)(item.Key())       //   Get the key and hash it.  IF the key isn't a
+			kh := sha256.Sum256(k[:])           //   hash, then this takes care of that case.
+			kb := [8]byte{}                     //   Get the first 8 bytes of the key hash
+			copy(kb[:], kh[:8])                 //	    in an independent byte array
+			if _, exists := keys[kb]; !exists { //   delete keys not in the summary
+				addedKeys = append(addedKeys, k[:])
+				cntDel++
+				continue
+			}
+			err = item.Value(func(val []byte) error {
+				vh := sha256.Sum256(val)
+				if keys[kb] != *(*[8]byte)(vh[:]) {
+					modifiedKeys = append(modifiedKeys, kb[:]) // Revert keys that exist but value is changed
+				}
+				return nil
+			})
+			checkf(err, "read of value failed")
+			delete(keys, kb) // Remove the keys from the good db that are found in the bad db
+		}
+		return nil
+	})
+	println()
+	checkf(err, "View of keys failed")
+
+	for kh := range keys {
+		kh := kh
+		kb := [8]byte{}
+		copy(kb[:], kh[:])
+		modifiedKeys = append(modifiedKeys, kb[:]) // All the keys we did not find had to be added back
+	}
+	fmt.Printf("\nModified: %d Added: %d \n", len(modifiedKeys), len(addedKeys))
+
+	var buff [32]byte
+	f, err := os.Create(diffFile)
+	defer func() { _ = f.Close() }()
+
+	wrt64 := func(v uint64) {
+		binary.BigEndian.PutUint64(buff[:], v)
+		_, err := f.Write(buff[:8])
+		check(err)
+	}
+	wrt64int := func(v int) {
+		wrt64(uint64(v))
+	}
+
+	wrt64int(len(addedKeys))       //   Number of keys to delete
+	for _, dk := range addedKeys { //   32 bytes each
+		check2(f.Write(dk))
+		if len(dk) != 32 {
+			fatalf("Key is not a hash")
+		}
+	}
+	wrt64int(len(modifiedKeys))       //   Number of keys to revert
+	for _, uk := range modifiedKeys { //   8 bytes of key hashes
+		check2(f.Write(uk))
+	}
+}
---
+++ tools/cmd/dbrepair/print_diff.go
@@ -0,0 +1,124 @@
+// Copyright 2023 The Accumulate Authors
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"os"
+
+	"github.com/dgraph-io/badger"
+	"github.com/spf13/cobra"
+)
+
+func runPrintDiff(_ *cobra.Command, args []string) {
+	diffFile := args[0]
+	goodDB := args[1]
+	printDiff(diffFile, goodDB)
+}
+
+// print a Diff file
+// Use a goodDB to pull the actual addresses
+func printDiff(diffFile, goodDB string) {
+	boldCyan.Println("\n PrintDiff")
+
+	var AddedKeys [][]byte     // List of keys added to the bad state
+	var ModifiedKeys [][8]byte // Map of keys and values modified from the good state
+
+	var buff [32]byte // General buffer for reading entries
+	f, err := os.Open(diffFile)
+	checkf(err, "buildFix failed to open %s", diffFile)
+
+	// Read Keys to be deleted
+	NumAdded := read8(f, buff[:])
+	for i := uint64(0); i < NumAdded; i++ {
+		read32(f, buff[:])
+		key := [32]byte{}
+		copy(key[:], buff[:])
+		AddedKeys = append(AddedKeys, key[:])
+	}
+
+	// Read Keys modified by the bad state
+	NumModified := read8(f, buff[:])
+	for i := uint64(0); i < NumModified; i++ {
+		read8(f, buff[:])
+		key := [8]byte{}
+		copy(key[:], buff[:])
+		ModifiedKeys = append(ModifiedKeys, key)
+	}
+
+	// Build a map of key hash prefixes to keys from the good DB
+	db, close := OpenDB(goodDB)
+	defer close()
+	Hash2Key := buildHash2Key(db)
+
+	fmt.Printf("Found: modify %d Delete %d\n\n", len(ModifiedKeys), len(AddedKeys))
+
+	fmt.Println("Modified Keys:")
+	for _, k := range ModifiedKeys { // list all the keys added to the bad db
+		key, ok := Hash2Key[k]
+		if !ok {
+			fatalf("missing")
+		}
+		fmt.Printf("   %x ", key)
+
+		err := db.View(func(txn *badger.Txn) error { // Get the value and write it
+			item, err := txn.Get(key)
+			checkf(err, "key/value failed to produce the value")
+			err = item.Value(func(val []byte) error {
+				vLen := len(val)
+				if vLen > 40 {
+					fmt.Printf("%x ... len %d\n", val[:40], vLen)
+				} else {
+					fmt.Printf("%x\n", val)
+				}
+				return nil
+			})
+			checkf(err, "in view")
+			return nil
+		})
+		checkf(err, "Modified keys")
+	}
+
+	fmt.Println("Added Keys (to be deleted):")
+	for _, k := range AddedKeys { // list all the keys added to the bad db
+		fmt.Printf("   %x\n", k)
+	}
+}
+
+func buildHash2Key(db *badger.DB) map[[8]byte][]byte {
+	Hash2Key := make(map[[8]byte][]byte)
+
+	var cnt int
+	err := db.View(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		opts.PrefetchValues = false // <= What this does is go through the keys in the db
+		it := txn.NewIterator(opts) //    in whatever order is best for badger for speed
+		defer it.Close()
+		for it.Rewind(); it.Valid(); it.Next() {
+			item := it.Item()
+			k := *(*[32]byte)(item.Key())
+			kh := sha256.Sum256(k[:])
+			key := *(*[8]byte)(kh[:])
+			if _, exists := Hash2Key[key]; exists {
+				return fmt.Errorf("collision on %08x", key)
+			}
+			Hash2Key[key] = k[:]
+
+			cnt++
+			if cnt%100000 == 0 {
+				print(".")
+			}
+		}
+		return nil
+	})
+	println()
+	checkf(err, "failed to collect all keys")
+	fmt.Printf("\nkeys in db: %d\n", len(Hash2Key))
+
+	return Hash2Key
+}
