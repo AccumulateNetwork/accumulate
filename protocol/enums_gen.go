6c4fc70a4d524aa84044ecbe2e4ba49f9436b156
Ethan Reesor <ethan.reesor@gmail.com>
2023-08-28 16:44:45 -0500

    Bootstrap for local testnet
    Former-commit-id: da59df85d3ef8fbca4564d22eca1ee2a8d1665ab

--- protocol/enums_gen.go
+++ protocol/enums_gen.go
@@ -173,6 +173,9 @@ const PartitionTypeBlockValidator PartitionType = 2
 // PartitionTypeBlockSummary .
 const PartitionTypeBlockSummary PartitionType = 3
 
+// PartitionTypeBootstrap .
+const PartitionTypeBootstrap PartitionType = 4
+
 // SignatureTypeUnknown is used when the signature type is not known.
 const SignatureTypeUnknown SignatureType = 0
 
@@ -991,7 +994,7 @@ func (v PartitionType) GetEnumValue() uint64 { return uint64(v) }
 func (v *PartitionType) SetEnumValue(id uint64) bool {
 	u := PartitionType(id)
 	switch u {
-	case PartitionTypeDirectory, PartitionTypeBlockValidator, PartitionTypeBlockSummary:
+	case PartitionTypeDirectory, PartitionTypeBlockValidator, PartitionTypeBlockSummary, PartitionTypeBootstrap:
 		*v = u
 		return true
 	}
@@ -1007,6 +1010,8 @@ func (v PartitionType) String() string {
 		return "blockValidator"
 	case PartitionTypeBlockSummary:
 		return "blockSummary"
+	case PartitionTypeBootstrap:
+		return "bootstrap"
 	}
 	return fmt.Sprintf("PartitionType:%d", v)
 }
@@ -1024,6 +1029,8 @@ func PartitionTypeByName(name string) (PartitionType, bool) {
 		return PartitionTypeBlockSummary, true
 	case "block-summary":
 		return PartitionTypeBlockSummary, true
+	case "bootstrap":
+		return PartitionTypeBootstrap, true
 	}
 	return 0, false
 }
