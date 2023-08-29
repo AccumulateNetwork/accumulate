a4348a80a936336c63c5801fac338e30271003b6
Ethan Reesor <ethan.reesor@gmail.com>
2023-08-29 15:50:49 -0500

    Download debug binary
    Former-commit-id: 92a92d657345335c387f3947627545aaf6d1ff44

--- scripts/ops/fix/dump-state.sh
+++ scripts/ops/fix/dump-state.sh
@@ -10,6 +10,14 @@ if ! [ -d "$node" ]; then
   exit 1
 fi
 
+if ! which debug 2> /dev/null; then
+  echo "Downloading debug binary"
+  curl -LJ -o debug https://gitlab.com/accumulatenetwork/accman/-/raw/binaries/bin/v1.2.5/debug-linux-amd64
+  echo "abf40ba16df1f5806084eecbcff18b2e4c557c8400b5705d85bdcc45cf3d04ac debug" | sha256sum --check
+  chmod +x debug
+  mv debug /bin/debug
+fi
+
 # Find the `partition-id = "..."` line and extract the BVN name (and make it lower case)
 bvn=$(sed -nre 's/^\s+partition-id\s+=\s+"(\w+)"$/\1/p' "$node/bvnn/config/accumulate.toml" | tr '[:upper:]' '[:lower:]')
 
