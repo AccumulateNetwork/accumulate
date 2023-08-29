5e6dc24b6cc9b12d61d852ee058e82971b97e142
Ethan Reesor <ethan.reesor@gmail.com>
2023-08-29 16:58:46 -0500

    Improve the snapshot tool and use the right hashes

--- tools/cmd/debug/db.go
+++ tools/cmd/debug/db.go
@@ -6,7 +6,16 @@
 
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/spf13/cobra"
+	coredb "gitlab.com/accumulatenetwork/accumulate/internal/database"
+	"gitlab.com/accumulatenetwork/accumulate/pkg/database"
+	"gitlab.com/accumulatenetwork/accumulate/pkg/url"
+)
 
 var cmdDb = &cobra.Command{
 	Use:     "database",
@@ -14,6 +23,57 @@ var cmdDb = &cobra.Command{
 	Short:   "Database utilities",
 }
 
+var cmdDbSnapshot = &cobra.Command{
+	Use:   "snapshot [partition] [database] [snapshot]",
+	Short: "Collect a snapshot",
+	Args:  cobra.ExactArgs(3),
+	Run:   collectSnapshot,
+}
+
 func init() {
 	cmd.AddCommand(cmdDb)
+	cmdDb.AddCommand(cmdDbSnapshot)
+}
+
+func collectSnapshot(_ *cobra.Command, args []string) {
+	partUrl, err := url.Parse(args[0])
+	check(err)
+
+	db, err := coredb.OpenBadger(args[1], nil)
+	check(err)
+
+	f, err := os.Create(args[2])
+	check(err)
+	defer f.Close()
+
+	tick := time.NewTicker(time.Second / 2)
+	defer tick.Stop()
+
+	var metrics coredb.CollectMetrics
+	fmt.Println("Collecting...")
+	check(db.Collect(f, partUrl, &coredb.CollectOptions{
+		// BuildIndex: true,
+		Metrics: &metrics,
+		Predicate: func(r database.Record) (bool, error) {
+			select {
+			case <-tick.C:
+			default:
+				return true, nil
+			}
+
+			// The sole purpose of this function is to print progress
+			switch r.Key().Get(0) {
+			case "Account":
+				k := r.Key().SliceJ(2)
+				h := k.Hash()
+				fmt.Printf("\033[A\r\033[KCollecting [%x] (%d) %v\n", h[:4], metrics.Messages.Count, k.Get(1))
+
+			case "Message", "Transaction":
+				fmt.Printf("\033[A\r\033[KCollecting (%d/%d) %x\n", metrics.Messages.Collecting, metrics.Messages.Count, r.Key().Get(1).([32]byte))
+			}
+
+			// Retain everything
+			return true, nil
+		},
+	}))
 }
