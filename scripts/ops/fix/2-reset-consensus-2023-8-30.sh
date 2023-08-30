ddc25fb6abbe9a57ff6a7026b9590dc85d5ff0f7
Ethan Reesor <ethan.reesor@gmail.com>
2023-08-30 15:16:58 -0500

    Check genesis not the db

--- scripts/ops/fix/2-reset-consensus-2023-8-30.sh
+++ scripts/ops/fix/2-reset-consensus-2023-8-30.sh
@@ -17,22 +17,6 @@ if ! [ -d "$node" ]; then
   exit 1
 fi
 
-mkdir /tmpbin
-export PATH="/tmpbin:$PATH"
-
-function download {
-  name=$1
-  sha=$2
-
-  echo "Downloading $name binary"
-  curl -LJ -o $name https://gitlab.com/accumulatenetwork/accumulate/-/raw/main/scripts/bin/v1.2.5/$name-linux-amd64
-  echo "$sha $name" | sha256sum -c
-  chmod +x $name
-  mv $name /tmpbin/$name
-}
-
-download debug 3838acfe99321b0fbb8c8b148e70785bbf5440d9c9b7a113c680ef7aed5fe50b
-
 # Find the `partition-id = "..."` line and extract the BVN name (and make it lower case)
 bvn=$(sed -nre 's/^\s+partition-id\s+=\s+"(\w+)"$/\1/p' "$node/bvnn/config/accumulate.toml" | tr '[:upper:]' '[:lower:]')
 
@@ -55,17 +39,16 @@ esac
 
 function check-height {
   dir=$1
-  account=$2
-  want=$3
-  got="$(debug explore account $2 --node "$node/$dir" | jq -re .index)"
-  if [ "$got" -ge "$want" ]; then
-    die "Error: $dir height does not match: want <=$want, got $got"
+  want=$2
+  got="$(sed -nre 's/^\s*"initial_height":\s*"([0-9]+)",\s*$/\1/p' "$node/$dir/config/genesis.json")"
+  if [ "$got" = "$want" ]; then
+    die "Error: $dir genesis height does not match: want <=$want, got $got"
   fi
 }
 
 # Sanity check on height
-check-height dnn dn.acme/ledger $dnHeight
-check-height bvnn bvn-$bvn.acme/ledger $bvnHeight
+check-height dnn $dnHeight
+check-height bvnn $bvnHeight
 
 # Delete all databases
 rm -rf "$node"/{dnn,bvnn}/data/{*.db,cs.wal}
