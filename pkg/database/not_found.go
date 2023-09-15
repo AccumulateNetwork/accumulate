22ad692962c62e9911945b58fba851eed4d3dc4e
Ethan Reesor <ethan.reesor@gmail.com>
2023-09-15 22:40:21 +0000

    Add custom not found error type [#3392]
    Closes #3392. Improves database performance for querying values that do not exist by using a custom error type, eliminating sprintf calls.
    
    Changelog: refactor

---
+++ pkg/database/not_found.go
@@ -0,0 +1,33 @@
+// Copyright 2023 The Accumulate Authors
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+package database
+
+import (
+	"fmt"
+
+	"gitlab.com/accumulatenetwork/accumulate/pkg/errors"
+)
+
+type NotFoundError Key
+
+func (e *NotFoundError) Error() string {
+	return fmt.Sprintf("%v not found", (*Key)(e))
+}
+
+func (e *NotFoundError) Is(target error) bool {
+	return errors.Is(target, errors.NotFound)
+}
+
+func (e *NotFoundError) As(target any) bool {
+	switch target := target.(type) {
+	case **errors.Error:
+		*target = errors.NotFound.With(e.Error())
+	default:
+		return false
+	}
+	return true
+}
