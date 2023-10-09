56bbd9053ff8d11b8f9c5af90dfd1dfb4ebf7172
Ethan Reesor <ethan.reesor@gmail.com>
2023-10-09 11:28:28 -0500

    test

---
+++ internal/api/routing/types.go
@@ -0,0 +1,10 @@
+// Copyright 2022 The Accumulate Authors
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+package routing
+
+//go:generate go run github.com/vektra/mockery/v2
+//go:generate go run github.com/rinchsan/gosimports/cmd/gosimports -w .
