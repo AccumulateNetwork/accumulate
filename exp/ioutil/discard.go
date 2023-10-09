d67f44350c85faea205edd8e1dbc37af628b004b
Ethan Reesor <ethan.reesor@gmail.com>
2023-10-09 16:12:01 +0000

    Improve snapshot performance [#3410]
    Closes #3410. Optimizes database performance. Uses Paul's approach for tracking hashes when collecting snapshots.
    
    Changelog: refactor

---
+++ exp/ioutil/discard.go
@@ -0,0 +1,39 @@
+// Copyright 2023 The Accumulate Authors
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+package ioutil
+
+import (
+	"errors"
+	"io"
+)
+
+type Discard struct {
+	offset int64
+	end    int64
+}
+
+func (d *Discard) Write(p []byte) (n int, err error) {
+	d.offset += int64(len(p))
+	return len(p), nil
+}
+
+func (d *Discard) Seek(offset int64, whence int) (int64, error) {
+	if d.offset > d.end {
+		d.end = d.offset
+	}
+	switch whence {
+	case io.SeekCurrent:
+		d.offset += offset
+	case io.SeekStart:
+		d.offset = offset
+	case io.SeekEnd:
+		d.offset = d.end + offset
+	default:
+		return 0, errors.New("invalid whence")
+	}
+	return d.offset, nil
+}
