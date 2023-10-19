7fe665c834152008bb9c6998ef2c1c6e7daa186d
Ethan Reesor <ethan.reesor@gmail.com>
2023-10-19 19:22:55 +0000

    Separate dialing and tracking from p2p [#3443]
    Closes #3443. Moves dialing and peer tracking into their own packages. Refactors dialing.
    
    Changelog: refactor
    
    Former-commit-id: 85bbb9d583b25480583cb2905a766b9d43148f3d

---
+++ pkg/api/v3/p2p/dial/fake.go
@@ -0,0 +1,24 @@
+// Copyright 2023 The Accumulate Authors
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+package dial
+
+import (
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/multiformats/go-multiaddr"
+	"gitlab.com/accumulatenetwork/accumulate/pkg/api/v3"
+)
+
+var FakeTracker fakeTracker
+
+type fakeTracker struct{}
+
+var _ Tracker = fakeTracker{}
+
+func (fakeTracker) Mark(peer.ID, multiaddr.Multiaddr, api.KnownPeerStatus)        {}
+func (fakeTracker) Status(peer.ID, multiaddr.Multiaddr) api.KnownPeerStatus       { return 0 }
+func (fakeTracker) Next(multiaddr.Multiaddr, api.KnownPeerStatus) (peer.ID, bool) { return "", false }
+func (fakeTracker) All(multiaddr.Multiaddr, api.KnownPeerStatus) []peer.ID        { return nil }
