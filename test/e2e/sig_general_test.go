61fb932b51de4e06eb0650004b5a91a64578c832
Ethan Reesor <ethan.reesor@gmail.com>
2023-10-24 18:01:48 +0000

    Add signature memos [#3454]
    Closes #3454. Adds memo and metadata fields to signatures (excluding legacy types).
    
    Changelog: feature

---
+++ test/e2e/sig_general_test.go
@@ -0,0 +1,54 @@
+// Copyright 2023 The Accumulate Authors
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+package e2e
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gitlab.com/accumulatenetwork/accumulate/pkg/build"
+	"gitlab.com/accumulatenetwork/accumulate/pkg/types/messaging"
+	"gitlab.com/accumulatenetwork/accumulate/protocol"
+	. "gitlab.com/accumulatenetwork/accumulate/protocol"
+	. "gitlab.com/accumulatenetwork/accumulate/test/harness"
+	. "gitlab.com/accumulatenetwork/accumulate/test/helpers"
+	"gitlab.com/accumulatenetwork/accumulate/test/simulator"
+	acctesting "gitlab.com/accumulatenetwork/accumulate/test/testing"
+)
+
+func TestSignatureMemo(t *testing.T) {
+	// Tests AIP-006
+	alice := AccountUrl("alice")
+	aliceKey := acctesting.GenerateKey(alice)
+
+	// Initialize
+	sim := NewSim(t,
+		simulator.SimpleNetwork(t.Name(), 3, 1),
+		simulator.Genesis(GenesisTime),
+	)
+
+	MakeIdentity(t, sim.DatabaseFor(alice), alice, aliceKey[32:])
+	CreditCredits(t, sim.DatabaseFor(alice), alice.JoinPath("book", "1"), 1e12)
+
+	// Submit
+	st := sim.BuildAndSubmitSuccessfully(
+		build.Transaction().For(alice, "book", "1").
+			BurnCredits(1).
+			SignWith(alice, "book", "1").
+			Version(1).Timestamp(1).
+			Memo("foo").
+			Metadata("bar").
+			PrivateKey(aliceKey))
+
+	sim.StepUntil(
+		Txn(st[0].TxID).Succeeds())
+
+	// Verify the signature
+	sig := sim.QueryMessage(st[1].TxID, nil).Message.(*messaging.SignatureMessage).Signature.(*protocol.ED25519Signature)
+	require.Equal(t, "foo", sig.Memo)
+	require.Equal(t, "bar", string(sig.Data))
+}
