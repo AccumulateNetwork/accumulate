dc722f4cfde8af2ffdf76ef0a740ef41199ea806
Ethan Reesor <ethan.reesor@gmail.com>
2023-11-01 16:59:24 -0500

    Update check node

--- tools/cmd/debug/node.go
+++ tools/cmd/debug/node.go
@@ -11,14 +11,14 @@ import (
 	"crypto/ed25519"
 	"crypto/rand"
 	"fmt"
-	"time"
 
 	"github.com/fatih/color"
 	"github.com/multiformats/go-multiaddr"
 	"github.com/spf13/cobra"
-	"gitlab.com/accumulatenetwork/accumulate/pkg/accumulate"
+	"gitlab.com/accumulatenetwork/accumulate/internal/api/routing"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/api/v3"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/api/v3/jsonrpc"
+	"gitlab.com/accumulatenetwork/accumulate/pkg/api/v3/message"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/api/v3/p2p"
 )
 
@@ -39,6 +39,8 @@ func checkNode(_ *cobra.Command, args []string) {
 
 	jc := jsonrpc.NewClient("http://" + args[0] + ":16595/v3")
 	ns, err := jc.NetworkStatus(ctx, api.NetworkStatusOptions{})
+	check(err)
+	router, err := routing.NewStaticRouter(ns.Routing, nil)
 	check(err)
 
 	ni, err := jc.NodeInfo(ctx, api.NodeInfoOptions{})
@@ -73,34 +75,30 @@ func checkNode(_ *cobra.Command, args []string) {
 		},
 	})
 	check(err)
-	time.Sleep(time.Second)
 
-	svcAddr, err := multiaddr.NewMultiaddr("/acc-svc/node/p2p/" + ni.PeerID.String())
+	svcAddr, err := multiaddr.NewMultiaddr("/p2p/" + ni.PeerID.String())
 	check(err)
+	svcAddr = api.ServiceTypeNode.Address().Multiaddr().Encapsulate(svcAddr)
 	_, err = pc.DialNetwork().Dial(ctx, svcAddr)
 	if err == nil {
-		fmt.Println(color.GreenString("✔"), "Direct connection")
+		fmt.Println(color.GreenString("✔"), "Can connect")
 	} else {
-		fmt.Println(color.RedString("🗴"), "Direct connection")
+		fmt.Println(color.RedString("🗴"), "Can connect")
 		fmt.Println(err)
 	}
-	check(pc.Close())
 
-	// Indirect
-	pc, err = p2p.New(p2p.Options{
-		Network:        ni.Network,
-		Key:            key,
-		BootstrapPeers: accumulate.BootstrapServers,
-	})
-	check(err)
-	time.Sleep(time.Second)
-
-	_, err = pc.DialNetwork().Dial(ctx, svcAddr)
+	mc := &message.Client{Transport: &message.RoutedTransport{
+		Network: ni.Network,
+		Dialer:  pc.DialNetwork(),
+		Router:  routing.MessageRouter{Router: router},
+	}}
+	_, err = mc.NodeInfo(ctx, api.NodeInfoOptions{PeerID: ni.PeerID})
 	if err == nil {
-		fmt.Println(color.GreenString("✔"), "Indirect connection")
+		fmt.Println(color.GreenString("✔"), "Can query")
 	} else {
-		fmt.Println(color.RedString("🗴"), "Indirect connection")
+		fmt.Println(color.RedString("🗴"), "Can query")
 		fmt.Println(err)
 	}
+
 	check(pc.Close())
 }
