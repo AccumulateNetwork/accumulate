bd5a62604c4559b63d4935542e9bb7f370fdf2ac
Ethan Reesor <ethan.reesor@gmail.com>
2023-11-05 18:13:11 +0000

    Fix supply issue in simulator [#3464]
    * Closes #3464. Sets the supply to 1M ACME by default to prevent negative supply issues.
    * Refactors the snapshot indexer to allow mmap to be disabled due to [a bug in rr](https://github.com/rr-debugger/rr/issues/3635).
    * Panics if batch commit fails, since that is a fatal error and if returned it can be masked.
    
    Changelog: fix

--- internal/util/indexing/bucket.go
+++ internal/util/indexing/bucket.go
@@ -7,14 +7,12 @@
 package indexing
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/edsrzf/mmap-go"
-	"golang.org/x/exp/slog"
 )
 
 const pageSize = 1 << 10 // 1024 entries
@@ -23,6 +21,17 @@ const pageSize = 1 << 10 // 1024 entries
 type Bucket struct {
 	files     [256]*bucketFile
 	valueSize int
+}
+
+type bucketFile struct {
+	file  *os.File
+	mmap  mmap.MMap
+	count int
+}
+
+type Entry struct {
+	Hash  [32]byte
+	Value []byte
 }
 
 func OpenBucket(prefix string, valueSize int, create bool) (*Bucket, error) {
@@ -58,124 +67,26 @@ func OpenBucket(prefix string, valueSize int, create bool) (*Bucket, error) {
 	return b, nil
 }
 
-func (b *Bucket) Write(hash [32]byte, value []byte) error {
-	if len(value) != b.valueSize {
-		return errors.New("value does not match expected size")
-	}
-
-	// Map or remap the file
-	S := 32 + b.valueSize
-	f := b.files[hash[0]]
-	err := f.ensureSpace(S)
-	if err != nil {
-		return err
-	}
-
-	// Write
-	dst := f.mmap[f.count*S:]
-	copy(dst, hash[:])
-	copy(dst[32:], value)
-	f.count++
-	return nil
-}
-
 func (b *Bucket) Close() error {
 	var err error
 	for _, f := range b.files {
-		if f.mmap != nil {
-			e := f.mmap.Unmap()
-			if e != nil {
-				slog.Error("Error unmapping bucket", "error", e)
-				err = e
-			}
-		}
-
-		// Truncate extra space
-		e := f.file.Truncate(int64(f.count * (32 + b.valueSize)))
+		e := f.close(b.valueSize)
 		if e != nil {
-			slog.Error("Error truncating bucket", "error", e)
-			err = e
-		}
-
-		e = f.file.Close()
-		if e != nil {
-			slog.Error("Error closing bucket", "error", e)
 			err = e
 		}
 	}
 	return err
 }
 
-type Entry struct {
-	Hash  [32]byte
-	Value []byte
+func (e *Entry) fromBytes(b []byte, S, valueSize int) {
+	e.Hash = *(*[32]byte)(b[:32])
+	if valueSize > 0 {
+		e.Value = make([]byte, valueSize)
+		copy(e.Value, b[32:])
+	}
 }
 
-func (b *Bucket) Read(i byte) ([]Entry, error) {
-	// Check if the file is empty
-	f := b.files[i]
-	st, err := f.file.Stat()
-	if err != nil || st.Size() == 0 {
-		return nil, err
-	}
-
-	S := 32 + b.valueSize
-
-	var g mmap.MMap
-	if f.mmap == nil {
-		// Map the file
-		g, err = mmap.Map(f.file, mmap.RDONLY, 0)
-		if err != nil {
-			return nil, err
-		}
-		defer func() { _ = g.Unmap() }()
-
-		if len(g)%S != 0 {
-			return nil, errors.New("internal error: invalid file length")
-		}
-
-	} else {
-		// Or use the existing mapping
-		g = f.mmap[:f.count*S]
-	}
-
-	entries := make([]Entry, len(g)/S)
-	for i := range entries {
-		entries[i].Hash = *(*[32]byte)(g[i*S:])
-		if b.valueSize > 0 {
-			entries[i].Value = make([]byte, b.valueSize)
-			copy(entries[i].Value, g[i*S+32:])
-		}
-	}
-	return entries, nil
-}
-
-type bucketFile struct {
-	file  *os.File
-	mmap  mmap.MMap
-	count int
-}
-
-// ensureSpace ensures there is sufficient space for another record.
-func (f *bucketFile) ensureSpace(recordSize int) error {
-	if len(f.mmap) >= (f.count+1)*recordSize {
-		return nil
-	}
-
-	// Need more space, remap
-	newSize := len(f.mmap) + pageSize*recordSize
-	if f.mmap != nil {
-		err := f.mmap.Unmap()
-		if err != nil {
-			return err
-		}
-	}
-
-	err := f.file.Truncate(int64(newSize))
-	if err != nil {
-		return err
-	}
-
-	f.mmap, err = mmap.MapRegion(f.file, newSize, mmap.RDWR, 0, 0)
-	return err
+func (e Entry) toBytes(b []byte) {
+	copy(b, e.Hash[:])
+	copy(b[32:], e.Value)
 }
