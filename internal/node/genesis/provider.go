ab373dc7be3fb79b26d168ccbcddab62812b0ae3
Ethan Reesor <ethan.reesor@gmail.com>
2023-12-03 00:24:30 +0000

    Convert binary genesis to JSON [#3505]
    Closes #3505. Adds a function to convert a binary genesis snapshot to JSON. Fixes JSON encoding, using CometBFT's JSON package instead of the standard one.
    
    Changelog: fix
    
    Former-commit-id: cf1ee66f1954390ab2ebe3e505833dbb6afc6609

--- internal/node/genesis/provider.go
+++ internal/node/genesis/provider.go
@@ -8,12 +8,12 @@ package genesis
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/cometbft/cometbft/crypto/ed25519"
+	cmtjson "github.com/cometbft/cometbft/libs/json"
 	"github.com/cometbft/cometbft/node"
 	"github.com/cometbft/cometbft/types"
 	"gitlab.com/accumulatenetwork/accumulate/internal/node/config"
@@ -26,7 +26,7 @@ func ConvertJsonToSnapshot(doc *types.GenesisDoc) ([]byte, error) {
 	// contains sufficient information to recreate the JSON genesis doc. So all
 	// we need to do here is extract the snapshot.
 	var b []byte
-	err := json.Unmarshal(doc.AppState, &b)
+	err := cmtjson.Unmarshal(doc.AppState, &b)
 	return b, err
 }
 
@@ -44,52 +44,56 @@ func DocProvider(config *config.Config) node.GenesisDocProvider {
 			return nil, err
 		}
 
-		s, err := snapshot.Open(bytes.NewReader(all))
-		if err != nil {
-			return nil, err
-		}
-
-		// Read the consensus section
-		rd, err := s.Open(snapshot.SectionTypeConsensus)
-		if err != nil {
-			return nil, err
-		}
-		p := new(consensusDoc)
-		err = p.UnmarshalBinaryFrom(rd)
-		if err != nil {
-			return nil, err
-		}
-
-		// Convert
-		jsonBytes, err := json.Marshal(all)
-		if err != nil {
-			return nil, err
-		}
-
-		doc := &types.GenesisDoc{
-			GenesisTime:     s.Header.SystemLedger.Timestamp,
-			ChainID:         p.ChainID,
-			InitialHeight:   int64(s.Header.SystemLedger.Index) + 1,
-			ConsensusParams: (*types.ConsensusParams)(p.Params),
-			Validators:      make([]types.GenesisValidator, len(p.Validators)),
-			AppHash:         s.Header.RootHash[:],
-			AppState:        jsonBytes,
-		}
-		for i, v := range p.Validators {
-			u := types.GenesisValidator{
-				Address: v.Address,
-				Power:   v.Power,
-				Name:    v.Name,
-			}
-			switch v.Type {
-			case protocol.SignatureTypeED25519:
-				u.PubKey = ed25519.PubKey(v.PubKey)
-			default:
-				return nil, fmt.Errorf("unsupported key type %s", v.Type)
-			}
-			doc.Validators[i] = u
-		}
-
-		return doc, nil
+		return ConvertSnapshotToJson(all)
 	}
+}
+
+func ConvertSnapshotToJson(snap []byte) (*types.GenesisDoc, error) {
+	s, err := snapshot.Open(bytes.NewReader(snap))
+	if err != nil {
+		return nil, err
+	}
+
+	// Read the consensus section
+	rd, err := s.Open(snapshot.SectionTypeConsensus)
+	if err != nil {
+		return nil, err
+	}
+	p := new(consensusDoc)
+	err = p.UnmarshalBinaryFrom(rd)
+	if err != nil {
+		return nil, err
+	}
+
+	// Convert
+	jsonBytes, err := cmtjson.Marshal(snap)
+	if err != nil {
+		return nil, err
+	}
+
+	doc := &types.GenesisDoc{
+		GenesisTime:     s.Header.SystemLedger.Timestamp,
+		ChainID:         p.ChainID,
+		InitialHeight:   int64(s.Header.SystemLedger.Index) + 1,
+		ConsensusParams: (*types.ConsensusParams)(p.Params),
+		Validators:      make([]types.GenesisValidator, len(p.Validators)),
+		AppHash:         s.Header.RootHash[:],
+		AppState:        jsonBytes,
+	}
+	for i, v := range p.Validators {
+		u := types.GenesisValidator{
+			Address: v.Address,
+			Power:   v.Power,
+			Name:    v.Name,
+		}
+		switch v.Type {
+		case protocol.SignatureTypeED25519:
+			u.PubKey = ed25519.PubKey(v.PubKey)
+		default:
+			return nil, fmt.Errorf("unsupported key type %s", v.Type)
+		}
+		doc.Validators[i] = u
+	}
+
+	return doc, nil
 }
