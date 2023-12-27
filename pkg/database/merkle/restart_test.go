bb9091e6da225450fcffa2a2f34c57baf21ff7c0
Ethan Reesor <ethan.reesor@gmail.com>
2023-12-27 22:38:28 +0000

    Refactor merkle stuff [#3516]
    Closes #3516. Renames Merkle files and methods.
    
    Changelog: refactor
    
    Former-commit-id: 769a5c092b65d0cdf320602bd949d91d0648d569

--- pkg/database/merkle/restart_test.go
+++ pkg/database/merkle/restart_test.go
@@ -38,7 +38,7 @@ func TestRestart(t *testing.T) {
 				}
 			}
 			for rand.Int()%3 > 0 { //                                       Add 0 or more random hashes
-				require.NoError(t, MM1.AddHash(doSha(common.Int64Bytes(rand.Int63())), false)) // Generate and add one random hash
+				require.NoError(t, MM1.AddEntry(doSha(common.Int64Bytes(rand.Int63())), false)) // Generate and add one random hash
 			}
 			require.NoError(t, MM1.Commit())
 		}
@@ -64,7 +64,7 @@ func TestRestartCache(t *testing.T) {
 			for k := uint(0); k < uint(rand.Int())%4; k++ { //  Add 0 or more random hashes
 				h := rh.Next()
 				cached = append(cached, h)
-				require.NoError(t, MM1.AddHash(h, false)) // Generate and add one random hash
+				require.NoError(t, MM1.AddEntry(h, false)) // Generate and add one random hash
 			}
 
 			ended := rand.Int()%30 > 0 && len(cached) > 0 // Every so often we are going to write to disk
@@ -89,7 +89,7 @@ func TestRestartCache(t *testing.T) {
 				}
 
 				for _, h := range cached {
-					require.NoError(t, MM2.AddHash(h, false))
+					require.NoError(t, MM2.AddEntry(h, false))
 				}
 
 				if !h1.Equal(h2) { // MM2 should be the same as MM1
