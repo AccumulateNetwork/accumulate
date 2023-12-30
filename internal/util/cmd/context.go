f582f142309c939c93454950e7754913ac214eb1
Ethan Reesor <ethan.reesor@gmail.com>
2023-12-30 14:24:23 -0600

    Unified config for faucet [#3528]

--- internal/util/cmd/context.go
+++ internal/util/cmd/context.go
@@ -1,4 +1,4 @@
-// Copyright 2023 The Accumulate Authors
+// Copyright 2024 The Accumulate Authors
 //
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file or at
@@ -20,13 +20,19 @@ var ErrInterrupted = errors.New("interrupted")
 // receives SIGINT or SIGTERM. Once SIGINT/SIGTERM is received the handler is
 // removed, so a second signal will terminate the process as normal. When
 // canceled via a signal, [context.Context.Error] will return [ErrInterrupted].
+//
+// SeeAlso [signal.NotifyContext] - the only difference is that this version
+// cancels with [ErrInterrupted].
 func ContextForMainProcess(ctx context.Context) context.Context {
 	ctx, cancel := context.WithCancelCause(ctx)
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
-		<-sigs
+		select {
+		case <-sigs:
+		case <-ctx.Done():
+		}
 		signal.Stop(sigs)
 		cancel(ErrInterrupted)
 	}()
