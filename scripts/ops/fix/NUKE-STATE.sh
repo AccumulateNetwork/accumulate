4b456cf8976010588f507f9530477bc552c0e9bd
Ethan Reesor <ethan.reesor@gmail.com>
2024-01-17 17:47:16 -0600

    Add a reset script
    Former-commit-id: bb8a5ea0f136553f4efab208a4654eb0c4202826

---
+++ scripts/ops/fix/NUKE-STATE.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# image: registry.gitlab.com/accumulatenetwork/accumulate:v1-2-9
+
+node="$1"
+if [ -z "$node" ]; then
+  node=/node
+fi
+if ! [ -d "$node" ]; then
+  >&2 echo "Error: $node is not a directory"
+  >&2 echo "Usage: $0 [node dir]"
+  exit 1
+fi
+
+>&2 echo 'This will `"$node/{dnn,bvnn}/data/{*db,cs.wal}`. Are you sure you want to do that? Ctrl-C to cancel or [Enter] to continue.'
+read
+
+# Nuke all the state
+for i in {3..1}; do
+    >&2 echo "$i"
+    sleep 1
+done
+>&2 echo "Resetting state"
+rm -rf "$node/{dnn,bvnn}/data/{*db,cs.wal}
