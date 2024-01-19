5b73162007675078a0d5fc8592c6c6e4c71c0f81
Ethan Reesor <ethan.reesor@gmail.com>
2024-01-19 13:25:11 -0600

    Block size limit script
    Former-commit-id: cfbfe35c25731d6553e15085c4fbe7f259217eb6

---
+++ scripts/ops/fix/6-set-block-size-limit.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# image: registry.gitlab.com/accumulatenetwork/accumulate:v1-2-11
+
+set -e
+
+function update-config {
+    if `grep -q '^max-envelopes-per-block = ' "$1"`; then
+        awk '{gsub(/^max-envelopes-per-block = .*$/, "max-envelopes-per-block = 100");print$0}' "$1" > /tmp/file
+    else
+        { echo 'max-envelopes-per-block = 100'; cat "$1"; } > /tmp/file
+    fi
+    cat /tmp/file > "$1"
+}
+
+node="$1"
+if [ -z "$node" ]; then
+  node=/node
+fi
+if ! [ -d "$node" ]; then
+  >&2 echo "Error: $node is not a directory"
+  >&2 echo "Usage: $0 [node dir]"
+  exit 1
+fi
+
+update-config "$node/dnn/config/accumulate.toml"
+update-config "$node/bvnn/config/accumulate.toml"
+
+grep -A 1 "batch-replay-limit" "$node/dnn/config/accumulate.toml" "$node/bvnn/config/accumulate.toml"
