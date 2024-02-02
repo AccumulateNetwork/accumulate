013c0cb88201915083532fb1129e65ba0e23e1b8
Ethan Reesor <ethan.reesor@gmail.com>
2024-02-02 19:56:10 +0000

    Backport #3548
    Backports #3548 to 1.2.
    
    Changelog: merge
    
    Former-commit-id: 3fb71c8d542e684b9b4752c7ac9ca914ca009e18

---
+++ test/util/goroutine_leaks_test.go
@@ -0,0 +1,30 @@
+// Copyright 2024 The Accumulate Authors
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+package testutil
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTrackGoroutines(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	err := track(ctx, t.Name(), func(ctx context.Context) {
+		// Leak a goroutine
+		go func() { <-ctx.Done() }()
+	})
+
+	// Verify the leak is detected
+	require.Error(t, err)
+	require.IsType(t, (*LeakedGoroutines)(nil), err)
+
+	// Release the goroutine
+	cancel()
+}
