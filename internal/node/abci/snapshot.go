4e8d42e2f9a6b100f12c832528290c0c7a4bf037
Ethan Reesor <ethan.reesor@gmail.com>
2024-02-02 00:19:41 +0000

    Unified config for snapshots [#3531]
    Closes #3531. Unified config for snapshot collection.
    
    Changelog: refactor

--- internal/node/abci/snapshot.go
+++ internal/node/abci/snapshot.go
@@ -1,4 +1,4 @@
-// Copyright 2023 The Accumulate Authors
+// Copyright 2024 The Accumulate Authors
 //
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file or at
@@ -30,6 +30,10 @@ import (
 // ListSnapshots queries the node for available snapshots.
 func ListSnapshots(cfg *config.Config) ([]*snapshot.Header, error) {
 	snapDir := config.MakeAbsolute(cfg.RootDir, cfg.Accumulate.Snapshots.Directory)
+	return ListSnapshots2(snapDir)
+}
+
+func ListSnapshots2(snapDir string) ([]*snapshot.Header, error) {
 	info, err := listSnapshots(snapDir)
 	if err != nil {
 		return nil, err
