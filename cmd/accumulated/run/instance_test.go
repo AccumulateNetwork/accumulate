5ebd774bdffd2d1f382854219df483f05744c40e
Ethan Reesor <ethan.reesor@gmail.com>
2024-02-05 17:47:58 +0000

    Unified config for core validator [#3532]
    Closes #3532. Unified config for core validator nodes.
    
    Changelog: refactor
    
    Former-commit-id: d06c23304f3ed5222f7f0ba9f8e14074057c21ba

--- cmd/accumulated/run/instance_test.go
+++ cmd/accumulated/run/instance_test.go
@@ -8,6 +8,7 @@ package run
 
 import (
 	"context"
+	"fmt"
 	"testing"
 	"time"
 
@@ -15,8 +16,108 @@ import (
 	"github.com/stretchr/testify/require"
 	"gitlab.com/accumulatenetwork/accumulate/internal/logging"
 	"gitlab.com/accumulatenetwork/accumulate/pkg/types/record"
+	"gitlab.com/accumulatenetwork/accumulate/protocol"
 	"golang.org/x/exp/slog"
 )
+
+func TestCoreValidatorConfig(t *testing.T) {
+	c := &Config{
+		Network: "MainNet",
+		Configurations: []Configuration{
+			&CoreValidatorConfiguration{
+				Listen:        mustParseMulti("/tcp/16591"),
+				BVN:           "Apollo",
+				EnableHealing: ptr(true),
+				StorageType:   ptr(StorageTypeBadger),
+			},
+		},
+	}
+
+	// Apply configurations
+	for _, d := range c.Configurations {
+		require.NoError(t, d.apply(c))
+	}
+
+	c.Configurations = nil
+	b, err := c.Marshal(MarshalTOML)
+	require.NoError(t, err)
+
+	fmt.Printf("%s", b)
+}
+
+func TestRun(t *testing.T) {
+	t.Skip("Manual")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+
+	c := &Config{
+		Network: "DevNet",
+		Logging: &Logging{
+			Format: "plain",
+			Rules: []*LoggingRule{
+				{Level: slog.LevelInfo},
+			},
+		},
+		P2P: &P2P{
+			// BootstrapPeers: accumulate.BootstrapServers,
+			Key: &CometNodeKeyFile{Path: "node-1/dnn/config/node_key.json"},
+		},
+		Services: []Service{
+			&ConsensusService{
+				NodeDir: "node-1/dnn",
+				App: &CoreConsensusApp{
+					EnableHealing: ptr(true),
+					Partition: &protocol.PartitionInfo{
+						ID:   protocol.Directory,
+						Type: protocol.PartitionTypeDirectory,
+					},
+				},
+			},
+			&ConsensusService{
+				NodeDir: "node-1/bvnn",
+				App: &CoreConsensusApp{
+					EnableHealing: ptr(true),
+					Partition: &protocol.PartitionInfo{
+						ID:   "BVN1",
+						Type: protocol.PartitionTypeBlockValidator,
+					},
+				},
+			},
+			&StorageService{
+				Name: "directory",
+				Storage: &BadgerStorage{
+					Path: "node-1/dnn/data/accumulate.db",
+				},
+			},
+			&Querier{Partition: protocol.Directory},
+			&NetworkService{Partition: protocol.Directory},
+			&MetricsService{Partition: protocol.Directory},
+			&EventsService{Partition: protocol.Directory},
+			&HttpService{Router: ServiceReference[*RouterService]("directory")},
+
+			&StorageService{
+				Name: "BVN1",
+				Storage: &BadgerStorage{
+					Path: "node-1/bvnn/data/accumulate.db",
+				},
+			},
+			&Querier{Partition: "BVN1"},
+			&NetworkService{Partition: "BVN1"},
+			&MetricsService{Partition: "BVN1"},
+			&EventsService{Partition: "BVN1"},
+		},
+	}
+
+	c.file = "../../../.nodes/test.toml"
+	require.NoError(t, c.Save())
+
+	ctx = logging.With(ctx, "test", t.Name())
+	inst, err := Start(ctx, c)
+	require.NoError(t, err)
+
+	require.NoError(t, inst.Stop())
+}
 
 func TestMainNetHttp(t *testing.T) {
 	t.Skip("Manual")
