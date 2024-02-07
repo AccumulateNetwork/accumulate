9a416893d0d663b888c0a48d26d36679f7ddfe73
Ethan Reesor <ethan.reesor@gmail.com>
2024-02-07 11:08:04 -0600

    Colors

--- scripts/ops/fix/shrink-db.sh
+++ scripts/ops/fix/shrink-db.sh
@@ -12,16 +12,18 @@ if ! [ -d "$node" ]; then
 fi
 
   node=/node
->&2 printf '\033[33;1mThis will run forever in a loop. Ctrl+C at any time to stop. [Enter] to continue.\033[0m\n'
+>&2 printf '\033[0;33;1mThis will run forever in a loop.\033[0m\n'
+>&2 printf '\033[0;31;1mCtrl+C at any time to stop.\033[0m\n'
+>&2 printf '\033[1;33;1m[Enter] to continue.\033[0m\n'
 read -s
 
 while true; do
-    >&2 printf '\033[33;1mFlatten DNN\033[0m\n'
+    >&2 printf '\033[0;33mFlatten DNN\033[0m\n'
     debug badger flatten "$node/dnn/data/accumulate.db"
-    >&2 printf '\033[33;1mFlatten BVNN\033[0m\n'
+    >&2 printf '\033[0;33mFlatten BVNN\033[0m\n'
     debug badger flatten "$node/bvnn/data/accumulate.db"
-    >&2 printf '\033[33;1mCompact DNN\033[0m\n'
+    >&2 printf '\033[0;33mCompact DNN\033[0m\n'
     debug badger compact "$node/dnn/data/accumulate.db"
-    >&2 printf '\033[33;1mCompact BVNN\033[0m\n'
+    >&2 printf '\033[0;33mCompact BVNN\033[0m\n'
     debug badger compact "$node/bvnn/data/accumulate.db"
 done
