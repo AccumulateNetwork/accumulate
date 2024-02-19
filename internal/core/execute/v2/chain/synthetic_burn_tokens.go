98a6c953c280f8d0b71f5b72005513f26a872a2b
Ethan Reesor <ethan.reesor@gmail.com>
2024-02-19 17:29:16 +0000

    Separate ACME burn from anchor [#3552]
    Closes #3552. Send ACME burn as a separate message instead of including it with the anchor.
    
    Changelog: feature
    
    Former-commit-id: 1df1d6a94b667287976c072e9c3861a37d376d57

--- internal/core/execute/v2/chain/synthetic_burn_tokens.go
+++ internal/core/execute/v2/chain/synthetic_burn_tokens.go
@@ -1,4 +1,4 @@
-// Copyright 2023 The Accumulate Authors
+// Copyright 2024 The Accumulate Authors
 //
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file or at
@@ -9,6 +9,7 @@ package chain
 import (
 	"fmt"
 
+	"gitlab.com/accumulatenetwork/accumulate/pkg/errors"
 	"gitlab.com/accumulatenetwork/accumulate/protocol"
 )
 
@@ -47,7 +48,14 @@ func (x SyntheticBurnTokens) Execute(st *StateManager, tx *Delivery) (protocol.T
 		return nil, fmt.Errorf("invalid principal: want chain type %v, got %v", protocol.AccountTypeTokenIssuer, st.Origin.Type())
 	}
 
+	// Subtract the amount burnt from the amount issued
 	account.Issued.Sub(&account.Issued, &body.Amount)
+
+	// Verify that the result is not negative. The marshaller isn't capable of
+	// encoding negative bigint values, so this would fail anyways
+	if st.Globals.ExecutorVersion.V2VandenbergEnabled() && account.Issued.Sign() < 0 {
+		return nil, errors.Conflict.With("acme burnt exceeds amount issued")
+	}
 
 	err = st.Update(account)
 	if err != nil {
