d7ce4c84b34760342fcdf5822455844265af1471
Ethan Reesor <ethan.reesor@gmail.com>
2024-03-12 13:29:49 -0500

    Update check node

--- tools/cmd/debug/node.go
+++ tools/cmd/debug/node.go
@@ -11,8 +11,10 @@ import (
 	"crypto/ed25519"
 	"crypto/rand"
 	"fmt"
+	"net/url"
 
 	"github.com/fatih/color"
+	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/multiformats/go-multiaddr"
 	"github.com/spf13/cobra"
 	"gitlab.com/accumulatenetwork/accumulate/internal/api/routing"
@@ -29,44 +31,81 @@ var cmdCheckNode = &cobra.Command{
 	Run:   checkNode,
 }
 
+var flagCheckNode = struct {
+	UseP2P  string
+	Network string
+}{}
+
 func init() {
 	cmd.AddCommand(cmdCheckNode)
+	cmdCheckNode.Flags().StringVar(&flagCheckNode.UseP2P, "use-p2p", "", "Use libp2p for the network status check, with the given peer ID")
+	cmdCheckNode.Flags().StringVar(&flagCheckNode.Network, "network", "", "The network, required for --use-p2p")
+
+	cmdCheckNode.PreRunE = func(cmd *cobra.Command, args []string) error {
+		if flagCheckNode.UseP2P != "" && flagCheckNode.Network == "" {
+			return fmt.Errorf("--use-p2p requires --network")
+		}
+		return nil
+	}
 }
 
 func checkNode(_ *cobra.Command, args []string) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	jc := jsonrpc.NewClient("http://" + args[0] + ":16595/v3")
-	ns, err := jc.NetworkStatus(ctx, api.NetworkStatusOptions{})
+	u, err := url.Parse(args[0])
 	check(err)
-	router := routing.NewRouter(routing.RouterOptions{Initial: ns.Routing})
-
-	ni, err := jc.NodeInfo(ctx, api.NodeInfoOptions{})
-	check(err)
-	fmt.Printf("Peer ID:\t%v\n", ni.PeerID)
-	fmt.Printf("Network:\t%v\n", ni.Network)
-
-	cs, err := jc.ConsensusStatus(ctx, api.ConsensusStatusOptions{})
-	check(err)
-	fmt.Printf("Validator:\t%x\n", cs.ValidatorKeyHash)
-
-	_, val, ok := ns.Network.ValidatorByHash(cs.ValidatorKeyHash[:])
-	if ok && val.Operator != nil {
-		fmt.Printf("Operator:\t%v\n", val.Operator)
+	if u.Host == "" {
+		u, err = url.Parse("http://" + args[0])
+		check(err)
+	}
+	if u.Path == "" {
+		u.Path = "/v3"
 	}
 
-	fmt.Println()
+	p2pPort := "16593"
+	if u.Port() == "" {
+		if flagCheckNode.UseP2P != "" {
+			u.Host += ":16593"
+		} else {
+			u.Host += ":16595"
+		}
+	} else if flagCheckNode.UseP2P != "" {
+		p2pPort = u.Port()
+	}
 
-	// Direct
+	peerID, network := flagCheckNode.UseP2P, flagCheckNode.Network
+	if peerID == "" {
+		jc := jsonrpc.NewClient(u.String())
+		ns, err := jc.NetworkStatus(ctx, api.NetworkStatusOptions{})
+		check(err)
+
+		ni, err := jc.NodeInfo(ctx, api.NodeInfoOptions{})
+		check(err)
+		peerID, network = ni.PeerID.String(), ni.Network
+		fmt.Printf("Peer ID:\t%v\n", ni.PeerID)
+		fmt.Printf("Network:\t%v\n", ni.Network)
+
+		cs, err := jc.ConsensusStatus(ctx, api.ConsensusStatusOptions{})
+		check(err)
+		fmt.Printf("Validator:\t%x\n", cs.ValidatorKeyHash)
+
+		_, val, ok := ns.Network.ValidatorByHash(cs.ValidatorKeyHash[:])
+		if ok && val.Operator != nil {
+			fmt.Printf("Operator:\t%v\n", val.Operator)
+		}
+
+		fmt.Println()
+	}
+
 	_, key, err := ed25519.GenerateKey(rand.Reader)
 	check(err)
-	peerAddrTCP, err := multiaddr.NewMultiaddr("/dns/" + args[0] + "/tcp/16593/p2p/" + ni.PeerID.String())
+	peerAddrTCP, err := multiaddr.NewMultiaddr("/dns/" + u.Hostname() + "/tcp/" + p2pPort + "/p2p/" + peerID)
 	check(err)
-	peerAddrUDP, err := multiaddr.NewMultiaddr("/dns/" + args[0] + "/udp/16593/quic/p2p/" + ni.PeerID.String())
+	peerAddrUDP, err := multiaddr.NewMultiaddr("/dns/" + u.Hostname() + "/udp/" + p2pPort + "/quic/p2p/" + peerID)
 	check(err)
 	pc, err := p2p.New(p2p.Options{
-		Network: ni.Network,
+		Network: network,
 		Key:     key,
 		BootstrapPeers: []multiaddr.Multiaddr{
 			peerAddrTCP,
@@ -74,8 +113,9 @@ func checkNode(_ *cobra.Command, args []string) {
 		},
 	})
 	check(err)
+	defer func() { check(pc.Close()) }()
 
-	svcAddr, err := multiaddr.NewMultiaddr("/p2p/" + ni.PeerID.String())
+	svcAddr, err := multiaddr.NewMultiaddr("/p2p/" + peerID)
 	check(err)
 	svcAddr = api.ServiceTypeNode.Address().Multiaddr().Encapsulate(svcAddr)
 	_, err = pc.DialNetwork().Dial(ctx, svcAddr)
@@ -87,11 +127,13 @@ func checkNode(_ *cobra.Command, args []string) {
 	}
 
 	mc := &message.Client{Transport: &message.RoutedTransport{
-		Network: ni.Network,
+		Network: network,
 		Dialer:  pc.DialNetwork(),
-		Router:  routing.MessageRouter{Router: router},
+		Router:  routing.MessageRouter{},
 	}}
-	_, err = mc.NodeInfo(ctx, api.NodeInfoOptions{PeerID: ni.PeerID})
+	pid, err := peer.Decode(peerID)
+	check(err)
+	ni, err := mc.NodeInfo(ctx, api.NodeInfoOptions{PeerID: pid})
 	if err == nil {
 		fmt.Println(color.GreenString("✔"), "Can query")
 	} else {
@@ -99,5 +141,11 @@ func checkNode(_ *cobra.Command, args []string) {
 		fmt.Println(err)
 	}
 
-	check(pc.Close())
+	if ni != nil {
+		fmt.Println()
+
+		for _, s := range ni.Services {
+			fmt.Printf("Has %v\n", s)
+		}
+	}
 }
