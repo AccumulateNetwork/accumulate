ea13104f5a711a9359a119c62e74d1357bdb3447
Ethan Reesor <ethan.reesor@gmail.com>
2024-03-13 18:37:29 +0000

    Submit consensus state machine [#3563]
    Closes #3563. Adds a state machine for consensus on submissions.
    
    Changelog: refactor

---
+++ test/simulator/consensus/state.go
@@ -0,0 +1,52 @@
+// Copyright 2024 The Accumulate Authors
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+package consensus
+
+import (
+	"context"
+
+	"gitlab.com/accumulatenetwork/accumulate/internal/logging"
+	"golang.org/x/exp/slog"
+)
+
+type state[S any] interface {
+	// execute processes the message and returns the next state and messages
+	// that should be sent.
+	execute(Message) (S, []Message, error)
+}
+
+func executeState[S state[S]](ctx context.Context, s S, msg Message) (S, []Message, error) {
+	var allOut []Message
+again:
+	// Execute the message
+	r, out, err := s.execute(msg)
+	allOut = append(allOut, out...)
+	if err != nil {
+		return s, allOut, err
+	}
+	var z S
+	if any(r) == any(z) {
+		return z, allOut, nil
+	}
+
+	// Did we transition into the next state?
+	if any(r) == any(s) {
+		return s, allOut, nil
+	}
+
+	slog.DebugContext(ctx, "Transitioning", "to", logging.TypeOf(s))
+	s = r
+
+	// Keep stepping until the state doesn't change. Under normal circumstances
+	// this won't do anything. However, this is necessary if there are 1 or 2
+	// validators in the network. If there is only one validator, we must
+	// proceed directly through the phases of consensus instead of waiting to
+	// receive votes. Without this loop, each state would need logic to handle
+	// that scenario.
+	msg = nil
+	goto again
+}
