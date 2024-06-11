192a645f65cf765b8c5a909d999180471993290d
Ethan Reesor <ethan.reesor@gmail.com>
2024-06-11 17:31:46 +0000

    Fix array handling

--- internal/core/healing/types_gen.go
+++ internal/core/healing/types_gen.go
@@ -128,7 +128,7 @@ func (v *PeerInfo) UnmarshalJSON(data []byte) error {
 		v.Key = *x
 	}
 	v.Status = u.Status
-	if u.Addresses != nil {
+	if u.Addresses.Value != nil {
 		v.Addresses = make([]p2p.Multiaddr, len(u.Addresses.Value))
 		for i, x := range u.Addresses.Value {
 			v.Addresses[i] = x
