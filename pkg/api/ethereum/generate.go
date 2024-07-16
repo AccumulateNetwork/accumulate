1e8f4f2db84595f2cf9fce85e028b10c19e62f18
Ethan Reesor <ethan.reesor@gmail.com>
2024-07-16 13:02:08 -0500

    LINT OMFG

--- pkg/api/ethereum/generate.go
+++ pkg/api/ethereum/generate.go
@@ -8,3 +8,4 @@ package ethrpc
 
 //go:generate go run gitlab.com/accumulatenetwork/core/schema/cmd/generate schema schema.yml -w schema_gen.go
 //go:generate go run gitlab.com/accumulatenetwork/core/schema/cmd/generate types schema.yml -w types_gen.go
+//go:generate go run github.com/rinchsan/gosimports/cmd/gosimports -w .
