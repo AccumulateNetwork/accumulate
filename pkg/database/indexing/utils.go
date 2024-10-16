8a1e8eeb947d0f2e37af0f679d2f1b6e05199de2
Ethan Reesor <ethan.reesor@gmail.com>
2024-10-16 20:40:59 +0000

    Remove block ledger accounts [#3616]
    Closes #3616. Replaces block ledger accounts (e.g. `dn.acme/ledger/123`) with a standalone index. This reduces database pressure by omitting those accounts. It also reduces BPT pressure and would allow validators to drop the index at a later date, as the index state is not included in the BPT. This *may* decrease performance when running locally, **however** it should increase performance on real validators as those are currently limited by disk I/O, not memory or execution resources.
    
    Changelog: refactor

---
+++ pkg/database/indexing/utils.go
@@ -0,0 +1,36 @@
+// Copyright 2024 The Accumulate Authors
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+package indexing
+
+import (
+	"gitlab.com/accumulatenetwork/accumulate/pkg/database"
+	"gitlab.com/accumulatenetwork/accumulate/pkg/errors"
+	"gitlab.com/accumulatenetwork/accumulate/pkg/types/record"
+)
+
+func value[V any](k *record.Key, v *Value[V]) ValueResult[V] {
+	return ValueResult[V]{k, v}
+}
+
+func errResult[V any](err error) ErrorResult[V] {
+	return ErrorResult[V]{err}
+}
+
+func notFound[V any](key *record.Key) ErrorResult[V] {
+	return ErrorResult[V]{(*database.NotFoundError)(key)}
+}
+
+func rejectNotFound[V any](err error) ErrorResult[V] {
+	if !errors.Is(err, errors.NotFound) {
+		return errResult[V](err)
+	}
+
+	// Escalate NotFound to InternalError in cases where NotFound indicates a
+	// corrupted index. For example, if the head references a block and that
+	// block doesn't exist, that's a problem.
+	return errResult[V](errors.InternalError.WithFormat("corrupted index: %w", err))
+}
