bb6b6745c4dd4e28fe2c017e296122dc864f9a04
Ethan Reesor <ethan.reesor@gmail.com>
2025-03-09 17:16:43 -0500

    Tweaks for http and faucet.
    Fix --log-levels for http and faucet. Add --extra-services to HTTP.

--- internal/util/cmd/logging.go
+++ internal/util/cmd/logging.go
@@ -7,8 +7,13 @@
 package cmdutil
 
 import (
+	"fmt"
+	"log/slog"
+	"strings"
+
 	"github.com/cometbft/cometbft/libs/log"
 	"github.com/rs/zerolog"
+	"gitlab.com/accumulatenetwork/accumulate/cmd/accumulated/run"
 	"gitlab.com/accumulatenetwork/accumulate/internal/logging"
 )
 
@@ -20,4 +25,36 @@ func NewConsoleLogger(levels string) log.Logger {
 	logger, err := logging.NewTendermintLogger(zerolog.New(lw), ll, false)
 	Check(err)
 	return logger
+}
+
+type LogLevelFlag []*run.LoggingRule
+
+func (ll LogLevelFlag) Type() string { return "log-levels" }
+
+func (ll LogLevelFlag) String() string {
+	var s []string
+	for _, r := range ll {
+		for _, m := range r.Modules {
+			s = append(s, fmt.Sprintf("%s=%s", m, r.Level))
+		}
+	}
+	return strings.Join(s, ",")
+}
+
+func (ll *LogLevelFlag) Set(s string) error {
+	var level slog.Level
+	var err error
+	var modules []string
+	parts := strings.SplitN(s, "=", 2)
+	if len(parts) == 1 {
+		err = level.UnmarshalText([]byte(parts[0]))
+	} else {
+		err = level.UnmarshalText([]byte(parts[1]))
+		modules = parts[:1]
+	}
+	if err != nil {
+		return err
+	}
+	*ll = append(*ll, &run.LoggingRule{Modules: modules, Level: level})
+	return nil
 }
